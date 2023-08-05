043a0119a077fa6128b64d0bebba17d3c4adad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:42 +0000

    Add package netbox-cisco-maintenance/0.0.135/netbox_cisco_maintenance-0.0.135-py3-none-any.whl

---
+++ packages/netbox-cisco-maintenance/netbox_cisco_maintenance-0.0.135-py3-none-any.whl/netbox_cisco_support_plugin/tables.py
@@ -0,0 +1,30 @@
+import django_tables2 as tables
+from django.utils.safestring import mark_safe
+
+from netbox.tables import BaseTable, NetBoxTable
+from netbox.columns import ActionsColumn
+from .models import CiscoSupport, CiscoDeviceTypeSupport
+
+
+class CustomActionsColumn(ActionsColumn):
+    actions = {"delete": ("Delete", "trash-can-outline", "delete", "danger")}
+
+
+class CiscoSupportTable(NetBoxTable):
+    actions = CustomActionsColumn()
+    device = tables.Column(linkify=True)
+
+    class Meta(NetBoxTable.Meta):
+        model = CiscoSupport
+        # fmt: off
+        fields = (
+           "pk", "id", "device", "recommended_release", "desired_release", "current_release",
+            "desired_release_status", "current_release_status", "api_status", "sr_no_owner", "is_covered",
+            "contract_supplier", "coverage_end_date", "service_line_descr", "service_contract_number",
+            "warranty_end_date", "warranty_type",
+        )
+        default_columns = (
+            "device", "recommended_release", "desired_release", "current_release", "sr_no_owner",
+            "is_covered", "contract_supplier", "coverage_end_date", "service_line_descr",
+        )
+        # fmt: on
