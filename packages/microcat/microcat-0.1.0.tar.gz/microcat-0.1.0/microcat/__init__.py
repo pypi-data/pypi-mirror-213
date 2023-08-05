b0dea4d11ab4c43d1db6489c430fedcb36748652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:04 +0000

    Add package microcat/0.1.0/microcat-0.1.0.tar.gz

---
+++ packages/microcat/microcat-0.1.0.tar.gz/microcat-0.1.0/microcat/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from configer import MicrocatConfig
+from configer import parse_yaml
+from configer import update_config
+from configer import custom_help_formatter
+import sample
+
+# from __about__ import __version__,__author__
+
+name = "microcat"
+__version__ = '0.0.1'
+__author__ = "Changxing Su"
