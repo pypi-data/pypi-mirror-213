40c01e48a516438cd5031f90b0657ac3fd78a387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package dknovautils/0.1.7/dknovautils-0.1.7.tar.gz

---
+++ packages/dknovautils/dknovautils-0.1.7.tar.gz/dknovautils-0.1.7/README.md
@@ -0,0 +1,18 @@
+# Learning python
+
+This is a simple example package. 
+
+just for learning how to build a package.
+
+
+DEPRECATION: dknovautils is being installed using the legacy 'setup.py install' method, because it does not have a 'pyproject.toml' and the 'wheel' package is not installed. pip 23.1 will enforce this behaviour change. A possible replacement is to enable the '--use-pep517' option. Discussion can be found at https://github.com/pypa/pip/issues/8559
+
+
+
+
+
+
+
+
+
+
---
+++ packages/dknovautils/dknovautils-0.1.7.tar.gz/dknovautils-0.1.7/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import re
+import setuptools
+
+from distutils.core import setup
+from setuptools import find_packages
+
+DkAppVer = '0.1.7'
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="dknovautils",
+    version=DkAppVer,
+    author="dknova",
+    author_email="dknova@example.com",
+    description="This is the tools for dknova.",
+    long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="http://example.com",
+    install_requires=[
+        # 'requests!=2.9.0',
+        # 'lxml',
+        # 'monotonic>=1.5',
+        'numpy',
+        # 'beepy',
+    ],
+    packages=setuptools.find_packages(exclude=("test")),
+    classifiers=(
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+
+        "Programming Language :: Python :: 3.7"
+    ),
+    exclude_package_data={'': ["*.txt", "*.adoc", "*.sh", "dknovautils/test.py",
+                               "dknovautils/config.txt", "dknovautils/*.sh", "dknovautils/test*.py"]},
+)
