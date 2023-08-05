c0cbe34eaa1ae33da35dcf2edf94f68d08af29c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:54 +0000

    Add package gpt-fn/0.0.1.post2/gpt_fn-0.0.1.post2.tar.gz

---
+++ packages/gpt-fn/gpt_fn-0.0.1.post2.tar.gz/gpt_fn-0.0.1.post2/src/fn/utils/tests/test_pydantic_parser.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from syrupy.assertion import SnapshotAssertion
+
+from ..pydantic_parser import PydanticParser
+
+
+class Receipt(BaseModel):
+    """The Receipt for user"""
+
+    amount: float
+    currency: str = Field(description="ISO 4217 currency code")
+    customer: str
+
+
+def test_pydantic_parser_get_format_instructions(
+    snapshot: SnapshotAssertion,
+) -> None:
+    assert snapshot == PydanticParser[Receipt](pydantic_model=Receipt).get_format_instructions()
+
+
+def test_parse_output(snapshot: SnapshotAssertion) -> None:
+    assert snapshot == PydanticParser[Receipt](pydantic_model=Receipt).parse('{"amount": 1.0, "currency": "USD", "customer": "John Doe"}')
