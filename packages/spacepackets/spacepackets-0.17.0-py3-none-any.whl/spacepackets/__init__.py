731478ddad71c4ad215cd69145db8d5ad73be850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package spacepackets/0.17.0/spacepackets-0.17.0-py3-none-any.whl

---
+++ packages/spacepackets/spacepackets-0.17.0-py3-none-any.whl/spacepackets/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.17.0"
+
+import logging
+from spacepackets.ccsds import (
+    SpacePacketHeader,
+    SpacePacket,
+    PacketType,
+    SequenceFlags,
+)
+
+from spacepackets.exceptions import BytesTooShortError
+
+
+__LIB_LOGGER = logging.getLogger(__name__)
+
+
+def get_lib_logger() -> logging.Logger:
+    """Get the library logger. Can be used to modify the library logs or disable the
+    propagation."""
+    return __LIB_LOGGER
