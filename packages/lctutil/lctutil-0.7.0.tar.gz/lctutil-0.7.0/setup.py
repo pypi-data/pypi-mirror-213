088a90daa2772be1ad267a7aac54d2c0bbf93964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package lctutil/0.7.0/lctutil-0.7.0.tar.gz

---
+++ packages/lctutil/lctutil-0.7.0.tar.gz/lctutil-0.7.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lctutil',
+    version='0.7.0',
+    packages=find_packages(),
+    install_requires=[
+        "pyahocorasick",
+        "openai",
+        "tenacity"
+    ],
+    py_modules=["lctutil"],
+    # Metadata
+    author='LCT',
+    author_email='your.email@example.com',
+    description='LCT utils',
+    url='https://github.com/yourusername/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
