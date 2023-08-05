39d69146cc4b639bbad228da38b403a9c239a885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package py-gmp/0.0.2/py_gmp-0.0.2-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/py-gmp/py_gmp-0.0.2-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pygmp/daemons/interactive.py
@@ -0,0 +1,308 @@
+"""This is not a daemon. It is a simple interactive shell to work with the MRT in a Linux kernel.
+
+    This process takes the IGMP socket and can make changes to the MRT on-demand.  I use it for testing and experimentation.
+
+"""
+#  MIT License
+#
+#  Copyright (c) 2023 Jack Hart
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+import inspect
+import socket
+from inspect import Signature, Parameter, BoundArguments
+from itertools import zip_longest
+import sys
+from contextlib import contextmanager
+import threading
+import queue
+
+from pygmp import kernel, data
+from pygmp.daemons.utils import get_logger
+
+
+_logger = get_logger(__name__)
+
+
+def main(args, app):
+
+    with kernel.igmp_socket() as sock:
+        _clean(sock)
+
+        # create commands object with queue and socket
+        commands = Commands(sock, queue.Queue())
+
+        # start background thread to monitor socket
+        thread = threading.Thread(target=_read_from_socket, args=(sock, commands.queue), daemon=True)
+        thread.start()
+
+        while True:
+            with interactive_shell() as inputs:
+                command, args = inputs
+                commands.run_command(command, args)
+
+
+@contextmanager
+def interactive_shell():
+    try:
+        # Get input from user
+        raw_input = input("> ")
+        command, *args = raw_input.strip().split(" ")
+        yield command, args
+
+    except KeyboardInterrupt:
+        _logger.warning("Keyboard interrupt. Exiting.")
+        exit(0)
+    except Exception:
+        _logger.exception("Unknown error. Please report.")
+
+
+class Commands:
+    """Available commands: add vif, add mfc, del vif, del mfc, flush, status, msgs, bye help"""
+
+    def __init__(self, sock: socket.socket, queuew: queue.Queue):
+        self.sock = sock
+        self.queue = queuew
+
+    def run_command(self, command, args):
+        fn = None
+        try:
+            fn = self._parse_command(command, args)
+            signature = inspect.signature(fn)
+            bn = Commands._parse_raw_arguments(args, signature)
+        except TypeError as e:
+            print(e)
+            print(f"Usage: {fn.__doc__}" if fn else self.__doc__)
+            return
+
+        fn(**bn.arguments)
+
+    def bye(self):
+        """bye
+
+            Exits the interactive shell.
+        """
+        exit(0)
+
+    def list_vifs(self):
+        """list vifs
+
+            Prints contents of /proc/net/ip_mr_vif for the network namespace of the process.
+        """
+        print("\n")
+        for vif in kernel.ip_mr_vif():
+            print(vif)
+
+    def list_mfc(self):
+        """list mfc
+
+            Prints contents of /proc/net/ip_mr_cache for the network namespace of the process.
+        """
+        print("\n")
+        for mfc in kernel.ip_mr_cache():
+            print(mfc)
+
+    def help(self, command, subcommand=None):
+        """help <command> [subcommand]
+
+            Prints the docstring of the command.
+        """
+        try:
+            print(self._parse_command(command, [subcommand] if subcommand else []).__doc__)
+        except TypeError as e:
+            print(e)
+
+    def status(self):
+        """status
+
+            Prints the status of the MRT and PIM code in the kernel.
+        """
+        print(f"\nMR Version: {kernel.mrt_version(self.sock)}")
+        print(f"\nPIM: {kernel.pim_is_enabled(self.sock)}")
+
+    def flush(self):
+        """flush
+
+            Flushes the MRT.
+        """
+        kernel.flush(self.sock)
+
+    def add_vif(self, vif_index: str, interface_addr_or_indx: str, remote_addr: str = "0.0.0.0", ttl_threshold: str = '1'):
+        """add vif <vif_index> <interface_addr_or_indx> [remote_addr=0.0.0.0] [ttl_threshold=1]
+
+            Creates a virtual interface device for multicast routing.
+
+            vif_index: Interface index.  This must be a unique index for the new, multicast virtual interface.
+            interface_addr_or_indx: Address or index of network interface.  Each multicast VIF maps to a unicast interface.
+            remote_addr: IPIP tunnel address.  Not used in most cases.
+            ttl_threshold: Minimum TTL value for multicast packets on this interface.
+        """
+        if interface_addr_or_indx.isdigit():
+            interface_addr_or_indx = int(interface_addr_or_indx)
+
+        vif_ctl = data.VifCtl(int(vif_index), interface_addr_or_indx, remote_addr, int(ttl_threshold), 0)
+        kernel.add_vif(self.sock, vif_ctl)
+
+    def add_mfc(self, origin: str, mcastgroup: str, parent: str, ttls: str = ""):
+        """add mfc <origin> <group> <parent> [ttls='']
+
+            Creates a multicast forwarding cache entry.
+
+            origin: Source address of multicast packets.  Used for SSM ???  Optional?
+            group: Multicast group address.
+            parent: Interface index of incoming multicast interface.
+            ttls: Comma separated list of TTL values.  TODO - more context.
+        """
+        ttls_lst = [int(ttl) for ttl in ttls.split(",")] if ttls else []
+        mfc_ctl = data.MfcCtl(origin, mcastgroup, int(parent), ttls_lst)
+        kernel.add_mfc(self.sock, mfc_ctl)
+
+    def del_vif(self, vif_index: str):
+        """del vif <vif_index>
+
+            Deletes a virtual multicast interface device.
+
+            vif_index: Unique interface index assigned with the add vif command.
+        """
+        vif_ctl = data.VifCtl(int(vif_index))
+        kernel.del_vif(self.sock, vif_ctl)
+
+    def del_mfc(self, origin: str, mcastgroup: str, parent: str):
+        """del mfc <origin> <group> <parent>
+
+            Deletes a multicast forwarding cache entry.
+
+            origin: Source address of multicast packets.
+            group: Multicast group address.
+            parent: Interface index of incoming interface.
+
+        """
+        mfc_ctl = data.MfcCtl(origin, mcastgroup, int(parent))  # FIXME
+        kernel.del_mfc(self.sock, mfc_ctl)
+
+    def add_membership(self, multiaddr: str, interface: str):
+        """add membership <multiaddr> <interface>
+
+            multiaddr: multicast address.
+            interface: interface address.  Must be a valid address on a host network interface.
+
+            Runs setsockopt with IP_ADD_MEMBERSHIP option on the socket used for multicast routing.  This tells the kernel
+            to join a multicast group on the specified interface.  The kernel initially sends an IGMP join message,
+            then periodically sends IGMP membership reports.  When the socket is closed, the kernel will send
+            an IGMP message to leave the group.
+
+            This should not be necessary to do any multicast routing.  However, if your switch is configured with
+            IGMP-snooping, sometimes IGMP messages are not properly forwarded to the router.
+    """
+        ip_mreq = data.IpMreq(multiaddr, interface)
+        kernel.add_membership(self.sock, ip_mreq)
+
+    def drop_membership(self, multiaddr: str, interface: str):
+        """add membership <multiaddr> <interface>
+
+            TODO - required fields?
+
+            multiaddr: multicast address.
+            interface: interface address.  Must be a valid address on a host network interface.
+
+            Runs setsockopt with IP_DROP_MEMBERSHIP option on the socket used for multicast routing.  This tells the kernel
+            to send an IGMP leave message and stop sending IGMP membership reports for the group.
+    """
+        ip_mreq = data.IpMreq(multiaddr, interface)
+        kernel.drop_membership(self.sock, ip_mreq)
+
+    def msgs(self):
+        """msgs
+
+            Prints any new messages from the kernel over the IGMP routing socket.
+        """
+        print(f"# New Messages: {self.queue.qsize()}")
+        while not self.queue.empty():
+            print(self.queue.get())
+
+    def _parse_command(self, command: str, args: list[str]) -> callable:
+        try:
+            if command in {"list", "add", "del", "drop"}:
+                command = f"{command}_{args.pop(0)}"
+
+            fn = getattr(self, command)
+        except AttributeError:
+            raise TypeError(f"Unknown command: '{command.replace('_', ' ')}'")
+        except IndexError:
+            raise TypeError(f"Missing sub-command for command: '{command}'")
+        return fn
+
+    @staticmethod
+    def _parse_raw_arguments(arguments: list[str], signature: Signature) -> BoundArguments:
+        # TODO - cleanup
+        args, kwargs = [], {}
+        for param, arg in zip_longest(signature.parameters.values(), arguments):
+            if param is None:
+                if arg:
+                    raise TypeError(f"too many arguments")
+                break
+            if param.default is Parameter.empty:
+                if arg is None:
+                    raise TypeError(f"{param.name} is a required argument")
+                args.append(arg)
+                continue
+
+            if arg is None:
+                break
+
+            if "=" not in arg:
+                raise TypeError(f"keyword argument must contain '=' for assignment")
+
+            key, value = arg.split("=", 1)
+            if key not in signature.parameters:
+                raise TypeError(f"invalid keyword argument '{key}'")
+
+            kwargs[key] = value
+
+        ba = signature.bind(*args, **kwargs)
+        ba.apply_defaults()
+        return ba
+
+
+def _clean(sock: kernel.InetRawSocketType):
+    kernel.disable_pim(sock)
+    kernel.enable_mrt(sock)
+    kernel.flush(sock)
+
+
+def _read_from_socket(sock, qu):
+    try:
+        while True:
+            buff, _ = sock.recvfrom(6000) # FIXME - buffer size
+            qu.put(_filter_ip(kernel.parse_ip_header(buff), buff))
+    except Exception:
+        _logger.exception("Error in read_from_socket thread.  This will be ignored.")
+
+
+def _filter_ip(ip_header: data.IPHeader, buffer: bytes):
+    if ip_header.protocol == data.IPProtocol.IGMP:
+        return kernel.parse_igmp(buffer[ip_header.ihl * 4:])
+    if ip_header.protocol == data.IPProtocol.CONTROL:
+        return kernel.parse_igmp_control(buffer)
+    _logger.warning("warning, skipping packet...")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ packages/py-gmp/py_gmp-0.0.2-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pygmp/kernel.py
@@ -0,0 +1,278 @@
+""" # TODO Investigate syscalls:
+        - MRT_TABLE -- Linux can support multiple MRT tables.
+        - MRT_ADD_MFC_PROXY / MRT_DEL_MFC_PROXY -- It is unclear to me what these do.
+        - SIOCGETRPF  -- Get the RPF neighbor for a given source and group?
+"""
+#  MIT License
+#
+#  Copyright (c) 2023 Jack Hart
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+from __future__ import annotations
+import struct
+from contextlib import contextmanager
+from typing import TypeVar
+import socket
+import fcntl
+from ipaddress import ip_address
+
+
+from pygmp.data import VifReq, IpMreq, VifCtl, MfcCtl, SGReq, IPHeader, \
+    IGMPControl, Interface, VIFTableEntry, MFCEntry, \
+    IGMP, IGMPType, IGMPv3Query, IGMPv3MembershipReport
+from pygmp import utils
+from pygmp import _kernel
+
+
+IP_MR_CACHE_DIR = "/proc/net/ip_mr_cache"
+IP_MR_VIF_DIR = "/proc/net/ip_mr_vif"
+
+# This naming is used to distinguish socket types between methods.
+InetRawSocketType = TypeVar("InetRawSocketType", bound=socket.socket)  # FIXME - no good way to type a raw socket
+InetAnySocket = TypeVar("InetAnySocket", bound=socket.socket)
+
+
+@contextmanager
+def igmp_socket() -> InetRawSocketType:
+    """The IGMP socket. A raw socket used to communicate wither kernel multicast routing code."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IGMP)
+    yield sock
+    sock.close()
+
+
+def mrt_version(sock: InetRawSocketType) -> str:
+    """Get the version of the kernel mroute."""
+    return hex(sock.getsockopt(socket.IPPROTO_IP, _kernel.MRT_VERSION))
+
+
+def enable_mrt(sock: InetRawSocketType) -> None:
+    """Enable the kernel multicast routing socket to receive control messages."""
+    try:
+        sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_INIT, 1)
+    except OSError as e:
+        if e.errno == 98:
+            # "Address already in use"
+            raise OSError("The MRT socket is already enabled.")
+        raise
+
+
+def disable_mrt(sock: InetRawSocketType) -> None:
+    """Disable the kernel multicast routing socket to no longer receive control messages.
+
+        Ubuntu docs say setting MRT_INIT to 0 disabled it.
+            https://manpages.ubuntu.com/manpages/xenial/en/man4/multicast.4freebsd.html
+        Linux source code tells a different story.
+            https://github.com/torvalds/linux/blob/master/net/ipv4/ipmr.c#L1411
+    """
+    try:
+        sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_DONE, 1)
+    except PermissionError as e:
+        if e.errno == 13:
+            raise OSError("The MRT socket is already disabled.")
+        raise
+
+
+def enable_pim(sock: InetRawSocketType) -> None:
+    """Enable PIM code & PIM assert mode in the kernel.
+        TODO - the practical distinction between PIM and PIM assert mode is not clear.
+    """
+    sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_PIM, 1)
+    sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_ASSERT, 1)
+
+
+def disable_pim(sock: InetRawSocketType) -> None:
+    """Disable PIM code & PIM assert mode in the kernel."""
+    sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_PIM, 0)
+    sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_ASSERT, 0)
+
+
+def pim_is_enabled(sock: InetRawSocketType) -> bool:
+    """Check if PIM code & PIM assert mode is enabled in the kernel."""
+    return bool(sock.getsockopt(socket.IPPROTO_IP, _kernel.MRT_PIM)) \
+           and bool(sock.getsockopt(socket.IPPROTO_IP, _kernel.MRT_ASSERT))
+
+
+def add_vif(sock: InetRawSocketType, vifctl: VifCtl) -> None:
+    """Add a multicast VIF to the kernel multicast routing table."""
+    _kernel.add_vif(sock, vifctl.vifi, vifctl.threshold, vifctl.rate_limit, str(vifctl.lcl_addr), str(vifctl.rmt_addr))
+
+
+def del_vif(sock: InetRawSocketType, vifctl: VifCtl) -> None:
+    """Delete a multicast VIF from the kernel multicast routing table."""
+    _kernel.del_vif(sock, vifctl.vifi)
+
+
+def get_vif_counts(sock: InetRawSocketType, vif_req: VifReq) -> VifReq:
+    """Get packet and byte counts for a VIF in the multicast routing table."""
+    sioc_vif_req = struct.pack(vif_req.format, vif_req.vifi, vif_req.icount, vif_req.ocount, vif_req.ibytes, vif_req.obytes)
+    sioc_vif_result = fcntl.ioctl(sock.fileno(), _kernel.SIOCGETVIFCNT, sioc_vif_req, True)
+    return VifReq(*struct.unpack(VifReq.format, sioc_vif_result))
+
+
+def get_mfc_counts(sock: InetRawSocketType, sg_req: SGReq) -> SGReq:
+    """Get packet and byte counts for a source-group mfc entry."""
+    sioc_sg_req = struct.pack(sg_req.format, sg_req.src.packed, sg_req.grp.packed, sg_req.pktcnt, sg_req.bytecnt, sg_req.wrong_if)
+    sioc_sg_result = fcntl.ioctl(sock.fileno(), _kernel.SIOCGETSGCNT, sioc_sg_req, True)
+    unpacked_args = list(struct.unpack(SGReq.format, sioc_sg_result))
+    unpacked_args[0], unpacked_args[1] = ip_address(unpacked_args[0]), ip_address(unpacked_args[1])
+    return SGReq(*unpacked_args)
+
+
+def add_mfc(sock: InetRawSocketType, mfcctl: MfcCtl) -> None:
+    """Add a multicast forwarding cache entry to the kernel multicast routing table.
+        TODO - support expire field.
+    """
+    _kernel.add_mfc(sock, str(mfcctl.origin), str(mfcctl.mcastgroup), mfcctl.parent, mfcctl.ttls)
+
+
+def del_mfc(sock: InetRawSocketType, mfcctl: MfcCtl) -> None:
+    """Delete a multicast forwarding cache entry from the kernel multicast routing table."""
+    _kernel.del_mfc(sock, str(mfcctl.origin), str(mfcctl.mcastgroup), mfcctl.parent)
+
+
+def flush(sock: InetRawSocketType, vifs=True, mfc=True, static=True) -> None:
+    """Flush data in the kernel multicast routing table.
+        TODO - I do not understand the practical distinction between static and non-static entries.
+    """
+    vifs_mask = vifs * (_kernel.MRT_FLUSH_VIFS | (static * _kernel.MRT_FLUSH_VIFS_STATIC))
+    mfc_mask = mfc * (_kernel.MRT_FLUSH_MFC | (static * _kernel.MRT_FLUSH_MFC_STATIC))
+    sock.setsockopt(socket.IPPROTO_IP, _kernel.MRT_FLUSH, vifs_mask | mfc_mask)
+
+
+def add_membership(sock: InetAnySocket, ip_mreq: IpMreq) -> None:
+    """Runs setsockopt with IP_ADD_MEMBERSHIP option on the socket used for multicast routing.
+
+        This tells the kernel to join a multicast group on the specified interface.  The kernel sends an IGMP message
+        to join the group initially, and then it periodically sends IGMP membership reports to maintain the membership.
+        When the socket is closed, the kernel will send an IGMP message to leave the group.  This should not be necessary
+        for multicast routing.  However, if your switch is configured with IGMP-snooping, sometimes IGMP messages are
+        not properly forwarded to the router.
+    """
+    mreq_buff = struct.pack(ip_mreq.format, ip_mreq.multiaddr.packed, ip_mreq.interface.packed)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq_buff)
+
+
+def drop_membership(sock: InetAnySocket, ip_mreq: IpMreq) -> None:
+    """Runs setsockopt with IP_DROP_MEMBERSHIP option on the socket used for multicast routing.
+
+        This tells the kernel to send an IGMP message to leave the specified group.
+    """
+    mreq_buff = struct.pack(ip_mreq.format, ip_mreq.multiaddr.packed, ip_mreq.interface.packed)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq_buff)
+
+
+def ttl(sock: InetAnySocket) -> int:
+    return sock.getsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL)
+
+
+def loop(sock: InetAnySocket) -> bool:
+    return bool(sock.getsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP))
+
+
+def interface(sock: InetAnySocket) -> str:
+    in_buff = sock.getsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, 4)
+    return socket.inet_ntoa(in_buff)
+
+
+def parse_ip_header(buffer: bytes) -> IPHeader:
+    return IPHeader(**_kernel.parse_ip_header(buffer))
+
+
+def parse_igmp(buffer: bytes) -> IGMP | IGMPv3MembershipReport | IGMPv3Query:
+    result_dict = _kernel.parse_igmp(buffer)
+    msg_type = IGMPType(result_dict["type"])
+    if msg_type == IGMPType.V3_MEMBERSHIP_REPORT:
+        return IGMPv3MembershipReport(**result_dict)
+    if msg_type == IGMPType.MEMBERSHIP_QUERY and len(result_dict) > 4:
+        return IGMPv3Query(**result_dict)
+    return IGMP(**result_dict)
+
+
+def parse_igmp_control(buffer: bytes) -> IGMPControl:
+    return IGMPControl(**_kernel.parse_igmp_control(buffer))
+
+
+def network_interfaces() -> dict[str, Interface]:
+    """Get list of VIFs from kernel.  Returns the name, IP address, and if multicast is enabled."""
+    interfaces = dict()
+    for inf in _kernel.network_interfaces():
+        if inf["name"] not in interfaces:
+            interfaces[inf["name"]] = Interface(inf["name"], inf["index"], inf["flags"])
+        if inf["address"]:  # TODO - any case where flags are different but name is the same?
+            interfaces[inf["name"]].addresses.add(inf["address"])
+
+    return interfaces
+
+
+@utils.file_cache(IP_MR_VIF_DIR)
+def ip_mr_vif() -> list[VIFTableEntry]:
+    """Parse the /proc/net/ip_mr_vif file.  Linux specific, holds the IPv4 virtual interfaces used by the active multicast routing daemon.
+
+        Virtual file generated by the kernel code here: https://github.com/torvalds/linux/blob/master/net/ipv4/ipmr.c#L2922
+            interface bytesin pktsin bytesout pktsout flags local remote
+            %2td %-10s %8ld %7ld  %8ld %7ld %05X %08X %08X
+
+        Raises FileNotFoundError if the file does not exist.
+
+    """
+    vifs = []
+    with open(IP_MR_VIF_DIR, 'r') as f:
+        next(f) # skip header line
+        for line in f:
+            fields = line.split()
+            index, name, flags = int(fields[0]), fields[1], int(fields[6])
+            local = int(fields[7], 16) if flags & _kernel.VIFF_USE_IFINDEX else utils.host_hex_to_ip(fields[7])
+            remote = utils.host_hex_to_ip(fields[8])
+            vifs.append(VIFTableEntry(index=index, name=name,
+                                      bytes_in=int(fields[2]), pkts_in=int(fields[3]),
+                                      bytes_out=int(fields[4]), pkts_out=int(fields[5]), flags=flags,
+                                      local_addr_or_interface=local, remote_addr=remote))
+
+    return vifs
+
+
+@utils.file_cache(IP_MR_CACHE_DIR)
+def ip_mr_cache() -> list[MFCEntry]:
+    """Parse the /proc/net/ip_mr_cache file.  Linux specific, holds the multicast routing cache.
+
+        Virtual file generated by the kernel code here: https://github.com/torvalds/linux/blob/master/net/ipv4/ipmr.c#L2966
+            group origin iif pkts bytes wrong [oifs]
+            %08X %08X %-3hd %8lu %8lu %8lu [[%2d:%-3d] [%2d:%-3d] ...] or [ %2d:%-3d]
+
+        Raises FileNotFoundError if the file does not exist.
+
+    """
+    with open(IP_MR_CACHE_DIR, 'r') as f:
+        next(f) # skip header line
+        entries = []
+        for line in f:
+            fields = line.strip().split()
+
+            if len(fields) < 6 :
+                raise ValueError(f"Encountered malformed line in {IP_MR_CACHE_DIR}: {line}")
+
+            oifs = _parse_index_ttl(fields[6:]) if len(fields) > 6 else dict()
+            group, origin = utils.host_hex_to_ip(fields[0]), utils.host_hex_to_ip(fields[1])
+            entries.append(MFCEntry(group, origin, int(fields[2]), int(fields[3]), int(fields[4]), int(fields[5]), oifs))
+    return entries
+
+
+def _parse_index_ttl(pairs_list: list[str]) -> dict[int, int]:
+    """Parse a list of index:ttl pairs into a dictionary."""
+    return {int(pair.split(':')[0]): int(pair.split(':')[1]) for pair in pairs_list}
