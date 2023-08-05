9e26054e4a34cce0c5e433cea8794e81a8acd265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:54 +0000

    Add package pinecone-datasets/0.5.0rc1/pinecone_datasets-0.5.0rc1-py3-none-any.whl

---
+++ packages/pinecone-datasets/pinecone_datasets-0.5.0rc1-py3-none-any.whl/pinecone_datasets/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. include:: ../README.md
+"""
+
+__version__ = "0.5.0-rc.1"
+
+from .dataset import Dataset
+from .public import list_datasets, load_dataset
+from .catalog import DatasetMetadata, DenseModelMetadata
