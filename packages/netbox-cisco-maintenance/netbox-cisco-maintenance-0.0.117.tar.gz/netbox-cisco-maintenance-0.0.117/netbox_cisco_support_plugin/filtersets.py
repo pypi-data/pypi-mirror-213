3d3d0e7f5e55d8c2b601e335962a9c49d36c76ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:08 +0000

    Add package netbox-cisco-maintenance/0.0.117/netbox-cisco-maintenance-0.0.117.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.117.tar.gz/netbox-cisco-maintenance-0.0.117/netbox_cisco_support_plugin/filtersets.py
@@ -0,0 +1,21 @@
+from netbox.filtersets import NetBoxModelFilterSet
+from .models import CiscoSupport, CiscoDeviceTypeSupport
+
+class CiscoSupportFilterSet(NetBoxModelFilterSet):
+
+    class Meta:
+        model = CiscoSupport
+        fields = ["id", "sr_no_owner", "is_covered", "contract_supplier"]
+
+    def search(self, queryset, name, value):
+        return queryset.filter(description__icontains=value)
+
+
+class CiscoDeviceTypeSupportFilterSet(NetBoxModelFilterSet):
+
+    class Meta:
+        model = CiscoDeviceTypeSupport
+        fields = ["id", "eox_has_error"]
+
+    def search(self, queryset, name, value):
+        return queryset.filter(description__icontains=value)
