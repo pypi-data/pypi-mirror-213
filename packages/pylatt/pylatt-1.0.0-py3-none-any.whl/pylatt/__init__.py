d564ad4bba255f191461a08451334132ca6602b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:06 +0000

    Add package pylatt/1.0.0/pylatt-1.0.0-py3-none-any.whl

---
+++ packages/pylatt/pylatt-1.0.0-py3-none-any.whl/pylatt/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of pylatt
+# License: GPLv3
+# See the documentation at benvial.gitlab.io/pylatt
+
+from .__about__ import __author__, __description__, __version__
+
+try:
+    import numdiff
+    from numdiff import *
+    from numdiff import _reload_package
+
+    def set_backend(backend):
+        numdiff.set_backend(backend)
+        _reload_package("pylatt")
+
+except:
+    import numpy as backend
+
+    def set_backend(backend):
+        pass
+
+
+from .core import *
