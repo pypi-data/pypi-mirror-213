475f828ef963a04f7c94dda988c86b9a9c44c7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:24 +0000

    Add package example-helloworld/0.1/example_helloworld-0.1.tar.gz

---
+++ packages/example-helloworld/example_helloworld-0.1.tar.gz/example_helloworld-0.1/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name = "example-hello",
+      version = "0.1",
+      description = "Example hello world script",
+      author = "mishpro",
+      author_email = "root@venusbiser.ru",
+      packages = ['helloworld'])
