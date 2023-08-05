07fe6f05623710459106087af498fcd06923dfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:02 +0000

    Add package efemarai/0.4.0/efemarai-0.4.0.tar.gz

---
+++ packages/efemarai/efemarai-0.4.0.tar.gz/efemarai-0.4.0/setup.py
@@ -0,0 +1,57 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version():
+    filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "efemarai/__init__.py"
+    )
+    with open(filename, "r") as f:
+        for line in f.read().splitlines():
+            match = re.search("^__version__ = [\"'](?P<version>.*)[\"']$", line)
+            if match:
+                return match.group("version")
+
+        raise RuntimeError("Unable to find version string.")
+
+
+def get_requirements(file):
+    with open(file) as f:
+        requirements = [
+            line
+            for line in f.read().splitlines()
+            if not line.startswith("#") and len(line) > 0
+        ]
+    return requirements
+
+
+def get_readme():
+    with open("README.md") as f:
+        readme = f.read()
+    return readme
+
+
+setup(
+    name="efemarai",
+    version=get_version(),
+    description="A CLI and SDK for interacting with the Efemarai ML testing platform.",
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    author="Efemarai",
+    author_email="support@efemarai.com",
+    url="https://www.efemarai.com/",
+    license="MIT license",
+    packages=find_packages(),
+    package_data={"efemarai.domains": ["*.yaml"]},
+    entry_points={
+        "console_scripts": ["efemarai = efemarai.cli:main", "ef = efemarai.cli:main"]
+    },
+    install_requires=get_requirements("requirements.txt"),
+    extras_require={
+        "full": get_requirements("requirements_full.txt"),
+    },
+    python_requires=">=3.6",
+    zip_safe=False,
+)
