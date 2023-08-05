1daba268f78fed9cf7c5fbcb39dff2831c814791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:07 +0000

    Add package dsptoolbox/0.2.5/dsptoolbox-0.2.5.tar.gz

---
+++ packages/dsptoolbox/dsptoolbox-0.2.5.tar.gz/dsptoolbox-0.2.5/dsptoolbox/generators/__init__.py
@@ -0,0 +1,19 @@
+"""
+Generators
+----------
+This module contains some utility signal generators. Choose from:
+
+- `chirp()` (sweep)
+- `noise()` (white, pink, red, blue, violet, grey)
+- `dirac()` (impulse)
+- `harmonic()`
+
+"""
+from .generators import chirp, noise, dirac, harmonic
+
+__all__ = [
+    'chirp',
+    'noise',
+    'dirac',
+    'harmonic',
+]
