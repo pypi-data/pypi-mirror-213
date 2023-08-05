7e46b5be5ef5d42a888d0bc660e7bb9d427234a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:08 +0000

    Add package UzEduCorpus/0.0.4/UzEduCorpus-0.0.4.tar.gz

---
+++ packages/UzEduCorpus/UzEduCorpus-0.0.4.tar.gz/UzEduCorpus-0.0.4/README.md
@@ -0,0 +1,24 @@
+<div id="top"></div>
+
+<!-- PROJECT SHIELDS -->
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <h3 align="center">UzEduCorpus</h3>
+  <p>
+    Ushbu ta'lim korpusi O'zbekiston Respublikasi Xalq ta'lim vazirligida ro'yhatga olingan umumiy o'rta ta'lim kaktablari uchun mo'ljallangan kitoblardan shakllantirilgan lug'at shaklidagi ta'lim korpusi hisoblanadi. Bunda 1-11 sinfgacha bo'lgan xar bir sinf bo'yicha foydalanilgan lug'atlar mavjud. 
+</p>
+</div>
+
+```
+from UzEduCorpus import UzEduCorpus
+print(UzEduCorpus.grade_1())
+print(UzEduCorpus.grade_2())
+# ...
+print(UzEduCorpus.grade_9())
+```
+
+```angular2html
+['abadiydir', 'abdulla', 'abdurahmon', 'abdurahmonov', 'abdusodiq', 'abduvali',....
+```
---
+++ packages/UzEduCorpus/UzEduCorpus-0.0.4.tar.gz/UzEduCorpus-0.0.4/src/UzEduCorpus/UzEduCorpus.py
@@ -0,0 +1,91 @@
+import os
+
+def grade_1():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_1.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_2():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_2.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_3():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_3.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_4():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_4.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_5():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_5.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_6():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_6.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_7():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_7.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_8():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_8.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
+
+def grade_9():
+    words = list()
+    dirname = os.path.dirname(__file__) + "/"
+    with open(os.path.join(dirname + "grade_9.csv"), encoding='utf8') as file:
+        for line in file:
+            x = line.rstrip()
+            words.append(x)
+        words.remove(words[0])
+    return words
