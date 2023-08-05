763680e7332ecddde529eea6813bc8880b56955b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:11 +0000

    Add package getajob/0.1.4/getajob-0.1.4-py3-none-any.whl

---
+++ packages/getajob/getajob-0.1.4-py3-none-any.whl/getajob/contexts/notifications/repository.py
@@ -0,0 +1,17 @@
+from getajob.vendor.firebase import FirestoreDB
+from getajob.abstractions.repository import BaseRepository
+from getajob.models.entities import Entity
+
+from .models import entity_models
+
+
+class NotificationRepository(BaseRepository):
+    def __init__(self, db: FirestoreDB):
+        super().__init__(db, Entity.USER_NOTIFICATIONS.value, entity_models)
+
+    def get_applicant_notifications(self, company_id: str):
+        return self.query(
+            parent_collections={
+                "companies": company_id,
+            }
+        )
