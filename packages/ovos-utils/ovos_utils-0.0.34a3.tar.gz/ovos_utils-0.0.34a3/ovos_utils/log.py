b2da81e061d4e1c2ba79e1337e16cc6144b95ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:11 +0000

    Add package ovos-utils/0.0.34a3/ovos_utils-0.0.34a3.tar.gz

---
+++ packages/ovos-utils/ovos_utils-0.0.34a3.tar.gz/ovos_utils-0.0.34a3/ovos_utils/log.py
@@ -0,0 +1,172 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import inspect
+import logging
+import os
+import sys
+from logging.handlers import RotatingFileHandler
+from os.path import join
+
+
+class LOG:
+    """
+    Custom logger class that acts like logging.Logger
+
+    The logger name is gnerally set by the calling module, but the default name
+    is read from the envvar `OVOS_DEFAULT_LOG_NAME`.
+
+    The log level defaults to `INFO` and can be overridden by
+    `OVOS_DEFAULT_LOG_LEVEL`. Note that log level may be overridden by
+     configuration when calling `LOG.init`.
+
+    Usage:
+        >>> LOG.debug('My message: %s', debug_str)
+        13:12:43.673 - :<module>:1 - DEBUG - My message: hi
+        >>> LOG('custom_name').debug('Another message')
+        13:13:10.462 - custom_name - DEBUG - Another message
+    """
+    base_path = "stdout"
+    fmt = '%(asctime)s.%(msecs)03d - ' \
+          '%(name)s - %(levelname)s - %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(fmt, datefmt)
+    max_bytes = 50000000
+    backup_count = 3
+    name = os.getenv("OVOS_DEFAULT_LOG_NAME") or 'OVOS'
+    level = os.getenv("OVOS_DEFAULT_LOG_LEVEL") or "INFO"
+    diagnostic_mode = False
+    _loggers = {}
+
+    @classmethod
+    def __init__(cls, name='OVOS'):
+        cls.name = name
+
+    @classmethod
+    def init(cls, config=None):
+
+        try:
+            from ovos_config.meta import get_xdg_base
+            default_base = get_xdg_base()
+        except ImportError:
+            default_base = os.environ.get("OVOS_CONFIG_BASE_FOLDER") or \
+                "mycroft"
+        from ovos_utils.xdg_utils import xdg_state_home
+
+        config = config or {}
+        cls.base_path = config.get("path") or \
+            f"{xdg_state_home()}/{default_base}"
+        cls.max_bytes = config.get("max_bytes", 50000000)
+        cls.backup_count = config.get("backup_count", 3)
+        cls.level = config.get("level") or LOG.level
+        cls.diagnostic_mode = config.get("diagnostic", False)
+
+    @classmethod
+    def create_logger(cls, name, tostdout=True):
+        if name in cls._loggers:
+            return cls._loggers[name]
+        logger = logging.getLogger(name)
+        logger.propagate = False
+        # also log to stdout
+        if tostdout or cls.base_path == "stdout":
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            stdout_handler.setFormatter(cls.formatter)
+            logger.addHandler(stdout_handler)
+        # log to file
+        if cls.base_path != "stdout":
+            os.makedirs(cls.base_path, exist_ok=True)
+            path = join(cls.base_path,
+                        cls.name.lower().strip() + ".log")
+            handler = RotatingFileHandler(path, maxBytes=cls.max_bytes,
+                                          backupCount=cls.backup_count)
+            handler.setFormatter(cls.formatter)
+            logger.addHandler(handler)
+        logger.setLevel(cls.level)
+        cls._loggers[name] = logger
+        return logger
+
+    @classmethod
+    def set_level(cls, level):
+        cls.level = level
+        for l in cls._loggers:
+            cls._loggers[l].setLevel(level)
+
+    @classmethod
+    def _get_real_logger(cls):
+        name = ""
+        if cls.name is not None:
+            name = cls.name + " - "
+
+        # Stack:
+        # [0] - _log()
+        # [1] - debug(), info(), warning(), or error()
+        # [2] - caller
+        stack = inspect.stack()
+
+        # Record:
+        # [0] - frame object
+        # [1] - filename
+        # [2] - line number
+        # [3] - function
+        # ...
+        record = stack[2]
+        mod = inspect.getmodule(record[0])
+        module_name = mod.__name__ if mod else ''
+        name += module_name + ':' + record[3] + ':' + str(record[2])
+
+        logger = cls.create_logger(name, tostdout=True)
+        if cls.diagnostic_mode:
+            try:
+                from ovos_bus_client.message import dig_for_message
+                msg = dig_for_message()
+                if msg:
+                    logger.debug(f"DIAGNOSTIC - source bus message {msg.serialize()}")
+            except ImportError:
+                pass
+        return logger
+
+    @classmethod
+    def info(cls, *args, **kwargs):
+        cls._get_real_logger().info(*args, **kwargs)
+
+    @classmethod
+    def debug(cls, *args, **kwargs):
+        cls._get_real_logger().debug(*args, **kwargs)
+
+    @classmethod
+    def warning(cls, *args, **kwargs):
+        cls._get_real_logger().warning(*args, **kwargs)
+
+    @classmethod
+    def error(cls, *args, **kwargs):
+        cls._get_real_logger().error(*args, **kwargs)
+
+    @classmethod
+    def exception(cls, *args, **kwargs):
+        cls._get_real_logger().exception(*args, **kwargs)
+
+
+def init_service_logger(service_name):
+    # this is makes all logs from this service be configured to write to service_name.log file
+    # if this is not called in every __main__.py entrypoint logs will be written
+    # to a generic OVOS.log file shared across all services
+    try:
+        from ovos_config.config import read_mycroft_config
+        _cfg = read_mycroft_config()
+    except ImportError:
+        LOG.warning("ovos_config not available. Falling back to defaults")
+        _cfg = dict()
+    _log_level = _cfg.get("log_level", "INFO")
+    _logs_conf = _cfg.get("logs") or {}
+    _logs_conf["level"] = _log_level
+    LOG.name = service_name
+    LOG.init(_logs_conf)  # read log level from config
