d089fec9a4a8d06d505c6c46e16c028e3c3ec394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:41 +0000

    Add package raw-telegram-api/1.2/raw-telegram-api-1.2.tar.gz

---
+++ packages/raw-telegram-api/raw-telegram-api-1.2.tar.gz/raw-telegram-api-1.2/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(version = "1.2",
+      name = "raw-telegram-api",
+      description = "Raw API for telegram bots",
+      author = "mishpro",
+      author_email = "root@venusbiser.ru",
+      packages = ["raw_telegram"])
