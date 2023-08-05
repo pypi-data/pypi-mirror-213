7a9d99ba7a8d9991d0ecf1db6e3d6abdd8e1d8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:08 +0000

    Add package mario-airflow-utils/0.5.0/mario_airflow_utils-0.5.0.tar.gz

---
+++ packages/mario-airflow-utils/mario_airflow_utils-0.5.0.tar.gz/mario_airflow_utils-0.5.0/src/mario_airflow_utils/__init__.py
@@ -0,0 +1,8 @@
+"""Welcome to mario_airflow_utils
+
+mario_airflow_utils are fun, interesting, and fun programming.
+I will make a world with you.
+"""
+
+from .mario_airflow_utils import ping, hello
+from .mario_airflow_utils.airflow.utils import gen_bash_task
