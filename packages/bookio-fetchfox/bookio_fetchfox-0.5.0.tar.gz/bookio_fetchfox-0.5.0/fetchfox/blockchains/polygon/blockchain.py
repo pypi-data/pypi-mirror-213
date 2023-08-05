d857a144f1f4da9c2f4b7375cb07817550d3282c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:55 +0000

    Add package bookio-fetchfox/0.5.0/bookio_fetchfox-0.5.0.tar.gz

---
+++ packages/bookio-fetchfox/bookio_fetchfox-0.5.0.tar.gz/bookio_fetchfox-0.5.0/fetchfox/blockchains/polygon/blockchain.py
@@ -0,0 +1,18 @@
+from fetchfox.blockchains.evm import Evm
+from fetchfox.constants.blockchains import POLYGON
+from fetchfox.constants.currencies import MATIC
+
+
+class Polygon(Evm):
+    def __init__(
+        self,
+        moralisio_api_key: str = None,
+        openseaio_api_key: str = None,
+    ):
+        super().__init__(
+            name=POLYGON,
+            currency=MATIC,
+            logo="https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png",
+            moralisio_api_key=moralisio_api_key,
+            openseaio_api_key=openseaio_api_key,
+        )
