32acce7120ce665272e4cd6175384c13622a1c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:49 +0000

    Add package chrono-chatgpt/0.1.2/chrono_chatgpt-0.1.2.tar.gz

---
+++ packages/chrono-chatgpt/chrono_chatgpt-0.1.2.tar.gz/chrono_chatgpt-0.1.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import version
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='chrono_chatgpt',
+    version='0.1.2',
+    author="ChronoWalker",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liaoyuzh/chrono-chatgpt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
