cf72df32984363246cc4b1bcd1c0f152ff9352b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:21 +0000

    Add package course-work-oop/2.0/course_work_oop-2.0.tar.gz

---
+++ packages/course-work-oop/course_work_oop-2.0.tar.gz/course_work_oop-2.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+
+setuptools.setup(
+    name="course_work_oop",
+    version='2.0',
+    packages=setuptools.find_packages()
+)
---
+++ packages/course-work-oop/course_work_oop-2.0.tar.gz/course_work_oop-2.0/src/point_present/fraction.py
@@ -0,0 +1,27 @@
+from src.point_present.point import Point
+
+
+class Fraction(Point):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def __str__(self):
+        return f"{self.x}/{self.y}"
+
+    def custom_add(self, other):
+        new_x = self.x * other.y + self.y * other.x
+        new_y = self.y * other.y
+        return Fraction(new_x, new_y)
+
+    def change_sign(self):
+        if self.x > 0 and self.y > 0:
+            return f"{-1 *self.x}/{self.y}"
+        else:
+            return f"{-1 *self.x}/{-1*self.y}"
+
+    def compare(self, other):
+        return self.x * other.y == other.x * self.y
+
+
+
