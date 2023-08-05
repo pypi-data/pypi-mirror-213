3626d4fabadcd0294854ff4b7c1836833e53c4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:52 +0000

    Add package conlay/1.0.1/conlay-1.0.1.tar.gz

---
+++ packages/conlay/conlay-1.0.1.tar.gz/conlay-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+VERSION = "1.0.1"
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    DESC_LONG = readme.read()
+
+
+setuptools.setup(
+    name="conlay",
+    version=VERSION,
+    author="Salliii",
+    description="A python library for creating nice layouts in the console environment",
+    long_description_content_type="text/markdown",
+    long_description=DESC_LONG,
+    url="https://github.com/Salliii/conlay",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    keywords=["python", "library", "console", "layout"]
+)
