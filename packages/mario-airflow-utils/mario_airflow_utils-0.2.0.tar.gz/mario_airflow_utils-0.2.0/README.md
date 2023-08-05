a1dab833e96ffc1abcba82ed014c35fcbc48287e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package mario-airflow-utils/0.2.0/mario_airflow_utils-0.2.0.tar.gz

---
+++ packages/mario-airflow-utils/mario_airflow_utils-0.2.0.tar.gz/mario_airflow_utils-0.2.0/README.md
@@ -0,0 +1,13 @@
+# mario_airflow_utils
+### apache airflow utils
+
+- [https://pypi.org/project/mario_airflow_utils](https://pypi.org/project/mario_airflow_utils)
+
+```
+- mario_airflow_utils is interesting and interesting.
+```
+
+# User Guide
+- [https://dMario24.github.io/mario_airflow_utils](https://dMario24.github.io/mario_airflow_utils)
+
+![logo](https://dMario24.github.io/mario_airflow_utils/img/social_preview.png)
---
+++ packages/mario-airflow-utils/mario_airflow_utils-0.2.0.tar.gz/mario_airflow_utils-0.2.0/src/mario_airflow_utils/airflow/utils.py
@@ -0,0 +1,14 @@
+from airflow.operators.bash import BashOperator
+
+
+def gen_bash_task(name: str, cmd: str, dag, trigger="all_success"):
+    """airflow bash task 생성
+        - trigger-rules : https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/dags.html#trigger-rules
+    """
+    bash_task = BashOperator(
+        task_id=name,
+        bash_command=cmd,
+        trigger_rule=trigger,
+        dag=dag
+    )
+    return bash_task
