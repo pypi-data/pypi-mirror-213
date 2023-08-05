607ff6b6e0bf50a5f0a9da6afc28de3834733566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:41 +0000

    Add package helixd/0.14.0/helixd-0.14.0.tar.gz

---
+++ packages/helixd/helixd-0.14.0.tar.gz/helixd-0.14.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="helixd",
+    version="0.14.0",
+    description="Minimalist LXD API client",
+    long_description="TODO",
+    long_description_content_type="text/markdown",
+    author="Thomas JOUANNOT",
+    author_email="mazerty@gmail.com",
+    url="https://zebr0.io/projects/helixd",
+    download_url="https://gitlab.com/zebr0/helixd",
+    packages=["helixd"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Topic :: System"
+    ],
+    license="MIT",
+    install_requires=[
+        "requests-unixsocket"
+    ]
+)
