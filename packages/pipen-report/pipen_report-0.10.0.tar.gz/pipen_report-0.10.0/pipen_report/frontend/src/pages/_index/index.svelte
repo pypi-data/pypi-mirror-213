a20700fa37acb114439d090ef90e791ef96319fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:06 +0000

    Add package pipen-report/0.10.0/pipen_report-0.10.0.tar.gz

---
+++ packages/pipen-report/pipen_report-0.10.0.tar.gz/pipen_report-0.10.0/pipen_report/frontend/src/pages/_index/index.svelte
@@ -0,0 +1,35 @@
+<script>
+    import IndexLayout from "../../layouts/Index.svelte";
+    import ProcCard from "../../layouts/ProcCard.svelte";
+
+    export let procs;
+    export let pipeline;
+    export let versions;
+
+</script>
+
+<IndexLayout logo={pipeline.name} logotext={pipeline.desc} {versions}>
+    <div class="bx--grid proc-list {procs.length < 10 ? "col-1" : "col-2"}">
+        {#each procs as proc}
+        <div>
+            <ProcCard name={proc["name"]} slug={proc["slug"]} desc={proc["desc"]} />
+        </div>
+        {/each}
+    </div>
+</IndexLayout>
+
+<style>
+    .proc-list {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 0.1rem 1.6rem;
+    }
+
+    .proc-list.col-1 {
+        grid-template-columns: repeat(1, 1fr);
+    }
+
+    .proc-list > div {
+        min-height: 6.2rem;
+    }
+</style>
