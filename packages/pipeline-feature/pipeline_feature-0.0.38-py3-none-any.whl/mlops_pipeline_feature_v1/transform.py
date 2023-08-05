ca9cc2744bb38e1e395646babc0d51b84cf6fba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:13 +0000

    Add package pipeline-feature/0.0.38/pipeline_feature-0.0.38-py3-none-any.whl

---
+++ packages/pipeline-feature/pipeline_feature-0.0.38-py3-none-any.whl/mlops_pipeline_feature_v1/transform.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def cast_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    # TODO: here is hardcoded
+    df["open_time"] = pd.to_datetime(df["open_time"], unit="ms")
+    df["close_time"] = pd.to_datetime(df["close_time"], unit="ms")
+
+    for column in [
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "quote_asset_volume",
+        "taker_buy_base_asset_volume",
+        "taker_buy_quote_asset_volume",
+    ]:
+        df[column] = df[column].astype(float)
+    return df
