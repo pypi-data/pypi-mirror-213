6dc3df2c6a072a912e18b6ff79bdfef44b583d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:40 +0000

    Add package tj-kits-winds/0.0.8/tj_kits_winds-0.0.8.tar.gz

---
+++ packages/tj-kits-winds/tj_kits_winds-0.0.8.tar.gz/tj_kits_winds-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Some Kits'
+
+# with open("README.md", "r+") as f:
+#     long_description = f.read()
+
+setup(
+    name="tj_kits_winds",
+    version=VERSION,
+    author="tjno-1",
+    author_email="tjno-1@qq.com",
+    description=DESCRIPTION,
+    long_description="long_description",
+    packages=find_packages(),
+    install_requires=[
+        'pydantic >= 1.10.7',
+    ],
+    keywords=['python', 'build-in-module kits'],
+    license="MIT",
+    url="https://github.com/TJNo-1/tj_kits_wind.git",
+    python_requires='>=3.8',
+)
