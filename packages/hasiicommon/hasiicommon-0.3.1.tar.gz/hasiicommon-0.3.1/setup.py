1dbb579f9dcc53e1092f5719990486939d902bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:48 +0000

    Add package hasiicommon/0.3.1/hasiicommon-0.3.1.tar.gz

---
+++ packages/hasiicommon/hasiicommon-0.3.1.tar.gz/hasiicommon-0.3.1/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+LICENSE = (HERE / 'LICENSE').read_text()
+
+setup(
+    name="hasiicommon",
+    version="0.3.1",
+    author='Humberto A. Sanchez II',
+    author_email='Humberto.A.Sanchez.II@gmail.com',
+    maintainer='Humberto A. Sanchez II',
+    maintainer_email='humberto.a.sanchez.ii@gmail.com',
+    description='Humberto`s Common Stuff',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hasii2011/hasiicommon",
+    package_data={
+        'hasiicommon':           ['py.typed'],
+        'hasiicommon.resources':              ['py.typed'],
+        'hasiicommon.resources.images':       ['py.typed'],
+        'hasiicommon.resources.images.icons': ['py.typed'],
+        'hasiicommon.resources.images.icons.embedded16': ['py.typed'],
+        'hasiicommon.resources.images.icons.embedded32': ['py.typed'],
+
+        'hasiicommon.ui':         ['py.typed'],
+        'hasiicommon.ui.widgets': ['py.typed'],
+    },
+
+    packages=[
+        'hasiicommon',
+        'hasiicommon.resources',
+        'hasiicommon.resources.images',
+        'hasiicommon.resources.images.icons',
+        'hasiicommon.resources.images.icons.embedded16',
+        'hasiicommon.resources.images.icons.embedded32',
+        'hasiicommon.ui', 'hasiicommon.ui.widgets'
+    ],
+    install_requires=['hasiihelper==0.2.1', 'Deprecated~=1.2.14', 'wxPython~=4.2.1'],
+)
