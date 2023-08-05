46d28ac8c72656c3c508f3a402ceb631ec2ce8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:13 +0000

    Add package weconnect/0.55.1/weconnect-0.55.1-py3-none-any.whl

---
+++ packages/weconnect/weconnect-0.55.1-py3-none-any.whl/weconnect/domain.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class Domain(Enum):
+    ALL = 'all'
+    ALL_CAPABLE = 'allCapable'
+    ACCESS = 'access'
+    AUTOMATION = 'automation'
+    USER_CAPABILITIES = 'userCapabilities'
+    CHARGING = 'charging'
+    CHARGING_PROFILES = 'chargingProfiles'
+    BATTERY_CHARGING_CARE = 'batteryChargingCare'
+    CLIMATISATION = 'climatisation'
+    CLIMATISATION_TIMERS = 'climatisationTimers'
+    DEPARTURE_TIMERS = 'departureTimers'
+    FUEL_STATUS = 'fuelStatus'
+    VEHICLE_LIGHTS = 'vehicleLights'
+    LV_BATTERY = 'lvBattery'
+    READINESS = 'readiness'
+    VEHICLE_HEALTH_INSPECTION = 'vehicleHealthInspection'
+    VEHICLE_HEALTH_WARNINGS = 'vehicleHealthWarnings'
+    OIL_LEVEL = 'oilLevel'
+    MEASUREMENTS = 'measurements'
+    BATTERY_SUPPORT = 'batterySupport'
+    PARKING = 'parking'
+
+    def __str__(self):
+        return self.value
