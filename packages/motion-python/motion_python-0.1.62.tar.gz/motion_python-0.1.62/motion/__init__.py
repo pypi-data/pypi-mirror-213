c4d103c9cacda12bfa183beedd3e2ad739b67126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:51 +0000

    Add package motion-python/0.1.62/motion_python-0.1.62.tar.gz

---
+++ packages/motion-python/motion_python-0.1.62.tar.gz/motion_python-0.1.62/motion/__init__.py
@@ -0,0 +1,5 @@
+from motion.component import Component
+from motion.utils import FitEventGroup, clear_instance
+from motion.instance import ComponentInstance
+
+__all__ = ["Component", "FitEventGroup", "ComponentInstance", "clear_instance"]
