078709b2584d186e43216254d12b95e04083191e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:04 +0000

    Add package LUASprites/0.0.26/LUASprites-0.0.26.tar.gz

---
+++ packages/LUASprites/LUASprites-0.0.26.tar.gz/LUASprites-0.0.26/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+
+setup(
+    name = 'LUASprites',
+    version = '0.0.26',
+    author = 'AJ Steinhauser',
+    author_email = 'ajsteinhauser11@gmail.com',
+    license = 'MIT License',
+    description = 'Generate spritesheets and lua modules for easy image reference',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/AJSteinhauser/LUASprites',
+    package_dir = {"": "src"},
+    packages=['spritegen'],
+    include_package_data=True,
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        luasprite=spritegen.cli:main
+    '''
+)
