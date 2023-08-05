dbe5add6ff5618086789eba35a8b938d81572539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:23 +0000

    Add package solver4mpi/3.4.2/solver4mpi-3.4.2.tar.gz

---
+++ packages/solver4mpi/solver4mpi-3.4.2.tar.gz/solver4mpi-3.4.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='solver4mpi',
+    url='https://github.com/mathias77515/optimized_minimizer',
+    author='Mathias Regnier',
+    author_email='mathias.p.regnier@gmail.com',
+    # Needed to actually package something
+    packages=find_packages(),
+    # Needed for dependencies
+    install_requires=['scipy', 'pyoperators', 'numpy'],
+    # *strongly* suggested for sharing
+    version='3.4.2',
+    # The license can be anything you like
+    license='',
+    description='Optimized minimizer for MPI in python.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
