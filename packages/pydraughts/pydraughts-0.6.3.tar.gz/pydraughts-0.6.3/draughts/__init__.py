e0ccfb95d319c3efc5b6b43b985ddd4db91d1f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:17 +0000

    Add package pydraughts/0.6.3/pydraughts-0.6.3.tar.gz

---
+++ packages/pydraughts/pydraughts-0.6.3.tar.gz/pydraughts-0.6.3/draughts/__init__.py
@@ -0,0 +1,7 @@
+from draughts.core.game import Game
+from draughts.core.move import StandardMove
+from draughts.core.variant import Board, Move, WHITE, BLACK
+
+__author__ = "Ioannis Pantidis"
+__copyright__ = "2021-2023, " + __author__
+__version__ = "0.6.3"
