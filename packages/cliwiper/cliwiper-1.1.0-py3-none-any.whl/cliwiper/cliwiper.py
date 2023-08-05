fdc301e72f264551423d3d60daaf6fcb10889844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:10 +0000

    Add package cliwiper/1.1.0/cliwiper-1.1.0-py3-none-any.whl

---
+++ packages/cliwiper/cliwiper-1.1.0-py3-none-any.whl/cliwiper/cliwiper.py
@@ -0,0 +1,14 @@
+import os
+
+
+def wiper():
+    # for mac and linux(here, os.name is 'posix')
+    if os.name == "posix":
+        _ = os.system("clear")
+    else:
+        # for windows platfrom
+        _ = os.system("cls")
+
+
+if __name__ == "__main__":
+    wiper()
