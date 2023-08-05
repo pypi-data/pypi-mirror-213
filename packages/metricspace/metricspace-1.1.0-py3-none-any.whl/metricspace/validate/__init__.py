f0f7593adff14e149c12aa418fc0e17770889f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:54 +0000

    Add package metricspace/1.1.0/metricspace-1.1.0-py3-none-any.whl

---
+++ packages/metricspace/metricspace-1.1.0-py3-none-any.whl/metricspace/validate/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains the validation functions for the metricspace package.
+Helps ensure routines are expected and raises errors or warnings if undesired behavior is detected or expected. 
+
+For example, if providing a search resolution window and not using optimizations, a warning will be raised indicating that the computation 
+could take an astronomically long time. 
+
+"""
