3c1881291e2359e6815926ea3c646edb68dffd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:47 +0000

    Add package license-manager-agent/2.3.2/license_manager_agent-2.3.2.tar.gz

---
+++ packages/license-manager-agent/license_manager_agent-2.3.2.tar.gz/license_manager_agent-2.3.2/lm_agent/workload_managers/slurm/slurmctld_epilog.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+The EpilogSlurmctld executable.
+"""
+import asyncio
+import sys
+
+from lm_agent.backend_utils import remove_booking_for_job_id
+from lm_agent.config import settings
+from lm_agent.logs import init_logging, logger
+from lm_agent.reconciliation import update_report
+from lm_agent.workload_managers.slurm.cmd_utils import get_required_licenses_for_job
+from lm_agent.workload_managers.slurm.common import get_job_context
+
+
+async def epilog():
+    # Initialize the logger
+    init_logging("slurmctld-epilog")
+    job_context = get_job_context()
+    job_id = job_context["job_id"]
+    job_licenses = job_context["job_licenses"]
+
+    # Check if reconciliation should be triggered.
+    if settings.USE_RECONCILE_IN_PROLOG_EPILOG:
+        # Force a reconciliation before we attempt to remove bookings.
+        try:
+            await update_report()
+        except Exception as e:
+            logger.error(f"Failed to call reconcile with {e}")
+            sys.exit(1)
+
+    try:
+        required_licenses = get_required_licenses_for_job(job_licenses)
+    except Exception as e:
+        logger.error(f"Failed to call get_required_licenses_for_job with {e}")
+        sys.exit(1)
+
+    if not required_licenses:
+        logger.debug("No licenses required, exiting!")
+        sys.exit(0)
+
+    if len(required_licenses) > 0:
+        # Attempt to remove the booking and log the result.
+        booking_removed = await remove_booking_for_job_id(job_id)
+        if booking_removed:
+            logger.debug(f"Booking for job id: {job_id} successfully deleted.")
+        else:
+            logger.debug(f"Booking for job id: {job_id} not removed.")
+
+
+def main():
+    asyncio.run(epilog())
+
+
+if __name__ == "__main__":
+    main()
