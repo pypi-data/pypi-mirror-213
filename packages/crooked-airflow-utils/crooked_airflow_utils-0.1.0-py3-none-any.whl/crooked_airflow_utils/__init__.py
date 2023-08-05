43ef40688c1e91e35c4d28118d1ed92e6c358a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:33 +0000

    Add package crooked-airflow-utils/0.1.0/crooked_airflow_utils-0.1.0-py3-none-any.whl

---
+++ packages/crooked-airflow-utils/crooked_airflow_utils-0.1.0-py3-none-any.whl/crooked_airflow_utils/__init__.py
@@ -0,0 +1,7 @@
+"""Welcome to crooked_airflow_utils
+
+crooked_airflow_utils are fun, interesting, and fun programming.
+I will make a world with you.
+"""
+
+from .crooked_airflow_utils import ping
