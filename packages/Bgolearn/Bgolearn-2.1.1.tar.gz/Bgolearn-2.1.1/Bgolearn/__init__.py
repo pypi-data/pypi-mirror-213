67029d920db6f9cad657c13e4b319e2d652d4339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:43 +0000

    Add package Bgolearn/2.1.1/Bgolearn-2.1.1.tar.gz

---
+++ packages/Bgolearn/Bgolearn-2.1.1.tar.gz/Bgolearn-2.1.1/Bgolearn/__init__.py
@@ -0,0 +1,12 @@
+
+print('Bgolearn : A Bayesian global optimization package')
+print('__documents__ = https://bgolearn.netlify.app/')
+print('__url__ = https://github.com/Bin-Cao/Bgolearn')
+print('__author__ = Bin CAO : binjacobcao@gmail.com')
+
+__description__ = 'A Bayesian global optimization package'
+__documents__ = 'https://bgolearn.netlify.app/'
+__author__ = 'Bin CAO, ZheJiang LAB, Hangzhou, CHINA. (MGI, SHU, Shanghai, CHINA).'
+__author_email__ = 'binjacobcao@gmail.com'
+__url__ = 'https://github.com/Bin-Cao/Bgolearn'
+
