9c60288eae4de8023e0373c951926e0e1e96c324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:09 +0000

    Add package netbox-cisco-maintenance/0.0.124/netbox_cisco_maintenance-0.0.124-py3-none-any.whl

---
+++ packages/netbox-cisco-maintenance/netbox_cisco_maintenance-0.0.124-py3-none-any.whl/netbox_cisco_support_plugin/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from netbox.views.generic import ObjectChangeLogView
+from . import models, views
+
+
+urlpatterns = (
+    # Cisco Device Support
+    path("cisco-support/", views.CiscoSupportListView.as_view(), name="ciscosupport_list"),
+    path("cisco-support/add/", views.CiscoSupportEditView.as_view(), name="ciscosupport_add"),
+    path("cisco-support/<int:pk>/", views.CiscoSupportView.as_view(), name="ciscosupport"),
+    path("cisco-support/<int:pk>/edit/", views.CiscoSupportEditView.as_view(), name="ciscosupport_edit"),
+    path("cisco-support/<int:pk>/delete/", views.CiscoSupportDeleteView.as_view(), name="ciscosupport_delete"),
+    # Cisco Device Type Support
+)
