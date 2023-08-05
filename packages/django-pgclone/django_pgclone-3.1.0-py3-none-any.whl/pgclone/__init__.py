d695afdadfde20e31924c69e9835f8fb2fafd1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:05 +0000

    Add package django-pgclone/3.1.0/django_pgclone-3.1.0-py3-none-any.whl

---
+++ packages/django-pgclone/django_pgclone-3.1.0-py3-none-any.whl/pgclone/__init__.py
@@ -0,0 +1,6 @@
+from pgclone.dump_cmd import dump
+from pgclone.ls_cmd import ls
+from pgclone.restore_cmd import restore
+from pgclone.version import __version__
+
+__all__ = ["dump", "ls", "restore", "__version__"]
