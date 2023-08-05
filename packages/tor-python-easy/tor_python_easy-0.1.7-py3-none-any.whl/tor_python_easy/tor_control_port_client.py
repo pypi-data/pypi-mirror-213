618382b205499cc826532867df5bbf22fc1135f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:38 +0000

    Add package tor-python-easy/0.1.7/tor_python_easy-0.1.7-py3-none-any.whl

---
+++ packages/tor-python-easy/tor_python_easy-0.1.7-py3-none-any.whl/tor_python_easy/tor_control_port_client.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+import time
+from typing import Optional
+
+
+class TorControlPortClient:
+    control_address: str
+    control_port: int
+    control_password: Optional[str]
+
+    def __init__(
+            self,
+            control_address: str,
+            control_port: int,
+            control_password: Optional[str] = None
+    ):
+        self.control_address = control_address
+        self.control_port = control_port
+        self.control_password = control_password
+
+    def change_connection_ip(self, seconds_wait: int = 5) -> bool:
+        time.sleep(seconds_wait)
+        try:
+            tor_connection = socket.create_connection((self.control_address, self.control_port))
+            password_value = self.control_password if self.control_password is not None else ''
+            message = f'AUTHENTICATE "{password_value}"\r\nSIGNAL NEWNYM\r\n'
+            tor_connection.send(message.encode('utf-8'))
+            response = tor_connection.recv(1024)
+            if response != b'250 OK\r\n250 OK\r\n':
+                sys.stderr.write('Unexpected response from Tor control port: {}\n'.format(response))
+                return False
+            return True
+        except Exception as e:
+            print(e)
+            sys.stderr.write('Error connecting to Tor control port: {}\n'.format(repr(e)))
+            return False
