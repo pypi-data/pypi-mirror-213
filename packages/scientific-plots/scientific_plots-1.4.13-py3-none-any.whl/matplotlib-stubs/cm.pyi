df2b1303056ca0f92967fce7eaeb412be8462f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:38 +0000

    Add package scientific-plots/1.4.13/scientific_plots-1.4.13-py3-none-any.whl

---
+++ packages/scientific-plots/scientific_plots-1.4.13-py3-none-any.whl/matplotlib-stubs/cm.pyi
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Stub-file for usage with matplot-lib. This file registers color-maps for later
+usage.
+"""
+from typing import Union
+
+from .colors import LinearSegmentedColormap, ListedColormap
+
+
+def register_cmap(name: str, cmap: Union[LinearSegmentedColormap,
+                  ListedColormap]) -> None: ...
