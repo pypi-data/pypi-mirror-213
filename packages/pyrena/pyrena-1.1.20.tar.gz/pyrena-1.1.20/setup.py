c4755f4563339d219161a6be2107685c8c58cbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:47 +0000

    Add package pyrena/1.1.20/pyrena-1.1.20.tar.gz

---
+++ packages/pyrena/pyrena-1.1.20.tar.gz/pyrena-1.1.20/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as file:
+	readme = file.read()
+
+setuptools.setup(
+	name="pyrena",
+	version="1.1.20",
+	description="Python wrapper for Arena QMS API.",
+	url="https://github.com/thecodeforge/pyrena",
+	py_modules=["pyrena"],
+	install_requires=[
+		"mistletoe",
+		"requests"
+	],
+	packages=setuptools.find_packages(),
+	package_dir={
+		"pyrena":"pyrena"
+	},
+	long_description = readme,
+	long_description_content_type = "text/markdown"
+)
