2c1db8a8fc93e13307dee5c455d80459282dada9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:44 +0000

    Add package netbox-cisco-maintenance/0.0.126/netbox_cisco_maintenance-0.0.126-py3-none-any.whl

---
+++ packages/netbox-cisco-maintenance/netbox_cisco_maintenance-0.0.126-py3-none-any.whl/netbox_cisco_support_plugin/tables.py
@@ -0,0 +1,44 @@
+import django_tables2 as tables
+from django.utils.safestring import mark_safe
+
+from netbox.tables import BaseTable, NetBoxTable
+from .models import CiscoSupport, CiscoDeviceTypeSupport
+
+
+class CiscoSupportTable(NetBoxTable):
+    del actions
+
+    device = tables.Column(linkify=True)
+
+    class BooleanColumn(tables.Column):
+        """
+        Custom implementation of BooleanColumn to render a nicely-formatted checkmark or X icon instead of a Unicode
+        character.
+        """
+        def render(self, value):
+            if value:
+                rendered = '<span class="text-success"><i class="mdi mdi-check-circle text-success"></i></span>'
+            elif value is None:
+                rendered = '<span class="text-muted">&mdash;</span>'
+            else:
+                rendered = '<span class="text-danger"><i class="mdi mdi-check-circle text-danger"></i></span>'
+            return mark_safe(rendered)
+
+        def value(self, value):
+            return str(value)
+
+    class Meta(NetBoxTable.Meta):
+        model = CiscoSupport
+        # fmt: off
+        fields = (
+           "pk", "id", "device", "recommended_release", "desired_release", "current_release",
+            "desired_release_status", "current_release_status", "api_status", "sr_no_owner", "is_covered",
+            "contract_supplier", "coverage_end_date", "service_line_descr", "service_contract_number",
+            "warranty_end_date", "warranty_type",
+        )
+        default_columns = (
+            "device", "recommended_release", "desired_release", "current_release", "api_status", "sr_no_owner",
+            "is_covered", "contract_supplier", "coverage_end_date", "service_line_descr",
+            "service_contract_number",
+        )
+        # fmt: on
