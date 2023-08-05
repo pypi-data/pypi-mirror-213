742defc2d1c39c3c712bcd62cae6f7336a029a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:10 +0000

    Add package cove-unified-logs/0.1.6/cove_unified_logs-0.1.6.tar.gz

---
+++ packages/cove-unified-logs/cove_unified_logs-0.1.6.tar.gz/cove_unified_logs-0.1.6/cove_unified_logs/middleware.py
@@ -0,0 +1,22 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
+from your_module import UnifiedLogger
+from django.apps import apps
+
+
+class LoggingMiddleware(MiddlewareMixin):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.app_name = apps.get_app_config(__package__.split('.')[0]).verbose_name
+        self.logger = UnifiedLogger(self.app_name, config='all')
+        self.logger.set_level(settings.LOG_LEVEL)
+
+    def process_request(self, request):
+        self.logger.debug('Request received', path=request.path)
+
+    def process_response(self, request, response):
+        self.logger.debug('Response sent', status_code=response.status_code)
+        return response
+
+    def process_exception(self, request, exception):
+        self.logger.error('Exception encountered', exception=str(exception))
