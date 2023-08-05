96872ad09eaea3a1ecc0f8e96b5c7924ea806f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:42 +0000

    Add package agixt/1.2.1b0/agixt-1.2.1b0-py3-none-any.whl

---
+++ packages/agixt/agixt-1.2.1b0-py3-none-any.whl/agixt/Prompts.py
@@ -0,0 +1,53 @@
+import os
+
+
+class Prompts:
+    def add_prompt(self, prompt_name, prompt):
+        # if prompts folder does not exist, create it
+        if not os.path.exists("prompts"):
+            os.mkdir("prompts")
+        # if prompt file does not exist, create it
+        if not os.path.exists(os.path.join("prompts", f"{prompt_name}.txt")):
+            with open(os.path.join("prompts", f"{prompt_name}.txt"), "w") as f:
+                f.write(prompt)
+
+    def get_prompt(self, prompt_name, model="default"):
+        model_prompt_file = f"prompts/{model}/{prompt_name}.txt"
+        default_prompt_file = os.path.join("prompts", f"{prompt_name}.txt")
+
+        prompt_file = (
+            model_prompt_file
+            if os.path.isfile(model_prompt_file)
+            else default_prompt_file
+        )
+        with open(prompt_file, "r") as f:
+            prompt = f.read()
+            return prompt
+
+    def get_prompts(self):
+        # Get all files in prompts folder that end in .txt and replace .txt with empty string
+        prompts = []
+        for file in os.listdir("prompts"):
+            if file.endswith(".txt"):
+                prompts.append(file.replace(".txt", ""))
+        return prompts
+
+    def get_prompt_args(self, prompt_name):
+        prompt = self.get_prompt(prompt_name)
+        # Find anything in the file between { and } and add them to a list to return
+        prompt_vars = []
+        for word in prompt.split():
+            if word.startswith("{") and word.endswith("}"):
+                prompt_vars.append(word[1:-1])
+        return prompt_vars
+
+    def delete_prompt(self, prompt_name):
+        os.remove(os.path.join("prompts", f"{prompt_name}.txt"))
+
+    def update_prompt(self, prompt_name, prompt):
+        with open(os.path.join("prompts", f"{prompt_name}.txt"), "w") as f:
+            f.write(prompt)
+
+    def get_model_prompt(self, prompt_name, model="default"):
+        with open(f"prompts/{model}/{prompt_name}.txt", "r") as f:
+            return f.read()
