abca374c19b33df8b9522f146bed8ced3070f863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package netbox-cisco-maintenance/0.0.137/netbox_cisco_maintenance-0.0.137-py3-none-any.whl

---
+++ packages/netbox-cisco-maintenance/netbox_cisco_maintenance-0.0.137-py3-none-any.whl/netbox_cisco_support_plugin/forms.py
@@ -0,0 +1,15 @@
+from netbox.forms import NetBoxModelForm
+from .models import CiscoSupport, CiscoDeviceTypeSupport
+from utilities.forms.fields import CommentField
+
+
+class CiscoSupportForm(NetBoxModelForm):
+    class Meta:
+        model = CiscoSupport
+        # fmt: off
+        fields = ("recommended_release", "desired_release", "current_release",
+            "desired_release_status", "current_release_status", "api_status", "sr_no_owner", "is_covered",
+            "contract_supplier", "coverage_end_date", "service_line_descr", "service_contract_number",
+            "warranty_end_date", "warranty_type", "tags",
+        )
+        # fmt: on
