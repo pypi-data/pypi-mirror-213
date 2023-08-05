643649e119f9e1e9817113cf2e64c2e30ccaf0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:26 +0000

    Add package diffErrBnk/0.0.1/diffErrBnk-0.0.1.tar.gz

---
+++ packages/diffErrBnk/diffErrBnk-0.0.1.tar.gz/diffErrBnk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='diffErrBnk',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/SOH4C4759/diffBnk',
+    license='',
+    author='Sora',
+    description='用于自动化修复Wwise升级2022版本后，安卓平台bnk构建异常的问题'
+)
