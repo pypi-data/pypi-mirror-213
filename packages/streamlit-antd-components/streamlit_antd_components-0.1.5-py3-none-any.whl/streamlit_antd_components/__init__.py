906be429be26f2e67ffe2f75dfd7f15825d3acfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:05 +0000

    Add package streamlit-antd-components/0.1.5/streamlit_antd_components-0.1.5-py3-none-any.whl

---
+++ packages/streamlit-antd-components/streamlit_antd_components-0.1.5-py3-none-any.whl/streamlit_antd_components/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time     : 2023/4/26 10:49
+@Author   : ji hao ran
+@File     : __init__.py.py
+@Project  : StreamlitAntdComponents
+@Software : PyCharm
+"""
+_RELEASE = True
+
+from .buttons import buttons, ButtonsItem
+from .menu import menu, MenuItem
+from .tabs import tabs, TabsItem
+from .tree import tree, TreeItem
