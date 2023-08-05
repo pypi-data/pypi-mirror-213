ebc6fecf38ae41d6efb46733b5ee6fdb25b6f6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:30 +0000

    Add package scaleway-async/0.14.1/scaleway_async-0.14.1.tar.gz

---
+++ packages/scaleway-async/scaleway_async-0.14.1.tar.gz/scaleway_async-0.14.1/scaleway_async/container/v1beta1/__init__.py
@@ -0,0 +1,77 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import ContainerHttpOption
+from .types import ContainerPrivacy
+from .types import ContainerProtocol
+from .types import ContainerStatus
+from .types import CronStatus
+from .types import DomainStatus
+from .types import ListContainersRequestOrderBy
+from .types import ListCronsRequestOrderBy
+from .types import ListDomainsRequestOrderBy
+from .types import ListLogsRequestOrderBy
+from .types import ListNamespacesRequestOrderBy
+from .types import ListTokensRequestOrderBy
+from .types import LogStream
+from .types import NamespaceStatus
+from .types import NullValue
+from .types import TokenStatus
+from .types import Container
+from .types import Cron
+from .types import Domain
+from .types import ListContainersResponse
+from .types import ListCronsResponse
+from .types import ListDomainsResponse
+from .types import ListLogsResponse
+from .types import ListNamespacesResponse
+from .types import ListTokensResponse
+from .types import Log
+from .types import Namespace
+from .types import Secret
+from .types import SecretHashedValue
+from .types import Token
+from .content import CONTAINER_TRANSIENT_STATUSES
+from .content import CRON_TRANSIENT_STATUSES
+from .content import DOMAIN_TRANSIENT_STATUSES
+from .content import NAMESPACE_TRANSIENT_STATUSES
+from .content import TOKEN_TRANSIENT_STATUSES
+from .api import ContainerV1Beta1API
+
+__all__ = [
+    "ContainerHttpOption",
+    "ContainerPrivacy",
+    "ContainerProtocol",
+    "ContainerStatus",
+    "CronStatus",
+    "DomainStatus",
+    "ListContainersRequestOrderBy",
+    "ListCronsRequestOrderBy",
+    "ListDomainsRequestOrderBy",
+    "ListLogsRequestOrderBy",
+    "ListNamespacesRequestOrderBy",
+    "ListTokensRequestOrderBy",
+    "LogStream",
+    "NamespaceStatus",
+    "NullValue",
+    "TokenStatus",
+    "Container",
+    "Cron",
+    "Domain",
+    "ListContainersResponse",
+    "ListCronsResponse",
+    "ListDomainsResponse",
+    "ListLogsResponse",
+    "ListNamespacesResponse",
+    "ListTokensResponse",
+    "Log",
+    "Namespace",
+    "Secret",
+    "SecretHashedValue",
+    "Token",
+    "CONTAINER_TRANSIENT_STATUSES",
+    "CRON_TRANSIENT_STATUSES",
+    "DOMAIN_TRANSIENT_STATUSES",
+    "NAMESPACE_TRANSIENT_STATUSES",
+    "TOKEN_TRANSIENT_STATUSES",
+    "ContainerV1Beta1API",
+]
