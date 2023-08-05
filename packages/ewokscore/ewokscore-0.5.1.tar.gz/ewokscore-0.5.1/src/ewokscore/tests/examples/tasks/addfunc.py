50f24942f2ed04eec5de4bfd841ecf0f9432c4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:38 +0000

    Add package ewokscore/0.5.1/ewokscore-0.5.1.tar.gz

---
+++ packages/ewokscore/ewokscore-0.5.1.tar.gz/ewokscore-0.5.1/src/ewokscore/tests/examples/tasks/addfunc.py
@@ -0,0 +1,3 @@
+def addfunc(arg):
+    """Demo task: add 1 to argument"""
+    return arg + 1
---
+++ packages/ewokscore/ewokscore-0.5.1.tar.gz/ewokscore-0.5.1/src/ewokscore/tests/examples/tasks/simplemethods.py
@@ -0,0 +1,8 @@
+def add(*args):
+    """Demo task: add objects to 1"""
+    return sum(args) + 1
+
+
+def append(*args):
+    """Demo task: pack object in a tuple"""
+    return args
