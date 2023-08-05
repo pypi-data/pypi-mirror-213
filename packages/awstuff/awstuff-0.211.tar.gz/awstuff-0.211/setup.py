5045b7cc11945b5a67d3f5acde49f5f1d5910e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:22 +0000

    Add package awstuff/0.211/awstuff-0.211.tar.gz

---
+++ packages/awstuff/awstuff-0.211.tar.gz/awstuff-0.211/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='awstuff',
+      version='0.211',
+      description='Helper functions for interacting with AWS and other misc tasks',
+      packages=['awstuff'],
+      author_email='danieldbrickell@gmail.com',
+      zip_safe=False)
