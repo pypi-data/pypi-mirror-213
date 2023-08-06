948888d761893d57c400baaa9965ba1f57c6a485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:51 +0000

    Add package resotolib/3.5.2/resotolib-3.5.2-py3-none-any.whl

---
+++ packages/resotolib/resotolib-3.5.2-py3-none-any.whl/resotolib/web/metrics.py
@@ -0,0 +1,30 @@
+import os
+import cherrypy
+from prometheus_client.exposition import generate_latest, CONTENT_TYPE_LATEST
+
+
+class WebApp:
+    def __init__(self, mountpoint: str = "/") -> None:
+        self.mountpoint = mountpoint
+        local_path = os.path.abspath(os.path.dirname(__file__))
+        config = {
+            "tools.gzip.on": True,
+            "tools.staticdir.index": "index.html",
+            "tools.staticdir.on": True,
+            "tools.staticdir.dir": f"{local_path}/static",
+        }
+        self.config = {"/": config}
+        if self.mountpoint not in ("/", ""):
+            self.config[self.mountpoint] = config
+
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=["GET"])
+    def health(self) -> str:
+        cherrypy.response.headers["Content-Type"] = "text/plain"
+        return "ok\r\n"
+
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=["GET"])
+    def metrics(self) -> bytes:
+        cherrypy.response.headers["Content-Type"] = CONTENT_TYPE_LATEST
+        return generate_latest()
