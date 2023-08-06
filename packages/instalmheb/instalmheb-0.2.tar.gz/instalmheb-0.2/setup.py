00c945e8a14e40a17eff00a89c2fc11e1fa093a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package instalmheb/0.2/instalmheb-0.2.tar.gz

---
+++ packages/instalmheb/instalmheb-0.2.tar.gz/instalmheb-0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'instalmheb',
+    version= '0.2',
+    author= 'Wael-almheb',
+    description= 'Python library for extracting Instagram cookies and many more features',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
