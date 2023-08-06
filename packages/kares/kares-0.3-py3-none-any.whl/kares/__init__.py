b9dd51b8644be52e985bd92986fffc1a43071b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:16 +0000

    Add package kares/0.3/kares-0.3-py3-none-any.whl

---
+++ packages/kares/kares-0.3-py3-none-any.whl/kares/__init__.py
@@ -0,0 +1,19 @@
+def keras(val):
+    pracs = {
+        1:"Practical_1.txt",
+        2:"Practical_2.txt",
+        3:"Practical_3.txt",
+        4:"Practical_4.txt",
+        5:"Practical_5.txt",
+        6:"Practical_6.txt",
+        7:"Practical_7.txt",
+        8:"Practical_8.txt",
+        9:"Practical_9.txt"
+    }
+    filename = pracs.get(val)
+    if filename is not None:
+        with open(filename, "r") as file:
+            data = file.read()
+        return data
+    else:
+        return "Invalid value"
