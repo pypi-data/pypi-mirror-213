2d79c4c495e4bf62620e3c4a3becb4b8f7658eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:09 +0000

    Add package epub2txt/0.1.5/epub2txt-0.1.5.tar.gz

---
+++ packages/epub2txt/epub2txt-0.1.5.tar.gz/epub2txt-0.1.5/epub2txt/browse_filename.py
@@ -0,0 +1,36 @@
+"""Browse to a file.
+
+https://pythonspot.com/tk-file-dialogs/
+filedialog.askopenfilename(
+    initialdir = "/",
+    title = "Select file",
+    filetypes=(("jpeg files","*.jpg"),
+    ("all files","*.*")))
+"""
+import tkinter as Tkinter
+
+# import sys
+from pathlib import Path
+from tkinter import filedialog as tkFileDialog
+
+
+# fmt: off
+def browse_filename(
+        initialdir=Path.cwd(),
+        title="Select a file",
+        filetypes=(
+            ("epub files", "*.epub"),
+            ("all files", "*.*"),
+        )
+):
+    # fmt: on
+    """Browse for a filename."""
+    root = Tkinter.Tk()
+    root.withdraw()
+    filename = tkFileDialog.askopenfilename(
+        parent=root,
+        initialdir=initialdir,
+        title=title,
+        filetypes=filetypes,
+    )
+    return filename
