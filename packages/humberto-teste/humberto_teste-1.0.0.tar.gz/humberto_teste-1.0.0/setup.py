90ce4d28e320bbff3200944be2aabac5a942cbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:55 +0000

    Add package humberto-teste/1.0.0/humberto_teste-1.0.0.tar.gz

---
+++ packages/humberto-teste/humberto_teste-1.0.0.tar.gz/humberto_teste-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="humberto_teste",
+    version="1.0.0",
+    description="Modulo Python dedicado a evolução de circuitos lógicos através de algoritmos genéticos",
+    author="Humberto Távora, Stefan Blawid, Yasmin Maria",
+    packages=find_packages()
+    )
+
