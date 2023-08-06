8a00b3bba24b8fbdd14912977551c002ea2c5d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package git-sim/0.3.2/git_sim-0.3.2-py3-none-any.whl

---
+++ packages/git-sim/git_sim-0.3.2-py3-none-any.whl/git_sim/settings.py
@@ -0,0 +1,48 @@
+import pathlib
+from typing import List, Union
+
+from pydantic import BaseSettings
+
+from git_sim.enums import StyleOptions, ColorByOptions, ImgFormat, VideoFormat
+
+
+class Settings(BaseSettings):
+    allow_no_commits = False
+    animate = False
+    auto_open = True
+    n_default = 5
+    n = 5
+    files: Union[List[pathlib.Path], None] = None
+    hide_first_tag = False
+    img_format: ImgFormat = ImgFormat.JPG
+    INFO_STRING = "Simulating: git"
+    light_mode = False
+    transparent_bg = False
+    logo = pathlib.Path(__file__).parent.resolve() / "logo.png"
+    low_quality = False
+    max_branches_per_commit = 1
+    max_tags_per_commit = 1
+    media_dir = pathlib.Path().cwd()
+    outro_bottom_text = "Learn more at initialcommit.com"
+    outro_top_text = "Thanks for using Initial Commit!"
+    reverse = False
+    show_intro = False
+    show_outro = False
+    speed = 1.5
+    title = "Git-Sim, by initialcommit.com"
+    video_format: VideoFormat = VideoFormat.MP4
+    stdout = False
+    output_only_path = False
+    quiet = False
+    invert_branches = False
+    hide_merged_branches = False
+    all = False
+    color_by: Union[ColorByOptions, None] = None
+    highlight_commit_messages = False
+    style: Union[StyleOptions, None] = StyleOptions.CLEAN
+
+    class Config:
+        env_prefix = "git_sim_"
+
+
+settings = Settings()
