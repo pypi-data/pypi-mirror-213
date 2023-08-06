683d8ac3fc08a4417fc2d8f939d36108f761ceec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package gracy/1.19.0/gracy-1.19.0.tar.gz

---
+++ packages/gracy/gracy-1.19.0.tar.gz/gracy-1.19.0/src/gracy/__init__.py
@@ -0,0 +1,54 @@
+"""Gracefully manage your API interactions"""
+from __future__ import annotations
+
+import logging
+
+from . import exceptions, replays
+from ._core import Gracy, graceful, graceful_generator
+from ._models import (
+    DEFAULT_CONFIG,
+    BaseEndpoint,
+    GracefulRetry,
+    GracefulRetryState,
+    GracefulThrottle,
+    GracefulValidator,
+    GracyConfig,
+    GracyRequestContext,
+    LogEvent,
+    LogLevel,
+    ThrottleRule,
+)
+from ._reports._models import GracyAggregatedRequest, GracyAggregatedTotal, GracyReport
+from .replays.storages._base import GracyReplay, GracyReplayStorage
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+
+__version__ = "1.19.0"
+
+__all__ = [
+    "exceptions",
+    # Core
+    "Gracy",
+    "graceful",
+    "graceful_generator",
+    # Models
+    "BaseEndpoint",
+    "GracefulRetry",
+    "GracefulRetryState",
+    "GracefulValidator",
+    "GracyRequestContext",
+    "LogEvent",
+    "LogLevel",
+    "GracefulThrottle",
+    "ThrottleRule",
+    "GracyConfig",
+    "DEFAULT_CONFIG",
+    # Replays
+    "replays",
+    "GracyReplay",
+    "GracyReplayStorage",
+    # Reports
+    "GracyReport",
+    "GracyAggregatedTotal",
+    "GracyAggregatedRequest",
+]
