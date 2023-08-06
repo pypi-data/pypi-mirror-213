72c14f1fa2d2385678037f8d1b870203c99a2151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:51 +0000

    Add package pdm-conda/0.11.0b3/pdm_conda-0.11.0b3-py3-none-any.whl

---
+++ packages/pdm-conda/pdm_conda-0.11.0b3-py3-none-any.whl/pdm_conda/cli/utils.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+import functools
+from typing import TYPE_CHECKING
+
+from pdm_conda.models.candidates import CondaCandidate
+from pdm_conda.models.requirements import CondaRequirement, as_conda_requirement
+
+if TYPE_CHECKING:
+    from pdm.project import Project
+
+    from pdm_conda.models.candidates import Candidate
+    from pdm_conda.models.requirements import Requirement
+
+_patched = False
+
+
+def remove_quotes(req: str) -> str:
+    for quote in ("'", '"'):
+        if req.startswith(quote) and req.endswith(quote):
+            req = req[1:-1]
+    return req
+
+
+def wrap_save_version_specifiers(func):
+    @functools.wraps(func)
+    def wrapper(
+        requirements: dict[str, dict[str, Requirement]],
+        resolved: dict[str, Candidate],
+        save_strategy: str,
+    ) -> None:
+        func(requirements, resolved, save_strategy)
+        for reqs in requirements.values():
+            for name in reqs:
+                if isinstance(can := resolved[name], CondaCandidate):
+                    req = as_conda_requirement(reqs[name])
+                    req.version_mapping.update(can.req.version_mapping)
+                    req.is_python_package = can.req.is_python_package
+                    reqs[name] = req
+
+    return wrapper
+
+
+def wrap_format_lockfile(func):
+    @functools.wraps(func)
+    def wrapper(
+        project: Project,
+        mapping: dict[str, Candidate],
+        fetched_dependencies: dict[tuple[str, str | None], list[Requirement]],
+    ) -> dict:
+        for deps in fetched_dependencies.values():
+            for i, dep in enumerate(deps):
+                if isinstance(dep, CondaRequirement):
+                    setattr(dep, "as_line", functools.partial(dep.as_line, with_build_string=True))
+        version_mapping = {}
+        for name, can in mapping.items():
+            if isinstance(can, CondaCandidate):
+                fetched_dependencies[(can.name, can.conda_version)] = fetched_dependencies.pop(
+                    (can.name, can.version),
+                    [],
+                )
+                version_mapping[name] = can.version
+                can.version = can.conda_version
+        try:
+            res = func(project, mapping, fetched_dependencies)
+            return res
+        finally:
+            for deps in fetched_dependencies.values():
+                for i, dep in enumerate(deps):
+                    if isinstance(dep, CondaRequirement):
+                        setattr(dep, "as_line", dep.as_line.func)  # type: ignore
+            for name, version in version_mapping.items():
+                can = mapping[name]
+                can.version = version
+                fetched_dependencies[(can.name, can.version)] = fetched_dependencies.pop(
+                    (can.name, can.conda_version),
+                    [],
+                )
+
+    return wrapper
+
+
+if not _patched:
+    from pdm.cli import actions, utils
+
+    save_version_specifiers = wrap_save_version_specifiers(utils.save_version_specifiers)
+    format_lockfile = wrap_format_lockfile(utils.format_lockfile)
+    for m in [utils, actions]:
+        setattr(m, "save_version_specifiers", save_version_specifiers)
+        setattr(m, "format_lockfile", format_lockfile)
+    _patched = True
---
+++ packages/pdm-conda/pdm_conda-0.11.0b3-py3-none-any.whl/pdm_conda/resolver/providers.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pdm.resolver.providers import BaseProvider, EagerUpdateProvider, ReusePinProvider
+from pdm.resolver.python import find_python_matches
+from unearth.utils import LazySequence
+
+from pdm_conda.models.requirements import CondaRequirement
+
+if TYPE_CHECKING:
+    from typing import Callable, Iterator, Mapping, Sequence
+
+    from pdm.models.candidates import Candidate
+    from pdm.models.requirements import Requirement
+    from pdm.resolver.providers import Comparable
+    from resolvelib.resolvers import RequirementInformation
+
+
+class CondaBaseProvider(BaseProvider):
+    def get_preference(
+        self,
+        identifier: str,
+        resolutions: dict[str, Candidate],
+        candidates: dict[str, Iterator[Candidate]],
+        information: dict[str, Iterator[RequirementInformation]],
+        backtrack_causes: Sequence[RequirementInformation],
+    ) -> tuple[Comparable, ...]:
+        preference = super().get_preference(identifier, resolutions, candidates, information, backtrack_causes)
+        return (
+            *preference[:3],
+            not any(isinstance(i.requirement, CondaRequirement) for i in information[identifier]),
+            *preference[3:],
+        )
+
+    def find_matches(
+        self,
+        identifier: str,
+        requirements: Mapping[str, Iterator[Requirement]],
+        incompatibilities: Mapping[str, Iterator[Candidate]],
+    ) -> Callable[[], Iterator[Candidate]]:
+        def matches_gen() -> Iterator[Candidate]:
+            incompat = list(incompatibilities[identifier])
+            if identifier == "python":
+                candidates = find_python_matches(identifier, requirements)
+                return (c for c in candidates if c not in incompat)
+            elif identifier in self.overrides:
+                return iter(self.get_override_candidates(identifier))
+            reqs = sorted(requirements[identifier], key=self.requirement_preference)
+            # iterates over requirements
+            candidates = []
+            for req in reqs:
+                candidates = self._find_candidates(req)
+                candidates = LazySequence(
+                    can for can in candidates if can not in incompat and all(self.is_satisfied_by(r, can) for r in reqs)
+                )
+                if candidates:
+                    break
+            return iter(candidates)
+
+        return matches_gen
+
+
+class CondaReusePinProvider(ReusePinProvider, CondaBaseProvider):
+    pass
+
+
+class CondaEagerUpdateProvider(EagerUpdateProvider, CondaBaseProvider):
+    pass
