9d9a03f66b197c1298938fa0a3aceabe05c35f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:06 +0000

    Add package KTensors/0.0.4/KTensors-0.0.4.tar.gz

---
+++ packages/KTensors/KTensors-0.0.4.tar.gz/KTensors-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import find_packages, setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "KTensors",
+    version = "0.0.4",
+    author = "Hanchao Zhang",
+    author_email = "hz1641@nyu.edu",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "Clustering Positive Semi-Denfinite Matrices",
+    # url = "http://packages.python.org/an_example_pypi_project",
+    packages=find_packages(where="ktensors"),
+    long_description=read('README.md'),
+    # classifiers=[
+    #     "Development Status :: 3 - Alpha",
+    #     "Topic :: Utilities",
+    #     "License :: OSI Approved :: BSD License",
+    # ],
+)
