db11b4d6f8cb60934800397a7fead4d977e65490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:06 +0000

    Add package netbox-cisco-maintenance/0.0.143/netbox-cisco-maintenance-0.0.143.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.143.tar.gz/netbox-cisco-maintenance-0.0.143/netbox_cisco_support_plugin/views.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from django.utils.translation import gettext_lazy
+from netbox.views import generic
+from . import forms, models, tables
+
+
+class CiscoSupportListView(generic.ObjectListView):
+    actions=("export", "bulk_delete")
+    action_perms=defaultdict(set, **{"bulk_delete": {"delete"}})
+    queryset = models.CiscoSupport.objects.all()
+    table = tables.CiscoSupportTable
+
+"""
+class CiscoSupportView(generic.ObjectView):
+    queryset = models.CiscoSupport.objects.all()
+"""
+
+class CiscoSupportEditView(generic.ObjectEditView):
+    queryset = models.CiscoSupport.objects.all()
+    form = forms.CiscoSupportForm
+
+
+class CiscoSupportDeleteView(generic.ObjectDeleteView):
+    queryset = models.CiscoSupport.objects.all()
