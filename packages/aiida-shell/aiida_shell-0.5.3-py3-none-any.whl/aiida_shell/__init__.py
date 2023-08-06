223f1e32872faf08e9aa4a30e7e3c23ef4ee2f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package aiida-shell/0.5.3/aiida_shell-0.5.3-py3-none-any.whl

---
+++ packages/aiida-shell/aiida_shell-0.5.3-py3-none-any.whl/aiida_shell/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""AiiDA plugin that makes running shell commands easy."""
+from .calculations import ShellJob
+from .data import ShellCode
+from .engine import launch_shell_job
+from .parsers import ShellParser
+
+__version__ = '0.5.3'
