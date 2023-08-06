d1d3dbefa31c2e8bb5146ccbe640a378493b2b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:40 +0000

    Add package slippy-api/0.5/slippy_api-0.5.tar.gz

---
+++ packages/slippy-api/slippy_api-0.5.tar.gz/slippy_api-0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='slippy_api',
+    version='0.5',
+    author='Sophia',
+    author_email='sophimander@gmail.com',
+    description='API wrapper for slippi.gg',
+    long_description='Simple wrapper for the GraphQL api on slippi.gg',
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests~=2.29.0',
+        'ratelimiter~=1.2.0.post0',
+        'pytest~=7.3.1'
+    ],
+    url='https://github.com/ConstObject/slippy-api',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
