c3e7280507349e45b795a4105c066095cba60cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package minha-biblioteca-humb/1.0.0/minha_biblioteca_humb-1.0.0-py3-none-any.whl

---
+++ packages/minha-biblioteca-humb/minha_biblioteca_humb-1.0.0-py3-none-any.whl/minha_biblioteca/__init__.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="NomeBiblioteca",
+    version="0.0.1"
+    description="Uma bilioteca python",
+    author="Humberto",
+    packages=find_packages())
+
