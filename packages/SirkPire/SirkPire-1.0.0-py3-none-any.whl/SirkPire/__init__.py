01b9bdc755e52baca44faecc0a974e8687dc94e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:09 +0000

    Add package SirkPire/1.0.0/SirkPire-1.0.0-py3-none-any.whl

---
+++ packages/SirkPire/SirkPire-1.0.0-py3-none-any.whl/SirkPire/__init__.py
@@ -0,0 +1,8 @@
+from .methods import methods as Bot
+from .message import message as Message
+from .tools import tools as Tools
+
+__version__ = '1.0.0'
+__author__ = 'MrTelepathic'
+__github__ = 'https://github.com/MrTelepathic/SirkPire'
+__rubika__ = '@SirkPire'
