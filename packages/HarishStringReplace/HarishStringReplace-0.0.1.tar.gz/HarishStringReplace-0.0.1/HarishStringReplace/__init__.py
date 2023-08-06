2a629c52f9adbc969390f3c1d364e90518385cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:28 +0000

    Add package HarishStringReplace/0.0.1/HarishStringReplace-0.0.1.tar.gz

---
+++ packages/HarishStringReplace/HarishStringReplace-0.0.1.tar.gz/HarishStringReplace-0.0.1/HarishStringReplace/__init__.py
@@ -0,0 +1,4 @@
+def String_replace(wholestring,currentword, replaceWord):
+    txt = wholestring
+    x = txt.replace(currentword,replaceWord)
+    return x
---
+++ packages/HarishStringReplace/HarishStringReplace-0.0.1.tar.gz/HarishStringReplace-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a very simple string replace function which is add whole words,need to change word and give exact word
