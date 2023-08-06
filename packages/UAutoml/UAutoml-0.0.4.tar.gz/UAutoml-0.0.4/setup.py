896f45013cf08395bc4e02ccafd895df41d7026c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:55 +0000

    Add package UAutoml/0.0.4/UAutoml-0.0.4.tar.gz

---
+++ packages/UAutoml/UAutoml-0.0.4.tar.gz/UAutoml-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='UAutoml',
+    version='0.0.4',
+    description='Automated Machine Learning Framework for Data Analysis.',
+    author= 'Ujjwal Reddy K S',
+    url = 'https://github.com/ujjwalredd/Automated-Machine-Learning-Framework-for-Data-Analysis',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['ML', 'Auto Data Analysis', 'models'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['UAutoml'],
+    package_dir={'':'src'},
+    install_requires = [
+        'numpy',
+        'scikit-learn'
+    ]
+)
