623390b262822036138ca74c70cedcea5b6c491f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:57 +0000

    Add package peaqevcore/18.1.5/peaqevcore-18.1.5-py3-none-any.whl

---
+++ packages/peaqevcore/peaqevcore-18.1.5-py3-none-any.whl/peaqevcore/models/hub/chargerswitch.py
@@ -0,0 +1,61 @@
+import logging
+from .hubmember import HubMember
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ChargerSwitch(HubMember):
+    def __init__(
+        self,
+        hass,
+        data_type: type,
+        listenerentity,
+        initval,
+        # currentname: str,
+        hubdata=None,
+        init_override: bool = False,
+    ):
+        self._hubdata = hubdata
+        self._hass = hass
+        self._value = initval
+        self._current = None
+        # self._current_attr_name = currentname
+        super().__init__(
+            data_type=data_type,
+            listenerentity=listenerentity,
+            init_override=init_override,
+            initval=initval,
+            hass=hass,
+        )
+
+    @property
+    def is_initialized(self) -> bool:
+        if (
+            self._hubdata.chargerobject is not None
+            and not self._hubdata.chargerobject.is_initialized
+        ):
+            return False
+        return super().is_initialized
+
+    @property  # todo: remove, not used
+    def current(self) -> int:
+        if self._current is None:
+            return 0
+        return self._current
+
+    @current.setter  # todo: remove, not used
+    def current(self, value):
+        try:
+            self._current = int(value)
+        except ValueError:
+            pass
+
+    HASLOGGED_INITWARN = False
+
+    async def async_log_warning_once(self):
+        if not self.HASLOGGED_INITWARN:
+            _LOGGER.warning(
+                "Chargerobject state was None while getting current. "
+                "Updatecurrent will still process but you may see a missmatch in frontend."
+            )
+            self.HASLOGGED_INITWARN = True
