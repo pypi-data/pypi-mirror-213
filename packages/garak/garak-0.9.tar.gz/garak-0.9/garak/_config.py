69bd0900f0a5df2152924c8864439991c9db0250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:16 +0000

    Add package garak/0.9/garak-0.9.tar.gz

---
+++ packages/garak/garak-0.9.tar.gz/garak-0.9/garak/_config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+args = None
+reportfile = None
+seed = 320
---
+++ packages/garak/garak-0.9.tar.gz/garak-0.9/garak/probes/snowball.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# probe for confident, incorrect answers to complex reasoning/retrieval questions
+
+import json
+
+from garak.probes.base import TextProbe
+
+
+class GraphConnectivity(TextProbe):
+    name = "connectingflights"
+    description = "Probe asking for impossible flight routings"
+    bcp47 = "en"
+    uri = "https://github.com/Nanami18/Snowballed_Hallucination"
+    recommended_detector = ["snowball.DontStartYes"]
+
+    def __init__(self):
+        super().__init__()
+        with open("resources/graph_connectivity.json", "r") as f:
+            self.prompts = json.load(f)
+
+
+class Primes(TextProbe):
+    name = "primes"
+    description = "Ask model to check a list of higher primes"
+    bcp47 = "en"
+    uri = "https://github.com/Nanami18/Snowballed_Hallucination"
+    recommended_detector = ["snowball.DontStartNo"]
+
+    def __init__(self):
+        super().__init__()
+        with open("resources/primality_testing.json", "r") as f:
+            dataset = json.load(f)
+            self.prompts = [entry["question"] for entry in dataset]
+
+
+class Senators(TextProbe):
+    name = "senators"
+    description = "Asking for senators that don't exist"
+    bcp47 = "en"
+    uri = "https://github.com/Nanami18/Snowballed_Hallucination"
+    recommended_detector = ["snowball.DontStartYes"]
+
+    def __init__(self):
+        super().__init__()
+        with open("resources/senator_search.json", "r") as f:
+            self.prompts = json.load(f)
