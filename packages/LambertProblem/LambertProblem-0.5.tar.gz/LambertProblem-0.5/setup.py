897d2aeb51716f840e5664b4943f5d3cb86796d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:35 +0000

    Add package LambertProblem/0.5/LambertProblem-0.5.tar.gz

---
+++ packages/LambertProblem/LambertProblem-0.5.tar.gz/LambertProblem-0.5/setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # ######################################################################
+    # BASIC DESCRIPTION
+    # ######################################################################
+    name='LambertProblem',
+    author='juaniuwu',
+    author_email='juanita.agudelo@udea.edu.co',
+    description='Solve the lambert problem for orbital mechanics',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/LambertProblem',
+    keywords='Lambert equation orbital mechanics solve',
+    license='MIT',
+
+    # ######################################################################
+    # CLASSIFIER
+    # ######################################################################
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    version='0.5',
+
+    # ######################################################################
+    # FILES
+    # ######################################################################
+    package_dir={'': '.'},
+    packages=setuptools.find_packages(where='.'),
+    
+    # ######################################################################
+    # ENTRY POINTS
+    # ######################################################################
+    entry_points={
+        'console_scripts': ['install=pryngles.install:main'],
+    },
+
+    # ######################################################################
+    # TESTS
+    # ######################################################################
+    test_suite='nose.collector',
+    tests_require=['nose'],
+
+    # ######################################################################
+    # DEPENDENCIES
+    # ######################################################################
+    install_requires=['scipy','numpy'],
+
+    # ######################################################################
+    # OPTIONS
+    # ######################################################################
+    include_package_data=True,
+    package_data={'': ['data/*.*', 'tests/*.*']},
+)
