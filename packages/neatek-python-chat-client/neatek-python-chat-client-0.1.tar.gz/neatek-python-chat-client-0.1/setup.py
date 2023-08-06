4b9bcb962dfbd9027d3d84039a70577fefbeeb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:09 +0000

    Add package neatek-python-chat-client/0.1/neatek-python-chat-client-0.1.tar.gz

---
+++ packages/neatek-python-chat-client/neatek-python-chat-client-0.1.tar.gz/neatek-python-chat-client-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neatek-python-chat-client",
+    version="0.1",
+    description="Client packet",
+    packages=find_packages(),
+    author_email="neatek@icloud.com",
+    author="Vladimir Zhelnov",
+    # install_requeres=["PyQt5", "sqlalchemy", "pycruptodome", "pycryptodomex"],
+)
