d984ca7380de391af85a426a7cf942d713046f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package scikit-recommender/0.0.4/scikit_recommender-0.0.4-cp36-cp36m-win_amd64.whl

---
+++ packages/scikit-recommender/scikit_recommender-0.0.4-cp36-cp36m-win_amd64.whl/skrec/recommender/Pop.py
@@ -0,0 +1,45 @@
+__author__ = "Zhongchuan Sun"
+__email__ = "zhongchuansun@gmail.com"
+
+__all__ = ["Pop"]
+
+import numpy as np
+import pandas as pd
+from typing import Dict
+from ..utils.py import Config
+from .base import AbstractRecommender
+from ..io.dataset import CFDataset
+from ..run_config import RunConfig
+
+
+class PopConfig(Config):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+
+class Pop(AbstractRecommender):
+    def __init__(self, run_config: RunConfig, model_config: Dict):
+        self.dataset = CFDataset(run_config.data_dir, run_config.sep, run_config.file_column)
+        self.config = PopConfig(**model_config)
+        super().__init__(run_config, self.config, self.dataset)
+        self.users_num, self.items_num = self.dataset.num_users, self.dataset.num_items
+        self.ranking_score = np.zeros([self.items_num], dtype=np.float32)
+
+    def fit(self):
+        self.logger.info("metrics:".ljust(12) + f"\t{self.evaluator.metrics_str}")
+        items = self.dataset.train_data.to_user_item_pairs()[:, 1]
+        items_count = pd.value_counts(items, sort=False)
+        items = items_count.index.values
+        count = items_count.values
+        self.ranking_score[items] = count
+
+        result = self.evaluate()
+        self.logger.info(f"Pop results:".ljust(12) + f"\t{result.values_str}")
+
+    def evaluate(self):
+        return self.evaluator.evaluate(self)
+
+    def predict(self, users, neg_items=None):
+        ratings = np.tile(self.ranking_score, len(users))
+        ratings = np.reshape(ratings, newshape=[len(users), self.items_num])
+        return ratings
---
+++ packages/scikit-recommender/scikit_recommender-0.0.4-cp36-cp36m-win_amd64.whl/skrec/recommender/base.py
@@ -0,0 +1,58 @@
+__author__ = "Zhongchuan Sun"
+__email__ = "zhongchuansun@gmail.com"
+
+__all__ = ["AbstractRecommender"]
+
+
+import os
+import time
+from typing import Union, List, Dict, Callable
+import numpy as np
+from ..io import Logger, CFDataset
+from ..utils.py import Config, slugify
+from ..utils.py import MetricReport
+from ..run_config import RunConfig
+from ..utils.py import RankingEvaluator
+
+
+class AbstractRecommender(object):
+    def __init__(self, run_config: RunConfig, model_config: Config, dataset: CFDataset):
+        self.evaluator = RankingEvaluator(dataset.train_data.to_user_dict(),
+                                          dataset.test_data.to_user_dict(),
+                                          metric=run_config.metric, top_k=run_config.top_k,
+                                          batch_size=run_config.test_batch_size,
+                                          num_thread=run_config.test_thread)
+        self.logger: Logger = self._create_logger(dataset, model_config)
+
+    def _create_logger(self, dataset: CFDataset, config: Config) -> Logger:
+        timestamp = time.time()
+        model_name = self.__class__.__name__
+        data_name = dataset.data_name
+
+        param_str = f"{data_name}_{model_name}_{config.to_string('_')}"
+        param_str = slugify(param_str, max_length=255 - 100)
+        # run_id: data_name, model_name, hyper-parameters, timestamp
+        run_id = f"{param_str}_{timestamp:.8f}"
+
+        log_dir = os.path.join("log", data_name, self.__class__.__name__)
+        logger_name = os.path.join(log_dir, run_id + ".log")
+        logger = Logger(logger_name)
+
+        # show basic information
+        logger.info(f"PID: {os.getpid()}")
+        logger.info(f"Model: {self.__class__.__module__}")
+
+        logger.info(f"\n{dataset.statistic_info}")
+        cfg_str = config.to_string('\n')
+        logger.info(f"\nHyper-parameters:\n{cfg_str}\n")
+
+        return logger
+
+    def fit(self):
+        raise NotImplementedError
+
+    def evaluate(self) -> MetricReport:
+        raise NotImplementedError
+
+    def predict(self, users: Union[List[int], np.ndarray]) -> np.ndarray:
+        raise NotImplementedError
