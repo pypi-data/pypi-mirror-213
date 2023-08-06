cfc6b114caf05ed1d736743318d9c518381ff305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:37 +0000

    Add package tabNamesCat/0.1.3/tabNamesCat-0.1.3.tar.gz

---
+++ packages/tabNamesCat/tabNamesCat-0.1.3.tar.gz/tabNamesCat-0.1.3/tabNamesCat/__init__.py
@@ -0,0 +1,11 @@
+from .main import load_model, get_mappings_to_category, sentence_clean
+
+
+"""
+Categorization of Tab Names
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Denisalik'
+__credits__ = 'Innopolis University'
