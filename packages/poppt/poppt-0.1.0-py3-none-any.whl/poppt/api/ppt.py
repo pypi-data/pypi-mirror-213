9108519983443af74a74a2bb99c3b4fb5fb670e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:21 +0000

    Add package poppt/0.1.0/poppt-0.1.0-py3-none-any.whl

---
+++ packages/poppt/poppt-0.1.0-py3-none-any.whl/poppt/api/ppt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: ppt.py
+# Mail: 1957875073@qq.com
+# Created Time:  2022-4-25 10:17:34
+# Description: 有关 ppt 的自动化操作
+#############################################
+from poppt.core.PPTType import MainPPT
+
+mainPPT = MainPPT()
+
+
+# todo：输入文件路径
+# @except_dec()
+def ppt2pdf(path: str, output_path='./'):
+    mainPPT.ppt2pdf(path, output_path)
+
+
+# def ppt2img(intput_path, output_path=r'./', img_type='img'):
+#     mainPPT.ppt2img(intput_path, output_path, img_type)
+def ppt2img(input_path, output_path, img_type='jpg', merge=False):
+    """
+    :param intput_path:
+    :param output_path:
+    :param img_type:
+    :return:
+    """
+    mainPPT.ppt2img(input_path, output_path, img_type, merge)
+
+
+def merge4ppt(input_path, output_path=r'./', output_name='merge4ppt.pptx'):
+    mainPPT.merge4ppt(input_path, output_path, output_name)
