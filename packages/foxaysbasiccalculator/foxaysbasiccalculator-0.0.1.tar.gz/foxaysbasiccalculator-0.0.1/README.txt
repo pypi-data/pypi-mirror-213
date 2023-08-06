7a52f5fbada8ec56f79a20525fd63272343c6114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package foxaysbasiccalculator/0.0.1/foxaysbasiccalculator-0.0.1.tar.gz

---
+++ packages/foxaysbasiccalculator/foxaysbasiccalculator-0.0.1.tar.gz/foxaysbasiccalculator-0.0.1/README.txt
@@ -0,0 +1,2 @@
+This is a basic Calculator Library
+You can also see this as a text project
---
+++ packages/foxaysbasiccalculator/foxaysbasiccalculator-0.0.1.tar.gz/foxaysbasiccalculator-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='foxaysbasiccalculator',
+      version='0.0.1',
+      description='operation & greeting library',
+      packages=['foxaysbasiccalculator'],
+      author_email='foxay.tv@gmail.com',
+      zip_safe=False)
