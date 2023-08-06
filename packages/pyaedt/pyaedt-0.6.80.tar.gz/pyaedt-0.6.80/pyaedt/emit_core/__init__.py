ff82a5ca97fe3a12ff1b65bc9d29fa5d0676ab8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:54 +0000

    Add package pyaedt/0.6.80/pyaedt-0.6.80.tar.gz

---
+++ packages/pyaedt/pyaedt-0.6.80.tar.gz/pyaedt-0.6.80/pyaedt/emit_core/__init__.py
@@ -0,0 +1,34 @@
+import imp
+from importlib import import_module
+import os
+import sys
+
+from pyaedt import pyaedt_logger as logger
+
+EMIT_API_PYTHON = None
+
+
+def emit_api_python():
+    """
+    Get the Emit backend API.
+
+    """
+    return EMIT_API_PYTHON
+
+
+# need this as a function so that it can be set
+# for the correct aedt version that the user is running
+def _set_api(aedt_version):
+    numeric_version = int(aedt_version[-3:])
+    desktop_path = os.environ.get(aedt_version)
+    if desktop_path and numeric_version > 231:
+        path = os.path.join(desktop_path, "Delcross")
+        override_path_key = "ANSYS_DELCROSS_PYTHON_PATH"
+        if override_path_key in os.environ:
+            path = os.environ.get(override_path_key)
+        sys.path.insert(0, path)
+        module_path = imp.find_module("EmitApiPython")[1]
+        logger.info("Importing EmitApiPython from: {}".format(module_path))
+        global EMIT_API_PYTHON
+        EMIT_API_PYTHON = import_module("EmitApiPython")
+        logger.info("Loaded {}".format(EMIT_API_PYTHON.EmitApi().get_version(True)))
