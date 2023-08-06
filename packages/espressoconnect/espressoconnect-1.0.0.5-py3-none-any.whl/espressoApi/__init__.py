c8a39874f17b888078261d54805c6c405710b6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:20 +0000

    Add package espressoconnect/1.0.0.5/espressoconnect-1.0.0.5-py3-none-any.whl

---
+++ packages/espressoconnect/espressoconnect-1.0.0.5-py3-none-any.whl/espressoApi/__init__.py
@@ -0,0 +1,5 @@
+from espressoApi.espressoConnect import EspressoConnect
+# from espresso_API.webSocket  import webSocket
+from espressoApi.espressoApiWebsocket import EspressoWebSocket
+
+__all__ = ["EspressoConnect","EspressoWebSocket"]
