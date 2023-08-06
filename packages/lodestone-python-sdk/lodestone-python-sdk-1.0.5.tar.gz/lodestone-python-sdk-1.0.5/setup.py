5b7ff4c76302636ee080e956dee62c7e3612ea19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:27 +0000

    Add package lodestone-python-sdk/1.0.5/lodestone-python-sdk-1.0.5.tar.gz

---
+++ packages/lodestone-python-sdk/lodestone-python-sdk-1.0.5.tar.gz/lodestone-python-sdk-1.0.5/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="lodestone-python-sdk",
+    version="1.0.5",
+    author="bocai",
+    author_email="peijianbo@tuyoogame.com",
+    description="Auth to tuyoo's Lodestone",
+    long_description=README,
+    long_description_content_type="text/markdown",
+
+    install_requires=['requests', 'urllib3<2.0'],
+    packages=find_packages(),
+
+    python_requires='>3.2',
+)
