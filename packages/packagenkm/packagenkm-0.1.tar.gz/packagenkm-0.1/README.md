7c77c4c41c560c3b5b088ac469f3da2b5fc81700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:22 +0000

    Add package packagenkm/0.1/packagenkm-0.1.tar.gz

---
+++ packages/packagenkm/packagenkm-0.1.tar.gz/packagenkm-0.1/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+
+Steps To Create Own Python Package.
+
---
+++ packages/packagenkm/packagenkm-0.1.tar.gz/packagenkm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="packagenkm",
+    version="0.1",
+    description="sampple code",
+    author="nilesh",
+    packages=['packagenkm'],
+    install_requires=[]
+)
