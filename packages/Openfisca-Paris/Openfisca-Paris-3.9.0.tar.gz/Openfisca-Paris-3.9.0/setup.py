331f594a9b9581467534ffe55cbf6aaeb86a063b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package Openfisca-Paris/3.9.0/Openfisca-Paris-3.9.0.tar.gz

---
+++ packages/Openfisca-Paris/Openfisca-Paris-3.9.0.tar.gz/Openfisca-Paris-3.9.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="Openfisca-Paris",
+    version="3.9.0",
+    author="OpenFisca Team",
+    author_email="contact@openfisca.fr",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+    description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
+    keywords="benefit france paris microsimulation social tax",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'OpenFisca-Core >= 35.2.0, < 36',
+        'OpenFisca-France >= 102, < 148',
+    ],
+    extras_require={
+        'test': [
+            'nose',
+            'pytest >= 5.4.2'
+        ]
+    },
+)
