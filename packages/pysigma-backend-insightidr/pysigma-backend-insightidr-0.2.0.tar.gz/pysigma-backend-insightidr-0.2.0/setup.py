093c7402aecb12d35fd294f77f7025ce258ae4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:22 +0000

    Add package pysigma-backend-insightidr/0.2.0/pysigma-backend-insightidr-0.2.0.tar.gz

---
+++ packages/pysigma-backend-insightidr/pysigma-backend-insightidr-0.2.0.tar.gz/pysigma-backend-insightidr-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sigma', 'sigma.backends.insight_idr', 'sigma.pipelines.insight_idr']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pysigma>=0.9.11,<0.10.0']
+
+setup_kwargs = {
+    'name': 'pysigma-backend-insightidr',
+    'version': '0.2.0',
+    'description': 'pySigma Rapid7 InsightIDR backend',
+    'long_description': 'None',
+    'author': 'Micah Babinski',
+    'author_email': 'm.babinski.88@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/SigmaHQ/pySigma-backend-insightidr',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
