8361ed26e4148db73518dab9851c4720eadda804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:36 +0000

    Add package btclib/2023.5.30/btclib-2023.5.30.tar.gz

---
+++ packages/btclib/btclib-2023.5.30.tar.gz/btclib-2023.5.30/btclib/psbt/psbt_out.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+# Copyright (C) The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""Partially Signed Bitcoin Transaction Output (PsbtOut).
+
+Dataclass and functions.
+https://github.com/bitcoin/bips/blob/master/bip-0174.mediawiki
+"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Mapping
+
+from btclib.alias import Octets
+from btclib.bip32 import (
+    BIP32KeyOrigin,
+    HdKeyPaths,
+    assert_valid_hd_key_paths,
+    decode_from_bip32_derivs,
+    decode_hd_key_paths,
+    encode_to_bip32_derivs,
+)
+from btclib.psbt.psbt_utils import (
+    assert_valid_redeem_script,
+    assert_valid_unknown,
+    assert_valid_witness_script,
+    decode_dict_bytes_bytes,
+    deserialize_bytes,
+    encode_dict_bytes_bytes,
+    serialize_bytes,
+    serialize_dict_bytes_bytes,
+    serialize_hd_key_paths,
+)
+from btclib.utils import bytes_from_octets
+
+PSBT_OUT_REDEEM_SCRIPT = b"\x00"
+PSBT_OUT_WITNESS_SCRIPT = b"\x01"
+PSBT_OUT_BIP32_DERIVATION = b"\x02"
+# 0xfc is reserved for proprietary
+# explicit code support for proprietary (and por) is unnecessary
+# see https://github.com/bitcoin/bips/pull/1038
+# PSBT_OUT_PROPRIETARY = b"\xfc"
+
+
+@dataclass
+class PsbtOut:
+    redeem_script: bytes
+    witness_script: bytes
+    hd_key_paths: HdKeyPaths
+    unknown: dict[bytes, bytes]
+
+    def __init__(
+        self,
+        redeem_script: Octets = b"",
+        witness_script: Octets = b"",
+        hd_key_paths: Mapping[Octets, BIP32KeyOrigin] | None = None,
+        unknown: Mapping[Octets, Octets] | None = None,
+        check_validity: bool = True,
+    ) -> None:
+        self.redeem_script = bytes_from_octets(redeem_script)
+        self.witness_script = bytes_from_octets(witness_script)
+        self.hd_key_paths = decode_hd_key_paths(hd_key_paths)
+        self.unknown = dict(sorted(decode_dict_bytes_bytes(unknown).items()))
+
+        if check_validity:
+            self.assert_valid()
+
+    def assert_valid(self) -> None:
+        """Assert logical self-consistency."""
+        assert_valid_redeem_script(self.redeem_script)
+        assert_valid_witness_script(self.witness_script)
+        assert_valid_hd_key_paths(self.hd_key_paths)
+        assert_valid_unknown(self.unknown)
+
+    def to_dict(self, check_validity: bool = True) -> dict[str, Any]:
+        if check_validity:
+            self.assert_valid()
+
+        return {
+            # TODO make it { "asm": "", "hex": "" }
+            "redeem_script": self.redeem_script.hex(),
+            # TODO make it { "asm": "", "hex": "" }
+            "witness_script": self.witness_script.hex(),
+            "bip32_derivs": encode_to_bip32_derivs(self.hd_key_paths),
+            "unknown": dict(sorted(encode_dict_bytes_bytes(self.unknown).items())),
+        }
+
+    @classmethod
+    def from_dict(
+        cls: type[PsbtOut], dict_: Mapping[str, Any], check_validity: bool = True
+    ) -> PsbtOut:
+        return cls(
+            dict_["redeem_script"],
+            dict_["witness_script"],
+            # FIXME
+            decode_from_bip32_derivs(dict_["bip32_derivs"]),  # type: ignore
+            dict_["unknown"],
+            check_validity,
+        )
+
+    def serialize(self, check_validity: bool = True) -> bytes:
+        if check_validity:
+            self.assert_valid()
+
+        psbt_out_bin: list[bytes] = []
+
+        if self.redeem_script:
+            psbt_out_bin.append(
+                serialize_bytes(PSBT_OUT_REDEEM_SCRIPT, self.redeem_script)
+            )
+
+        if self.witness_script:
+            psbt_out_bin.append(
+                serialize_bytes(PSBT_OUT_WITNESS_SCRIPT, self.witness_script)
+            )
+
+        if self.hd_key_paths:
+            psbt_out_bin.append(
+                serialize_hd_key_paths(PSBT_OUT_BIP32_DERIVATION, self.hd_key_paths)
+            )
+
+        if self.unknown:
+            psbt_out_bin.append(serialize_dict_bytes_bytes(b"", self.unknown))
+
+        return b"".join(psbt_out_bin)
+
+    @classmethod
+    def parse(
+        cls: type[PsbtOut],
+        output_map: Mapping[bytes, bytes],
+        check_validity: bool = True,
+    ) -> PsbtOut:
+        """Return a PsbtOut by parsing binary data."""
+        # FIX parse must use BinaryData
+        redeem_script = b""
+        witness_script = b""
+        hd_key_paths: dict[Octets, BIP32KeyOrigin] = {}
+        unknown: dict[Octets, Octets] = {}
+
+        for k, v in output_map.items():
+            if k[:1] == PSBT_OUT_REDEEM_SCRIPT:
+                redeem_script = deserialize_bytes(k, v, "redeem script")
+            elif k[:1] == PSBT_OUT_WITNESS_SCRIPT:
+                witness_script = deserialize_bytes(k, v, "witness script")
+            elif k[:1] == PSBT_OUT_BIP32_DERIVATION:
+                #  parse just one hd key path at time :-(
+                hd_key_paths[k[1:]] = BIP32KeyOrigin.parse(v)
+            else:  # unknown
+                unknown[k] = v
+
+        return cls(
+            redeem_script,
+            witness_script,
+            hd_key_paths,
+            unknown,
+            check_validity,
+        )
---
+++ packages/btclib/btclib-2023.5.30.tar.gz/btclib-2023.5.30/tests/script/test_taproot.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+# Copyright (C) The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""Tests for the `btclib.script.taproot` module."""
+
+import json
+from os import path
+
+import pytest
+
+from btclib import b32
+from btclib.ec import mult
+from btclib.exceptions import BTClibValueError
+from btclib.script import (
+    TaprootScriptTree,
+    Witness,
+    check_output_pubkey,
+    input_script_sig,
+    is_p2tr,
+    output_prvkey,
+    output_pubkey,
+    parse,
+    serialize,
+    type_and_payload,
+)
+from btclib.tx import TxOut
+
+
+def test_valid_script_path() -> None:
+    fname = "tapscript_test_vector.json"
+    filename = path.join(path.dirname(__file__), "_data", fname)
+    with open(filename, encoding="ascii") as file_:
+        data = json.load(file_)
+
+    for x in data:
+        prevouts = [TxOut.parse(prevout) for prevout in x["prevouts"]]
+        index = x["index"]
+
+        if not is_p2tr(prevouts[index].script_pub_key.script):
+            continue
+
+        script_sig = x["success"]["scriptSig"]
+        assert not script_sig
+
+        witness = Witness(x["success"]["witness"])
+        if len(witness.stack) >= 2 and witness.stack[-1][0] == 0x50:
+            witness.stack = witness.stack[:-1]
+
+        # check script paths
+        if len(witness.stack) < 2:
+            continue
+
+        Q = type_and_payload(prevouts[index].script_pub_key.script)[1]
+
+        script = witness.stack[-2]
+        control = witness.stack[-1]
+
+        assert check_output_pubkey(Q, script, control)
+
+
+def test_taproot_key_tweaking() -> None:
+    prv_key = 123456
+    pub_key = mult(prv_key)
+
+    script_trees = [
+        None,
+        [(0xC0, ["OP_1"])],
+        [[(0xC0, ["OP_2"])], [(0xC0, ["OP_3"])]],
+    ]
+
+    for script_tree in script_trees:
+        tweaked_prvkey = output_prvkey(prv_key, script_tree)
+        tweaked_pubkey = output_pubkey(pub_key, script_tree)[0]
+
+        assert tweaked_pubkey == mult(tweaked_prvkey)[0].to_bytes(32, "big")
+
+
+def test_invalid_control_block() -> None:
+    err_msg = "control block too long"
+    with pytest.raises(BTClibValueError, match=err_msg):
+        check_output_pubkey(b"\x00" * 32, b"\x00", b"\x00" * 4130)
+
+    err_msg = "invalid control block length"
+    with pytest.raises(BTClibValueError, match=err_msg):
+        check_output_pubkey(b"\x00" * 32, b"\x00", b"\x00" * 100)
+
+
+def test_unspendable_script() -> None:
+    err_msg = "missing data"
+    with pytest.raises(BTClibValueError, match=err_msg):
+        output_pubkey()
+
+
+def test_control_block() -> None:
+    script_tree = [[(0xC0, ["OP_2"])], [(0xC0, ["OP_3"])]]
+    pub_key = output_pubkey(None, script_tree)[0]
+    script, control = input_script_sig(None, script_tree, 0)
+    assert check_output_pubkey(pub_key, serialize(script), control)
+
+    prv_key = 123456
+    internal_pubkey = mult(prv_key)
+    script_tree = [[(0xC0, ["OP_2"])], [(0xC0, ["OP_3"])]]
+    pub_key = output_pubkey(internal_pubkey, script_tree)[0]
+    script, control = input_script_sig(internal_pubkey, script_tree, 0)
+    assert check_output_pubkey(pub_key, serialize(script), control)
+
+
+def convert_script_tree(script_tree: TaprootScriptTree) -> TaprootScriptTree:
+    if isinstance(script_tree, list):
+        return [convert_script_tree(x) for x in script_tree]
+    if isinstance(script_tree, dict):
+        return [[script_tree["leafVersion"], parse(script_tree["script"])]]
+    return []
+
+
+def test_bip_test_vector() -> None:
+    fname = "taproot_test_vector.json"
+    filename = path.join(path.dirname(__file__), "_data", fname)
+    with open(filename, encoding="ascii") as file_:
+        data = json.load(file_)["scriptPubKey"]
+
+    for test in data:
+        pub_key = test["given"]["internalPubkey"]
+        script_tree = convert_script_tree(test["given"]["scriptTree"])
+
+        tweaked_pubkey = output_pubkey(f"02{pub_key}", script_tree)[0]
+        address = b32.p2tr(tweaked_pubkey)
+
+        assert tweaked_pubkey.hex() == test["intermediary"]["tweakedPubkey"]
+        assert address == test["expected"]["bip350Address"]
