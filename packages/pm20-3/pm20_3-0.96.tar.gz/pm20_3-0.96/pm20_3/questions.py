f3d92a1e1b7df84e6cbb6dc4765e1b52392f1a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:34 +0000

    Add package pm20-3/0.96/pm20_3-0.96.tar.gz

---
+++ packages/pm20-3/pm20_3-0.96.tar.gz/pm20_3-0.96/pm20_3/questions.py
@@ -0,0 +1,73 @@
+from importlib.resources import contents, path
+from PIL import ImageGrab
+from IPython.display import display, Image
+import os
+from pm20_3 import *
+
+
+def theory(qn=0):
+    print()
+    if not qn:
+        with path(
+                q,
+                'task_1.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+        with path(
+                q,
+                'task_2.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+        with path(
+                q,
+                'task_3.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+        with path(
+                q,
+                'task_4.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+        with path(
+                q,
+                'task_5.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+        with path(
+                q,
+                'task_6.png'
+        ) as pt:
+            img = Image(filename=pt)
+            display(img)
+
+    else:
+        qn = str(qn)
+        files = sorted(contents(q))
+        to_disp = []
+        for elem in files:
+            if qn + '_' in elem:
+                to_disp.append(elem)
+        if not to_disp:
+            to_disp.append(qn + '.png')
+        to_disp.sort()
+        for elem in to_disp:
+            with path(
+                    q,
+                    elem
+            ) as pt:
+                img = Image(filename=pt)
+                display(img)
+
+
+def checker(pt):
+    print(os.listdir(pt))
