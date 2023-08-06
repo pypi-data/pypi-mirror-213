620fdcaf35aea3c54b0b94a1cd832280bb5da530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:33 +0000

    Add package ngql-gen/0.0.8/ngql_gen-0.0.8.tar.gz

---
+++ packages/ngql-gen/ngql_gen-0.0.8.tar.gz/ngql_gen-0.0.8/ngql_gen/model/edge_type.py
@@ -0,0 +1,7 @@
+class EdgeType:
+    name: str
+    properties: dict
+
+    def __init__(self, name, properties):
+        self.name = name
+        self.properties = properties
---
+++ packages/ngql-gen/ngql_gen-0.0.8.tar.gz/ngql_gen-0.0.8/ngql_gen/model/tag.py
@@ -0,0 +1,7 @@
+class Tag:
+    name: str
+    properties: dict
+
+    def __init__(self, name, properties):
+        self.name = name
+        self.properties = properties
