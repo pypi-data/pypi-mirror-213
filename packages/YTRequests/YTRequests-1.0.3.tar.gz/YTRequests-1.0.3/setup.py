32d5d33c3b47185e8ba4806a784657b58d67bbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:47 +0000

    Add package YTRequests/1.0.3/YTRequests-1.0.3.tar.gz

---
+++ packages/YTRequests/YTRequests-1.0.3.tar.gz/YTRequests-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YTRequests',
+    author="tsubasa652",
+    author_email="mail@tsubasa.ml",
+    maintaner="tsubasa652",
+    maintainer_email="mail@tsubasa.ml",
+    url="https://github.com/tsubasa652/python-YTRequests",
+    download_url="https://github.com/tsubasa652/python-YTRequests",
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=["requests"],
+    license="MIT",
+    source="https://github.com/tsubasa652/python-YTRequests"
+)
