e227cf1aa8b43ef91dbc33b4bf5529f1779db9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:40 +0000

    Add package econbeautylib/0.0.2/econbeautylib-0.0.2.tar.gz

---
+++ packages/econbeautylib/econbeautylib-0.0.2.tar.gz/econbeautylib-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 18:07:05 2022
+@author:
+Зайцева Дарья
+"""
+
+from setuptools import setup, find_packages
+long_description = '''Library for the 5 semester'''
+setup(name='econbeautylib',
+      version='0.0.2',
+      url='https://github.com/dashkazaitseva',
+      packages=['econbeautylib'],
+      license='MIT',
+      description='',
+      zip_safe=False,
+      package_data={'econbeautylib': ['*.txt', '*.docx']},
+      include_package_data=True
+      )
