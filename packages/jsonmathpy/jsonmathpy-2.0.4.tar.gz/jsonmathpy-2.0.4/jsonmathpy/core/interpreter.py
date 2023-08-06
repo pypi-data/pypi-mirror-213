63b8ebbb2a353aa0a4187f61ec54ce2c58be8a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:38 +0000

    Add package jsonmathpy/2.0.4/jsonmathpy-2.0.4.tar.gz

---
+++ packages/jsonmathpy/jsonmathpy-2.0.4.tar.gz/jsonmathpy-2.0.4/jsonmathpy/core/interpreter.py
@@ -0,0 +1,23 @@
+from cgitb import handler
+from jsonmathpy.core.types import NodeKeys
+from jsonmathpy.interfaces.interpreter import IInterpreter
+
+class Interpreter(IInterpreter):
+
+    def __init__(self, node):
+        self.node = node
+    
+    def interpret(self, mathjson):
+        if isinstance(mathjson, dict):
+            node_type = mathjson[NodeKeys.Node.value]
+            handler   = mathjson[NodeKeys.Handler.value]
+            arguments = mathjson[NodeKeys.Arguments.value]
+            node_methods = dir(self.node)
+            if (node_type in ['object', 'function']) and (handler not in node_methods) and (node_type in node_methods):
+                return getattr(self.node, node_type)(*[self.interpret(arg) for arg in arguments])
+            elif handler in node_methods:
+                return getattr(self.node, handler)(*[self.interpret(arg) for arg in arguments])
+            else:
+                raise Exception(f"Method '{handler}' has not been inplemented by {self.node}. Please implement '{handler}' within {self.node.__class__} and declaire it in NodeConfiguration parameter.")
+        else:
+            return mathjson
