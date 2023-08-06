2f252e2cfecba78931c030b58fb563042c930d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:43 +0000

    Add package django-jalalify/1.1.1/django-jalalify-1.1.1.tar.gz

---
+++ packages/django-jalalify/django-jalalify-1.1.1.tar.gz/django-jalalify-1.1.1/django_jalalify/__init__.py
@@ -0,0 +1,8 @@
+from khayyam import JalaliDate, JalaliDatetime
+
+__author__ = "Mohammad Javad Nikbakht"
+__email__ = "javadnikbakht@mail.com"
+
+__version__ = "1.1.1"
+
+__all__ = ["JalaliDate", "JalaliDatetime"]
