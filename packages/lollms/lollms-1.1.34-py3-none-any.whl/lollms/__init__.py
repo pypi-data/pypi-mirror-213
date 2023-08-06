b91b0d0dd3117998b2cc3d3ffe08cde6c5e8e38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:50 +0000

    Add package lollms/1.1.34/lollms-1.1.34-py3-none-any.whl

---
+++ packages/lollms/lollms-1.1.34-py3-none-any.whl/lollms/__init__.py
@@ -0,0 +1,14 @@
+
+
+__author__ = "ParisNeo"
+__github__ = "https://github.com/ParisNeo/lollms"
+__copyright__ = "Copyright 2023, "
+__license__ = "Apache 2.0"
+
+
+from lollms.binding import LLMBinding
+from lollms.personality import AIPersonality, MSG_TYPE
+#from lollms.binding import  LLMBinding
+
+
+
