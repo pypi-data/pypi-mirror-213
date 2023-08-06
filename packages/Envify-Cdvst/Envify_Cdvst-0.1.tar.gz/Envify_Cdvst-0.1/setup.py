a808ac8607945c89aa5e4d32f9b82652b1f4486c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:47 +0000

    Add package Envify-Cdvst/0.1/Envify_Cdvst-0.1.tar.gz

---
+++ packages/Envify-Cdvst/Envify_Cdvst-0.1.tar.gz/Envify_Cdvst-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools 
+setuptools.setup( 
+    name="Envify_Cdvst", 
+    version="0.1", 
+    author="Philpp Juen", 
+    author_email="support@now4free.de", 
+    description=" Envify", 
+    long_description=open("README_de.md").read(), 
+    long_description_content_type="text/markdown", 
+    url="https://now4free.de/envify_Cdvst", 
+    packages=setuptools.find_packages(), 
+) 
