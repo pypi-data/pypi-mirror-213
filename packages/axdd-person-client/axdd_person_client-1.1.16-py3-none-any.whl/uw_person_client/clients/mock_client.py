02951412dbb48ccfbb4a3657a54f4184de2600ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:19 +0000

    Add package axdd-person-client/1.1.16/axdd_person_client-1.1.16-py3-none-any.whl

---
+++ packages/axdd-person-client/axdd_person_client-1.1.16-py3-none-any.whl/uw_person_client/clients/mock_client.py
@@ -0,0 +1,133 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+import json
+import glob
+import os
+from uw_person_client.clients import AbstractUWPersonClient
+from uw_person_client.components import Person
+from uw_person_client.exceptions import PersonNotFoundException
+
+
+class MockedUWPersonClient(AbstractUWPersonClient):
+    paths = []
+
+    @classmethod
+    def register_mock_path(self, path):
+        if path not in MockedUWPersonClient.paths:
+            MockedUWPersonClient.paths.append(path)
+
+    def get_registered_paths(self):
+        return MockedUWPersonClient.paths
+
+    def _get_mock_paths(self):
+        default_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '../fixtures/'))
+        return self.get_registered_paths() + [default_path]
+
+    def _read_person_file(self, search_value):
+        """Return first matching mocked person record
+        """
+        for path in self._get_mock_paths():
+            files = glob.glob(os.path.join(path, search_value), recursive=True)
+            try:
+                return self._load_person_from_file(files[0])
+            except IndexError:
+                pass
+
+        raise PersonNotFoundException()
+
+    def _read_person_files(self, search_value):
+        """Return all matching mocked person records
+        First matched uwnetid in record wins
+        """
+        persons = {}
+        for path in self._get_mock_paths():
+            for file_name in glob.glob(
+                    os.path.join(path, search_value), recursive=True):
+                person = self._load_person_from_file(file_name)
+                if person.uwnetid not in persons:
+                    persons[person.uwnetid] = person
+
+        return list(persons.values())
+
+    def _load_person_from_file(files, filename):
+        return Person().from_dict(json.load(open(filename)))
+
+    def _paginate(self, values, page=None, page_size=None):
+        if page is not None and page_size is not None:
+            offset = (page - 1) * page_size
+            values = values[offset:offset+page_size]
+        return values
+
+    def get_person_by_uwnetid(self, uwnetid):
+        return self._read_person_file(f'**/*{uwnetid}*.json')
+
+    def get_person_by_uwregid(self, uwregid):
+        return self._read_person_file(f'**/*{uwregid}*.json')
+
+    def get_person_by_student_number(self, student_number):
+        return self._read_person_file(f'**/*{student_number}*.json')
+
+    def get_person_by_system_key(self, system_key, **kwargs):
+        return self._read_person_file(f'**/*{system_key}*.json')
+
+    def get_persons(self, page=None, page_size=None):
+        return self._paginate(
+            self._read_person_files('**/**.json'),
+            page=page,
+            page_size=page_size)
+
+    def get_registered_students(self, page=None, page_size=None):
+        persons = self._read_person_files('**/students/**.json')
+        registered_persons = [person for person in persons if
+                              person.student.enroll_status_code == '12']
+        return self._paginate(
+            registered_persons,
+            page=page,
+            page_size=page_size)
+
+    def get_active_students(self, page=None, page_size=None):
+        return self._paginate(
+            self._read_person_files('**/students/**.json'),
+            page=page,
+            page_size=page_size)
+
+    def get_active_employees(self, page=None, page_size=None):
+        return self._paginate(
+            self._read_person_files('**/employees/**/*.json'),
+            page=page,
+            page_size=page_size)
+
+    def get_advisers(self, advising_program=None):
+        advisers = self._read_person_files('**/employees/advisers/*.json')
+        if advising_program:
+            filtered_advisers = []
+            for person in advisers:
+                if (person.employee.adviser.advising_program ==
+                        advising_program):
+                    filtered_advisers.append(person)
+            return filtered_advisers
+        else:
+            return advisers
+
+    def get_persons_by_adviser_netid(self, uwnetid):
+        students = self.get_active_students()
+        persons = []
+        for person in students:
+            for adviser in person.student.advisers:
+                if adviser.uwnetid == uwnetid:
+                    persons.append(person)
+                    break
+        return persons
+
+    def get_persons_by_adviser_regid(self, uwregid):
+        students = self.get_active_students()
+        persons = []
+        for person in students:
+            for adviser in person.student.advisers:
+                if adviser.uwregid == uwregid:
+                    persons.append(person)
+                    break
+        return persons
---
+++ packages/axdd-person-client/axdd_person_client-1.1.16-py3-none-any.whl/uw_person_client/components.py
@@ -0,0 +1,179 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+import json
+from copy import copy
+
+
+class AbstractBase():
+
+    def __getattr__(self, name):
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            raise AttributeError(f"Attribute '{name}' does not exist for "
+                                 f"'{type(self).__name__}' instance.")
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+    def to_dict(self):
+
+        def format(value):
+            if isinstance(value, AbstractBase):
+                return value.to_dict()
+            elif isinstance(value, list):
+                return [format(item) for item in value]
+            else:
+                return value
+
+        data = copy(self.__dict__)
+        for key, value in data.items():
+            data[key] = format(value)
+        return data
+
+    def to_json(self):
+        return json.dumps(self.to_dict(),
+                          sort_keys=True,
+                          indent=2,
+                          default=str)
+
+    def from_dict(self, data, obj=None):
+        if obj is None:
+            obj = self
+        for key, value in data.items():
+            if isinstance(value, dict):
+                if key == "employee":
+                    new_obj = Employee()
+                    obj.employee = new_obj.from_dict(value, obj=new_obj)
+                elif key == "adviser":
+                    new_obj = Adviser()
+                    obj.adviser = new_obj.from_dict(value, obj=new_obj)
+                elif key == "student":
+                    new_obj = Student()
+                    obj.student = new_obj.from_dict(value, obj=new_obj)
+                elif key == "tran_term":
+                    new_obj = Term()
+                    obj.tran_term = new_obj.from_dict(value, obj=new_obj)
+                elif key == "leave_ends_term":
+                    new_obj = Term()
+                    obj.leave_ends_term = new_obj.from_dict(value, obj=new_obj)
+            elif isinstance(value, list):
+                if key == "majors" or key == "pending_majors":
+                    obj_cls = Major
+                elif key == "advisers":
+                    obj_cls = Adviser
+                elif key == "transcripts":
+                    obj_cls = Transcript
+                elif key == "transfers":
+                    obj_cls = Transfer
+                elif key == "sports":
+                    obj_cls = Sport
+                elif key == "holds":
+                    obj_cls = Hold
+                items = []
+                for list_value in value:
+                    if isinstance(list_value, dict):
+                        new_obj = obj_cls()
+                        items.append(
+                            new_obj.from_dict(list_value, obj=new_obj))
+                    else:
+                        items.append(list_value)
+                obj.__setattr__(key, items)
+            else:
+                obj.__setattr__(key, value)
+        return obj
+
+
+class Person(AbstractBase):
+    pass
+
+
+class Student(AbstractBase):
+    ETHNIC_GROUP_DESCRIPTIONS = {
+        "1": "African American",
+        "2": "American Indian",
+        "3": "White",
+        "4": "Hispanic/Latino",
+        "5": "Asian American",
+        "6": "Hawaiian/Pacific Islander",
+        "7": "Not Indicated",
+        "99": "International",
+    }
+
+
+class Employee(AbstractBase):
+    pass
+
+
+class Transcript(AbstractBase):
+    pass
+
+
+class Transfer(AbstractBase):
+    pass
+
+
+class Hold(AbstractBase):
+    TYPE_DESCRIPTIONS = {
+        1: "REGISTRATION HOLD",
+        2: "TRANSCRIPT HOLD",
+        3: "REGISTRATION AND TRANSCRIPT HOLD"
+    }
+    OFFICE_DESCRIPTIONS = {
+        "EOP": "EDUCATIONAL OPPTNY PROGR"
+    }
+
+
+class Degree(AbstractBase):
+    GRAD_HONOR_DESCRIPTIONS = {
+        1: "SUMMA CUM LAUDE",
+        2: "MAGNA CUM LAUDE",
+        3: "CUM LAUDE"
+    }
+
+
+class Major(AbstractBase):
+    CAMPUS_NAMES = {
+        0: "Seattle",
+        1: "Bothell",
+        2: "Tacoma"
+    }
+    COLLEGE_FULL_NAMES = {
+        "A": "Interdisciplinary Undergraduate Programs",
+        "B": "College of Built Environments",
+        "C": "College of Arts & Sciences",
+        "D": "College of the Environment",
+        "E": "Foster School of Business",
+        "H": "College of Education",
+        "J": "College of Engineering",
+        "J2": "School of Computer Science & Engineering",
+        "K": "College of Ocean & Fishery Sciences",
+        "L": "College of Forest Resources",
+        "M": "School of Public Health",
+        "N": "School of Nursing",
+        "O": "Interschool or Intercollege Programs",
+        "P": "School of Pharmacy",
+        "Q": "Evans School of Public Affairs",
+        "R": "Interdisciplinary Graduate Programs",
+        "S": "The Information School",
+        "T": "School of Social Work",
+        "U": "School of Dentistry",
+        "V": "UW Bothell",
+        "X": "School of Law",
+        "Y": "School of Medicine",
+        "Z": "UW Tacoma"
+    }
+
+
+class Sport(AbstractBase):
+    pass
+
+
+class Adviser(AbstractBase):
+    pass
+
+
+class Term(AbstractBase):
+    TERM_NAMES = {1: "Winter", 2: "Spring", 3: "Summer", 4: "Autumn"}
