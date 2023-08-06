0826c0ef7bee7c0a0f1b7792be1f82a732bf3921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:40 +0000

    Add package Secweb/1.7.2/Secweb-1.7.2-py3-none-any.whl

---
+++ packages/Secweb/Secweb-1.7.2-py3-none-any.whl/Secweb/XPermittedCrossDomainPolicies/XPermittedCrossDomainPoliciesMiddleware.py
@@ -0,0 +1,36 @@
+'''  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+  Copyright 2021-2023, Motagamwala Taha Arif Ali '''
+
+from starlette.datastructures import MutableHeaders
+
+class XPermittedCrossDomainPolicies:
+    ''' XPermittedCrossDomainPolicies class sets X-Permitted-Cross-Domain-Policies header it takes one Parameter
+
+    Example:
+        app.add_middleware(XPermittedCrossDomainPolicies, Option={})
+
+    Parameter:
+
+    Option={} This is a dictionary'''
+    def __init__(self, app, Option = {'X-Permitted-Cross-Domain-Policies': 'none'}):
+        self.app = app
+        self.Option = Option
+        Policies = ['none', 'master-only', 'by-content-type', 'all']
+        if self.Option['X-Permitted-Cross-Domain-Policies'] not in Policies:
+            raise SyntaxError('X-Permitted-Cross-Domain-Policies has four values 1> "none" 2> "master-only" 3> "by-content-type" 4> "all"')
+
+    async def __call__(self, scope, receive, send):
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+
+        async def set_x_Permitted_Cross_Domain_Policies(message):
+            if message["type"] == "http.response.start":
+                headers = MutableHeaders(scope=message)
+                headers.append('X-Permitted-Cross-Domain-Policies', self.Option['X-Permitted-Cross-Domain-Policies'])
+
+            await send(message)
+
+        await self.app(scope, receive, set_x_Permitted_Cross_Domain_Policies)
