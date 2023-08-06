afb35427eba2cefa7e2613bdc743a48cccc66e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:34 +0000

    Add package kr8s/0.5.0/kr8s-0.5.0-py3-none-any.whl

---
+++ packages/kr8s/kr8s-0.5.0-py3-none-any.whl/kr8s/__init__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023, Dask Developers, Yuvi Panda, Anaconda Inc, NVIDIA
+# SPDX-License-Identifier: BSD 3-Clause License
+from functools import partial
+
+from ._api import ALL  # noqa
+from ._api import Api as _AsyncApi
+from ._asyncio import run_sync as _run_sync
+from ._asyncio import sync as _sync  # noqa
+from ._exceptions import NotFoundError  # noqa
+from .asyncio import api as _api  # noqa
+
+__version__ = "0.5.0"
+
+
+@_sync
+class Api(_AsyncApi):
+    __doc__ = _AsyncApi.__doc__
+
+
+api = _run_sync(partial(_api, _asyncio=False))
