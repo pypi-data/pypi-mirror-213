7137b0104bb2ffafffbe5b07e321082a364d696c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:17 +0000

    Add package labeltransform2/1.6/labeltransform2-1.6.tar.gz

---
+++ packages/labeltransform2/labeltransform2-1.6.tar.gz/labeltransform2-1.6/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="labeltransform2",
+    version="1.6",
+    author="victor",
+    description="dataset label style transform tool",
+    url="https://gitee.com/free_bigD/labeltransform.git",
+    packages=setuptools.find_packages(),
+    install_requires=["opencv-python", "pandas"],
+    python_requires='>=3'
+)
