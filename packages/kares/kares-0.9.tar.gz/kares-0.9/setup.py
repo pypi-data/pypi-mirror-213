bc3b24e703b56a833279507f011522d41d1a21f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:33 +0000

    Add package kares/0.9/kares-0.9.tar.gz

---
+++ packages/kares/kares-0.9.tar.gz/kares-0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='kares',
+    version='0.9',
+    description='A Python module for kares',
+    packages=['kares'],
+    author='NoBody',
+    author_email='your@nobody.com',
+    url='https://nobody.com/',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+    ],
+    include=[
+        "pracs/*.txt",
+    ],
+)
