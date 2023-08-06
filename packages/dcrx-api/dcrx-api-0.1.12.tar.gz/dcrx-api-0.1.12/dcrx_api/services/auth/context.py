64740868f7ff5d9b3ea1765b7bcfc59cb009a4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package dcrx-api/0.1.12/dcrx-api-0.1.12.tar.gz

---
+++ packages/dcrx-api/dcrx-api-0.1.12.tar.gz/dcrx-api-0.1.12/dcrx_api/services/auth/context.py
@@ -0,0 +1,20 @@
+from dcrx_api.context.types import ContextType
+from dcrx_api.env import Env
+from pydantic import BaseModel
+from .manager import AuthorizationSessionManager
+
+
+
+class AuthServiceContext(BaseModel):
+    env: Env
+    manager: AuthorizationSessionManager
+    context_type: ContextType=ContextType.AUTH_SERVICE
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    async def initialize(self):
+        await self.manager.connect()
+
+    async def close(self):
+        await self.manager.close()
