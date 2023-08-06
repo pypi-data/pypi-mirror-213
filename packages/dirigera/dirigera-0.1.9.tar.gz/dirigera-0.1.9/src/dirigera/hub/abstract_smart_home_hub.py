409a2d441dda5484ce92044b6bb5e90615b50cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:54 +0000

    Add package dirigera/0.1.9/dirigera-0.1.9.tar.gz

---
+++ packages/dirigera/dirigera-0.1.9.tar.gz/dirigera-0.1.9/src/dirigera/hub/abstract_smart_home_hub.py
@@ -0,0 +1,26 @@
+import abc
+from typing import Any, Dict, List
+
+
+class AbstractSmartHomeHub(abc.ABC):
+    @abc.abstractmethod
+    def patch(self, route: str, data: Dict[str, Any]) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, route: str) -> Dict[str, Any]:
+        raise NotImplementedError
+
+
+class FakeDirigeraHub(AbstractSmartHomeHub):
+    def __init__(self) -> None:
+        self.patch_actions: List = []
+        self.get_actions: List = []
+        self.get_action_replys: Dict = {}
+
+    def patch(self, route: str, data: Dict[str, Any]) -> Dict[str, Any]:
+        self.patch_actions.append({"route": route, "data": data})
+
+    def get(self, route: str) -> Dict[str, Any]:
+        self.get_actions.append({"route": route})
+        return self.get_action_replys[route]
