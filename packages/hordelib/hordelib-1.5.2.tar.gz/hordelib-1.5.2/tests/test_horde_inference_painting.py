2f86e4c5c848e4f4e2cb66611411c52b158f0f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:14 +0000

    Add package hordelib/1.5.2/hordelib-1.5.2.tar.gz

---
+++ packages/hordelib/hordelib-1.5.2.tar.gz/hordelib-1.5.2/tests/test_horde_inference_painting.py
@@ -0,0 +1,151 @@
+import os
+
+import pytest
+from PIL import Image
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+from hordelib.utils.distance import are_images_identical
+
+
+class TestHordeInference:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        self.horde = HordeLib()
+
+        self.default_model_manager_args = {
+            "compvis": True,
+        }
+        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate Inpainting")
+        assert (
+            SharedModelManager.manager.compvis.is_model_loaded(
+                "Deliberate Inpainting",
+            )
+            is True
+        )
+        TestHordeInference.distance_threshold = int(os.getenv("IMAGE_DISTANCE_THRESHOLD", "100000"))
+        yield
+        del self.horde
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_inpainting_alpha_mask(self):
+        data = {
+            "sampler_name": "euler",
+            "cfg_scale": 8,
+            "denoising_strength": 1,
+            "seed": 836138046008,
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dinosaur",
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate Inpainting",
+            "source_image": Image.open("images/test_inpaint_alpha.png"),
+            "source_processing": "inpainting",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        img_filename = "inpainting_mask_alpha.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+    def test_inpainting_separate_mask(self):
+        data = {
+            "sampler_name": "euler",
+            "cfg_scale": 8,
+            "denoising_strength": 1,
+            "seed": 836138046008,
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dinosaur",
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate Inpainting",
+            "source_image": Image.open("images/test_inpaint_original.png"),
+            "source_mask": Image.open("images/test_inpaint_mask.png"),
+            "source_processing": "inpainting",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        img_filename = "inpainting_mask_separate.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+    def test_inpainting_alpha_mask_mountains(self):
+        data = {
+            "sampler_name": "k_dpmpp_2m",
+            "cfg_scale": 7.5,
+            "denoising_strength": 0.72,
+            "seed": 1312,
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a river through the mountains",
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate Inpainting",
+            "source_image": Image.open("images/test_inpaint.png"),
+            "source_processing": "inpainting",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        assert pil_image.size == (512, 512)
+        img_filename = "inpainting_mountains.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+    def test_outpainting_alpha_mask_mountains(self):
+        data = {
+            "sampler_name": "euler",
+            "cfg_scale": 8.0,
+            "denoising_strength": 1.0,
+            "seed": 836913938046008,
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a river through the mountains, blue sky with clouds.",
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "Deliberate Inpainting",
+            "source_image": Image.open("images/test_outpaint.png"),
+            "source_processing": "outpainting",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        assert pil_image.size == (512, 512)
+        img_filename = "outpainting_mountains.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
---
+++ packages/hordelib/hordelib-1.5.2.tar.gz/hordelib-1.5.2/tests/test_inference.py
@@ -0,0 +1,129 @@
+# test_inference.py
+import os
+
+import pytest
+from PIL import Image
+
+from hordelib.comfy_horde import Comfy_Horde
+from hordelib.shared_model_manager import SharedModelManager
+from hordelib.utils.distance import are_images_identical
+
+
+class TestInference:
+    comfy: Comfy_Horde
+
+    @pytest.fixture(autouse=True, scope="class")
+    def setup_and_teardown(self):
+        TestInference.comfy = Comfy_Horde()
+        SharedModelManager.loadModelManagers(compvis=True)
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate")
+        TestInference.distance_threshold = int(os.getenv("IMAGE_DISTANCE_THRESHOLD", "100000"))
+        yield
+        del TestInference.comfy
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_unknown_pipeline(self):
+        result = self.comfy.run_image_pipeline("non-existent-pipeline", {})
+        assert result is None
+
+    def test_stable_diffusion_pipeline(self):
+        params = {
+            "sampler.sampler_name": "dpmpp_2m",
+            "sampler.cfg": 7.5,
+            "sampler.denoise": 1.0,
+            "sampler.seed": 12345,
+            "empty_latent_image.width": 512,
+            "empty_latent_image.height": 512,
+            "empty_latent_image.batch_size": 1,
+            "sampler.scheduler": "karras",
+            "sampler.steps": 25,
+            "prompt.text": "a closeup photo of a confused dog",
+            "negative_prompt.text": "cat, black and white, deformed",
+            "model_loader.model_name": "Deliberate",
+            "clip_skip.stop_at_clip_layer": -1,
+            "model_loader.model_manager": SharedModelManager,
+        }
+        images = self.comfy.run_image_pipeline("stable_diffusion", params)
+        assert images is not None
+
+        pil_image = Image.open(images[0]["imagedata"])
+        img_filename = "pipeline_stable_diffusion.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+    def test_stable_diffusion_pipeline_clip_skip(self):
+        params = {
+            "sampler.sampler_name": "dpmpp_2m",
+            "sampler.cfg": 7.5,
+            "sampler.denoise": 1.0,
+            "sampler.seed": 12345,
+            "empty_latent_image.width": 512,
+            "empty_latent_image.height": 512,
+            "empty_latent_image.batch_size": 1,
+            "sampler.scheduler": "karras",
+            "sampler.steps": 25,
+            "prompt.text": "a closeup photo of a confused dog",
+            "negative_prompt.text": "cat, black and white, deformed",
+            "model_loader.model_name": "Deliberate",
+            "clip_skip.stop_at_clip_layer": -2,
+            "model_loader.model_manager": SharedModelManager,
+        }
+        images = self.comfy.run_image_pipeline("stable_diffusion", params)
+        assert images is not None
+
+        pil_image = Image.open(images[0]["imagedata"])
+        img_filename = "pipeline_stable_diffusion_clip_skip_2.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+    def test_stable_diffusion_hires_fix_pipeline(self):
+        params = {
+            "sampler.seed": 1014,
+            "sampler.cfg": 7.5,
+            "sampler.scheduler": "normal",
+            "sampler.sampler_name": "euler",
+            "sampler.denoise": 1.0,
+            "sampler.steps": 30,
+            "prompt.text": (
+                "photograph of medusa, snakes in hair, reverence princesss, cinematic, realistic, "
+                "hyperrealistic, very realistic, extremely detailed, detailed, sharp focus, "
+                "establishing shot, 8k resolution, dramatic lighting, award winning "
+                "photograph, masterpiece, very detailed, sharp focus, very realistic lighting"
+            ),
+            "negative_prompt.text": (
+                "render, cg, drawing, painting, artist, graphics, deformed, black and white, deformed eyes"
+            ),
+            "model_loader.model_name": "Deliberate",
+            "model_loader.model_manager": SharedModelManager,
+            "empty_latent_image.width": 512,
+            "empty_latent_image.height": 512,
+            "latent_upscale.width": 768,
+            "latent_upscale.height": 768,
+            "latent_upscale.crop": "disabled",
+            "latent_upscale.upscale_method": "nearest-exact",
+            "upscale_sampler.seed": 450089106307460,
+            "upscale_sampler.steps": 30,
+            "upscale_sampler.cfg": 8.0,
+            "upscale_sampler.sampler_name": "euler",
+            "upscale_sampler.scheduler": "simple",
+            "upscale_sampler.denoise": 0.65,
+            "clip_skip.stop_at_clip_layer": -1,
+        }
+        images = self.comfy.run_image_pipeline("stable_diffusion_hires_fix", params)
+        assert images is not None
+
+        pil_image = Image.open(images[0]["imagedata"])
+        img_filename = "pipeline_stable_diffusion_hires_fix.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+        params["clip_skip.stop_at_clip_layer"] = -2
+        images = self.comfy.run_image_pipeline("stable_diffusion_hires_fix", params)
+        assert images is not None
+
+        pil_image = Image.open(images[0]["imagedata"])
+        img_filename = "pipeline_stable_diffusion_hires_fix_clip_skip_2.png"
+        pil_image.save(f"images/{img_filename}", quality=100)
+        assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
