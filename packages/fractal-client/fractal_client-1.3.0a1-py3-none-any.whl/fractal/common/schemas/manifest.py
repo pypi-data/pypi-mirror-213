47f533dcb69f73115079ea0e47ae02c34b6df361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:48 +0000

    Add package fractal-client/1.3.0a1/fractal_client-1.3.0a1-py3-none-any.whl

---
+++ packages/fractal-client/fractal_client-1.3.0a1-py3-none-any.whl/fractal/common/schemas/manifest.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Any
+from typing import Optional
+from typing import TypeVar
+
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import validator
+
+
+__all__ = ("TaskManifestV1", "ManifestV1")
+
+
+class _TaskManifestBase(BaseModel):
+    """
+    Base class for TaskManifest
+
+    Represents a task within a manfest
+
+    Attributes:
+        name:
+            The task name
+        executable:
+            Path to the executable relative to the package root
+
+            Note: by package root we mean "as it will be installed". If a
+            package `Pkg` installs in the folder `pkg` the executable
+            `pkg/executable.py`, this attribute must contain only
+            `executable.py`.
+        input_type:
+            The input type accepted by the task
+        output_type:
+            The output type returned by the task
+        default_args:
+            An arbitrary, JSON-serializable dictionary containing the default
+            parameters that will be passed to the task
+        meta:
+            Additional information about the package, such as hash of the
+            executable, specific runtime requirements (e.g., need_gpu=True),
+            etc.
+    """
+
+    name: str
+    executable: Path
+    input_type: str
+    output_type: str
+    default_args: Optional[dict[str, Any]] = Field(default_factory=dict)
+    meta: Optional[dict[str, Any]] = Field(default_factory=dict)
+
+
+TaskManifestType = TypeVar("TaskManifestType", bound=_TaskManifestBase)
+
+
+class _ManifestBase(BaseModel):
+    """
+    Manifest base class
+
+    Packages containing tasks are required to include a special file
+    `__FRACTAL_MANIFEST__.json` in order to be discovered and used by Fractal.
+
+    This model class and the model classes it depends on provide the base
+    schema to read, write and validate manifests.
+
+    Attributes:
+        manifest_version:
+            A version string that provides indication for compatibility between
+            manifests as the schema evolves. This is for instance used by
+            Fractal to determine which subclass of the present base class needs
+            be used to read and validate the input.
+        task_list : list[TaskManifestType]
+            The list of tasks, represented as specified by subclasses of the
+            _TaskManifestBase (a.k.a. TaskManifestType)
+    """
+
+    manifest_version: str
+    task_list: list[TaskManifestType]  # type: ignore
+
+
+class TaskManifestV1(_TaskManifestBase):
+    pass
+
+
+class ManifestV1(_ManifestBase):
+    """
+    Manifest schema version 1
+    """
+
+    task_list: list[TaskManifestV1]
+
+    @validator("manifest_version")
+    def manifest_version_1(cls, value):
+        if value != "1":
+            raise ValueError("Wrong manifest version")
---
+++ packages/fractal-client/fractal_client-1.3.0a1-py3-none-any.whl/fractal/common/schemas/task.py
@@ -0,0 +1,107 @@
+from typing import Any
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import validator
+
+from ._validators import valstr
+
+__all__ = (
+    "TaskCreate",
+    "TaskUpdate",
+    "TaskRead",
+    "TaskImport",
+    "TaskExport",
+)
+
+
+class _TaskBase(BaseModel):
+    """# TODO fix me
+    Task base class
+
+    A Task is the elemental unit of a workflow, and must be a self-standing
+    executable.
+
+    Attributes
+        name:
+            A human readable name for the task
+        command:
+            The command(s) that executes the task
+        source:
+            Path or url to task source. This is the information is used to
+            match tasks across fractal installations when a workflow is
+            imported.
+        input_type:
+            The type of data the task expects as input
+        output_type:
+            The type of data the task expects as output
+        default_args: Optional[dict[str, Any]]
+            dictionary (saved as JSON) of the default parameters of the task
+    """
+
+    source: str
+    _source = validator("source", allow_reuse=True)(valstr("source"))
+
+
+class TaskUpdate(_TaskBase):
+    name: Optional[str]
+    input_type: Optional[str]
+    output_type: Optional[str]
+    command: Optional[str]
+    source: Optional[str]
+    default_args: Optional[dict[str, Any]]
+    meta: Optional[dict[str, Any]]
+    version: Optional[str]
+
+    # Validators
+    _name = validator("name", allow_reuse=True)(valstr("name"))
+    _input_type = validator("input_type", allow_reuse=True)(
+        valstr("input_type")
+    )
+    _output_type = validator("output_type", allow_reuse=True)(
+        valstr("output_type")
+    )
+    _command = validator("command", allow_reuse=True)(valstr("command"))
+    _version = validator("version", allow_reuse=True)(valstr("version"))
+
+
+class TaskImport(_TaskBase):
+    pass
+
+
+class TaskExport(_TaskBase):
+    pass
+
+
+class TaskRead(_TaskBase):
+    id: int
+    name: str
+    command: str
+    input_type: str
+    output_type: str
+    default_args: Optional[dict[str, Any]] = Field(default={})
+    meta: Optional[dict[str, Any]] = Field(default={})
+    owner: Optional[str]
+    version: Optional[str]
+
+
+class TaskCreate(_TaskBase):
+    name: str
+    command: str
+    input_type: str
+    output_type: str
+    default_args: Optional[dict[str, Any]] = Field(default={})
+    meta: Optional[dict[str, Any]] = Field(default={})
+    version: Optional[str]
+
+    # Validators
+    _name = validator("name", allow_reuse=True)(valstr("name"))
+    _input_type = validator("input_type", allow_reuse=True)(
+        valstr("input_type")
+    )
+    _output_type = validator("output_type", allow_reuse=True)(
+        valstr("output_type")
+    )
+    _command = validator("command", allow_reuse=True)(valstr("command"))
+    _version = validator("version", allow_reuse=True)(valstr("version"))
