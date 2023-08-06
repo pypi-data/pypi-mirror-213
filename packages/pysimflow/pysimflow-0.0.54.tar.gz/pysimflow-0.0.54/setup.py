ecc0afb0fdb6142039a5e30bf355dc492892ecdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:46 +0000

    Add package pysimflow/0.0.54/pysimflow-0.0.54.tar.gz

---
+++ packages/pysimflow/pysimflow-0.0.54.tar.gz/pysimflow-0.0.54/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pysimflow',
+    version='0.0.54',
+    author='sl.truman',
+    author_email='sl.truman@live.com',
+    url='',
+    description=u'',
+    packages=['digitaltwin','digitaltwin/end_effector','py3dbp','digitaltwin_data'],
+    install_requires=[
+        'numpy',
+        'pybullet',
+        'scipy'
+    ],
+    include_package_data=True
+)
