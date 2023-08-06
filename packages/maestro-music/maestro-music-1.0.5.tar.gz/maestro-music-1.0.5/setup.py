3ad4a6abd8a68f42204cc047386b9850a53684f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:23 +0000

    Add package maestro-music/1.0.5/maestro-music-1.0.5.tar.gz

---
+++ packages/maestro-music/maestro-music-1.0.5.tar.gz/maestro-music-1.0.5/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup
+
+MAC_DEPS = [
+    "pyobjc-core; sys_platform == 'darwin'",
+    "pyobjc-framework-ApplicationServices; sys_platform == 'darwin'",
+    "pyobjc-framework-AVFoundation; sys_platform == 'darwin'",
+    "pyobjc-framework-Cocoa; sys_platform == 'darwin'",
+    "pyobjc-framework-CoreAudio; sys_platform == 'darwin'",
+    "pyobjc-framework-CoreMedia; sys_platform == 'darwin'",
+    "pyobjc-framework-MediaPlayer; sys_platform == 'darwin'",
+    "pyobjc-framework-Quartz; sys_platform == 'darwin'",
+]
+
+setup(
+    name="maestro-music",
+    version="1.0.5",
+    author="Prajwal Vandana",
+    url="https://github.com/PrajwalVandana/maestro-cli",
+    description="A simple command line tool to play songs (or any audio files, really).",
+    long_description=open("readme.md", encoding="utf-8").read(),
+    license="MIT",
+    license_files=["LICENSE"],
+    long_description_content_type="text/markdown",
+    keywords=[
+        "music",
+        "sound",
+        "audio",
+        "music-player",
+        "cli",
+        "ogg",
+        "flac",
+        "mp3",
+        "wav",
+        "spotify",
+        "youtube",
+        "audio-visualization",
+        "audio-visualizer",
+    ],
+    py_modules=["maestro", "mac_presence", "icon", "helpers"],
+    install_requires=[
+        "click",
+        "just_playback",
+        "music-tag",
+        "pypresence",
+        "yt-dlp",
+        "spotdl",
+        "ytmusicapi",
+        "librosa",
+        "numba",
+        "numpy",
+        "windows-curses; sys_platform == 'win32'",
+    ]
+    + MAC_DEPS,
+    entry_points={
+        "console_scripts": [
+            "maestro = maestro:cli",
+        ],
+    },
+)
