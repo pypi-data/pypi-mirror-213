629d41d42c853b39e87efbac3250f35b52596de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package scikit-recommender/0.0.4/scikit_recommender-0.0.4-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/scikit-recommender/scikit_recommender-0.0.4-cp38-cp38-macosx_10_15_x86_64.whl/skrec/__init__.py
@@ -0,0 +1,12 @@
+__author__ = "Zhongchuan Sun"
+__email__ = "zhongchuansun@gmail.com"
+
+# __all__ = []
+
+from .io import *
+from .utils.py import *
+from .utils import *
+from .run_config import RunConfig
+
+import colorama
+colorama.init()
