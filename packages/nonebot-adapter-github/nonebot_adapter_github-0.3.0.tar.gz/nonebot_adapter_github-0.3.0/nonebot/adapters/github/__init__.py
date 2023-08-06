5adfee5787130fe2338643bdaee3a33c550fd407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package nonebot-adapter-github/0.3.0/nonebot_adapter_github-0.3.0.tar.gz

---
+++ packages/nonebot-adapter-github/nonebot_adapter_github-0.3.0.tar.gz/nonebot_adapter_github-0.3.0/nonebot/adapters/github/__init__.py
@@ -0,0 +1,12 @@
+from .event import *
+from .bot import Bot as Bot
+from .bot import OAuthBot as OAuthBot
+from .adapter import Adapter as Adapter
+from .bot import GitHubBot as GitHubBot
+from .message import Message as Message
+from .exception import ActionFailed as ActionFailed
+from .exception import GraphQLError as GraphQLError
+from .exception import NetworkError as NetworkError
+from .exception import ActionTimeout as ActionTimeout
+from .message import MessageSegment as MessageSegment
+from .exception import GitHubAdapterException as GitHubAdapterException
