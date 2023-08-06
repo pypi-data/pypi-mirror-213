10b90567a2fccebbab6ffaf86dde953dd5940c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:04 +0000

    Add package pyansa/1.0.0/pyansa-1.0.0.tar.gz

---
+++ packages/pyansa/pyansa-1.0.0.tar.gz/pyansa-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='pyansa',
+    version      ='1.0.0',
+    py_modules   =['pyansa'],
+    author       ='xinxing.zhang',
+    author_email ='zxx4477@126.com',
+    description  ='Tools for ANSA CAE',
+    )
