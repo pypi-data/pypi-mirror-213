05fe10675682d3f3fa91cc484366490751a97542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:28 +0000

    Add package anycluster/2.3.1/anycluster-2.3.1-py3-none-any.whl

---
+++ packages/anycluster/anycluster-2.3.1-py3-none-any.whl/anycluster/api/serializers.py
@@ -0,0 +1,112 @@
+from django.conf import settings
+from rest_framework import serializers
+
+import jsonschema
+
+from .json_schemas import FEATURE_OR_FEATURECOLLECTION_SCHEMA
+
+from anycluster.definitions import GEOMETRY_TYPES, GEOMETRY_TYPE_VIEWPORT
+from anycluster.MapClusterer import Gis
+
+
+def filters_are_allowed(filters):
+
+    for filter in filters:
+
+        is_nested = 'filters' in filter
+
+        if is_nested == False:
+            column = filter.get('column', None)
+            if not column:
+                raise serializers.ValidationError('Filter require a column')
+        
+            if column not in settings.ANYCLUSTER_FILTERS:
+                raise serializers.ValidationError('It is not allowed to filter Column {0}'.format(column))
+        
+        else:
+            filters_are_allowed(filter['filters'])
+
+# needs to supprt FeatureCollection and Multipolygon
+class ClusterRequestSerializer(serializers.Serializer):
+
+    output_srid = serializers.CharField(default='EPSG:4326', required=False, write_only=True)
+
+    geometry_type = serializers.ChoiceField(choices=GEOMETRY_TYPES, write_only=True)
+
+    geojson = serializers.JSONField(write_only=True)
+    filters = serializers.ListField(required=False, default=[], write_only=True)
+
+    clear_cache = serializers.BooleanField(default=False, required=False, write_only=True)
+
+    # viewport can be one or two polygons
+    def validate(self, data):
+        if data['geometry_type'] == GEOMETRY_TYPE_VIEWPORT:
+
+            geojson = data['geojson']
+            coordinates = geojson['geometry']['coordinates'][0]
+            if not len(coordinates) == 5:
+                raise serializers.ValidationError('Viewport must consist of 5 points')
+
+        return data
+
+
+    def validate_geojson(self, value):
+        try:
+            is_valid = jsonschema.validate(value, FEATURE_OR_FEATURECOLLECTION_SCHEMA)
+        except jsonschema.exceptions.ValidationError as e:
+            raise serializers.ValidationError(e.message)
+        return value
+
+    def validate_filters(self, value):
+        filters_are_allowed(value)
+        return value
+
+
+
+class AreaContentRequestSerializer(ClusterRequestSerializer):
+
+    limit = serializers.IntegerField(required=False, write_only=True)
+    offset = serializers.IntegerField(required=False, write_only=True)
+
+
+class MapContentCountSerializer(ClusterRequestSerializer):
+    # additional filters, which are applied on top of filters, only for the count query
+    modulations = serializers.JSONField(required=False, default={}, write_only=True)
+
+    def validate_modulations(self, value):
+
+        if value:
+
+            for modulation_name, modulation in value.items():
+                filters_are_allowed(modulation['filters'])
+
+        return value
+
+
+class GroupedMapContentSerializer(ClusterRequestSerializer):
+    group_by = serializers.CharField(write_only=True)
+    
+
+class ClusterContentRequestSerializer(serializers.Serializer):
+
+    output_srid = serializers.CharField(default='EPSG:4326', required=False, write_only=True)
+    input_srid = serializers.CharField(default='EPSG:4326', required=False, write_only=True)
+
+    geometry_type = serializers.ChoiceField(choices=GEOMETRY_TYPES, write_only=True)
+
+    ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    x = serializers.FloatField(write_only=True)
+    y = serializers.FloatField(write_only=True)
+    filters = serializers.ListField(required=False, default=[], write_only=True)
+
+    def validate_filters(self, value):
+        filters_are_allowed(value)
+        return value
+
+
+
+class GisModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Gis
+        fields = '__all__'
