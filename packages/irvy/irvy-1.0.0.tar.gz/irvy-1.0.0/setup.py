448d9e3902062adfe4ff7372dc2eba3a0ea6a3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package irvy/1.0.0/irvy-1.0.0.tar.gz

---
+++ packages/irvy/irvy-1.0.0.tar.gz/irvy-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Open the README file.
+with open(file="README.md", mode="r") as readme_handle:
+    long_description = readme_handle.read()
+
+setup(
+    name='irvy',
+    version='1.0.0',
+    description='A brief description of your project',  # add this
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # this should automatically find your packages
+    include_package_data=True,
+    install_requires=[
+        # list of dependencies
+    ],
+    entry_points={
+        'console_scripts': [
+            'irvy=irvy.irvy:main',
+        ],
+    },
+)
