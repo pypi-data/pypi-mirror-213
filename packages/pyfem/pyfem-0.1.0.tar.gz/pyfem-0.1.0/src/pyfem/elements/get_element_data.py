42ebe57d8eed98534c25b2e52a3405325d44afbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:49 +0000

    Add package pyfem/0.1.0/pyfem-0.1.0.tar.gz

---
+++ packages/pyfem/pyfem-0.1.0.tar.gz/pyfem-0.1.0/src/pyfem/elements/get_element_data.py
@@ -0,0 +1,50 @@
+from numpy import ndarray
+
+from pyfem.elements.BaseElement import BaseElement
+from pyfem.elements.IsoElementShape import IsoElementShape
+from pyfem.elements.SolidPlaneSmallStrain import SolidPlaneSmallStrain
+from pyfem.elements.SolidVolumeSmallStrain import SolidVolumeSmallStrain
+from pyfem.fem.Timer import Timer
+from pyfem.io.Dof import Dof
+from pyfem.io.Material import Material
+from pyfem.io.Section import Section
+from pyfem.materials.BaseMaterial import BaseMaterial
+from pyfem.utils.colors import error_style
+
+element_data_dict = {
+    'SolidPlaneStressSmallStrain': SolidPlaneSmallStrain,
+    'SolidPlaneStrainSmallStrain': SolidPlaneSmallStrain,
+    'SolidVolumeSmallStrain': SolidVolumeSmallStrain
+}
+
+
+# @trace_calls
+def get_element_data(element_id: int,
+                     iso_element_shape: IsoElementShape,
+                     connectivity: ndarray,
+                     node_coords: ndarray,
+                     dof: Dof,
+                     material: Material,
+                     section: Section,
+                     material_data: BaseMaterial,
+                     timer: Timer) -> BaseElement:
+    class_name = f'{section.category}{section.type}{section.option}'.strip().replace(' ', '')
+
+    if class_name in element_data_dict:
+        return element_data_dict[class_name](element_id=element_id,
+                                             iso_element_shape=iso_element_shape,
+                                             connectivity=connectivity,
+                                             node_coords=node_coords,
+                                             dof=dof,
+                                             material=material,
+                                             section=section,
+                                             material_data=material_data,
+                                             timer=timer)
+    else:
+        error_msg = f'{class_name} element is not supported.\n'
+        error_msg += f'The allowed element types are {list(element_data_dict.keys())}.'
+        raise NotImplementedError(error_style(error_msg))
+
+
+if __name__ == "__main__":
+    pass
