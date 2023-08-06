3e391dc667523a65f4d70d2e9ad5ce14bd0ed58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:34 +0000

    Add package django-k8s-health-check/1.2.0/django_k8s_health_check-1.2.0-py3-none-any.whl

---
+++ packages/django-k8s-health-check/django_k8s_health_check-1.2.0-py3-none-any.whl/django_k8s_health_check/views.py
@@ -0,0 +1,36 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
+
+from django_k8s_health_check.settings import api_settings
+from django_k8s_health_check.utils import HealthCheck
+
+
+class HealthView(GenericAPIView):
+    permission_classes = api_settings.PERMISSION_CLASSES
+    authentication_classes = api_settings.AUTHENTICATION_CLASSES
+    serializer_class = api_settings.SERIALIZER_CLASS
+    action = 'get'
+
+    def get(self, *args, **kwargs):  # pylint: disable=unused-argument
+        data, has_erro = self.mount_response_data()
+        return Response(data, status=HTTP_500_INTERNAL_SERVER_ERROR if has_erro else HTTP_200_OK)
+
+    def mount_response_data(self):
+        data, has_error = {}, False
+
+        if api_settings.SERVICE_NAME:
+            data['service'] = api_settings.SERVICE_NAME
+
+        if api_settings.SERVICE_VERSION:
+            data['version'] = api_settings.SERVICE_VERSION
+
+        if api_settings.CHECK_DATABASE:
+            db_error, data['databases'] = HealthCheck.test_databases()
+            has_error = not has_error and not db_error
+
+        if api_settings.CHECK_CACHE:
+            cache_error, data['caches'] = HealthCheck.test_caches()
+            has_error = not has_error and not cache_error
+
+        return data, has_error
