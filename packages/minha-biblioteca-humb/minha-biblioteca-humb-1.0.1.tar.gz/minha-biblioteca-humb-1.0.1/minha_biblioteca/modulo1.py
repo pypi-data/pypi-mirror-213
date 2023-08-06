e9f08052870f27df77e0b1a04edec9f526338f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package minha-biblioteca-humb/1.0.1/minha-biblioteca-humb-1.0.1.tar.gz

---
+++ packages/minha-biblioteca-humb/minha-biblioteca-humb-1.0.1.tar.gz/minha-biblioteca-humb-1.0.1/minha_biblioteca/modulo1.py
@@ -0,0 +1,2 @@
+def tutorialHello():
+    print("Hello Module")
---
+++ packages/minha-biblioteca-humb/minha-biblioteca-humb-1.0.1.tar.gz/minha-biblioteca-humb-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="minha-biblioteca-humb",
+    version="1.0.1",
+    description="Uma bilioteca python",
+    author="Humberto",
+    packages=find_packages()
+    )
+
