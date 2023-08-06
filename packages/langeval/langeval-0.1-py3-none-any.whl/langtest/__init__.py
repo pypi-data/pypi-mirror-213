9ad9652b8ee74c6350e5dddfc5949a39b2e1832f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:38 +0000

    Add package langeval/0.1/langeval-0.1-py3-none-any.whl

---
+++ packages/langeval/langeval-0.1-py3-none-any.whl/langtest/__init__.py
@@ -0,0 +1,6 @@
+from . import integrity
+from . import consistency
+from . import performance
+from . import robustness
+from . import fairness
+from . import utils
---
+++ packages/langeval/langeval-0.1-py3-none-any.whl/langtest/performance/tests.py
@@ -0,0 +1,14 @@
+from langtest.performance.rouge import RougeTest
+
+
+def test(method, **kwargs):
+    df = kwargs.pop('df', None)
+    rouge_examples = ['rouge1', 'rougeL']
+
+    if method in rouge_examples:
+        tester = RougeTest()
+        result_df = tester.apply(method, df, **kwargs)
+    else:
+        raise ValueError(f"Unknown method: {method}")
+
+    return result_df
