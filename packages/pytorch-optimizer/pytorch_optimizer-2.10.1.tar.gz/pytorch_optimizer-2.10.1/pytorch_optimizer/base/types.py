e5cd6ff241d22fd41ab135be04512c86b92fc972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:21 +0000

    Add package pytorch-optimizer/2.10.1/pytorch_optimizer-2.10.1.tar.gz

---
+++ packages/pytorch-optimizer/pytorch_optimizer-2.10.1.tar.gz/pytorch_optimizer-2.10.1/pytorch_optimizer/base/types.py
@@ -0,0 +1,16 @@
+from typing import Any, Callable, Dict, Iterable, Literal, Optional, Tuple, Type, Union
+
+import torch
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import _LRScheduler
+
+CLOSURE = Optional[Callable[[], float]]
+LOSS = Optional[float]
+BETAS = Union[Tuple[float, float], Tuple[float, float, float]]
+DEFAULTS = Dict[str, Any]
+PARAMETERS = Optional[Union[Iterable[Dict[str, Any]], Iterable[torch.Tensor]]]
+STATE = Dict[str, Any]
+OPTIMIZER = Type[Optimizer]
+SCHEDULER = Type[_LRScheduler]
+
+HUTCHINSON_G = Literal['gaussian', 'rademacher']
