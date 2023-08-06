16b2fa9799caaf6a2fad719964c6759225b7be02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:21 +0000

    Add package BMIckwrlTest001/0.0.0.2/BMIckwrlTest001-0.0.0.2-py3-none-any.whl

---
+++ packages/BMIckwrlTest001/BMIckwrlTest001-0.0.0.2-py3-none-any.whl/BMIckwrlTest001/BMI_test.py
@@ -0,0 +1,18 @@
+def BMI_check():
+    weight = int(input('체중을 입력하세요 : ',))
+    height = int(input('키를 입력하세요 : ',))
+    BMI = weight/((height*0.01)**2)
+    list = ['저체중', '최적 범위', '과체중', '1급 비만', '2급 비만', '3급 비만']
+    if BMI < 18.5:
+        i = 0
+    elif BMI < 25:
+        i = 1
+    elif BMI < 30:
+        i = 2
+    elif BMI < 35:
+        i = 3
+    elif BMI < 40:
+        i = 4
+    else:
+        i = 5
+    return print(f'당신의 BMI는 {int(BMI)}이며, {list[i]}입니다.')
