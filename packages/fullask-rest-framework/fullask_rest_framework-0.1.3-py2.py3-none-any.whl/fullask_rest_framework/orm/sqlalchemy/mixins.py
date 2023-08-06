abe10e0df4dab2baef691b7ccdd3f21713fb38da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:45 +0000

    Add package fullask-rest-framework/0.1.3/fullask_rest_framework-0.1.3-py2.py3-none-any.whl

---
+++ packages/fullask-rest-framework/fullask_rest_framework-0.1.3-py2.py3-none-any.whl/fullask_rest_framework/orm/sqlalchemy/mixins.py
@@ -0,0 +1,36 @@
+from uuid import uuid4
+
+from fullask_rest_framework.factory.extensions import db
+
+
+class BaseMixin:
+    """
+    The parent mixin of all mixins. Prevents mixins from being used as classes themselves.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        if type(self) is self.__class__:
+            raise TypeError(
+                f"You can only use {self.__class__.__name__} for implementing Mixin."
+            )
+        super().__init__(*args, **kwargs)
+
+
+class TimeStampedMixin(BaseMixin):
+    """Mixin for auto-saving creation and modification dates"""
+
+    created_at = db.Column(
+        db.DateTime, default=db.func.now(timezone=True), nullable=False
+    )
+    updated_at = db.Column(
+        db.DateTime,
+        default=db.func.now(),
+        onupdate=db.func.now(),
+        nullable=False,
+    )
+
+
+class UUIDMixin(BaseMixin):
+    """UUID 필드를 더해주는 믹스인"""
+
+    uuid = db.Column(db.String(36), unique=True, default=str(uuid4()), nullable=False)
