e36b439a9d28a7b36032df293597c51f2ad90a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package mheblib/0.5/mheblib-0.5.tar.gz

---
+++ packages/mheblib/mheblib-0.5.tar.gz/mheblib-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+setup(
+    name= 'mheblib',
+    version= '0.5',
+    author= 'Wael-almheb',
+    description= 'Python library for extracting Instagram cookies and many more features',
+    packages=find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
