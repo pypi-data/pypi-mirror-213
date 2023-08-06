f12e798906e1855c0c8eb265b295ed301acd9932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:07 +0000

    Add package genocide/121/genocide-121.tar.gz

---
+++ packages/genocide/genocide-121.tar.gz/genocide-121/test/__init__.py
@@ -0,0 +1,7 @@
+# This file is placed in the Public Domain.
+
+
+"test imports"
+
+
+import genocide
---
+++ packages/genocide/genocide-121.tar.gz/genocide-121/test/test_decoder.py
@@ -0,0 +1,29 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0114,C0115,C0116,W0703,C0413
+# pylama: ignore=E402
+
+
+import os
+import sys
+import unittest
+
+
+sys.path.insert(0, os.getcwd())
+
+
+from genocide.decoder import loads
+from genocide.encoder import dumps
+from genocide.objects import Object
+
+
+class TestDecoder(unittest.TestCase):
+
+    def test_loads(self):
+        obj = Object()
+        obj.test = "bla"
+        oobj = loads(dumps(obj))
+        self.assertEqual(oobj.test, "bla")
+
+    def test_doctest(self):
+        self.assertTrue(__doc__ is None)
