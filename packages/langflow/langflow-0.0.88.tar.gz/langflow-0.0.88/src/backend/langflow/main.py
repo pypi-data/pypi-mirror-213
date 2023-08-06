f8dea07332ef1b1402d740bb455736e8551706cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package langflow/0.0.88/langflow-0.0.88.tar.gz

---
+++ packages/langflow/langflow-0.0.88.tar.gz/langflow-0.0.88/src/backend/langflow/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from langflow.api import router
+
+
+def create_app():
+    """Create the FastAPI app and include the router."""
+    app = FastAPI()
+
+    origins = [
+        "*",
+    ]
+
+    @app.get("/health")
+    def get_health():
+        return {"status": "OK"}
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router)
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=7860)
