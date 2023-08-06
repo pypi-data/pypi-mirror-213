5ee40953e0483a28380cb09c70e9c45a5000016e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:11 +0000

    Add package pyqt5-utils/1.0.4/pyqt5-utils-1.0.4.tar.gz

---
+++ packages/pyqt5-utils/pyqt5-utils-1.0.4.tar.gz/pyqt5-utils-1.0.4/README.md
@@ -0,0 +1,21 @@
+# PyQt5-Utils
+
+This is a collection of useful tools for working with PyQt5.
+
+## Installation
+
+You can install this package with pip:
+
+```bash
+pip install pyqt5-utils
+```
+
+## Usage
+
+#### Conversion from .ui files to .py files:
+
+```python
+from pyqt5_utils import Utils
+
+Utils.convert_ui_to_py(path_where_ui_files_are_located)
+```
---
+++ packages/pyqt5-utils/pyqt5-utils-1.0.4.tar.gz/pyqt5-utils-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyqt5-utils',
+    version='1.0.4',
+    description='Utilities for PyQt5',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Hadi Hassan',
+    author_email='hassanhadi17@hotmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
