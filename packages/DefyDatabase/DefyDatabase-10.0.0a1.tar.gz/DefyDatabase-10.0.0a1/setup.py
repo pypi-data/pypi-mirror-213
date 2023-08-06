14a4dfed4078230604c252a30eb765273664f3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:35 +0000

    Add package DefyDatabase/10.0.0a1/DefyDatabase-10.0.0a1.tar.gz

---
+++ packages/DefyDatabase/DefyDatabase-10.0.0a1.tar.gz/DefyDatabase-10.0.0a1/setup.py
@@ -0,0 +1,21 @@
+import setuptools,defy
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DefyDatabase",
+    version=defy.version,
+    author="Defymen",
+    author_email="vmuonline@126.com",
+    description="Database for personal data based on SQLite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
