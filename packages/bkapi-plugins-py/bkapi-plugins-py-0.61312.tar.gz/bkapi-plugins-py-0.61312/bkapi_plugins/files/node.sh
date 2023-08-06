5c991aecc23f289d282d9d8c94a037dfd641b017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:05 +0000

    Add package bkapi-plugins-py/0.61312/bkapi-plugins-py-0.61312.tar.gz

---
+++ packages/bkapi-plugins-py/bkapi-plugins-py-0.61312.tar.gz/bkapi-plugins-py-0.61312/bkapi_plugins/files/node.sh
@@ -0,0 +1,12 @@
+  affinity:
+    podAntiAffinity:
+      preferredDuringSchedulingIgnoredDuringExecution:
+      - weight: 100
+        podAffinityTerm:
+          labelSelector:
+            matchExpressions:
+              - key: app
+                operator: In
+                values:
+                - middle
+          topologyKey: kubernetes.io/hostname
