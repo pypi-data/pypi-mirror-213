76b25bf2d29f93b22a63fc1e01177fb0e3e7772b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:27 +0000

    Add package torchqtm/0.1.0/torchqtm-0.1.0.tar.gz

---
+++ packages/torchqtm/torchqtm-0.1.0.tar.gz/torchqtm-0.1.0/setup.py
@@ -0,0 +1,51 @@
+from distutils.core import setup
+from setuptools import find_packages
+from Cython.Build import cythonize
+import numpy
+import glob
+
+extensions = glob.glob("torchqtm/_C/*.pyx")
+
+install_requires = [
+    'numpy',
+    'pandas',
+    'pandas_market_calendars',
+    'cython',
+]
+
+
+def main():
+    setup(
+        ext_modules=cythonize(extensions, language='c++'),
+        include_dirs=[numpy.get_include()],
+        name="torchqtm",
+        version="0.1.0",
+        author="ny",
+        author_email="nymath@163.com",
+        install_requires=install_requires,
+        description="None",
+        long_description=open('README.md', 'r').read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/nymath/torchquantum/tree/main",
+        download_url="https://github.com/nymath/torchquantum/releases/tag/",
+        packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: Unix",
+        ],
+        python_requires='>=3.6',
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+## setup cython
+# python setup.py build_ext --inplace
+## setup package
+# pip install twine
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+# twine upload --skip-existing dist/*
+
---
+++ packages/torchqtm/torchqtm-0.1.0.tar.gz/torchqtm-0.1.0/torchqtm/op/__init__.py
@@ -0,0 +1,1 @@
+from torchqtm.base import BaseAlpha, Momentum, Reversion, Fundamental, Volatility, Parameter
