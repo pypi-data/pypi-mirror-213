6f5b9ceda355742b6ae7271b1ebb4f9e63c1a6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package robyn/0.33.0/robyn-0.33.0.tar.gz

---
+++ packages/robyn/robyn-0.33.0.tar.gz/robyn-0.33.0/integration_tests/helpers/http_methods_helpers.py
@@ -0,0 +1,109 @@
+from typing import Optional
+
+import requests
+
+BASE_URL = "http://127.0.0.1:8080"
+
+
+def check_response(response: requests.Response, expected_status_code: int):
+    assert response.status_code == expected_status_code
+    assert "global_after" in response.headers
+    assert response.headers["global_after"] == "global_after_request"
+    assert "server" in response.headers
+    assert response.headers["server"] == "robyn"
+
+
+def get(
+    endpoint: str, expected_status_code: int = 200, headers: dict = {}
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.get(f"{BASE_URL}/{endpoint}", headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def post(
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.post(f"{BASE_URL}/{endpoint}", data=data, headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def put(
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.put(f"{BASE_URL}/{endpoint}", data=data, headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def patch(
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.patch(f"{BASE_URL}/{endpoint}", data=data, headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def delete(
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.delete(f"{BASE_URL}/{endpoint}", data=data, headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def head(
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    response = requests.head(f"{BASE_URL}/{endpoint}", data=data, headers=headers)
+    check_response(response, expected_status_code)
+    return response
+
+
+def generic_http_helper(
+    method: str,
+    endpoint: str,
+    data: Optional[dict] = None,
+    expected_status_code: int = 200,
+    headers: dict = {},
+) -> requests.Response:
+    endpoint = endpoint.strip("/")
+    if method not in ["get", "post", "put", "patch", "delete", "options", "trace"]:
+        raise ValueError(
+            f"{method} method must be one of get, post, put, patch, delete"
+        )
+    if method == "get":
+        response = requests.get(f"{BASE_URL}/{endpoint}", headers=headers)
+    else:
+        response = requests.request(
+            method, f"{BASE_URL}/{endpoint}", data=data, headers=headers
+        )
+    try:
+        check_response(response, expected_status_code)
+    except AssertionError:
+        raise AssertionError(
+            f"response status code is {response.status_code}, expected {expected_status_code} of method {method}"
+        )
+    return response
---
+++ packages/robyn/robyn-0.33.0.tar.gz/robyn-0.33.0/src/executors/mod.rs
@@ -0,0 +1,97 @@
+/// This is the module that has all the executor functions
+/// i.e. the functions that have the responsibility of parsing and executing functions.
+use std::sync::Arc;
+
+use anyhow::{Context, Result};
+use log::debug;
+use pyo3::prelude::*;
+use pyo3_asyncio::TaskLocals;
+
+use crate::types::{function_info::FunctionInfo, request::Request, response::Response};
+
+fn get_function_output<'a, T>(
+    function: &'a FunctionInfo,
+    py: Python<'a>,
+    input: &T,
+) -> Result<&'a PyAny, PyErr>
+where
+    T: ToPyObject,
+{
+    let handler = function.handler.as_ref(py);
+
+    // this makes the request object accessible across every route
+    match function.number_of_params {
+        0 => handler.call0(),
+        1 => handler.call1((input.to_object(py),)),
+        // this is done to accommodate any future params
+        2_u8..=u8::MAX => handler.call1((input.to_object(py),)),
+    }
+}
+
+pub async fn execute_middleware_function<T>(input: &T, function: &FunctionInfo) -> Result<T>
+where
+    T: for<'a> FromPyObject<'a> + ToPyObject,
+{
+    if function.is_async {
+        let output: Py<PyAny> = Python::with_gil(|py| {
+            pyo3_asyncio::tokio::into_future(get_function_output(function, py, input)?)
+        })?
+        .await?;
+
+        Python::with_gil(|py| -> Result<T> {
+            let output: (T,) = output
+                .extract(py)
+                .context("Failed to get middleware response")?;
+            Ok(output.0)
+        })
+    } else {
+        Python::with_gil(|py| -> Result<T> {
+            let output: (T,) = get_function_output(function, py, input)?
+                .extract()
+                .context("Failed to get middleware response")?;
+            Ok(output.0)
+        })
+    }
+}
+
+#[inline]
+pub async fn execute_http_function(
+    request: &Request,
+    function: &FunctionInfo,
+) -> PyResult<Response> {
+    if function.is_async {
+        let output = Python::with_gil(|py| {
+            let function_output = get_function_output(function, py, request)?;
+            pyo3_asyncio::tokio::into_future(function_output)
+        })?
+        .await?;
+
+        return Python::with_gil(|py| -> PyResult<Response> { output.extract(py) });
+    };
+
+    Python::with_gil(|py| -> PyResult<Response> {
+        get_function_output(function, py, request)?.extract()
+    })
+}
+
+pub async fn execute_event_handler(
+    event_handler: Option<Arc<FunctionInfo>>,
+    task_locals: &TaskLocals,
+) -> Result<()> {
+    if let Some(function) = event_handler {
+        if function.is_async {
+            debug!("Startup event handler async");
+            Python::with_gil(|py| {
+                pyo3_asyncio::into_future_with_locals(
+                    task_locals,
+                    function.handler.as_ref(py).call0()?,
+                )
+            })?
+            .await?;
+        } else {
+            debug!("Startup event handler");
+            Python::with_gil(|py| function.handler.call0(py))?;
+        }
+    }
+    Ok(())
+}
