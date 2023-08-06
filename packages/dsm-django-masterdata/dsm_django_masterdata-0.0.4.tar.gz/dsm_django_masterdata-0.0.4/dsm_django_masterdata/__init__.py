cc93fe3e915b48080daf9dbb802ccdf7444bcf55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:23 +0000

    Add package dsm-django-masterdata/0.0.4/dsm_django_masterdata-0.0.4.tar.gz

---
+++ packages/dsm-django-masterdata/dsm_django_masterdata-0.0.4.tar.gz/dsm_django_masterdata-0.0.4/dsm_django_masterdata/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'DigitalStoreMesh Django MasterData'
+__version__ = '0.0.4'
+__author__ = 'DigitalStoreMesh Co.,Ltd'
+__license__ = 'MIT'
+
+VERSION = __version__
+
+from .hscode.models import HScode
+from .area.models import Area
