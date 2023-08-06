ccd3c5230f81608462ca960e5cc1a00a3ebfd448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package ape-bsc/0.6.1/ape_bsc-0.6.1-py3-none-any.whl

---
+++ packages/ape-bsc/ape_bsc-0.6.1-py3-none-any.whl/ape_bsc/__init__.py
@@ -0,0 +1,36 @@
+from ape import plugins
+from ape.api import NetworkAPI, create_network_type
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape_geth import GethProvider
+from ape_test import LocalProvider
+
+from .ecosystem import BSC, NETWORKS, BSCConfig
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return BSCConfig
+
+
+@plugins.register(plugins.EcosystemPlugin)
+def ecosystems():
+    yield BSC
+
+
+@plugins.register(plugins.NetworkPlugin)
+def networks():
+    for network_name, network_params in NETWORKS.items():
+        yield "bsc", network_name, create_network_type(*network_params)
+
+    # NOTE: This works for development providers, as they get chain_id from themselves
+    yield "bsc", LOCAL_NETWORK_NAME, NetworkAPI
+    yield "bsc", "mainnet-fork", NetworkAPI
+    yield "bsc", "testnet-fork", NetworkAPI
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for network_name in NETWORKS:
+        yield "bsc", network_name, GethProvider
+
+    yield "bsc", LOCAL_NETWORK_NAME, LocalProvider
---
+++ packages/ape-bsc/ape_bsc-0.6.1-py3-none-any.whl/ape_bsc/ecosystem.py
@@ -0,0 +1,104 @@
+from typing import Optional, Type, Union, cast
+
+from ape.api import TransactionAPI
+from ape.api.config import PluginConfig
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape.exceptions import ApeException
+from ape.types import TransactionSignature
+from ape_ethereum.ecosystem import Ethereum, NetworkConfig
+from ape_ethereum.transactions import DynamicFeeTransaction, StaticFeeTransaction, TransactionType
+from eth_typing import HexStr
+from eth_utils import add_0x_prefix, decode_hex
+
+NETWORKS = {
+    # chain_id, network_id
+    "mainnet": (56, 56),
+    "testnet": (97, 97),
+}
+
+
+class BSCConfig(PluginConfig):
+    mainnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=3)
+    testnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=3)
+    local: NetworkConfig = NetworkConfig(default_provider="test")
+    default_network: str = LOCAL_NETWORK_NAME
+
+
+class BSC(Ethereum):
+    @property
+    def config(self) -> BSCConfig:  # type: ignore[override]
+        return cast(BSCConfig, self.config_manager.get_config("bsc"))
+
+    def create_transaction(self, **kwargs) -> TransactionAPI:
+        """
+        Returns a transaction using the given constructor kwargs.
+        Overridden because does not support
+
+        **kwargs: Kwargs for the transaction class.
+
+        Returns:
+            :class:`~ape.api.transactions.TransactionAPI`
+        """
+
+        transaction_type = _get_transaction_type(kwargs.get("type"))
+        kwargs["type"] = transaction_type.value
+        txn_class = _get_transaction_cls(transaction_type)
+
+        if "required_confirmations" not in kwargs or kwargs["required_confirmations"] is None:
+            # Attempt to use default required-confirmations from `ape-config.yaml`.
+            required_confirmations = 0
+            active_provider = self.network_manager.active_provider
+            if active_provider:
+                required_confirmations = active_provider.network.required_confirmations
+
+            kwargs["required_confirmations"] = required_confirmations
+
+        if isinstance(kwargs.get("chainId"), str):
+            kwargs["chainId"] = int(kwargs["chainId"], 16)
+
+        if "input" in kwargs:
+            kwargs["data"] = decode_hex(kwargs.pop("input").hex())
+
+        if all(field in kwargs for field in ("v", "r", "s")):
+            kwargs["signature"] = TransactionSignature(
+                v=kwargs["v"],
+                r=bytes(kwargs["r"]),
+                s=bytes(kwargs["s"]),
+            )
+
+        return txn_class.parse_obj(kwargs)
+
+
+def _get_transaction_type(_type: Optional[Union[int, str, bytes]]) -> TransactionType:
+    if not _type:
+        return TransactionType.STATIC
+
+    if _type is None:
+        _type = TransactionType.STATIC.value
+    elif isinstance(_type, int):
+        _type = f"0{_type}"
+    elif isinstance(_type, bytes):
+        _type = _type.hex()
+
+    suffix = _type.replace("0x", "")
+    if len(suffix) == 1:
+        _type = f"{_type.rstrip(suffix)}0{suffix}"
+
+    return TransactionType(add_0x_prefix(HexStr(_type)))
+
+
+class ApeBSCError(ApeException):
+    """
+    Raised in the ape-bsc plugin.
+    """
+
+
+def _get_transaction_cls(transaction_type: TransactionType) -> Type[TransactionAPI]:
+    transaction_types = {
+        TransactionType.STATIC: StaticFeeTransaction,
+        TransactionType.DYNAMIC: DynamicFeeTransaction,
+    }
+    if transaction_type not in transaction_types:
+        raise ApeBSCError(f"Transaction type '{transaction_type}' not supported.")
+
+    return transaction_types[transaction_type]
