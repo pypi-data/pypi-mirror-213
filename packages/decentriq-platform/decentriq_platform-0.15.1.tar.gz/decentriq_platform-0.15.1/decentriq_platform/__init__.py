039120fa1e1d266e6fe263dc8c99bde920229be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:59 +0000

    Add package decentriq-platform/0.15.1/decentriq_platform-0.15.1.tar.gz

---
+++ packages/decentriq-platform/decentriq_platform-0.15.1.tar.gz/decentriq_platform-0.15.1/decentriq_platform/__init__.py
@@ -0,0 +1,71 @@
+"""
+.. include:: ../../decentriq_platform_docs/gcg_getting_started.md
+___
+"""
+__docformat__ = "restructuredtext"
+__pdoc__ = {
+    "api": False,
+    "attestation": True,
+    "authentication": True,
+    "builders": False,
+    "certs": False,
+    "client": False,
+    "compute": False,
+    "config": False,
+    "container": True,
+    "graphql": False,
+    "helpers": False,
+    "node": True,
+    "permission": False,
+    "proto": False,
+    "s3_sink": True,
+    "data_source_s3": True,
+    "dataset_sink": True,
+    "session": True,
+    "sql": True,
+    "storage": True,
+    "types": True,
+    "verification": False,
+}
+
+from .client import Client, create_client, Session
+from .builders import (
+    DataRoomBuilder,
+    DataRoomCommitBuilder,
+    GovernanceProtocol
+)
+from .compute import Noop, StaticContent
+from .permission import Permissions
+from .storage import Key
+from .attestation import enclave_specifications, EnclaveSpecifications
+
+from .endorsement import Endorser
+from .keychain import Keychain
+
+__all__ = [
+    "create_client",
+    "Client",
+    "Session",
+    "DataRoomBuilder",
+    "DataRoomCommitBuilder",
+    "Permissions",
+    "GovernanceProtocol",
+    "enclave_specifications",
+    "EnclaveSpecifications",
+    "Key",
+    "StaticContent",
+    "Noop",
+    "sql",
+    "container",
+    "s3_sink",
+    "data_source_s3",
+    "dataset_sink",
+    "storage",
+    "attestation",
+    "types",
+    "authentication",
+    "session",
+    "node",
+    "Endorser",
+    "Keychain",
+]
