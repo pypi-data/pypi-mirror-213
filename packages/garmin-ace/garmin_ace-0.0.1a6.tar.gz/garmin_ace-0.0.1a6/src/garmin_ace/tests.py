72674d78de604b0c818389c2ed67f468bf7e8ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package garmin-ace/0.0.1a6/garmin_ace-0.0.1a6.tar.gz

---
+++ packages/garmin-ace/garmin_ace-0.0.1a6.tar.gz/garmin_ace-0.0.1a6/src/garmin_ace/tests.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import filecmp
+
+from garmin_ace.decoder import ACEFileDecoder
+from garmin_ace.encoder import ACEFileEncoder
+
+class TestACEFileDecoderEncoder(unittest.TestCase):
+    def test_decoding_encoding(self):
+        original_file_path = 'data/checklist.ace'
+        tmp_file_path = '/tmp/temp.ace'
+
+        checklist = ACEFileDecoder.read_from_file(original_file_path)
+
+        encoder = ACEFileEncoder(checklist)
+        encoder.write_to_file(tmp_file_path)
+
+        # Check that the original file and the re-encoded file are identical
+        self.assertTrue(filecmp.cmp(original_file_path, tmp_file_path, shallow=False))
+
+        os.remove(tmp_file_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
