d7929c76849f6935bba2360cc758bda453238053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package PoolFlow/0.0.2/PoolFlow-0.0.2.tar.gz

---
+++ packages/PoolFlow/PoolFlow-0.0.2.tar.gz/PoolFlow-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from subprocess import run as running
+import setuptools
+import os
+
+setup(name='PoolFlow',
+      version='0.0.2',
+      description='Job flow management',
+      url='https://victorgarric.github.io/PoolFlow/',
+      license='MIT',
+      long_description='README.md',
+      long_description_content_type="text/markdown",
+      author='Victor Garric',
+      packages=setuptools.find_packages(),
+      author_email='victor.garric@gmail.com',
+      include_package_data=True,
+      keywords='',
+      install_requires=['rich'],
+      entry_points={"console_scripts": ["", ], },
+      tests_require=['pytest'],
+      zip_safe=False)
