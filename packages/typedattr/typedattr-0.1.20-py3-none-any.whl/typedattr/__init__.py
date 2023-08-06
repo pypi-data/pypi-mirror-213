a5b9c5b64cd13964864a76e76ba132eee29dfc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:17 +0000

    Add package typedattr/0.1.20/typedattr-0.1.20-py3-none-any.whl

---
+++ packages/typedattr/typedattr-0.1.20-py3-none-any.whl/typedattr/__init__.py
@@ -0,0 +1,6 @@
+from ._typedattr import definenumpy, attrs_from_dict
+from .attrutils import get_attr_names
+from .const import Const
+
+__all__ = ["definenumpy", "attrs_from_dict", "get_attr_names", "Const"]
+__version__ = "0.1.20"
