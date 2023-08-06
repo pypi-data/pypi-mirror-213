1c3a5e9762d5afb257d5633f826d04dc14fff5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:59 +0000

    Add package jupytercad/0.3.0/jupytercad-0.3.0.tar.gz

---
+++ packages/jupytercad/jupytercad-0.3.0.tar.gz/jupytercad-0.3.0/packages/jupytercad-app/build/sharedscope.js
@@ -0,0 +1,8 @@
+import '@jupyterlab/filebrowser';
+import '@jupyterlab/fileeditor';
+import '@jupyterlab/launcher';
+import '@jupyterlab/logconsole';
+import '@jupyterlab/notebook';
+import '@jupyterlab/rendermime';
+import '@jupyterlab/settingregistry';
+import '@jupyterlab/statedb';
