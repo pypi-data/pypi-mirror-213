ffddebc8347c3ba719a548ff5aa0a34a281faee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:06 +0000

    Add package dcrx-api/0.2.8/dcrx_api-0.2.8-py3-none-any.whl

---
+++ packages/dcrx-api/dcrx_api-0.2.8-py3-none-any.whl/dcrx_api/services/monitoring/cpu/monitor.py
@@ -0,0 +1,16 @@
+import psutil
+import itertools
+import statistics
+from dcrx_api.services.monitoring.base.monitor import BaseMonitor
+
+
+class CPUMonitor(BaseMonitor):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def update_monitor(self, monitor_name: str):
+        self.active[monitor_name] = psutil.cpu_percent()
+
+
+        
