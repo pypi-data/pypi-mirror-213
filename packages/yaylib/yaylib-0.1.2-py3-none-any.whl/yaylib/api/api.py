fc5666f11bb24ff6bfda929a6260da8bdcb43793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:47 +0000

    Add package yaylib/0.1.2/yaylib-0.1.2-py3-none-any.whl

---
+++ packages/yaylib/yaylib-0.1.2-py3-none-any.whl/yaylib/api/api.py
@@ -0,0 +1,151 @@
+import os
+import logging
+
+from json import JSONDecodeError
+from typing import Optional, Dict, Any
+
+import httpx
+
+from ..config import *
+from ..errors import *
+from ..responses import LoginUserResponse
+from ..utils import *
+
+
+current_path = os.path.abspath(os.getcwd())
+
+
+class API:
+
+    def __init__(
+            self,
+            access_token: str = None,
+            refresh_token: str = None,
+            proxy: str = None,
+            timeout=60,
+            base_path=current_path,
+            loglevel_stream=logging.INFO,
+            host=Configs.YAY_PRODUCTION_HOST,
+    ):
+        self.yaylib_version = Configs.YAYLIB_VERSION
+        self.api_version = Configs.YAY_API_VERSION
+        self.api_key = Configs.YAY_API_KEY
+
+        self.login_data = LoginUserResponse({
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+        })
+
+        self.proxy = {}
+        if proxy is not None:
+            self.proxy["https"] = proxy
+
+        self.timeout = timeout
+        self.base_path = base_path
+        self.host = "https://" + host
+
+        self.generate_all_uuids()
+        self.session = httpx.Client(proxies=self.proxy, timeout=self.timeout)
+        self.session.headers.update(Configs.REQUEST_HEADERS)
+        self.session.headers.update({"X-Device-Uuid": self.device_uuid})
+        if access_token:
+            self.session.headers.setdefault(
+                "Authorization", f"Bearer {access_token}"
+            )
+
+        self.logger = logging.getLogger(
+            "yaylib version: " + self.yaylib_version
+        )
+        ch = logging.StreamHandler()
+        ch.setLevel(loglevel_stream)
+        ch.setFormatter(logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        ))
+
+        handler_existed = False
+        for handler in self.logger.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                handler_existed = True
+                break
+        if not handler_existed:
+            self.logger.addHandler(ch)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.logger.info("yaylib version: " + self.yaylib_version + " started")
+
+    def request(self, method, endpoint, params=None, payload=None, user_auth=True, headers=None):
+
+        if headers is None:
+            headers = self.session.headers
+        if not user_auth:
+            headers["Authorization"] = None
+
+        self.logger.debug(
+            "Making API request:\n\n"
+            f"{method}: {endpoint}\n\n"
+            f"Parameters: {params}\n\n"
+            f"Headers: {headers}\n\n"
+            f"Body: {payload}\n"
+        )
+
+        response = self.session.request(
+            method, endpoint, params=params, json=payload, headers=headers
+        )
+
+        self.logger.debug(
+            "Received API response:\n\n"
+            f"Status Code: {response.status_code}\n\n"
+            f"Headers: {response.headers}\n\n"
+            f"Response: {response.text}\n"
+        )
+        self._handle_response(response)
+
+        try:
+            return response.json()
+        except JSONDecodeError:
+            return response.text
+
+    def _make_request(
+            self, method: str, endpoint: str, params: dict = None,
+            payload: dict = None, data_type=None, user_auth=True, headers=None
+    ):
+        response = self.request(
+            method, endpoint, params, payload, user_auth, headers
+        )
+        if data_type:
+            return self._construct_response(response, data_type)
+        return response
+
+    def _construct_response(self, data, data_type):
+        if data_type is not None:
+            if isinstance(data, list):
+                data = [data_type(result) for result in data]
+            elif data is not None:
+                data = data_type(data)
+        return data
+
+    def _check_authorization(self) -> None:
+        if self.session.headers.get("Authorization") is None:
+            raise AuthenticationError(
+                "Authorization is not present in the header.")
+
+    def _handle_response(self, response):
+        if response.status_code == 400:
+            raise BadRequestError(response.text)
+        if response.status_code == 401:
+            raise AuthenticationError(response.text)
+        if response.status_code == 403:
+            raise ForbiddenError(response.text)
+        if response.status_code == 404:
+            raise NotFoundError(response.text)
+        if response.status_code == 429:
+            raise RateLimitError(response.text)
+        if response.status_code == 500:
+            raise YayServerError(response.text)
+        if response.status_code and not 200 <= response.status_code < 300:
+            raise HTTPError(response.text)
+        return response
+
+    def generate_all_uuids(self):
+        self.device_uuid = generate_uuid()[0]
+        self.uuid, self.url_uuid = generate_uuid()
---
+++ packages/yaylib/yaylib-0.1.2-py3-none-any.whl/yaylib/config.py
@@ -0,0 +1,113 @@
+class Configs:
+
+    YAYLIB_VERSION = "0.1.2"
+    YAY_API_VERSION = "3.16"
+    YAY_VERSION_NAME = "3.16.1"
+    YAY_API_VERSION_KEY = "e83a1d2588918c2061280427c88e6f56"
+    YAY_API_KEY = "ccd59ee269c01511ba763467045c115779fcae3050238a252f1bd1a4b65cfec6"
+    YAY_SHARED_KEY = "yayZ1"
+    YAY_STORE_KEY = "yayZ1payment"
+    ID_CARD_CHECK_SECRET_KEY = "4aa6d1c301a97154bc1098c2"
+    YAY_REVIEW_HOST_1 = "review.yay.space"
+    YAY_REVIEW_HOST_2 = "cas-stg.yay.space"
+    YAY_STAGING_HOST_1 = "stg.yay.space"
+    YAY_STAGING_HOST_2 = "cas.yay.space"
+    YAY_PRODUCTION_HOST = "api.yay.space"
+    YAY_API_URL = "https://" + YAY_PRODUCTION_HOST
+    ID_CARD_CHECK_HOST_PRODUCTION = "idcardcheck.com"
+    ID_CARD_CHECK_HOST_STAGING = "stg.idcardcheck.com"
+    USER_AGENT = "android 11 (3.5x 1440x2960 Galaxy S9)"
+    REQUEST_HEADERS = {
+        "Host": YAY_PRODUCTION_HOST,
+        "X-App-Version": YAY_API_VERSION,
+        "User-Agent": USER_AGENT,
+        "X-Device-Info": f"yay {YAY_VERSION_NAME} {USER_AGENT}",
+        "X-Device-Uuid": "",
+        "X-Connection-Type": "wifi",
+        "Accept-Language": "ja",
+        "Content-Type": "application/json;charset=UTF-8"
+    }
+
+
+class Endpoints:
+
+    # api v1 endpoints
+    USERS_V1 = Configs.YAY_API_URL + "/v1/users"
+    PAYMENTS_V1 = Configs.YAY_API_URL + "/v1/payments"
+    THREADS_V1 = Configs.YAY_API_URL + "/v1/threads"
+    PINNED_V1 = Configs.YAY_API_URL + "/v1/pinned"
+    POSTS_V1 = Configs.YAY_API_URL + "/v1/posts"
+    CONVERSATIONS_V1 = Configs.YAY_API_URL + "/v1/conversations"
+    HIDDEN_V1 = Configs.YAY_API_URL + "/v1/hidden"
+    GROUPS_V1 = Configs.YAY_API_URL + "/v1/groups"
+    CHAT_ROOMS_V1 = Configs.YAY_API_URL + "/v1/chat_rooms"
+    CALLS_V1 = Configs.YAY_API_URL + "/v1/calls"
+    SURVEYS_V1 = Configs.YAY_API_URL + "/v1/surveys"
+    FRIENDS_V1 = Configs.YAY_API_URL + "/v1/friends"
+    GAMES_V1 = Configs.YAY_API_URL + "/v1/games"
+    GENRES_V1 = Configs.YAY_API_URL + "/v1/genres"
+    ANONYMOUS_CALLS_V1 = Configs.YAY_API_URL + "/v1/anonymous_calls"
+    NOTIFICATION_SETTINGS_V1 = Configs.YAY_API_URL + "/v1/notification_settings"
+    STICKER_PACKS_V1 = Configs.YAY_API_URL + "/v1/sticker_packs"
+    SNS_THUMBNAIL_V1 = Configs.YAY_API_URL + "/v1/sns_thumbnail"
+    EMAIL_VERIFICATION_URL_V1 = Configs.YAY_API_URL + "/v1/email_verification_urls"
+    BUCKETS_V1 = Configs.YAY_API_URL + "/v1/buckets"
+    ID_CHECK_V1 = Configs.YAY_API_URL + "/v1/id_check"
+    PROMOTIONS_V1 = Configs.YAY_API_URL + "/v1/promotions"
+    SKYFALL_V1 = Configs.YAY_API_URL + "/v1/skyfall"
+    GENUINE_DEVICES_V1 = Configs.YAY_API_URL + "/v1/genuine_devices"
+    LOBI_FRIENDS_V1 = Configs.YAY_API_URL + "/v1/lobi_friends"
+
+    # api v2 endpoints
+    USERS_V2 = Configs.YAY_API_URL + "/v2/users"
+    PAYMENTS_V2 = Configs.YAY_API_URL + "/v2/payments"
+    THREADS_V2 = Configs.YAY_API_URL + "/v2/threads"
+    PINNED_V2 = Configs.YAY_API_URL + "/v2/pinned"
+    POSTS_V2 = Configs.YAY_API_URL + "/v2/posts"
+    CONVERSATIONS_V2 = Configs.YAY_API_URL + "/v2/conversations"
+    HIDDEN_V2 = Configs.YAY_API_URL + "/v2/hidden"
+    GROUPS_V2 = Configs.YAY_API_URL + "/v2/groups"
+    CHAT_ROOMS_V2 = Configs.YAY_API_URL + "/v2/chat_rooms"
+    CALLS_V2 = Configs.YAY_API_URL + "/v2/calls"
+    SURVEYS_V2 = Configs.YAY_API_URL + "/v2/surveys"
+    FRIENDS_V2 = Configs.YAY_API_URL + "/v2/friends"
+    GAMES_V2 = Configs.YAY_API_URL + "/v2/games"
+    GENRES_V2 = Configs.YAY_API_URL + "/v2/genres"
+    ANONYMOUS_CALLS_V2 = Configs.YAY_API_URL + "/v2/anonymous_calls"
+    NOTIFICATION_SETTINGS_V2 = Configs.YAY_API_URL + "/v2/notification_settings"
+    STICKER_PACKS_V2 = Configs.YAY_API_URL + "/v2/sticker_packs"
+    SNS_THUMBNAIL_V2 = Configs.YAY_API_URL + "/v2/sns_thumbnail"
+    EMAIL_VERIFICATION_URL_V2 = Configs.YAY_API_URL + "/v2/email_verification_urls"
+    BUCKETS_V2 = Configs.YAY_API_URL + "/v2/buckets"
+    ID_CHECK_V2 = Configs.YAY_API_URL + "/v2/id_check"
+    PROMOTIONS_V2 = Configs.YAY_API_URL + "/v2/promotions"
+    SKYFALL_V2 = Configs.YAY_API_URL + "/v2/skyfall"
+    GENUINE_DEVICES_V2 = Configs.YAY_API_URL + "/v2/genuine_devices"
+    LOBI_FRIENDS_V2 = Configs.YAY_API_URL + "/v2/lobi_friends"
+
+    # api v3 endpoints
+    USERS_V3 = Configs.YAY_API_URL + "/v3/users"
+    PAYMENTS_V3 = Configs.YAY_API_URL + "/v3/payments"
+    THREADS_V3 = Configs.YAY_API_URL + "/v3/threads"
+    PINNED_V3 = Configs.YAY_API_URL + "/v3/pinned"
+    POSTS_V3 = Configs.YAY_API_URL + "/v3/posts"
+    CONVERSATIONS_V3 = Configs.YAY_API_URL + "/v3/conversations"
+    HIDDEN_V3 = Configs.YAY_API_URL + "/v3/hidden"
+    GROUPS_V3 = Configs.YAY_API_URL + "/v3/groups"
+    CHAT_ROOMS_V3 = Configs.YAY_API_URL + "/v3/chat_rooms"
+    CALLS_V3 = Configs.YAY_API_URL + "/v3/calls"
+    SURVEYS_V3 = Configs.YAY_API_URL + "/v3/surveys"
+    FRIENDS_V3 = Configs.YAY_API_URL + "/v3/friends"
+    GAMES_V3 = Configs.YAY_API_URL + "/v3/games"
+    GENRES_V3 = Configs.YAY_API_URL + "/v3/genres"
+    ANONYMOUS_CALLS_V3 = Configs.YAY_API_URL + "/v3/anonymous_calls"
+    NOTIFICATION_SETTINGS_V3 = Configs.YAY_API_URL + "/v3/notification_settings"
+    STICKER_PACKS_V3 = Configs.YAY_API_URL + "/v3/sticker_packs"
+    SNS_THUMBNAIL_V3 = Configs.YAY_API_URL + "/v3/sns_thumbnail"
+    EMAIL_VERIFICATION_URL_V3 = Configs.YAY_API_URL + "/v3/email_verification_urls"
+    BUCKETS_V3 = Configs.YAY_API_URL + "/v3/buckets"
+    ID_CHECK_V3 = Configs.YAY_API_URL + "/v3/id_check"
+    PROMOTIONS_V3 = Configs.YAY_API_URL + "/v3/promotions"
+    SKYFALL_V3 = Configs.YAY_API_URL + "/v3/skyfall"
+    GENUINE_DEVICES_V3 = Configs.YAY_API_URL + "/v3/genuine_devices"
+    LOBI_FRIENDS_V3 = Configs.YAY_API_URL + "/v3/lobi_friends"
