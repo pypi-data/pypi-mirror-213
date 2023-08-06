c8cd03c46a8ee112df26f3c00f6d3a1134dc8735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:23 +0000

    Add package routexplorer/0.0.3/routexplorer-0.0.3.tar.gz

---
+++ packages/routexplorer/routexplorer-0.0.3.tar.gz/routexplorer-0.0.3/README.md
@@ -0,0 +1,70 @@
+
+
+<p align="center">
+  <img src="https://i.imgur.com/PLB1GAQ.png">
+</p>
+
+<br>
+<br>
+
+[![Python](https://img.shields.io/badge/Python-3.0%2B-blue.svg)](https://www.python.org/downloads/release/python-300/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Development Status](https://img.shields.io/badge/Development%20Status-Alpha-orange)](https://github.com/vsg-root/RouteXplorer)
+
+RouteXplorer is a Python library developed to solve the Traveling Salesman Problem (TSP) in complete graphs. Its goal is to find the most efficient sequence of visiting nodes in a graph, minimizing the total distance traveled.
+<br>
+
+## Install
+
+```python
+$ pip install routexplorer
+```
+
+##### Try your first RouteXplorer program
+```python
+$ python
+```
+```python
+>>> from routexplorer.utils.input_reader import InputReader
+>>> from routexplorer.algorithms.BruteForceSearch import BruteForceSearch
+
+>>> # Datas of graph
+>>> graph = InputReader.read_file(input("Enter file path: ")) 
+
+>>> # Calculation of the best path and distance
+>>> best_path, best_distance = BruteForceSearch.find_best_path(graph, True, True)
+```
+##### Create a .txt file
+```bash
+4 5
+0 0 0 0 D
+0 A 0 0 0
+0 0 0 0 C
+R 0 B 0 0
+```
+```python
+$ Enter file path: file/path/file.txt
+```
+```
+100% [■■■■■■■■■■] - Generating permutations
+100% [■■■■■■■■■■] - Checking paths
+Best path: ['A', 'D', 'C', 'B'] 
+Shorter distance: (14)
+```
+
+## Key Features
+
+- Efficient Algorithms: RouteXplorer implements a variety of optimized algorithms to solve the Traveling Salesman Problem, allowing you to find solutions quickly and efficiently.
+
+- Flexible Customization: The library offers flexible options to customize the algorithm settings, allowing you to adapt the optimization process to the specific needs of your project.
+
+- Simple Integration: The library is easy to integrate into your existing Python projects, allowing you to leverage its powerful functionality without complications.
+
+
+## Contribution
+
+RouteXplorer is an open source project and we encourage community contributions. If you want to contribute, please follow the guidelines outlined in the repository's CONTRIBUTING.md file.
+
+## License
+
+RouteXplorer is distributed under the MIT license. For more information, see the LICENSE.md file.
---
+++ packages/routexplorer/routexplorer-0.0.3.tar.gz/routexplorer-0.0.3/routexplorer/algorithms/BruteForceSearch.py
@@ -0,0 +1,70 @@
+from .Algorithm import Algorithm
+from ..utils.Graph import Graph
+from ..utils.Utils import Utils
+
+
+class BruteForceSearch(Algorithm):
+    """Class implementing the Brute Force Search algorithm."""
+
+    @staticmethod
+    def __permutation(vertices: list[str], show_load: bool, depth: int = 0) -> list[list[str]]:
+        """
+        Generate all possible permutations of vertices using a recursive approach.
+
+        Args:
+            vertices (list[str]): The list of vertices to permute.
+            show_load (bool): Flag indicating whether to show load progress.
+            depth (int): The current depth of recursion.
+
+        Returns:
+            list[list[str]]: List of all possible permutations of vertices.
+        """
+        if len(vertices) == 1:
+            return [vertices]
+        result = []
+        for i in range(len(vertices)):
+            if show_load and depth == 0:
+                Utils.generate_load(len(vertices), i + 1, "Generating permutations")
+            current_vertice = vertices[i]
+            remaining_vertices = vertices[:i] + vertices[i + 1:]
+            remaining_permutations = BruteForceSearch.__permutation(remaining_vertices, show_load, depth + 1)  # Corrected method name
+            for permutation in remaining_permutations:
+                result.append([current_vertice] + permutation)
+        return result
+
+    @staticmethod
+    def find_best_path(graph: Graph, show_load: bool = False, show_result: bool = False):
+        """
+        Find the best path in the given graph using Brute Force Search.
+
+        Args:
+            graph (Graph): The graph object.
+            show_load (bool): Flag indicating whether to show load progress.
+            show_result (bool): Flag indicating whether to show the result.
+
+        Returns:
+            tuple: A tuple containing the best path and its minimum distance.
+        """
+        min_distance = float('inf')
+        best_path = None
+
+        permutations = BruteForceSearch.__permutation(graph.get_vertices(), show_load)
+
+        cont = 0
+        for permutation in permutations:
+            permutations.remove(permutation[::-1])  # Verificar se é permitido!!!
+
+            if show_load:
+                Utils.generate_load(len(permutations), cont + 1, "Checking paths")
+
+            path = ['R'] + permutation + ['R']
+            distance = graph.cust_calculate(path)
+            if distance < min_distance:
+                min_distance = distance
+                best_path = path[1:-1]
+            cont += 1
+
+        if show_result:
+            print(f"Best path: {best_path} \nShorter distance: ({min_distance})")
+
+        return best_path, min_distance
