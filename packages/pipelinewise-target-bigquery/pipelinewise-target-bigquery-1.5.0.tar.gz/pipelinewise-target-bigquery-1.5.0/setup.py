c0c6d90d93c8c9a46e2466985fdcbf5b1e192994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package pipelinewise-target-bigquery/1.5.0/pipelinewise-target-bigquery-1.5.0.tar.gz

---
+++ packages/pipelinewise-target-bigquery/pipelinewise-target-bigquery-1.5.0.tar.gz/pipelinewise-target-bigquery-1.5.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name="pipelinewise-target-bigquery",
+      version="1.5.0",
+      description="Singer.io target for loading data to BigQuery - PipelineWise compatible",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author="jmriego",
+      url='https://github.com/transferwise/pipelinewise-target-bigquery',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=["target_bigquery"],
+      install_requires=[
+          'pipelinewise-singer-python>=1,<3',
+          'google-cloud-bigquery>=2.20.0,<3.1.0',
+          'fastavro>=0.22.8,<1.4.11'
+      ],
+      extras_require={
+          "test": [
+              'pytest==7.0.1',
+              'pylint==2.13.4',
+              'pytest-cov==3.0.0',
+          ]
+      },
+      entry_points="""
+          [console_scripts]
+          target-bigquery=target_bigquery:main
+      """,
+      packages=["target_bigquery"],
+      package_data={},
+      include_package_data=True,
+)
