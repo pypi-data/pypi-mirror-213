1c6d616507535bc81aef992e6e5cab4c4e1a75d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:33 +0000

    Add package pn-probability/0.1/pn_probability-0.1.tar.gz

---
+++ packages/pn-probability/pn_probability-0.1.tar.gz/pn_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pn_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pn_probability'],
+      author='Kefah Khalil',
+      author_email='kefahstudent2018@gmail.com',
+      zip_safe=False)
