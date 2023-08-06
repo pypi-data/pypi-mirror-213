acfbf2234f5f089ea8a35e9c2c54073f576905ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:47 +0000

    Add package opower/0.0.9/opower-0.0.9.tar.gz

---
+++ packages/opower/opower-0.0.9.tar.gz/opower-0.0.9/src/opower/utilities/base.py
@@ -0,0 +1,46 @@
+"""Base class that each utility needs to extend."""
+
+
+import aiohttp
+
+
+class UtilityBase:
+    """Base class that each utility needs to extend."""
+
+    subclasses: list[type["UtilityBase"]] = []
+
+    def __init_subclass__(cls, **kwargs) -> None:
+        """Keep track of all subclass implementations."""
+        super().__init_subclass__(**kwargs)
+        cls.subclasses.append(cls)
+
+    @staticmethod
+    def name() -> str:
+        """Distinct recognizable name of the utility."""
+        raise NotImplementedError
+
+    @staticmethod
+    def subdomain() -> str:
+        """Return the opower.com subdomain for this utility."""
+        raise NotImplementedError
+
+    @staticmethod
+    def timezone() -> str:
+        """Return the timezone.
+
+        Should match the siteTimeZoneId of the API responses.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    async def login(
+        session: aiohttp.ClientSession, username: str, password: str
+    ) -> str | None:
+        """Login to the utility website.
+
+        Returns either a URL for SAML authentication with opower.com,
+        or directly handles authentication and returns None.
+
+        Any failure to login should raise ClientResponseError with status 401 or 403.
+        """
+        raise NotImplementedError
