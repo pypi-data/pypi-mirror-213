573305cfd161759e439f758fb5d70e35b28d1f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:36 +0000

    Add package alltime-athletics-python/0.17.0/alltime_athletics_python-0.17.0.tar.gz

---
+++ packages/alltime-athletics-python/alltime_athletics_python-0.17.0.tar.gz/alltime_athletics_python-0.17.0/alltime_athletics_python/app.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+from alltime_athletics_python.io import (
+    download_data,
+    import_running_only_events,
+)
+
+if __name__ == "__main__":
+    download_data()
+    df = import_running_only_events("./data")
+    today = date.today()
+    df.write_parquet(f"./dataframes/alltime_athletics_version_{today}.parquet")
+    df.write_csv(f"./dataframes/alltime_athletics_version_{today}.csv")
