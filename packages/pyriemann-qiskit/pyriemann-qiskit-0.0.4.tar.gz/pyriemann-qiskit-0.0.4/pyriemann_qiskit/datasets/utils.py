4a71c609e8b8fa8dfefcf40b3829d36178d97e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:53 +0000

    Add package pyriemann-qiskit/0.0.4/pyriemann-qiskit-0.0.4.tar.gz

---
+++ packages/pyriemann-qiskit/pyriemann-qiskit-0.0.4.tar.gz/pyriemann-qiskit-0.0.4/pyriemann_qiskit/datasets/utils.py
@@ -0,0 +1,258 @@
+from warnings import warn
+import numpy as np
+
+try:
+    from mne import io, read_events, pick_types, Epochs
+    from mne.datasets import sample
+except Exception:
+    warn("mne not available. get_mne_sample will fail.")
+from qiskit_machine_learning.datasets import ad_hoc_data
+from sklearn.datasets import make_classification
+
+
+def get_mne_sample(n_trials=10):
+    """Return sample data from the mne dataset.
+
+    ```
+    In this experiment, checkerboard patterns were presented to the subject
+    into the left and right visual field, interspersed by tones to the
+    left or right ear. The interval between the stimuli was 750 ms.
+    Occasionally a smiley face was presented at the center of the visual field.
+    The subject was asked to press a key with the right index finger
+    as soon as possible after the appearance of the face.
+    ``` [1]_
+
+    The samples returned by this method are epochs of duration 1s.
+    Only visual left and right trials are selected.
+    Data are returned filtered.
+
+    Parameters
+    ----------
+    n_trials : int (default:10)
+        Number of trials to return.
+        If -1, then all trials are returned.
+
+    Returns
+    -------
+    X : ndarray, shape (n_trials, n_channels, n_times)
+        ndarray of trials.
+    y : ndarray, shape (n_trials,)
+        Predicted target vector relative to X.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
+    References
+    ----------
+    .. [1] Available from: \
+        https://mne.tools/stable/overview/datasets_index.html
+
+    """
+    data_path = str(sample.data_path())
+
+    # Set parameters and read data
+    raw_fname = data_path + "/MEG/sample/sample_audvis_filt-0-40_raw.fif"
+    event_fname = data_path + "/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif"
+    tmin, tmax = -0.0, 1
+    event_id = dict(vis_l=3, vis_r=4)  # select only two classes
+
+    # Setup for reading the raw data
+    raw = io.Raw(raw_fname, preload=True, verbose=False)
+    raw.filter(2, None, method="iir")  # replace baselining with high-pass
+    events = read_events(event_fname)
+
+    raw.info["bads"] = ["MEG 2443"]  # set bad channels
+    picks = pick_types(
+        raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads"
+    )
+
+    # Read epochs
+    epochs = Epochs(
+        raw,
+        events,
+        event_id,
+        tmin,
+        tmax,
+        proj=False,
+        picks=picks,
+        baseline=None,
+        preload=True,
+        verbose=False,
+    )
+
+    X = epochs.get_data()[:n_trials, :-1, :-1]
+    y = epochs.events[:n_trials, -1]
+
+    return X, y
+
+
+def get_qiskit_dataset():
+    """Return qiskit dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
+    Returns
+    -------
+    X : ndarray, shape (n_samples, n_features)
+        Input vector, where `n_samples` is the number of samples and
+        `n_features` is the number of features.
+    y: ndarray, shape (n_samples,)
+        Predicted target vector relative to X.
+
+    """
+
+    feature_dim = 2
+    X, _, _, _ = ad_hoc_data(
+        training_size=30, test_size=0, n=feature_dim, gap=0.3, plot_data=False
+    )
+
+    y = np.concatenate(([0] * 30, [1] * 30))
+
+    return (X, y)
+
+
+def get_linearly_separable_dataset():
+    """Return a linearly separable dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
+    Returns
+    -------
+    X : ndarray, shape (n_samples, n_features)
+        Input vector, where `n_samples` is the number of samples and
+        `n_features` is the number of features.
+    y: ndarray, shape (n_samples,)
+        Predicted target vector relative to X.
+
+    """
+    X, y = make_classification(
+        n_features=2,
+        n_redundant=0,
+        n_informative=2,
+        random_state=1,
+        n_clusters_per_class=1,
+    )
+    rng = np.random.RandomState(2)
+    X += 2 * rng.uniform(size=X.shape)
+
+    return (X, y)
+
+
+def get_feature_dimension(dataset):
+    # This code is part of Qiskit.
+    #
+    # (C) Copyright IBM 2018, 2021.
+    #
+    # This code is licensed under the Apache License, Version 2.0. You may
+    # obtain a copy of this license in the LICENSE.txt file
+    # in the root directory of this source tree or at
+    # http://www.apache.org/licenses/LICENSE-2.0.
+    #
+    # Any modifications or derivative works of this code must retain this
+    # copyright notice, and modified files need to carry a notice indicating
+    # that they have been altered from the originals.
+    """
+    Return the feature dimension of a given dataset.
+
+    Parameters
+    ----------
+    dataset : dict
+        key is the class name and value is the data.
+
+    Returns
+    -------
+        n_features : int
+            The feature dimension, -1 denotes no data in the dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.2
+
+    Raises
+    -------
+    TypeError
+        invalid data set
+
+    """
+    if not isinstance(dataset, dict):
+        raise TypeError("Dataset is not formatted as a dict. Please check it.")
+
+    for v in dataset.values():
+        if not isinstance(v, np.ndarray):
+            v = np.asarray(v)
+        return v.shape[1]
+
+    return -1
+
+
+class MockDataset:
+    """A dataset with mock data.
+
+    Parameters
+    ----------
+    dataset_gen : Callable[[], (List, List)]
+        A function to generate datasets.
+        The function accepts no parameters and returns a pair of lists.
+        The first list contains the samples, the second one the labels.
+    n_subjects : int
+        The number of subjects in the dataset.
+        A dataset will be generated for all subjects using the handler
+        `dataset_gen`.
+
+    Attributes
+    ----------
+    code_ : str
+        The code of the dataset.
+        The code of the dataset is also the string representation
+        of the dataset.
+    data_ : Dict
+        A dictionnary representing the dataset. Ex:
+        ```
+        {
+            subject1: (samples1, labels1),
+            subject2: (samples2, labels2),
+            ...
+        }
+        ```
+    subjects_ : List[int]
+        The subjects of the dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.3
+
+    """
+
+    def __init__(self, dataset_gen, n_subjects: int):
+        self.code_ = "MockDataset"
+        self.subjects_ = range(n_subjects)
+        self.data_ = {}
+        for subject in self.subjects_:
+            self.data_[subject] = dataset_gen()
+
+    def get_data(self, subject):
+        """
+        Returns the data of a subject.
+
+        Parameters
+        ----------
+        subject : int
+            A subject in the list of subjects `subjects_`.
+
+        Returns
+        -------
+        data : the subject's data.
+
+        Notes
+        -----
+        .. versionadded:: 0.0.3
+        """
+        return self.data_[subject]
+
+    def __str__(self) -> str:
+        return self.code_
---
+++ packages/pyriemann-qiskit/pyriemann-qiskit-0.0.4.tar.gz/pyriemann-qiskit-0.0.4/tests/test_classification.py
@@ -0,0 +1,241 @@
+import pytest
+import numpy as np
+from pyriemann.classification import TangentSpace
+from pyriemann.estimation import XdawnCovariances
+from pyriemann_qiskit.classification import (
+    QuanticSVM,
+    QuanticVQC,
+    QuanticMDM,
+    QuantumClassifierWithDefaultRiemannianPipeline,
+)
+from pyriemann_qiskit.datasets import get_mne_sample
+from pyriemann_qiskit.utils.filtering import NaiveDimRed
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from operator import itemgetter
+
+
+@pytest.mark.parametrize(
+    "estimator",
+    [
+        make_pipeline(
+            XdawnCovariances(nfilter=1),
+            TangentSpace(),
+            NaiveDimRed(),
+            QuanticSVM(quantum=False),
+        ),
+        make_pipeline(XdawnCovariances(nfilter=1), QuanticMDM(quantum=False)),
+        QuantumClassifierWithDefaultRiemannianPipeline(nfilter=1, shots=None),
+    ],
+)
+def test_get_set_params(estimator):
+    skf = StratifiedKFold(n_splits=2)
+    X, y = get_mne_sample()
+    scr = cross_val_score(
+        estimator, X, y, cv=skf, scoring="roc_auc", error_score="raise"
+    )
+    assert scr.mean() > 0
+
+
+def test_vqc_classical_should_return_value_error():
+    with pytest.raises(ValueError):
+        QuanticVQC(quantum=False)
+
+
+def test_qsvm_init_quantum_wrong_token():
+    with pytest.raises(Exception):
+        q = QuanticSVM(quantum=True, q_account_token="INVALID")
+        q._init_quantum()
+
+
+@pytest.mark.parametrize("quantum", [False, True])
+def test_qsvm_init(quantum):
+    """Test init of quantum classifiers"""
+
+    q = QuanticSVM(quantum=quantum)
+    q._init_quantum()
+    assert q.quantum == quantum
+    assert hasattr(q, "_backend") == quantum
+
+    # A provider is only assigned when running on a real quantum backend
+    assert not hasattr(q, "_provider")
+
+
+class BinaryTest:
+    def prepare(self, n_samples, n_features, quantum_instance, type):
+        self.n_classes = 2
+        self.n_samples = n_samples
+        self.n_features = n_features
+        self.quantum_instance = quantum_instance
+        self.type = type
+        self.class_len = n_samples // self.n_classes
+
+    def test(self, get_dataset):
+        # there is no __init__ method with pytest
+        n_samples, n_features, quantum_instance, type = itemgetter(
+            "n_samples", "n_features", "quantum_instance", "type"
+        )(self.get_params())
+        self.prepare(n_samples, n_features, quantum_instance, type)
+        self.samples, self.labels = get_dataset(
+            self.n_samples, self.n_features, self.n_classes, self.type
+        )
+        self.additional_steps()
+        self.check()
+
+    def get_params(self):
+        raise NotImplementedError
+
+    def additional_steps(self):
+        raise NotImplementedError
+
+    def check(self):
+        raise NotImplementedError
+
+
+class TestQSVMSplitClasses(BinaryTest):
+    """Test _split_classes method of quantum classifiers"""
+
+    def get_params(self):
+        quantum_instance = QuanticSVM(quantum=False)
+        return {
+            "n_samples": 100,
+            "n_features": 9,
+            "quantum_instance": quantum_instance,
+            "type": "rand",
+        }
+
+    def additional_steps(self):
+        # As fit method is not called here, classes_ is not set.
+        # so we need to provide the classes ourselves.
+        self.quantum_instance.classes_ = range(0, self.n_classes)
+        self.x_class1, self.x_class0 = self.quantum_instance._split_classes(
+            self.samples, self.labels
+        )
+
+    def check(self):
+        assert np.shape(self.x_class1) == (self.class_len, self.n_features)
+        assert np.shape(self.x_class0) == (self.class_len, self.n_features)
+
+
+class BinaryFVT(BinaryTest):
+    def additional_steps(self):
+        self.quantum_instance.fit(self.samples, self.labels)
+        self.prediction = self.quantum_instance.predict(self.samples)
+        print(self.labels, self.prediction)
+
+
+class TestClassicalSVM(BinaryFVT):
+    """Perform functional validation testing of Quantic SVM"""
+
+    def get_params(self):
+        quantum_instance = QuanticSVM(quantum=False, verbose=False)
+        return {
+            "n_samples": 100,
+            "n_features": 9,
+            "quantum_instance": quantum_instance,
+            "type": "bin",
+        }
+
+    def check(self):
+        assert (
+            self.prediction[: self.class_len].all() == self.quantum_instance.classes_[0]
+        )
+        assert (
+            self.prediction[self.class_len :].all() == self.quantum_instance.classes_[1]
+        )
+
+
+class TestQuanticSVM(TestClassicalSVM):
+    """Perform SVC on a simulated quantum computer.
+    This test can also be run on a real computer by providing a qAccountToken
+    To do so, you need to use your own token, by registering on:
+    https://quantum-computing.ibm.com/
+    Note that the "real quantum version" of this test may also take some time.
+    """
+
+    def get_params(self):
+        quantum_instance = QuanticSVM(quantum=True, verbose=False)
+        return {
+            "n_samples": 10,
+            "n_features": 4,
+            "quantum_instance": quantum_instance,
+            "type": "bin",
+        }
+
+
+class TestQuanticPegasosSVM(TestClassicalSVM):
+    """Same as TestQuanticSVM, expect it uses
+    PegasosQSVC instead of QSVC implementation.
+    """
+
+    def get_params(self):
+        quantum_instance = QuanticSVM(quantum=True, verbose=False, pegasos=True)
+        return {
+            "n_samples": 10,
+            "n_features": 4,
+            "quantum_instance": quantum_instance,
+            "type": "bin",
+        }
+
+
+class TestQuanticVQC(BinaryFVT):
+    """Perform VQC on a simulated quantum computer"""
+
+    def get_params(self):
+        quantum_instance = QuanticVQC(verbose=False)
+        # To achieve testing in a reasonnable amount of time,
+        # we will lower the size of the feature and the number of trials
+        return {
+            "n_samples": 6,
+            "n_features": 4,
+            "quantum_instance": quantum_instance,
+            "type": "rand",
+        }
+
+    def check(self):
+        # Considering the inputs, this probably make no sense to test accuracy.
+        # Instead, we could consider this test as a canary test
+        assert len(self.prediction) == len(self.labels)
+
+
+class TestClassicalMDM(BinaryFVT):
+    """Test the classical version of MDM is used
+    when quantum is false
+    https://quantum-computing.ibm.com/
+    Note that the "real quantum version" of this test may also take some time.
+    """
+
+    def get_params(self):
+        quantum_instance = QuanticMDM(quantum=False, verbose=False)
+        return {
+            "n_samples": 100,
+            "n_features": 9,
+            "quantum_instance": quantum_instance,
+            "type": "bin_cov",
+        }
+
+    def check(self):
+        assert (
+            self.prediction[: self.class_len].all() == self.quantum_instance.classes_[0]
+        )
+        assert (
+            self.prediction[self.class_len :].all() == self.quantum_instance.classes_[1]
+        )
+
+
+class TestQuantumClassifierWithDefaultRiemannianPipeline(BinaryFVT):
+    """Functional testing for riemann quantum classifier."""
+
+    def get_params(self):
+        quantum_instance = QuantumClassifierWithDefaultRiemannianPipeline(
+            params={"verbose": False}
+        )
+        return {
+            "n_samples": 4,
+            "n_features": 4,
+            "quantum_instance": quantum_instance,
+            "type": None,
+        }
+
+    def check(self):
+        assert len(self.prediction) == len(self.labels)
