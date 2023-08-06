a1e32dc31ba05e699285576910ab755bdf89c4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:24 +0000

    Add package shellx/1.0/shellx-1.0.tar.gz

---
+++ packages/shellx/shellx-1.0.tar.gz/shellx-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='shellx',
+    version='1.0',
+    author='Chowdhury Faizal Ahammed',
+    description='All in one Tool for generating reverse shells easily and automating the boring stuff like URL encoding the command and setting up a listener.',
+    install_requires=['pyperclip', 'colorama', 'readchar', 'netifaces']
+)
