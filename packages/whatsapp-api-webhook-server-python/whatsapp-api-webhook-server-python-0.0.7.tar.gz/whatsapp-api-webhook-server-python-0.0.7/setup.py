08623193286e50a7612fbc0c098b549d9b1108a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:40 +0000

    Add package whatsapp-api-webhook-server-python/0.0.7/whatsapp-api-webhook-server-python-0.0.7.tar.gz

---
+++ packages/whatsapp-api-webhook-server-python/whatsapp-api-webhook-server-python-0.0.7.tar.gz/whatsapp-api-webhook-server-python-0.0.7/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="UTF-8") as file:
+    long_description = file.read()
+
+setup(
+    name="whatsapp-api-webhook-server-python",
+    version="0.0.7",
+    description=(
+        "This library helps you easily create"
+        " a Python server endpoint to receive WhatsApp message webhooks."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="GREEN API",
+    author_email="support@green-api.com",
+    url="https://github.com/green-api/whatsapp-api-webhook-server-python",
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    license=(
+        "Creative Commons Attribution-NoDerivatives 4.0 International"
+        " (CC BY-ND 4.0)"
+    ),
+    install_requires=["tornado==6.3.2"],
+    python_requires=">=3.7"
+)
