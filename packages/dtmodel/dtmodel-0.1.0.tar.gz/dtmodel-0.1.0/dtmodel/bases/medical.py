0ccefaa5020cac60cbd6b8ba71f878947a597fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:42 +0000

    Add package dtmodel/0.1.0/dtmodel-0.1.0.tar.gz

---
+++ packages/dtmodel/dtmodel-0.1.0.tar.gz/dtmodel-0.1.0/dtmodel/bases/medical.py
@@ -0,0 +1,31 @@
+__all__ = [
+        'MainComplaint',
+        'SOAP',
+        'Intro',
+        'TreatmentResponse'
+]
+
+from dtfield import dataclass, Validator, ContextBase
+
+@dataclass
+class MainComplaint(ContextBase):
+    main_complaint: str = Validator(default=None)
+    
+    
+@dataclass
+class Intro(ContextBase):
+    intro: str = Validator(default=None, repr=False)
+
+@dataclass
+class SOAP(ContextBase):
+    subjective: str = Validator(default=None, repr=False)
+    objective: str = Validator(default=None, repr=False)
+    assessment: str = Validator(default=None, repr=False)
+    plan: str = Validator(default=None, repr=False)
+    
+    
+@dataclass
+class TreatmentResponse(ContextBase):
+    treatment: str = Validator(default=None, repr=False)
+    response: str = Validator(default=None, repr=False)
+    
---
+++ packages/dtmodel/dtmodel-0.1.0.tar.gz/dtmodel-0.1.0/dtmodel/models/medical.py
@@ -0,0 +1,49 @@
+__all__ = [
+        'MedicalVisit',
+        'Event'
+]
+
+import datetime
+from dtfield import context_model, dataclass, InitVar, re, Validator, Parser, descdataclass
+from ..bases import *
+from ..functions import *
+
+
+@context_model
+@descdataclass
+class MedicalVisit(SelfKey, Creator, TreatmentResponse, SOAP, Intro, MainComplaint, Date, PatientKey):
+    pass
+
+
+@context_model
+@descdataclass
+class Event(SelfKey, Creator, Age, RequiredName, PatientKey):
+    when: str = Validator(auto=lambda self: self.patient.years, db=False)
+    
+    @property
+    def bdate(self):
+        return self.patient.person.bdate
+    
+    def __post_init__(self):
+        if not self.age:
+            if self.when:
+                year = re.match(r'^[1-2][09]\d\d', self.when)
+                if year:
+                    self.age = years(self.bdate, datetime.date(int(year.group()), self.bdate.month, self.bdate.day))
+                else:
+                    month_year = re.match(r'^([0-3]?\d)[.\-/]([1-2][09]\d\d)', self.when)
+                    if month_year:
+                        self.age = years(self.bdate, datetime.date(int(month_year.group(2)), int(month_year.group(1)),
+                                                                   self.bdate.day))
+                    else:
+                        day_month_year = re.match(r'^([0]?[1-9]|2[0-9]|3[0-1])[.\-/]([0]?[1-9]|10|11|12)[.\-/]([1-2][09]\d\d)', self.when)
+                        if day_month_year:
+                            self.age = years(self.bdate,
+                                             datetime.date(int(day_month_year.group(3)), int(day_month_year.group(2)),
+                                                           int(day_month_year.group(1))))
+                        else:
+                            self.age = Parser.get(self.when, float)
+                        
+                        
+                    
+    
