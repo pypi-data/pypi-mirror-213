1ef6c4dce57177697fb1e15da6d825dd97df8fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:39 +0000

    Add package ANBUtils/1.5.5/ANBUtils-1.5.5.tar.gz

---
+++ packages/ANBUtils/ANBUtils-1.5.5.tar.gz/ANBUtils-1.5.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ANBUtils",
+    version="1.5.5",
+    packages=find_packages(),
+    author="redbson",
+    author_email="redbson@gmail.com",
+    python_requires='>=3.8',
+    install_requires=[
+        "matplotlib>=3.0.0",
+        "numpy>=1.0.0",
+        "pandas>=1.0.0",
+        "pymongo>=4.0.2",
+        "requests>=2.0.0",
+        "matplotlib>=3.0.0",
+
+    ]
+)
