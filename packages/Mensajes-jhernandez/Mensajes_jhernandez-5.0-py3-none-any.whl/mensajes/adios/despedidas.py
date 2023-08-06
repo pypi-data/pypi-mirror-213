13622933dce07e2b04f081b5d858e27f0fe75476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:51 +0000

    Add package Mensajes-jhernandez/5.0/Mensajes_jhernandez-5.0-py3-none-any.whl

---
+++ packages/Mensajes-jhernandez/Mensajes_jhernandez-5.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,9 @@
+def despedir():
+    print("Adios, me despido desde despedidas.despedir()")
+
+def nmdn():
+    print("Nada más de nuevo desde despedidas.nmdn()")
+
+class Despedida:
+    def __init__(self):
+        print("Adios, me despido desde Despedida.__init__()")
