798c47c37338e6d356510f61aa4a293cb537850f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:53 +0000

    Add package nefarious/0.0.0/nefarious-0.0.0.tar.gz

---
+++ packages/nefarious/nefarious-0.0.0.tar.gz/nefarious-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='nefarious',
+      version='0.0.0',
+      description='placeholder',
+      url='http://github.com/SK/nefarious',
+      author='stephen king',
+      author_email='sking@gmail.com',
+      license='MIT',
+      packages=['nefarious'],
+      zip_safe=False)
