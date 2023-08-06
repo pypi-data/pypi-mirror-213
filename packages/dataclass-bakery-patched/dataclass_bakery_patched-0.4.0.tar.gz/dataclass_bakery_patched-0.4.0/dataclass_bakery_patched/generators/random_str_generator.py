69a6fbc2f475e6d67a0972033fd0e08d36efa0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:48 +0000

    Add package dataclass-bakery-patched/0.4.0/dataclass_bakery_patched-0.4.0.tar.gz

---
+++ packages/dataclass-bakery-patched/dataclass_bakery_patched-0.4.0.tar.gz/dataclass_bakery_patched-0.4.0/dataclass_bakery_patched/generators/random_str_generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+from dataclass_bakery_patched.generators import defaults
+from dataclass_bakery_patched.generators.random_generator import RandomGenerator
+
+
+class RandomStrGenerator(RandomGenerator):
+    """
+    Class to generate random string
+    """
+
+    def generate(self, *args, **kwargs) -> str:
+        max_length = kwargs.get(defaults.MAX_LENGTH_ARG, defaults.MAX_STR_LENGTH)
+        characters = string.ascii_uppercase + string.ascii_lowercase
+        random_choices = random.choices(characters, k=max_length)
+        random_str = "".join(random_choices)
+        return random_str
