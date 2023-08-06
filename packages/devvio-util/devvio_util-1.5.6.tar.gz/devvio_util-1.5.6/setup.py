9491297bbe06afd722c6a748fce7310d2e48c85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:44 +0000

    Add package devvio-util/1.5.6/devvio_util-1.5.6.tar.gz

---
+++ packages/devvio-util/devvio_util-1.5.6.tar.gz/devvio_util-1.5.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='devvio_util',
+      version='1.5.6',
+      long_description=open('README.txt').read(),
+      long_description_content_type='text/markdown',
+      description='Utility to be used inside Devvio projects',
+      author='Mateus Sens',
+      author_email='mateus.sens@tarmac.io',
+      license='Devvio',
+      packages=['devvio_util', 'devvio_util/primitives'],
+      install_requires=['flake8==6.0.0', 'cryptography==41.0.1'],
+      zip_safe=False
+      )
