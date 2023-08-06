1bd76ba1f80eebc4ae5ff492becd251f4680739b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:04 +0000

    Add package csle-system-identification/0.2.13/csle_system_identification-0.2.13-py3-none-any.whl

---
+++ packages/csle-system-identification/csle_system_identification-0.2.13-py3-none-any.whl/csle_system_identification/constants/constants.py
@@ -0,0 +1,28 @@
+
+
+class EMULATOR:
+    """
+    String constants related to the emulator
+    """
+    MAXIMUM_TRACES_FOR_DATA_COLLECTION_JOB = 3
+    INFINITY_KL_DIVERGENCE_PLACEHOLDER = 1000000
+
+
+class SYSTEM_IDENTIFICATION:
+    CONDITIONAL_DISTRIBUTIONS = "conditional_distributions"
+    METRICS = "metrics"
+
+
+class EXPECTATION_MAXIMIZATION:
+    """
+    String constants related to the expectation maximization algorithm
+    """
+    NUM_MIXTURES_PER_CONDITIONAL = "num_mixtures_per_conditional"
+
+
+class GAUSSIAN_PROCESS_REGRESSION:
+    """
+    String constants related to the gaussian process regression algorithm
+    """
+    LEARNING_RATE = "learning_rate"
+    TRAINING_ITERATIONS = "training_iterations"
