e6d3161e2147670cf354cf07615f4f4d9d4be2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:28 +0000

    Add package netbox-cisco-maintenance/0.0.140/netbox-cisco-maintenance-0.0.140.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.140.tar.gz/netbox-cisco-maintenance-0.0.140/netbox_cisco_support_plugin/tables.py
@@ -0,0 +1,23 @@
+import django_tables2 as tables
+
+from netbox.tables import BaseTable, NetBoxTable, columns
+from .models import CiscoSupport, CiscoDeviceTypeSupport
+
+
+class CiscoSupportTable(BaseTable):
+    device = tables.Column(linkify=True)
+
+    class Meta(BaseTable.Meta):
+        model = CiscoSupport
+        # fmt: off
+        fields = (
+            "device", "recommended_release", "desired_release", "current_release", "desired_release_status",
+            "current_release_status", "api_status", "sr_no_owner", "is_covered", "contract_supplier",
+            "coverage_end_date", "service_line_descr", "service_contract_number", "warranty_end_date",
+            "warranty_type",
+        )
+        default_columns = (
+            "device", "recommended_release", "desired_release", "current_release", "sr_no_owner",
+            "is_covered", "contract_supplier", "coverage_end_date", "service_line_descr",
+        )
+        # fmt: on
