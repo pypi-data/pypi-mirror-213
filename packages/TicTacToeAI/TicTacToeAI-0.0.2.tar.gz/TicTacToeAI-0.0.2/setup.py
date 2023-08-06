e33efc1467821fa7c104c0a528d92e5da488ae0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package TicTacToeAI/0.0.2/TicTacToeAI-0.0.2.tar.gz

---
+++ packages/TicTacToeAI/TicTacToeAI-0.0.2.tar.gz/TicTacToeAI-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='TicTacToeAI',
+    version='0.0.2',
+    description='Tic Tac Toe with AI.',
+    author= 'Ujjwal Reddy K S',
+    url = 'https://github.com/ujjwalredd/Tic-Tac-Toe-with-AI',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['MinMax', 'Diffcult', 'AI'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['TicTacToeAI'],
+    package_dir={'':'src'},
+    install_requires = [
+        'numpy'
+    ]
+)
