12dd7904569ab7b2421f8d036f3bd52db26fc6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:21 +0000

    Add package PySimplePolls/1.0.0/PySimplePolls-1.0.0-py3-none-any.whl

---
+++ packages/PySimplePolls/PySimplePolls-1.0.0-py3-none-any.whl/PySimplePolls/SimplePolls.py
@@ -0,0 +1,59 @@
+import requests
+
+def create(question, options, api_key, background='#228C22', foreground='#613B16', text='#FFFFFF', max_ip='1'):
+    url = "https://polls.vishok.tech/api/create"
+    params = {
+        "question": question,
+        "options": options,
+        "background": background,
+        "foreground": foreground,
+        "text": text,
+        "max": max_ip,
+        "api_key": api_key
+    }
+    response = requests.get(url, params=params)
+    response = response.json()
+    if response['message'] == 'success':
+        return response['poll_id']
+    else:
+        return response['message'], response['error']
+    
+def find(pid):
+    url = "https://polls.vishok.tech/api/poll"
+    params = {
+        "pid": pid
+    }
+    response = requests.get(url, params=params)
+    response = response.json()
+    if response['message'] == 'success':
+        return response
+    else:
+        return response['message'], response['error']
+
+def vote(pid, vote):
+    url = "https://polls.vishok.tech/api/vote"
+    params = {
+        "pid": pid,
+        "vote": vote
+    }
+    response = requests.get(url, params=params)
+    response = response.json()
+    if response['message'] == 'success':
+        return response
+    else:
+        return response['message'], response['error']
+
+def delete(pid, api_key):
+    url = "https://polls.vishok.tech/api/delete"
+    params = {
+        "pid": pid,
+        "api_key": api_key
+    }
+    response = requests.get(url, params=params)
+    response = response.json()
+    if response['message'] == 'success':
+        return True
+    else:
+        return response['message'], response['error']
+
+
