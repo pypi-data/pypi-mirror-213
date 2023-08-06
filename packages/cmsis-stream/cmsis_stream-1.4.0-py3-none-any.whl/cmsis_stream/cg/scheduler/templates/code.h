8cf32aafd134dd2d5fc783b3cec465803d86df1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:15 +0000

    Add package cmsis-stream/1.4.0/cmsis_stream-1.4.0-py3-none-any.whl

---
+++ packages/cmsis-stream/cmsis_stream-1.4.0-py3-none-any.whl/cmsis_stream/cg/scheduler/templates/code.h
@@ -0,0 +1,48 @@
+/*
+
+Generated with CMSIS-DSP Compute Graph Scripts.
+The generated code is not covered by CMSIS-DSP license.
+
+The support classes and code is covered by CMSIS-DSP license.
+
+*/
+
+#ifndef _{{config.schedulerCFileName |replace(".h","")|upper()}}_H_ 
+#define _{{config.schedulerCFileName |replace(".h","")|upper()}}_H_
+
+{% macro optionalargs() -%}
+{% if config.cOptionalArgs %},{{config.cOptionalArgs}}{% endif %}
+{% endmacro -%}
+
+{% if config.CAPI -%}
+#ifdef   __cplusplus
+extern "C"
+{
+#endif
+{% endif %}
+
+{% if config.eventRecorder %}
+#include "EventRecorder.h"
+
+#define EvtSched 0x01 
+
+#define Evt_Scheduler   EventID (EventLevelAPI,   EvtSched, 0x00)
+#define Evt_Node        EventID (EventLevelAPI,   EvtSched, 0x01)
+#define Evt_Error       EventID (EventLevelError,   EvtSched, 0x02)
+
+{% endif %}
+
+{% if config.heapAllocation %}
+extern int init_{{config.schedName}}();
+extern void free_{{config.schedName}}();
+{% endif %}
+extern uint32_t {{config.schedName}}(int *error{{optionalargs()}});
+
+{% if config.CAPI -%}
+#ifdef   __cplusplus
+}
+#endif
+{% endif %}
+
+#endif
+
