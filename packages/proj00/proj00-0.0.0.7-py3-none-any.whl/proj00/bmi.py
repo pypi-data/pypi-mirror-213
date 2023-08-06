4e801181cde622da5d49b8679af65db01f0a669e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package proj00/0.0.0.7/proj00-0.0.0.7-py3-none-any.whl

---
+++ packages/proj00/proj00-0.0.0.7-py3-none-any.whl/proj00/bmi.py
@@ -0,0 +1,28 @@
+
+def calculateBMI(height, weight):
+    return (weight/((height/100)**2))
+
+def BMItoString(bmi):
+    string = ''
+    if bmi <= 18.5:
+        string = '저체중'
+    elif bmi <= 22.9:
+        string = '정상'
+    elif bmi <= 24.9:
+        string = '과체중'
+    else:
+        string = '비만'
+    return string
+
+def returnMessage(bmi):
+    return f'MY BMI{bmi:f=.1f}\n나의BMI(신체질량지수)는 {bmi:f=.1f}이고, {BMItoString(bmi)}입니다.'
+
+def printBMIMessage(bmi):
+    print(returnMessage(bmi))
+
+
+def printHeightWeightMessage(height, weight):
+    bmi = calculateBMI(height, weight)
+    print(returnMessage(bmi))
+
+
---
+++ packages/proj00/proj00-0.0.0.7-py3-none-any.whl/proj00/test2.py
@@ -0,0 +1,7 @@
+def print_test2():
+    '''테스트 문장을 출력합니다.'''
+    print('Hi, This is Test2')
+
+from proj00 import bmi
+
+bmi.printHeightWeightMessage(175, 66)
