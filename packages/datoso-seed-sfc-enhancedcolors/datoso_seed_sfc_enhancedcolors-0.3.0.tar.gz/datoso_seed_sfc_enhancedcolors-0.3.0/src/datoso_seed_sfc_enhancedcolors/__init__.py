4fb0577e7a4c26b793fd869c28642bc6ba0cf6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package datoso-seed-sfc-enhancedcolors/0.3.0/datoso_seed_sfc_enhancedcolors-0.3.0.tar.gz

---
+++ packages/datoso-seed-sfc-enhancedcolors/datoso_seed_sfc_enhancedcolors-0.3.0.tar.gz/datoso_seed_sfc_enhancedcolors-0.3.0/src/datoso_seed_sfc_enhancedcolors/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__.py
+"""
+__all__ = ["__version__", "__author__", "__description__"]
+__version__ = "0.3.0"
+__author__ = "Lacides Miranda"
+__description__ = "Super Famicom - Enhanced Colors"
+__preffix__ = "sfc_ec"
---
+++ packages/datoso-seed-sfc-enhancedcolors/datoso_seed_sfc_enhancedcolors-0.3.0.tar.gz/datoso_seed_sfc_enhancedcolors-0.3.0/src/datoso_seed_sfc_enhancedcolors/rules.py
@@ -0,0 +1,26 @@
+from datoso_seed_sfc_enhancedcolors.dats import SFCEnhancedColorsDat
+
+rules = [
+    {
+        'name': 'SFCEnhancedColors Dat',
+        '_class': SFCEnhancedColorsDat,
+        'seed': 'sfc_ec',
+        'priority': 50,
+        'rules': [
+            {
+                'key': 'name',
+                'operator': 'contains',
+                'value': 'Enhanced Colors'
+            },
+            {
+                'key': 'name',
+                'operator': 'contains',
+                'value': 'Super Famicom'
+            }
+        ]
+    }
+]
+
+
+def get_rules():
+    return rules
