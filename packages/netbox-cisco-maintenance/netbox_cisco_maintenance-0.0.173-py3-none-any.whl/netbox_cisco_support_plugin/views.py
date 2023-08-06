36fb3975cba90b8f0a6971848820c4e4508e0ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package netbox-cisco-maintenance/0.0.173/netbox_cisco_maintenance-0.0.173-py3-none-any.whl

---
+++ packages/netbox-cisco-maintenance/netbox_cisco_maintenance-0.0.173-py3-none-any.whl/netbox_cisco_support_plugin/views.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from django.utils.translation import gettext_lazy
+from netbox.views import generic
+from . import forms, models, tables
+
+
+class CiscoDeviceSupportListView(generic.ObjectListView):
+    actions = ("export", "bulk_delete")
+    action_perms = defaultdict(set, **{"bulk_delete": {"delete"}})
+    queryset = models.CiscoDeviceSupport.objects.all()
+    table = tables.CiscoDeviceSupportTable
+
+
+"""
+class CiscoDeviceSupportView(generic.ObjectView):
+    queryset = models.CiscoDeviceSupport.objects.all()
+"""
+
+"""
+class CiscoDeviceSupportEditView(generic.ObjectEditView):
+    queryset = models.CiscoDeviceSupport.objects.all()
+    form = forms.CiscoDeviceSupportForm
+"""
+
+"""
+class CiscoDeviceSupportDeleteView(generic.ObjectDeleteView):
+    queryset = models.CiscoDeviceSupport.objects.all()
+"""
+
+
+class CiscoDeviceTypeSupportListView(generic.ObjectListView):
+    actions = ("export", "bulk_delete")
+    action_perms = defaultdict(set, **{"bulk_delete": {"delete"}})
+    queryset = models.CiscoDeviceTypeSupport.objects.all()
+    table = tables.CiscoDeviceTypeSupportTable
