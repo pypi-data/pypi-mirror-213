15f154545ad47e3064bd27fb8e13b44e95979074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:17 +0000

    Add package git-dummy/0.0.8/git-dummy-0.0.8.tar.gz

---
+++ packages/git-dummy/git-dummy-0.0.8.tar.gz/git-dummy-0.0.8/git_dummy/settings.py
@@ -0,0 +1,19 @@
+import pathlib
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    name = "dummy"
+    git_dir = pathlib.Path().cwd()
+    commits = 5
+    branches = 1
+    diverge_at = 0
+    merge = ""
+    no_subdir = False
+    constant_sha = False
+
+    class Config:
+        env_prefix = "git_dummy_"
+
+
+settings = Settings()
