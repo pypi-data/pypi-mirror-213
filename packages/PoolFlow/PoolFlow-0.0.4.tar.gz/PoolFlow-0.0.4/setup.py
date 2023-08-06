4975ec230660914ce61935b6f4f4b315e8f7c597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package PoolFlow/0.0.4/PoolFlow-0.0.4.tar.gz

---
+++ packages/PoolFlow/PoolFlow-0.0.4.tar.gz/PoolFlow-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from subprocess import run as running
+import setuptools
+import os
+
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='PoolFlow',
+      version='0.0.4',
+      description='A simple process management library',
+      url='https://victorgarric.github.io/PoolFlow/',
+      license='MIT',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      author='Victor Garric',
+      packages=setuptools.find_packages(),
+      author_email='victor.garric@gmail.com',
+      include_package_data=True,
+      keywords='',
+      install_requires=['rich'],
+      entry_points={"console_scripts": ["", ], },
+      tests_require=['pytest'],
+      zip_safe=False)
