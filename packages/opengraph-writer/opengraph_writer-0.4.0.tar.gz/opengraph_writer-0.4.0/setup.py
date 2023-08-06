8cf7d856b28097d7ef0e4a7081a60cd776a434d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:48 +0000

    Add package opengraph-writer/0.4.0/opengraph_writer-0.4.0.tar.gz

---
+++ packages/opengraph-writer/opengraph_writer-0.4.0.tar.gz/opengraph_writer-0.4.0/setup.py
@@ -0,0 +1,66 @@
+"""opengraph_writer installation script.
+"""
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+long_description = (
+    description
+) = "Lightweight OpenGraph support for writing and validating objects."
+with open(os.path.join(HERE, "README.md")) as r_file:
+    long_description = r_file.read()
+
+# store version in the init.py
+with open(os.path.join(HERE, "src", "opengraph_writer", "__init__.py")) as v_file:
+    VERSION = re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
+
+requires = [
+    "metadata_utils>=0.2.0",
+]
+tests_require = [
+    "pytest",
+    "pyramid",
+]
+testing_extras = tests_require + []
+setup(
+    name="opengraph_writer",
+    url="https://github.com/jvanasco/opengraph_writer",
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    version=VERSION,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    python_requires=">=3.6",
+    keywords="facebook opengraph open graph web pyramid",
+    install_requires=requires,
+    tests_require=requires,
+    extras_require={
+        "testing": testing_extras,
+    },
+    test_suite="tests",
+    packages=find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
+    package_data={"opengraph_writer": ["py.typed"]},
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Framework :: Pyramid",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+)
---
+++ packages/opengraph-writer/opengraph_writer-0.4.0.tar.gz/opengraph_writer-0.4.0/src/opengraph_writer/pyramid_helpers.py
@@ -0,0 +1,25 @@
+# stdlib
+from typing import TYPE_CHECKING
+
+# local
+from . import OpenGraphItem
+
+if TYPE_CHECKING:
+    from pyramid.config import Configurator  # type: ignore[import]
+    from pyramid.request import Request  # type: ignore[import]
+
+# ==============================================================================
+
+
+def new_OpenGraphItem(request: "Request") -> OpenGraphItem:
+    """simply creates a new hub"""
+    return OpenGraphItem()
+
+
+def includeme(config: "Configurator") -> None:
+    """
+    the pyramid includeme command
+    including this will automatically setup the OpenGraphItem object
+    for every request
+    """
+    config.add_request_method(new_OpenGraphItem, "opengraph_item", reify=True)
