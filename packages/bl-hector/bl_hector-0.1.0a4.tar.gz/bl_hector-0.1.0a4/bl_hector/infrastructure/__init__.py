5e7192f2ea6a88534b384fb463e12b7b3534381f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:40 +0000

    Add package bl-hector/0.1.0a4/bl_hector-0.1.0a4.tar.gz

---
+++ packages/bl-hector/bl_hector-0.1.0a4.tar.gz/bl_hector-0.1.0a4/bl_hector/infrastructure/__init__.py
@@ -0,0 +1,27 @@
+# Hector --- A collection manager.
+# Copyright © 2023 Bioneland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from bl_hector.domain.administration.enumerations import Permissions as P
+from bl_hector.domain.administration.repositories import Permissions
+from bl_hector.domain.administration.value_objects import UserId
+
+ONLY_USER = "admin"
+
+
+class FakePermissions(Permissions):
+    def is_authorized_to(self, user_id: UserId, permission: P) -> bool:
+        print(repr(user_id))
+        return bool(str(user_id))
