722dda09ac5f1007fa02e5823fab81969351520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:59 +0000

    Add package thipster/0.16.6/thipster-0.16.6-py3-none-any.whl

---
+++ packages/thipster/thipster-0.16.6-py3-none-any.whl/thipster/repository/local.py
@@ -0,0 +1,18 @@
+import os
+
+from .json import JSONRepo
+from .exceptions import ModelNotFoundError
+
+
+class LocalRepo(JSONRepo):
+    def __init__(self, path: str) -> None:
+        super().__init__()
+        self.__repo = path
+
+    def get_json(self, name: str) -> str | bytes | bytearray:
+        name += '.json'
+        try:
+            with open(os.path.join(self.__repo, name), 'r') as file:
+                return file.read()
+        except Exception:
+            raise ModelNotFoundError(name)
