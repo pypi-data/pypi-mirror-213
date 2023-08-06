a3f44717e9ba77ba62985e8a3f9663cbbaa3582c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:35 +0000

    Add package dartastlatools/0.0.5/dartastlatools-0.0.5.tar.gz

---
+++ packages/dartastlatools/dartastlatools-0.0.5.tar.gz/dartastlatools-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'Tools for DART and ASTLA project'
+LONG_DESCRIPTION = 'Several scripts about preprocessing and postprocessing (child) speech ASR input and output.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="dartastlatools", 
+        version=VERSION,
+        author="Wieke Harmsen",
+        author_email="<wiekeharmsen@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pandas', 'numpy', 'glob2==0.7', 'praat-textgrids==1.0.2'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        keywords=['python', 'child speech'],
+)
