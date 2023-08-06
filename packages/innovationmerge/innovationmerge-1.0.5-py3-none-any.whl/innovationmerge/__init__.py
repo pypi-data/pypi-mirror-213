71bc5907f406884160020def41470e1e8026f4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package innovationmerge/1.0.5/innovationmerge-1.0.5-py3-none-any.whl

---
+++ packages/innovationmerge/innovationmerge-1.0.5-py3-none-any.whl/innovationmerge/__init__.py
@@ -0,0 +1,4 @@
+from innovationmerge.src.ai.cpu.object_detection import cpuObjectDetectionTfLite, edgeObjectDetectionTfLite
+
+
+__all__ = ["cpuObjectDetectionTfLite", "edgeObjectDetectionTfLite"]
---
+++ packages/innovationmerge/innovationmerge-1.0.5-py3-none-any.whl/innovationmerge/src/ai/edge/object_detection.py
@@ -0,0 +1,6 @@
+def edge_object_detect():
+    return "object detection file"
+
+
+def edge_object_detect_mobilenet():
+    return "object detection mobilenet file"
