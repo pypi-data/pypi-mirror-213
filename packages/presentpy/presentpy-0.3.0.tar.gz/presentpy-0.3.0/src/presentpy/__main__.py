cf18df9b3e4e3d91e496fb3d855c1d2885742a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package presentpy/0.3.0/presentpy-0.3.0.tar.gz

---
+++ packages/presentpy/presentpy-0.3.0.tar.gz/presentpy-0.3.0/src/presentpy/__main__.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import click
+
+from presentpy.notebook_processor import NotebookProcessor
+from presentpy.writers.pptx_writer import PptxWriter
+
+CARRIAGE_RETURN = "\x0A"
+
+
+@click.command()
+@click.argument("notebook", type=click.Path(exists=True))
+def run_presentpy(notebook):
+    notebook_path = Path(notebook)
+    output_file_name = Path(notebook_path.stem + ".pptx")
+
+    pptx_writer = PptxWriter()
+    notebook_processor = NotebookProcessor(notebook_path, pptx_writer)
+    notebook_processor.process()
+
+    pptx_writer.save(output_file_name)
+
+    print(f"Running {notebook_path}...")
+
+
+if __name__ == "__main__":
+    run_presentpy()
