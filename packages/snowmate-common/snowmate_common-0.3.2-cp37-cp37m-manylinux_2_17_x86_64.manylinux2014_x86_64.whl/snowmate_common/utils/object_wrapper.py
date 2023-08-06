9101b3efe7f8e8b79e2f8044a1fa45b9065262b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:02 +0000

    Add package snowmate-common/0.3.2/snowmate_common-0.3.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/snowmate-common/snowmate_common-0.3.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/snowmate_common/utils/object_wrapper.py
@@ -0,0 +1,81 @@
+import copy
+from typing import AnyStr, Any
+
+JSONPICKLE_EXTERNAL_TYPE_KEY = "py/snow/external/type"
+FUNCS_INDEX = 1
+CLS_INDEX = 0
+
+
+def get_attribute_full_name(cls: Any) -> AnyStr:
+    mod = cls.__module__
+    if hasattr(cls, "__qualname__"):
+        return f"{mod}.{cls.__qualname__}"
+    return f"{mod}.{cls.__name__}"
+
+
+class ModuleWrapper:
+    def __init__(self, cls: type):
+        """
+        This object replace module because module isn't pickable
+        """
+
+
+class FunctionWrapper:
+    def __init__(self, fullname) -> None:
+        self.fullname = str(fullname)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return FunctionWrapper(*args)
+
+    def __reduce__(self):
+        # pylint: disable=import-outside-toplevel
+        from jsonpickle.unpickler import loadclass
+
+        return loadclass, (
+            self.fullname,
+        )  # this loads the fullname in the second unpickle!
+
+class ObjectWrapperBase:
+    def __new__(cls, *args):
+        new_cls = copy.copy(cls)
+        new_obj = object.__new__(new_cls) # needed in case of set attr failure
+        new_obj.funcs = {}
+        new_obj.cls = ""
+        try:
+            try:
+                for func in args[FUNCS_INDEX]:
+                    setattr(new_cls, func, args[FUNCS_INDEX][func])
+            except Exception:
+                pass
+            new_obj.__dict__ = args[FUNCS_INDEX]
+            new_obj = object.__new__(new_cls)
+            new_obj.funcs = args[FUNCS_INDEX]
+            new_obj.cls = args[CLS_INDEX]
+        except Exception:
+            pass
+        return new_obj
+
+class ObjectWrapper(ObjectWrapperBase):
+    def __new__(cls, *args):
+        return super().__new__(cls, *args)
+
+    def __init__(self, cls: Any, *_args):
+        """
+        This object wraps extranl types.
+        """
+        self.__setattr__(JSONPICKLE_EXTERNAL_TYPE_KEY, get_attribute_full_name(cls))
+
+    def __reduce__(self):
+        funcs = self.funcs
+        cls = self.cls
+        del self.funcs
+        del self.cls
+        return ObjectWrapper, (cls, funcs), self.__dict__
+
+
+class UnpicklableObject(ObjectWrapperBase):
+    def __new__(cls, *args):
+        return super().__new__(cls, *args)
+
+    def __reduce__(self):
+        return UnpicklableObject, (str(self.cls), self.funcs), self.__dict__
