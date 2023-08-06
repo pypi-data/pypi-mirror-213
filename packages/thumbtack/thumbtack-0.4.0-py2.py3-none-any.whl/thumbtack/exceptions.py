c2224cda89a4982efb93f94c41b4978204c949aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:02 +0000

    Add package thumbtack/0.4.0/thumbtack-0.4.0-py2.py3-none-any.whl

---
+++ packages/thumbtack/thumbtack-0.4.0-py2.py3-none-any.whl/thumbtack/exceptions.py
@@ -0,0 +1,13 @@
+class NoMountableVolumesError(Exception):
+    pass
+
+
+class UnexpectedDiskError(Exception):
+    pass
+
+
+class ImageNotInDatabaseError(Exception):
+    pass
+
+class DuplicateMountAttemptError(Exception):
+    pass
