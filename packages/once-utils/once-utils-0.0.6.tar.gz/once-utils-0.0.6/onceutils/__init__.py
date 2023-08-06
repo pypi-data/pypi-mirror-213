e2d68ee3c3d6261ac6ea9a5472b1cb704ee9bb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package once-utils/0.0.6/once-utils-0.0.6.tar.gz

---
+++ packages/once-utils/once-utils-0.0.6.tar.gz/once-utils-0.0.6/onceutils/__init__.py
@@ -0,0 +1,7 @@
+from ._convert import *
+from ._pymodule import *
+from ._cmd import *
+from ._iter import *
+from ._iter import filter2list as filter
+from ._random import *
+from .ex_dict import *
---
+++ packages/once-utils/once-utils-0.0.6.tar.gz/once-utils-0.0.6/onceutils/xjson/__init__.py
@@ -0,0 +1,7 @@
+import json
+
+from onceutils.xjson.encoder import XJSONEncoder
+from onceutils.xjson._common import *
+
+# dumps = xjson.dumps
+# dumps.__dict__.cls=XJSONEncoder
