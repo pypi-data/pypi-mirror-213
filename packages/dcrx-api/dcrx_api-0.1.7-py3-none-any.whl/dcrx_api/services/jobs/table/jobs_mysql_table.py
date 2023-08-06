d38e9d27207af3b733a74c5232fb56a220ff4e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:42 +0000

    Add package dcrx-api/0.1.7/dcrx_api-0.1.7-py3-none-any.whl

---
+++ packages/dcrx-api/dcrx_api-0.1.7-py3-none-any.whl/dcrx_api/services/jobs/table/jobs_mysql_table.py
@@ -0,0 +1,62 @@
+import sqlalchemy
+import uuid
+from dcrx_api.database.table_types import TableTypes
+
+
+class JobsMySQLTable:
+
+    def __init__(
+        self, 
+        jobs_table_name: str
+    ) -> None:
+        self.table = sqlalchemy.Table(
+            jobs_table_name,
+            sqlalchemy.MetaData(),
+            sqlalchemy.Column(
+                'id', 
+                sqlalchemy.String(36),
+                primary_key=True,
+                default=uuid.uuid4
+            ),
+            sqlalchemy.Column(
+                'name',
+                sqlalchemy.TEXT
+            ),
+            sqlalchemy.Column(
+                'image',
+                sqlalchemy.TEXT
+            ),
+            sqlalchemy.Column(
+                'tag',
+                sqlalchemy.TEXT
+            ),
+            sqlalchemy.Column(
+                'status',
+                sqlalchemy.TEXT
+            ),
+            sqlalchemy.Column(
+                'context',
+                sqlalchemy.TEXT
+            )
+        )
+
+        self.columns = {
+            'id': self.table.c.id,
+            'name': self.table.c.name,
+            'image': self.table.c.image,
+            'tag': self.table.c.tag,
+            'status': self.table.c.status,
+            'context': self.table.c.context,
+        }
+
+        self.types_map = {
+            'id': lambda value: str(value),
+            'name': lambda value: str(value),
+            'image': lambda value: str(value),
+            'tag': lambda value: str(value),
+            'status': lambda value: str(value),
+            'context': lambda value: str(value)
+        }
+
+        self.table_type = TableTypes.MYSQL
+
