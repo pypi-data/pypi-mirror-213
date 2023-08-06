79606cb5b571b7031ddad03b0301b51d644cbbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:34 +0000

    Add package pmsm-pm-temp-predict/0.0.1/pmsm_pm_temp_predict-0.0.1.tar.gz

---
+++ packages/pmsm-pm-temp-predict/pmsm_pm_temp_predict-0.0.1.tar.gz/pmsm_pm_temp_predict-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Permanent magnet temperature prediction'
+LONG_DESCRIPTION = 'This package provides an API to predict the temperature of the permanent magnet located on the rotor of the PMS motor based on the motor voltage, current, and temperature of the coolant and stator windings.'
+
+# Setting up
+setup(
+        name="pmsm_pm_temp_predict", 
+        version=VERSION,
+        author="Oleksandr",
+        author_email="<oleksandr.ohlashennyi.knm.2020@lpnu.ua>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pandas', 'sklearn', 'numpy'],
+        
+        keywords=['python', 'pmsm', 'motor', 'temperatue', 'prediction'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
