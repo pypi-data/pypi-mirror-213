443e6ea5b908702b2826b4839059b3c9773430f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:15 +0000

    Add package devcycle-python-server-sdk/2.0.0/devcycle-python-server-sdk-2.0.0.tar.gz

---
+++ packages/devcycle-python-server-sdk/devcycle-python-server-sdk-2.0.0.tar.gz/devcycle-python-server-sdk-2.0.0/devcycle_python_sdk/__init__.py
@@ -0,0 +1,4 @@
+# Simplify imports for the SDK entry point objects
+
+from devcycle_python_sdk.dvc_options import DevCycleCloudOptions
+from devcycle_python_sdk.dvc_cloud_client import DevCycleCloudClient
