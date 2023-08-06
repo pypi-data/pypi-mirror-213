a56f1a813178b18ca7a2affc2e6853ad83305359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:53 +0000

    Add package crispy-parser/1.0.0/crispy-parser-1.0.0.tar.gz

---
+++ packages/crispy-parser/crispy-parser-1.0.0.tar.gz/crispy-parser-1.0.0/README.md
@@ -0,0 +1,4 @@
+## crispy
+
+`crispy` is my take on parsing command-line arguments: it literally is a dead simple tool to define your own keys to
+gather variables systematically.
---
+++ packages/crispy-parser/crispy-parser-1.0.0.tar.gz/crispy-parser-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='crispy-parser',
+    version='1.0.0',
+    packages=['crispy', 'crispy.tests'],
+    url='https://github.com/fybx/crispy',
+    license='GNU LGPL-v2.1',
+    author='fybalaban',
+    author_email='fybalaban@fybx.dev',
+    description='Crispy is a simple command line argument parser, ready to be integrated to any project of any size!'
+)
