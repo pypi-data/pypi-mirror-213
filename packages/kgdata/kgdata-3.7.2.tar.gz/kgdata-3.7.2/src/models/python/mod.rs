91793d82f64bbc6563b3618ddb2824fbda47fa01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:45 +0000

    Add package kgdata/3.7.2/kgdata-3.7.2.tar.gz

---
+++ packages/kgdata/kgdata-3.7.2.tar.gz/kgdata-3.7.2/src/models/python/mod.rs
@@ -0,0 +1,28 @@
+use pyo3::prelude::*;
+
+pub mod entity;
+pub mod iterators;
+pub mod value;
+
+#[allow(dead_code)]
+pub(crate) fn register(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    let submodule = PyModule::new(py, "models")?;
+
+    m.add_submodule(submodule)?;
+
+    submodule.add_class::<self::entity::PyEntity>()?;
+    submodule.add_class::<self::entity::PyStatementView>()?;
+    submodule.add_class::<self::value::PyValueView>()?;
+    submodule.add_class::<self::value::PyValue>()?;
+    submodule.add_class::<self::value::PyEntityId>()?;
+    submodule.add_class::<self::value::PyTime>()?;
+    submodule.add_class::<self::value::PyQuantity>()?;
+    submodule.add_class::<self::value::PyGlobeCoordinate>()?;
+    submodule.add_class::<self::value::PyMonolingualText>()?;
+
+    py.import("sys")?
+        .getattr("modules")?
+        .set_item("kgdata.core.models", submodule)?;
+
+    Ok(())
+}
