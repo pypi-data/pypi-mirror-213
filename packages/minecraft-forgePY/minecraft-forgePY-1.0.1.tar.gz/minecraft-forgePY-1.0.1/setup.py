7117b9dc7fafbd3866d1f18d907361adf98dbf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:00 +0000

    Add package minecraft-forgePY/1.0.1/minecraft-forgePY-1.0.1.tar.gz

---
+++ packages/minecraft-forgePY/minecraft-forgePY-1.0.1.tar.gz/minecraft-forgePY-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import codecs, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Get Minecraft Forge download URLs.'
+LONG_DESCRIPTION = 'A package to get Forge latest and recommended download URLs.'
+
+setup(
+    name="minecraft-forgePY",
+    version=VERSION,
+    author="matejmajny",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["requests", "bs4"],
+    keywords=['python', "minecraft", "forge", "API"],
+)
