83b5625e3b29df25e50f78b4457bc5fc770fd0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:12 +0000

    Add package redgifs/1.7.0/redgifs-1.7.0-py3-none-any.whl

---
+++ packages/redgifs/redgifs-1.7.0-py3-none-any.whl/redgifs/parser.py
@@ -0,0 +1,255 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present scrazzz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+import logging
+from datetime import datetime
+from typing import Any, Dict
+
+from .utils import _to_web_url
+from .models import GIF, URL, CreatorResult, Image, User, SearchResult, CreatorsResult
+
+_log = logging.getLogger(__name__)
+
+# For GIFs
+def parse_search(searched_for: str, json: Dict[str, Any]) -> SearchResult:
+    _log.debug('Using `parse_search` for: {searched_for}')
+    json_gifs = json['gifs']
+    users = json['users']
+    return SearchResult(
+        searched_for=searched_for,
+        page=json['page'],
+        pages=json['pages'],
+        total=json['total'],
+        images=None,
+        gifs=[
+            GIF(
+                id=gif['id'],
+                create_date=datetime.utcfromtimestamp(gif['createDate']),
+                has_audio=gif['hasAudio'],
+                width=gif['width'],
+                height=gif['height'],
+                likes=gif['likes'],
+                tags=gif['tags'],
+                verified=gif['verified'],
+                views=gif['views'],
+                duration=int(gif['duration']) if gif['duration'] is not None else gif['duration'],
+                published=gif['published'],
+                urls=URL(
+                    sd=gif['urls']['sd'],
+                    hd=gif['urls']['hd'],
+                    poster=gif['urls']['poster'],
+                    thumbnail=gif['urls']['thumbnail'],
+                    vthumbnail=gif['urls']['vthumbnail'],
+                    web_url=_to_web_url(gif['id']),
+                ),
+                username=gif['userName'],
+                type=gif['type'],
+                avg_color=gif['avgColor'],
+            )
+            for gif in json_gifs
+        ],
+        users=[
+            User(
+                # I only had this occurrence once where redgifs did not
+                # send the response properly and messed up the entire JSON
+                # response, this is why I have used dict.get() here.
+                creation_time=datetime.utcfromtimestamp(user.get('creationtime'))
+                if user.get('creationtime') is not None else None,
+                description=user.get('description'),
+                followers=user.get('followers'),
+                following=user.get('following'),
+                gifs=user.get('gifs'),
+                name=user.get('name'),
+                profile_image_url=user.get('profileImageUrl'),
+                profile_url=user.get('profileUrl'),
+                published_collections=user.get('publishedCollections'),
+                status=user.get('status'),
+                published_gifs=user.get('publishedGifs'),
+                subscription=user.get('subscription'),
+                url=user.get('url'),
+                username=user.get('username'),
+                verified=user.get('verified'),
+                views=user.get('views'),
+                poster=user.get('poster'),
+                preview=user.get('preview'),
+                thumbnail=user.get('thumbnail'),
+            )
+            for user in users
+        ],
+        tags=json['tags'],
+    )
+
+# For images
+def parse_search_image(searched_for: str, json: Dict[str, Any]) -> SearchResult:
+    _log.debug('Using `parse_search` for: {searched_for}')
+    json_gifs = json['gifs']
+    users = json['users']
+    return SearchResult(
+        searched_for=searched_for,
+        page=json['page'],
+        pages=json['pages'],
+        total=json['total'],
+        gifs=None,
+        images=[
+            Image(
+                id=gif['id'],
+                create_date=datetime.utcfromtimestamp(gif['createDate']),
+                width=gif['width'],
+                height=gif['height'],
+                likes=gif['likes'],
+                tags=gif['tags'],
+                verified=gif['verified'],
+                views=gif['views'],
+                published=gif['published'],
+                urls=URL(
+                    sd=gif['urls']['sd'],
+                    hd=gif['urls']['hd'],
+                    poster=gif['urls']['poster'],
+                    thumbnail=gif['urls']['thumbnail'],
+                    vthumbnail=gif['urls']['vthumbnail'],
+                    web_url=_to_web_url(gif['id']),
+                ),
+                username=gif['userName'],
+                type=gif['type'],
+                avg_color=gif['avgColor'],
+            )
+            for gif in json_gifs
+        ],
+        users=[
+            User(
+                creation_time=datetime.utcfromtimestamp(user.get('creationtime'))
+                if user.get('creationtime') is not None else None,
+                description=user.get('description'),
+                followers=user.get('followers'),
+                following=user.get('following'),
+                gifs=user.get('gifs'),
+                name=user.get('name'),
+                profile_image_url=user.get('profileImageUrl'),
+                profile_url=user.get('profileUrl'),
+                published_collections=user.get('publishedCollections'),
+                status=user.get('status'),
+                published_gifs=user.get('publishedGifs'),
+                subscription=user.get('subscription'),
+                url=user.get('url'),
+                username=user.get('username'),
+                verified=user.get('verified'),
+                views=user.get('views'),
+                poster=user.get('poster'),
+                preview=user.get('preview'),
+                thumbnail=user.get('thumbnail'),
+            )
+            for user in users
+        ],
+        tags=json['tags'],
+    )
+
+def parse_creators(json: Dict[str, Any]) -> CreatorsResult:
+    _log.debug('Using `parse_creators`')
+    items = json['items']
+    return CreatorsResult(
+        items=[
+            User(
+                creation_time=datetime.utcfromtimestamp(user['creationtime']),
+                description=user['description'],
+                followers=user['followers'],
+                following=user['following'],
+                gifs=user['gifs'],
+                name=user['name'],
+                profile_image_url=user['profileImageUrl'],
+                profile_url=user['profileUrl'],
+                published_collections=user['publishedCollections'],
+                status=user['status'],
+                published_gifs=user['publishedGifs'],
+                subscription=user['subscription'],
+                url=user['url'],
+                username=user['username'],
+                verified=user['verified'],
+                views=user['views'],
+                poster=user['poster'],
+                preview=user['preview'],
+                thumbnail=user['thumbnail'],
+            )
+            for user in items
+        ],
+        pages=json['pages'],
+        page=json['page'],
+        total=json['total'],
+    )
+
+def parse_creator(json: Dict[str, Any]) -> CreatorResult:
+    _log.debug('Using `parse_creator`')
+    user = json['users'][0]
+    return CreatorResult(
+        creator=User(
+            creation_time=datetime.utcfromtimestamp(user['creationtime']),
+            description=user['description'],
+            followers=user['followers'],
+            following=user['following'],
+            gifs=json['gifs'],
+            name=user['name'],
+            profile_image_url=user['profileImageUrl'],
+            profile_url=user['profileUrl'],
+            published_collections=user['publishedCollections'],
+            status=user['status'],
+            published_gifs=user['publishedGifs'],
+            subscription=user['subscription'],
+            url=user['url'],
+            username=user['username'],
+            verified=user['verified'],
+            views=user['views'],
+            poster=user['poster'],
+            preview=user['preview'],
+            thumbnail=user['thumbnail'],
+        ),
+        page=json['page'],
+        pages=json['pages'],
+        total=json['total'],
+        gifs=[
+            GIF(
+                id=gif['id'],
+                create_date=datetime.utcfromtimestamp(gif['createDate']),
+                has_audio=gif['hasAudio'],
+                width=gif['width'],
+                height=gif['height'],
+                likes=gif['likes'],
+                tags=gif['tags'],
+                verified=gif['verified'],
+                views=gif['views'],
+                duration=int(gif['duration']) if gif['duration'] is not None else gif['duration'],
+                published=gif['published'],
+                urls=URL(
+                    sd=gif['urls']['sd'],
+                    hd=gif['urls']['hd'],
+                    poster=gif['urls']['poster'],
+                    thumbnail=gif['urls']['thumbnail'],
+                    vthumbnail=gif['urls']['vthumbnail'],
+                    web_url=_to_web_url(gif['id']),
+                ),
+                username=gif['userName'],
+                type=gif['type'],
+                avg_color=gif['avgColor'],
+            )
+            for gif in json['gifs']
+        ]
+    )
---
+++ packages/redgifs/redgifs-1.7.0-py3-none-any.whl/redgifs/utils.py
@@ -0,0 +1,61 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present scrazzz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+import re
+import json
+import pkgutil
+import asyncio
+from typing import Dict
+
+import yarl
+
+from .const import REDGIFS_THUMBS_RE
+
+def _to_web_url(id_or_url: str, use_regex: bool = False) -> str:
+    if not use_regex:
+        return f'https://redgifs.com/watch/{id_or_url.lower()}'
+
+    match = re.match(REDGIFS_THUMBS_RE, id_or_url)
+    if not match:
+        return ''
+
+    try:
+        id = match.group('id')
+        return f'https://redgifs.com/watch/{id.lower()}'
+    except IndexError:
+        return ''
+
+def strip_ip(url: str) -> str:
+    u = yarl.URL(url)
+    if u.query.get('for'):
+        return str(u % {'for': 'REDACTED'})
+    return url
+
+def _read_tags_json() -> Dict[str, str]:
+    file_ = pkgutil.get_data(__name__, 'tags.json') # type: ignore - We know this won't be None
+    return json.loads(file_) # type: ignore - same reason above
+
+async def _async_read_tags_json() -> Dict[str, str]:
+    r = await asyncio.get_event_loop().run_in_executor(None, _read_tags_json)
+    return r
