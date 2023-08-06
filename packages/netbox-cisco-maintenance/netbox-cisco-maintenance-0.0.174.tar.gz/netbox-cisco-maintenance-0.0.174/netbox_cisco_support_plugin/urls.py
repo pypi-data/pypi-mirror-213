b6f4df0b70f1c54f080a171e0302a0d777c86f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package netbox-cisco-maintenance/0.0.174/netbox-cisco-maintenance-0.0.174.tar.gz

---
+++ packages/netbox-cisco-maintenance/netbox-cisco-maintenance-0.0.174.tar.gz/netbox-cisco-maintenance-0.0.174/netbox_cisco_support_plugin/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from netbox.views.generic import ObjectChangeLogView
+from . import models, views
+
+
+urlpatterns = (
+    # Cisco Device Support
+    path("device-support/", views.CiscoDeviceSupportListView.as_view(), name="ciscodevicesupport_list"),
+    # Cisco Device Type Support
+    path("device-type-support/", views.CiscoDeviceTypeSupportListView.as_view(), name="ciscodevicetypesupport_list"),
+)
+
+"""
+urlpatterns = (
+    # Cisco Device Support
+    path("cisco-support/", views.CiscoDeviceSupportListView.as_view(), name="ciscodevicesupport_list"),
+    path("cisco-support/add/", views.CiscoDeviceSupportEditView.as_view(), name="ciscodevicesupport_add"),
+    path("cisco-support/<int:pk>/edit/", views.CiscoDeviceSupportEditView.as_view(), name="ciscodevicesupport_edit"),
+    path("cisco-support/<int:pk>/delete/", views.CiscoDeviceSupportDeleteView.as_view(), name="ciscodevicesupport_delete"),
+    path(
+        "cisco-support/<int:pk>/changelog/",
+        ObjectChangeLogView.as_view(),
+        name="CiscoDeviceSupport_changelog",
+        kwargs={"model": models.CiscoDeviceSupport},
+    ),
+    # Cisco Device Type Support
+)
+"""
