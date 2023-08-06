16e1a3ba8d1524ee19beb31e4fb368d6f156d13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:52 +0000

    Add package SmplML/1.0.3/SmplML-1.0.3.tar.gz

---
+++ packages/SmplML/SmplML-1.0.3.tar.gz/SmplML-1.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+ 
+# python setup.py sdist (for initial)
+
+# python setup.py sdist bdist_wheel (for update)
+
+# twine check dist/*
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Science/Research',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+desc = """SmplML is a user-friendly Python library for streamlined machine learning classification. It offers intuitive functions for data preprocessing, model training, and evaluation of both classification and regression tasks. Ideal for beginners and experts alike, SmplML simplifies tasks, enabling you to gain valuable insights from your data with ease."""
+ 
+dependencies = ['pandas', 
+                'numpy', 
+                'scikit-learn']
+
+setup(
+  name='SmplML',
+  version='1.0.3',
+  description=desc,
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/JhunBrian/SmplML',  
+  author='Jhun Brian Andam',
+  author_email='andam.jhunbrian@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['Machine Learning', 'Classification', 'Regression'], 
+  packages=find_packages(),
+  install_requires=dependencies
+)
