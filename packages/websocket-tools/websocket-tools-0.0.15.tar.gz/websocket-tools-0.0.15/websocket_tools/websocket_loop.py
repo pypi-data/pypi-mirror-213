ac1cf250cfc9cf00fd3c5a500366cfea18f58869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:04 +0000

    Add package websocket-tools/0.0.15/websocket-tools-0.0.15.tar.gz

---
+++ packages/websocket-tools/websocket-tools-0.0.15.tar.gz/websocket-tools-0.0.15/websocket_tools/websocket_loop.py
@@ -0,0 +1,35 @@
+import websocket
+
+
+class WebsocketLoop:
+    def __init__(
+        self,
+        id: str,
+        socket_host,
+        on_open,
+        on_message,
+        on_error,
+        on_close,
+    ):
+        self.id = id
+        self.socket_host = socket_host
+        self.on_open = on_open
+        self.on_message = on_message
+        self.on_error = on_error
+        self.on_close = on_close
+        self.ws = None
+
+    def run(self):
+        socket_host = self.socket_host
+        self.ws = websocket.WebSocketApp(
+            socket_host,
+            on_open=self.on_open,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+        )
+        self.ws.run_forever()
+
+    def terminate(self):
+        if self.ws is not None:
+            self.ws.close()
