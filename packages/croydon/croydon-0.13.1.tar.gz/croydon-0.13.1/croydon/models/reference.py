986152f3da8c769176f56d86e6252571c9b187df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:58 +0000

    Add package croydon/0.13.1/croydon-0.13.1.tar.gz

---
+++ packages/croydon/croydon-0.13.1.tar.gz/croydon-0.13.1/croydon/models/reference.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import Type, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .base_model import TBaseModel
+
+
+class OnDestroy(Enum):
+    CASCADE = "cascade"
+    RAISE = "raise"
+
+
+@dataclass
+class ModelReference:
+    ref_class: Type["TBaseModel"]
+    ref_field: str
+    on_destroy: OnDestroy
+
+    def __hash__(self):
+        return hash(f"{self.ref_class.__name__}.{self.ref_field}")
