a26014322d53bc5218ce102e036d2da1c81d0eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package python-office/0.3.15/python-office-0.3.15.tar.gz

---
+++ packages/python-office/python-office-0.3.15.tar.gz/python-office-0.3.15/office/api/ppt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: ppt.py
+# Mail: 1957875073@qq.com
+# Created Time:  2022-4-25 10:17:34
+# Description: 有关 ppt 的自动化操作
+#############################################
+# from office.lib.utils.except_utils import except_dec
+# from office.core.PPTType import MainPPT
+
+# mainPPT = MainPPT()
+
+import poppt
+
+# todo：输入文件路径
+# @except_dec()
+from office.lib.decorator_utils.instruction_url import instruction
+
+
+@instruction
+def ppt2pdf(path: str, output_path=r'./'):
+    poppt.ppt2pdf(path, output_path)
+
+
+@instruction
+def ppt2img(input_path, output_path=r'./', img_type='jpg',merge=False):
+    poppt.ppt2img(input_path, output_path, img_type,merge)
+
+
+def merge4ppt(input_path, output_path=r'./', output_name='merge4ppt.pptx'):
+    poppt.merge4ppt(input_path, output_path, output_name)
+
