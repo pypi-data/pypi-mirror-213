12aa70f09d31515ea2d378482443ecff9dc2ae8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:58 +0000

    Add package SmoobuExtractorFormatter/1.2.8/SmoobuExtractorFormatter-1.2.8.tar.gz

---
+++ packages/SmoobuExtractorFormatter/SmoobuExtractorFormatter-1.2.8.tar.gz/SmoobuExtractorFormatter-1.2.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='SmoobuExtractorFormatter',
+    version='1.2.8',
+    description='A library to format the output of the Smoobu application',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    uri='',
+    author='Badji Rayane',
+    author_email='rayanebadji.freelance@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Smoobu',
+    packages=['SmoobuExtractorFormatter'],
+    install_requires=['pandas','numpy','openpyxl','xlrd','requests']
+)
