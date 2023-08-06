805e22cd98115ea69b613ac70eb6097635c50c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:12 +0000

    Add package pynocaptcha/1.5.2/pynocaptcha-1.5.2.tar.gz

---
+++ packages/pynocaptcha/pynocaptcha-1.5.2.tar.gz/pynocaptcha-1.5.2/pynocaptcha/__init__.py
@@ -0,0 +1,13 @@
+
+
+from .crackers.cloudflare import CloudFlareCracker
+from .crackers.incapsula import IncapsulaReese84Cracker, IncapsulaUtmvcCracker
+from .crackers.hcaptcha import HcaptchaCracker
+from .crackers.akamai import AkamaiV2Cracker
+from .crackers.recaptcha import ReCaptchaUniversalCracker, ReCaptchaEnterpriseCracker, ReCaptchaSteamCracker
+
+__all__ = [
+    'pynocaptcha', 
+    'CloudFlareCracker', 'IncapsulaReese84Cracker', 'IncapsulaUtmvcCracker', 'HcaptchaCracker', 
+    'AkamaiV2Cracker', 'ReCaptchaUniversalCracker', 'ReCaptchaEnterpriseCracker', 'ReCaptchaSteamCracker'
+]
