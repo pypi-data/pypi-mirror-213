08b6bd66e6b0a3017e9981ed1100df2793eefb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:36 +0000

    Add package django-dellavrite/0.0/django-dellavrite-0.0.tar.gz

---
+++ packages/django-dellavrite/django-dellavrite-0.0.tar.gz/django-dellavrite-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django-dellavrite',
+      version='0.0',
+      description='Cheat for ws',
+      packages=['django-dellavrite'],
+      author_email='delavrite@gmail.com',
+      zip_safe=False)
