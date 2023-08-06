de12a82623cfbe08ef8930cacfcfcb989dfeb7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:59 +0000

    Add package GameWidgets/0.1.5/GameWidgets-0.1.5.tar.gz

---
+++ packages/GameWidgets/GameWidgets-0.1.5.tar.gz/GameWidgets-0.1.5/GameWidgets/Engine/Entity.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+
+class Entity:
+    def __init__(self):
+        pass
+
+    def createAnimation(self, frameCycle):
+        pass
+entity = Entity()
+entity.createAnimation()
