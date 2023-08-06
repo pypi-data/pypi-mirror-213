a274e9e87e6c41b3e54b46a4221da6b1fa343655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:11 +0000

    Add package raga-testing-platform/1.0.1/raga-testing-platform-1.0.1.tar.gz

---
+++ packages/raga-testing-platform/raga-testing-platform-1.0.1.tar.gz/raga-testing-platform-1.0.1/raga/utils/raga_config_reader.py
@@ -0,0 +1,29 @@
+import configparser
+import os
+
+def read_raga_config(config_file_path):
+    if not os.path.isfile(config_file_path):
+        raise FileNotFoundError(f"Config file '{config_file_path}' not found.")
+
+    config = configparser.ConfigParser()
+    try:
+        config.read(config_file_path)
+    except configparser.Error as e:
+        raise ValueError(f"Invalid config file format: {str(e)}")
+
+    config_data = {}
+    for section_name in config.sections():
+        config_data[section_name] = dict(config.items(section_name))
+
+    return config_data
+
+def get_config_value(config_data, section, option):
+    if section in config_data:
+        section_data = config_data[section]
+        if option in section_data:
+            return section_data[option]
+        else:
+            raise KeyError(f"Option '{option}' not found in section '{section}'.")
+    else:
+        raise KeyError(f"Section '{section}' not found in config data.")
+
