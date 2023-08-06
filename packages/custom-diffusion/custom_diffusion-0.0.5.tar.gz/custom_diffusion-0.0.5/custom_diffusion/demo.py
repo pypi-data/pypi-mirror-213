b475d342be55364d5c5b7a7cb743d99f098f75ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:55 +0000

    Add package custom-diffusion/0.0.5/custom_diffusion-0.0.5.tar.gz

---
+++ packages/custom-diffusion/custom_diffusion-0.0.5.tar.gz/custom_diffusion-0.0.5/custom_diffusion/demo.py
@@ -0,0 +1,33 @@
+from custom_diffusion.pipelines.controlnet_pipeline import StableDiffusionControlNetGenerator
+
+
+def main():
+    generator = StableDiffusionControlNetGenerator()
+
+
+    generated_image = generator.generate_image(
+        stable_model_path="runwayml/stable-diffusion-v1-5",
+        controlnet_model_path="lllyasviel/sd-controlnet-scribble",
+        scheduler_name="DDIM",
+        image_path="test.png",
+        prompts="anime girl",
+        negative_prompt="bad",
+        height=512,
+        width=512,
+        guess_mode=False,
+        num_images_per_prompt=1,
+        num_inference_steps=10,
+        guidance_scale=7.0,
+        controlnet_conditioning_scale=1.0,
+        generator_seed=0,
+        preprocess_type="Canny",
+        resize_type="center_crop_and_resize",
+    )
+
+    # Save or display your generated image
+    # For example, save it as a file
+    return generated_image
+
+
+if __name__ == "__main__":
+    main()
