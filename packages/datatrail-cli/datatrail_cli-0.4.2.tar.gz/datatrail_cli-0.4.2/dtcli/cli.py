e130f2e0933630ca2c52ce606c2e592f3fb15a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:15 +0000

    Add package datatrail-cli/0.4.2/datatrail_cli-0.4.2.tar.gz

---
+++ packages/datatrail-cli/datatrail_cli-0.4.2.tar.gz/datatrail_cli-0.4.2/dtcli/cli.py
@@ -0,0 +1,45 @@
+"""Datatrail Command Line Interface."""
+
+import click
+from click_aliasing import ClickAliasedGroup
+from pkg_resources import get_distribution
+from rich import console, pretty
+
+from dtcli import clear, config, ls, ps, pull
+
+pretty.install()
+terminal = console.Console()
+
+
+# Main CLI
+@click.group(cls=ClickAliasedGroup)
+def cli():
+    """Datatrail Command Line Interface."""
+    pass
+
+
+@cli.command(name="version", help="Show versions.")
+def version():
+    """Show version."""
+    terminal.print(
+        "Datatrail Versions",
+        style="bold",
+    )
+    terminal.print(
+        f"{get_distribution('datatrail-cli')}",
+        style="green",
+    )
+    terminal.print(
+        f"datatrail-server {'0.1.1'}",
+        style="green",
+    )
+
+
+cli.add_command(ls.list, aliases=["ls"])
+cli.add_command(ps.ps)
+cli.add_command(pull.pull)
+cli.add_command(clear.clear)
+cli.add_command(config.config)
+
+if __name__ == "__main__":
+    cli()
