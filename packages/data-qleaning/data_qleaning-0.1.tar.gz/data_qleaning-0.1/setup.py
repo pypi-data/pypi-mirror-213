f4b2ea116f243e661933a663f2d45bc6e885115e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:04 +0000

    Add package data-qleaning/0.1/data_qleaning-0.1.tar.gz

---
+++ packages/data-qleaning/data_qleaning-0.1.tar.gz/data_qleaning-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='data_qleaning',
+      version='0.1',
+      description='Data Cleaning',
+      packages=['data_qleaning'],
+      author_email='namithamanjunath4@gmail.com',
+      zip_safe=False)
