2fdf1afa96e90503702666de2eb905bdb4572490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package gremlin-python-tutorial/0.0.2/gremlin_python_tutorial-0.0.2.tar.gz

---
+++ packages/gremlin-python-tutorial/gremlin_python_tutorial-0.0.2.tar.gz/gremlin_python_tutorial-0.0.2/tests/base_gremlin_test.py
@@ -0,0 +1,30 @@
+'''
+Created on 2023-03-23
+
+@author: wf
+'''
+from tests.basetest import Basetest
+from gremlin.remote import RemoteTraversal
+from gremlin.examples import Examples, Volume
+
+class BaseGremlinTest(Basetest):
+    """
+    Basetest for Gremlin Python
+    """
+    
+    def setUp(self, debug=False, profile=True):
+        """
+        prepare the test environment
+        """
+        Basetest.setUp(self, debug, profile)
+        self.remote_traversal=RemoteTraversal()
+        self.g=self.remote_traversal.g()
+        self.volume=Volume.docker()
+        self.examples=Examples(volume=self.volume,debug=self.debug)
+        
+    def tearDown(self):
+        """
+        tear down
+        """
+        Basetest.tearDown(self)
+        self.remote_traversal.close()
