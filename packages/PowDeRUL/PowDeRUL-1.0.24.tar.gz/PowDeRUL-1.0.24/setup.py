4b3bc9c58ba66c05400cb38beab9c2afc8f8aeb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:57 +0000

    Add package PowDeRUL/1.0.24/PowDeRUL-1.0.24.tar.gz

---
+++ packages/PowDeRUL/PowDeRUL-1.0.24.tar.gz/PowDeRUL-1.0.24/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PowDeRUL',
+    version='1.0.24',
+    description ='The project aims to assess the lifetime and efficiency of electronic components by using thermal and electrical calculations based on data from an Excel file.',
+    readme = 'README.md',
+    license='MIT',
+    author='PGarn',
+    author_email='paul.garnier@ens-rennes.fr',
+    url='https://github.com/PGarn/LifeTime',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'scipy>=1.10.1',
+        'pandas>=2.0.1',
+        'matplotlib>=3.7.1',
+        'numpy>=1.24.1',
+        'rainflow>=3.2.0',
+        'openpyxl>=3.1.2',
+    ],
+)
