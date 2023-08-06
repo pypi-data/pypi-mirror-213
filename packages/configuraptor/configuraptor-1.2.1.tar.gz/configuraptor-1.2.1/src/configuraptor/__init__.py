3ed315e85470b260f9f40f36124e62802952d077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:40 +0000

    Add package configuraptor/1.2.1/configuraptor-1.2.1.tar.gz

---
+++ packages/configuraptor/configuraptor-1.2.1.tar.gz/configuraptor-1.2.1/src/configuraptor/__init__.py
@@ -0,0 +1,19 @@
+"""
+Exposes TypedConfig and load_into for this library.
+"""
+
+# SPDX-FileCopyrightText: 2023-present Robin van der Noord <robinvandernoord@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .cls import TypedConfig  # noqa: F401 imported for library reasons
+from .core import (  # noqa: F401 imported for library reasons
+    all_annotations,
+    check_and_convert_data,
+    convert_config,
+    ensure_types,
+    load_into,
+)
+from .singleton import (  # noqa: F401 imported for library reasons
+    Singleton,
+    SingletonMeta,
+)
