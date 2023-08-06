176a62f751f6bedf1d55fadb209ba3e434445b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package stale-maccarone/0.1.0/stale_maccarone-0.1.0.tar.gz

---
+++ packages/stale-maccarone/stale_maccarone-0.1.0.tar.gz/stale_maccarone-0.1.0/src/stale_maccarone/loader.mn.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import logging
+
+from importlib.abc import (
+    MetaPathFinder,
+    SourceLoader,
+)
+from importlib.machinery import ModuleSpec
+from fnmatch import fnmatchcase
+
+from stale_maccarone.openai import CachedChatAPI
+from stale_maccarone.preprocessor import preprocess_maccarone
+
+SNIPPET_START = "#" + "<<"
+
+class ImportFinder(MetaPathFinder):
+    def __init__(
+            self,
+            py_string_matching: bool,
+            include_pattern: str | None,
+            exclude_pattern: str | None,
+        ) -> None:
+        self.py_string_matching = py_string_matching
+        self.include_pattern = include_pattern
+        self.exclude_pattern = exclude_pattern
+
+    def find_spec(self, fullname, path, target=None):
+        # check against module name patterns, if configured
+        if self.exclude_pattern is not None:
+            if fnmatchcase(fullname, self.exclude_pattern) is not None:
+                return None
+
+        if self.include_pattern is not None:
+            if fnmatchcase(fullname, self.include_pattern) is None:
+                return None
+
+        # module name looks ok; check for maccarone snippets
+        def make_modulespec(filename):
+            return ModuleSpec(
+                fullname,
+                ImportLoader(fullname, filename),
+                origin=filename,
+                is_package=False
+            )
+
+        if path is None or path == '':
+            path = [os.getcwd()]  # top level import 
+
+        for entry in path:
+            basename = fullname.split(".")[-1]
+            base_filename = os.path.join(entry, basename)
+            py_filename = base_filename + '.py'
+            mn_filename = base_filename + '.mn.py'
+
+            if os.path.exists(mn_filename):
+                return make_modulespec(mn_filename)
+            elif self.py_string_matching and os.path.exists(py_filename):
+                with open(py_filename, "rt") as file:
+                    if SNIPPET_START in file.read():
+                        return make_modulespec(py_filename)
+
+        return None  # we don't know how to import this
+
+def py_path_to_cache_path(py_path):
+    #<<replace regex: r"\.py$" -> ".json">>
+
+class ImportLoader(SourceLoader):
+    def __init__(self, fullname, path):
+        self.fullname = fullname
+        self.path = path
+
+    def get_filename(self, fullname):
+        return self.path
+
+    def get_data(self, filename):
+        with open(self.path, 'r') as file:
+            in_source = file.read()
+
+        cache_path = py_path_to_cache_path(self.path)
+        chat_api = CachedChatAPI(cache_path)
+
+        return preprocess_maccarone(in_source, chat_api)
+
+def enable(
+        py_string_matching=True,
+        include_pattern=None,
+        exclude_pattern=None,
+    ):
+    """Exclude patterns take precedence over include patterns."""
+
+    import_finder = ImportFinder(
+        py_string_matching=py_string_matching,
+        include_pattern=include_pattern,
+        exclude_pattern=exclude_pattern,
+    )
+
+    sys.meta_path.insert(0, import_finder)
+
+    if os.environ.get("MACCARONE_LOGGING", False):
+        logging.basicConfig(level=logging.DEBUG)
---
+++ packages/stale-maccarone/stale_maccarone-0.1.0.tar.gz/stale_maccarone-0.1.0/src/stale_maccarone/preprocessor.py
@@ -0,0 +1,141 @@
+import re
+import logging
+
+from dataclasses import dataclass
+
+from stale_maccarone.openai import CachedChatAPI
+
+logger = logging.getLogger(__name__)
+
+class Piece:
+    pass
+
+@dataclass
+class PresentPiece(Piece):
+    text: str
+
+@dataclass
+class MissingPiece(Piece):
+    indent: str
+    guidance: str
+
+def raw_source_to_pieces(input: str) -> list[Piece]:
+    missing = re.finditer(r"(\n(?P<il>[ \t]*))?(?P<lm>#<<)(?P<gd>.+?)(?P<rm>>>)", input, re.DOTALL)
+    position = 0
+    pieces = []
+
+    for piece in missing:
+        left = piece.start("lm")
+
+        pieces += [
+            PresentPiece(text=input[position:left]),
+            MissingPiece(
+                indent=piece.group("il") or "",
+                guidance=piece.group("gd"),
+            ),
+        ]
+
+        position = piece.end("rm")
+
+    pieces += [PresentPiece(text=input[position:])]
+
+    return pieces
+
+def raw_pieces_to_tagged_input(raw_pieces: list[Piece]) -> str:
+    tag_source = ""
+    id = 0
+
+    for piece in raw_pieces:
+        match piece:
+            case PresentPiece(text):
+                tag_source += text
+            case MissingPiece(indent, guidance):
+                tag_source += f'# <write_this id="{id}">\n{indent}# {guidance}\n{indent}# </>'
+                id += 1
+            case _:
+                raise TypeError("unknown piece type", piece)
+
+    logger.debug("tagged input ↓\n%s", tag_source)
+
+    return tag_source
+
+def tagged_input_to_tagged_output(tagged_input: str, chat_api: CachedChatAPI) -> str:
+    system_prompt = """
+You are an expert programmer working on contract. Your client has written a partial program, but left pieces for you to complete. They have marked those with `<write_this>` tags inside Python comments, e.g.:
+
+```
+def add_two_numbers(x, y):
+    # <write_this id="0">
+    # add the two numbers
+    # </>
+
+# <write_this id="1">
+# add two numbers from command line args, using argparse
+# </>
+```
+
+You should produce a document that provides a `<completed>` tag for each missing piece, e.g.:
+
+```
+<completed id="0">
+return x + y
+</>
+<completed id="1">
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int)
+parser.add_argument("y", type=int)
+args = parser.parse_args()
+return add_two_numbers(args.x, args.y)
+</>
+```
+
+This formatting is very important. The client uses a custom tool to process your work product, and their tool requires this format. Follow this format exactly and do not copy anything outside a `<write_this>` tag.
+"""
+    chat_messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": tagged_input},
+    ]
+    tagged_output = chat_api.complete_chat("tagged_input_to_tagged_output", chat_messages)
+
+    logger.debug("tagged output ↓\n%s", tagged_output)
+
+    return tagged_output
+
+def tagged_output_to_completed_pieces(tagged_output: str) -> dict[int, str]:
+    pattern = re.compile(r'<completed id="(?P<id>\d+)">\n(?P<content>.+?)</>', re.DOTALL)
+    matches = pattern.finditer(tagged_output)
+    completed = {int(m.group("id")): m.group("content") for m in matches}
+
+    return completed
+
+def pieces_to_final_source(
+        raw_pieces: list[Piece],
+        completed_pieces: dict[int, str],
+    ) -> str:
+    id = 0
+    final_source = ""
+
+    for raw in raw_pieces:
+        match raw:
+            case PresentPiece(text):
+                final_source += text
+            case MissingPiece(indent):
+                completed = completed_pieces[id]
+                final_source += indent.join(completed.splitlines(True))
+                id += 1
+            case _:
+                raise TypeError("unknown piece type", raw)
+
+    logger.debug("final source ↓\n%s", final_source)
+
+    return final_source
+
+def preprocess_maccarone(raw_source: str, chat_api: CachedChatAPI) -> str:
+    raw_pieces = raw_source_to_pieces(raw_source)
+    tagged_input = raw_pieces_to_tagged_input(raw_pieces)
+    tagged_output = tagged_input_to_tagged_output(tagged_input, chat_api)
+    completed_pieces = tagged_output_to_completed_pieces(tagged_output)
+    final_source = pieces_to_final_source(raw_pieces, completed_pieces)
+
+    return final_source
