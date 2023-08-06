2a65b7aa539839a58b1bb9eef4b9f0f5783adea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:05 +0000

    Add package ralph-malph/3.7.0/ralph-malph-3.7.0.tar.gz

---
+++ packages/ralph-malph/ralph-malph-3.7.0.tar.gz/ralph-malph-3.7.0/src/ralph/models/xapi/__init__.py
@@ -0,0 +1,33 @@
+"""xAPI pydantic models."""
+
+# flake8: noqa
+
+from .navigation.statements import PageTerminated, PageViewed
+from .video.statements import (
+    VideoCompleted,
+    VideoEnableClosedCaptioning,
+    VideoInitialized,
+    VideoPaused,
+    VideoPlayed,
+    VideoScreenChangeInteraction,
+    VideoSeeked,
+    VideoTerminated,
+    VideoVolumeChangeInteraction,
+)
+from .virtual_classroom.statements import (
+    VirtualClassroomAnsweredPoll,
+    VirtualClassroomInitialized,
+    VirtualClassroomJoined,
+    VirtualClassroomLeft,
+    VirtualClassroomLoweredHand,
+    VirtualClassroomMuted,
+    VirtualClassroomPostedPublicMessage,
+    VirtualClassroomRaisedHand,
+    VirtualClassroomSharedScreen,
+    VirtualClassroomStartedCamera,
+    VirtualClassroomStartedPoll,
+    VirtualClassroomStoppedCamera,
+    VirtualClassroomTerminated,
+    VirtualClassroomUnmuted,
+    VirtualClassroomUnsharedScreen,
+)
