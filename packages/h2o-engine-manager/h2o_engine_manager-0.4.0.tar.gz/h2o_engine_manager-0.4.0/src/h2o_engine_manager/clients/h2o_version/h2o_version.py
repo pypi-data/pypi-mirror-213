a17b9380d0a78790338bf65fa3365cc815b62649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package h2o-engine-manager/0.4.0/h2o_engine_manager-0.4.0.tar.gz

---
+++ packages/h2o-engine-manager/h2o_engine_manager-0.4.0.tar.gz/h2o_engine_manager-0.4.0/src/h2o_engine_manager/clients/h2o_version/h2o_version.py
@@ -0,0 +1,17 @@
+import pprint
+from typing import Dict
+from typing import List
+
+
+class H2OVersion:
+    """Class represents H2O version."""
+
+    def __init__(
+        self, version: str, aliases: List[str], annotations: Dict[str, str]
+    ) -> None:
+        self.version = version
+        self.aliases = aliases
+        self.annotations = annotations
+
+    def __repr__(self) -> str:
+        return pprint.pformat(self.__dict__)
