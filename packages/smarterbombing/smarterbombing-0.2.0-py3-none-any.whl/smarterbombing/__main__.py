bff1d9b137485d24a9ff978c1ab8b274c0f64b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:09 +0000

    Add package smarterbombing/0.2.0/smarterbombing-0.2.0-py3-none-any.whl

---
+++ packages/smarterbombing/smarterbombing-0.2.0-py3-none-any.whl/smarterbombing/__main__.py
@@ -0,0 +1,34 @@
+"""Main application entrypoint"""
+from argparse import ArgumentParser
+import sys
+
+import smarterbombing.webui.ui as ui
+
+parser = ArgumentParser(
+    prog='smarterbombing',
+    description='Parse Eve Online combat logs and display statistics.',
+    epilog='Show your support by sending ISK in-game to Ageliten.')
+
+MODE_HELP = 'How to run smarterbombing -- webui (default)'
+PORT_HELP = 'Which port to use for hosting the webui.'
+
+parser.add_argument(
+    '--mode',
+    default='webui',
+    required=False,
+    help=MODE_HELP)
+
+parser.add_argument(
+    '--port',
+    default=42069,
+    required=False,
+    help=PORT_HELP
+)
+
+args = parser.parse_args()
+
+if args.mode == 'webui':
+    ui.run_webui(args.port)
+else:
+    print(f'unrecognized mode: {args.mode}')
+    sys.exit(1)
