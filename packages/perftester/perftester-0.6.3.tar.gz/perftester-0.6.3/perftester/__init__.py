95b7637aee654c02c71ceb6b5d3f638489ed6f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:30 +0000

    Add package perftester/0.6.3/perftester-0.6.3.tar.gz

---
+++ packages/perftester/perftester-0.6.3.tar.gz/perftester-0.6.3/perftester/__init__.py
@@ -0,0 +1,17 @@
+from .perftester import (
+    IncorrectArgumentError,
+    TimeTestError,
+    MemoryTestError,
+    FunctionError,
+    CLIPathError,
+    LogFilePathError,
+    Config,
+    MemLog,
+    config,
+    memory_usage_benchmark,
+    memory_usage_test,
+    time_benchmark,
+    time_test,
+    pp,
+    MiB_TO_MB_FACTOR,
+)
