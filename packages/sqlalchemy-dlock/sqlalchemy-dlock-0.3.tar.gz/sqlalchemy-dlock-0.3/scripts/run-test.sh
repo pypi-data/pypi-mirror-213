dd03433157b41a43cb110a3fca40d9806552691e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:04 +0000

    Add package sqlalchemy-dlock/0.3/sqlalchemy-dlock-0.3.tar.gz

---
+++ packages/sqlalchemy-dlock/sqlalchemy-dlock-0.3.tar.gz/sqlalchemy-dlock-0.3/scripts/run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+ENV_DIR="$(mktemp -d)"
+trap "{ echo 'Remove virtual Python environment ${ENV_DIR}'; rm -fr ${ENV_DIR}; }" EXIT
+
+echo "Create virtual Python environment ${ENV_DIR}"
+python -m venv ${ENV_DIR}
+
+${ENV_DIR}/bin/pip install -U --no-cache-dir .[asyncio] pytest pytest-cov -r tests/requirements.txt
+
+${ENV_DIR}/bin/pytest -o cache_dir=/tmp/pytest_cache --no-cov-on-fail --cov=sqlalchemy_dlock
