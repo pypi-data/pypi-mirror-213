15d93d558839abb6f500b40677699fff77d8701d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package jag-panzer/0.1.21/jag-panzer-0.1.21.tar.gz

---
+++ packages/jag-panzer/jag-panzer-0.1.21.tar.gz/jag-panzer-0.1.21/src/jag_panzer/jag_util.py
@@ -0,0 +1,17 @@
+
+
+def dict_pretty_print(d):
+	sex = '\n'
+	for key in d:
+		sex += f"""{('>' + str(key) + '<').ljust(30)} :: >{str(d[key])}<""" + '\n'
+
+	print(sex)
+
+def multireplace(src, replace_pairs):
+	for replace_what, replace_with in replace_pairs:
+		src = src.replace(replace_what, replace_with)
+	return src
+
+
+def clamp(num, tgt_min, tgt_max):
+	return max(tgt_min, min(num, tgt_max))
