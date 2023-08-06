4a62841280c62786cc12287de211d1e0d808a357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package hrin-msb/0.2.69/hrin_msb-0.2.69-py3-none-any.whl

---
+++ packages/hrin-msb/hrin_msb-0.2.69-py3-none-any.whl/msb/validation/exceptions.py
@@ -0,0 +1,36 @@
+from msb.exceptions import AppException
+
+
+class ValidationExceptions:
+	class InvalidValidationSchema(AppException):
+		_message = 'Validation schema should be of type ValidationSchema.'
+
+	class InvalidValidationInput(AppException):
+		_message = 'Validation input should be of type dict or list.'
+
+	class SchemaValidationFailed(AppException):
+		_message = 'Failed to validate the input against the given schema.'
+
+	class ErrorFormattingFailed(AppException):
+		_message = 'Failed to format the validation errors.'
+
+	class ValidationFailed(AppException):
+		_message = 'Failed to validate the request data.'
+
+	class InvalidPayloadException(AppException):
+		_message = 'Invalid payload recieved.'
+
+	class InvalidParamsException(AppException):
+		_message = 'Invalid request parameters recieved.'
+
+	class InvalidPayloadFormat(AppException):
+		_message = 'Malformed request data recieved.'
+
+
+
+
+
+
+
+
+__all__ = ["ValidationExceptions"]
