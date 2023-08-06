b0c516852281bc9e0bf11c39afb4a93fddf32f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:32 +0000

    Add package mapchete-xarray/2023.6.0/mapchete_xarray-2023.6.0.tar.gz

---
+++ packages/mapchete-xarray/mapchete_xarray-2023.6.0.tar.gz/mapchete_xarray-2023.6.0/mapchete_xarray/__init__.py
@@ -0,0 +1,5 @@
+from mapchete_xarray._input import InputData, InputTile
+from mapchete_xarray._output import METADATA, OutputDataReader, OutputDataWriter
+
+__all__ = ["InputData", "InputTile", "METADATA", "OutputDataWriter", "OutputDataReader"]
+__version__ = "2023.6.0"
