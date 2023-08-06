1efe48aead34305c6edf9d76c441e655923692e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:50 +0000

    Add package signway-sdk/0.1.1/signway_sdk-0.1.1.tar.gz

---
+++ packages/signway-sdk/signway_sdk-0.1.1.tar.gz/signway_sdk-0.1.1/signway_sdk/test_sing_url.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from signway_sdk.sign_url import sign_url
+
+
+def test_sign_url():
+    url = sign_url(
+        id='foo',
+        secret='secret',
+        expiry=600,
+        method='POST',
+        host='http://localhost:3000',
+        proxy_url='https://github.com/',
+        datetime_override=datetime.utcfromtimestamp(0)
+    )
+
+    assert url == 'http://localhost:3000/' \
+                  '?X-Sw-Algorithm=SW1-HMAC-SHA256' \
+                  '&X-Sw-Credential=foo%2F19700101' \
+                  '&X-Sw-Date=19700101T000000Z' \
+                  '&X-Sw-Expires=600' \
+                  '&X-Sw-Proxy=https%3A%2F%2Fgithub.com%2F' \
+                  '&X-Sw-SignedHeaders=' \
+                  '&X-Sw-Body=false' \
+                  '&X-Sw-Signature=ed15db76d806155fd5119e093a0f030063c90d943dfdd27e011a9044a77a90a6'  # noqa
