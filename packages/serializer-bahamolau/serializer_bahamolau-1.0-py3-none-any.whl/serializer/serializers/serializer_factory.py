ff4571cb4b004facea3dce250d701f5dac262631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:50 +0000

    Add package serializer-bahamolau/1.0/serializer_bahamolau-1.0-py3-none-any.whl

---
+++ packages/serializer-bahamolau/serializer_bahamolau-1.0-py3-none-any.whl/serializer/serializers/serializer_factory.py
@@ -0,0 +1,19 @@
+from serializer.serializers.serializer_shell import SerializerShell
+from serializer.serializers.json_serializer import Json
+from serializer.serializers.xml_serializer import Xml
+from .serializer_meta import MetaSerializer
+
+
+class SerializerFactory:
+    serializer_lib = {
+        'json': Json,
+        'xml': Xml
+    }
+
+    @classmethod
+    def get_serializer(cls, related_type: str):
+        return SerializerShell(cls.serializer_lib[related_type]())
+
+    @classmethod
+    def save_serializer(cls, related_type: str, serializer: MetaSerializer):
+        cls.serializer_lib[related_type] = serializer
