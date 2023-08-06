85bf44968dea00e0b2a25df167555cd09c90554c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:22 +0000

    Add package CAL-B/0.0.2/CAL-B-0.0.2.tar.gz

---
+++ packages/CAL-B/CAL-B-0.0.2.tar.gz/CAL-B-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers =[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX :: Linux",
+]
+    
+setup(
+    name= 'CAL-B',
+    version= '0.0.2',
+    description= 'CAL-B A basic Calculator Python Package',
+    long_description= open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/swastkk/CAL-B',
+    project_urls= {
+        "Bug Tracker": "https://github.com/swastkk/CAL-B/issues"
+    },
+    author= 'Swastik Sharma',
+    author_email= 'swastkk@gmail.com',
+    license= 'MIT',
+    classifiers= classifiers,
+    keywords= 'calculators',
+    packages= find_packages(),
+    install_requires= ['']
+
+
+)
