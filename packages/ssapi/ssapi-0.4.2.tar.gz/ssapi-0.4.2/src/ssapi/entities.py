2367cf9c53305c0e1eb9184cb5f76b03271506e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:10 +0000

    Add package ssapi/0.4.2/ssapi-0.4.2.tar.gz

---
+++ packages/ssapi/ssapi-0.4.2.tar.gz/ssapi-0.4.2/src/ssapi/entities.py
@@ -0,0 +1,34 @@
+import operator
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import Optional
+
+from ssapi.relational import Table
+
+constraint = namedtuple("constraint", ("comparator", "value", "operator"))
+
+
+@dataclass
+class Transaction(Table):
+    """A generic transaction"""
+
+    id: Optional[int]
+    product: str
+    shop: str
+    quantity: int
+    date: Optional[str] = None
+
+    class Constraints:
+        product = constraint(len, 255, operator.lt)
+        shop = constraint(len, 255, operator.lt)
+
+
+@dataclass
+class Sale(Transaction):
+    """A sale of a certain amount of product"""
+
+    is_discounted: bool = False
+
+
+class Return(Transaction):
+    """A return of a certain amount of product"""
