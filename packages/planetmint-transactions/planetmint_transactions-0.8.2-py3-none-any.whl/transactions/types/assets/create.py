adaf66d6f97659c02be5e1ff963fc58e4e9f6e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:44 +0000

    Add package planetmint-transactions/0.8.2/planetmint_transactions-0.8.2-py3-none-any.whl

---
+++ packages/planetmint-transactions/planetmint_transactions-0.8.2-py3-none-any.whl/transactions/types/assets/create.py
@@ -0,0 +1,84 @@
+# Copyright © 2020 Interplanetary Database Association e.V.,
+# Planetmint and IPDB software contributors.
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
+from typing import Optional
+from cid import is_cid
+
+from transactions.common.transaction import Transaction, CREATE
+from transactions.common.input import Input
+
+
+class Create(Transaction):
+    OPERATION = CREATE
+    ALLOWED_OPERATIONS = (OPERATION,)
+
+    @classmethod
+    def validate_create(
+        cls,
+        tx_signers: list[str],
+        recipients: list[tuple[list[str], int]],
+        assets: Optional[list[dict]],
+        metadata: Optional[dict],
+    ):
+        if not isinstance(tx_signers, list):
+            raise TypeError("`tx_signers` must be a list instance")
+        if not isinstance(recipients, list):
+            raise TypeError("`recipients` must be a list instance")
+        if len(tx_signers) == 0:
+            raise ValueError("`tx_signers` list cannot be empty")
+        if len(recipients) == 0:
+            raise ValueError("`recipients` list cannot be empty")
+        if not assets is None:
+            if not isinstance(assets, list) and len(assets) != 1:
+                raise TypeError("`assets` must be a list of length 1 or None")
+            if "data" in assets[0]:
+                if assets[0]["data"] is not None and not is_cid(assets[0]["data"]):
+                    raise TypeError("`asset.data` must be a CID string or None")
+        if not (metadata is None or is_cid(metadata)):
+            raise TypeError("`metadata` must be a CID string or None")
+
+        return True
+
+    @classmethod
+    def generate(
+        cls,
+        tx_signers: list[str],
+        recipients: list[tuple[list[str], int]],
+        metadata: Optional[dict] = None,
+        assets: Optional[list] = [{"data": None}],
+        inputs: Optional[list[Input]] = None,
+        script: Optional[dict] = None,
+    ):
+        """A simple way to generate a `CREATE` transaction.
+
+        Note:
+            This method currently supports the following Cryptoconditions
+            use cases:
+                - Ed25519
+                - ThresholdSha256
+
+            Additionally, it provides support for the following Planetmint
+            use cases:
+                - Multiple inputs and outputs.
+
+        Args:
+            tx_signers (:obj:`list` of :obj:`str`): A list of keys that
+                represent the signers of the CREATE Transaction.
+            recipients (:obj:`list` of :obj:`tuple`): A list of
+                ([keys],amount) that represent the recipients of this
+                Transaction.
+            metadata (dict): The metadata to be stored along with the
+                Transaction.
+            assets (:obj:`list` of :obj:`dict`): The metadata associated with the asset that will
+                be created in this Transaction.
+
+        Returns:
+            :class:`~planetmint.common.transaction.Transaction`
+        """
+
+        Create.validate_create(tx_signers, recipients, assets, metadata)
+        (generated_inputs, outputs) = Transaction.complete_tx_i_o(tx_signers, recipients)
+        inputs = inputs if inputs is not None else generated_inputs
+        return cls(cls.OPERATION, assets, inputs, outputs, metadata, script=script)
---
+++ packages/planetmint-transactions/planetmint_transactions-0.8.2-py3-none-any.whl/transactions/types/elections/election.py
@@ -0,0 +1,74 @@
+# Copyright © 2020 Interplanetary Database Association e.V.,
+# Planetmint and IPDB software contributors.
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
+from uuid import uuid4
+from typing import Optional
+
+from transactions.common.transaction import Transaction
+from transactions.common.schema import validate_transaction_schema, SchemaValidationError
+
+
+class Election(Transaction):
+    """Represents election transactions.
+
+    To implement a custom election, create a class deriving from this one
+    with OPERATION set to the election operation, ALLOWED_OPERATIONS
+    set to (OPERATION,), CREATE set to OPERATION.
+    """
+
+    OPERATION: Optional[str] = None
+    # Election Statuses:
+    ONGOING: str = "ongoing"
+    CONCLUDED: str = "concluded"
+    INCONCLUSIVE: str = "inconclusive"
+    # Vote ratio to approve an election
+    ELECTION_THRESHOLD = 2 / 3
+
+    @classmethod
+    def validate_election(cls, tx_signers, recipients, assets, metadata):
+        if not isinstance(tx_signers, list):
+            raise TypeError("`tx_signers` must be a list instance")
+        if not isinstance(recipients, list):
+            raise TypeError("`recipients` must be a list instance")
+        if len(tx_signers) == 0:
+            raise ValueError("`tx_signers` list cannot be empty")
+        if len(recipients) == 0:
+            raise ValueError("`recipients` list cannot be empty")
+        if not isinstance(assets, list) and len(assets) != 1:
+            raise TypeError("`assets` must be a list containing exactly on element")
+        if not (metadata is None or isinstance(metadata, str)):
+            # add check if metadata is ipld marshalled CID string
+            raise TypeError("`metadata` must be a CID string or None")
+
+        return True
+
+    @classmethod
+    def generate(
+        cls,
+        initiator,
+        voters,
+        election_data,
+        metadata=None,
+        script: Optional[dict] = None,
+    ):
+        # Break symmetry in case we need to call an election with the same properties twice
+        uuid = uuid4()
+        election_data[0]["data"]["seed"] = str(uuid)
+
+        Election.validate_election(initiator, voters, election_data, metadata)
+        (inputs, outputs) = Transaction.complete_tx_i_o(initiator, voters)
+        election = cls(cls.OPERATION, election_data, inputs, outputs, metadata, script=script)
+        cls.validate_schema(election.to_dict())
+        return election
+
+    @classmethod
+    def validate_schema(cls, tx):
+        """Validate the election transaction. Since `ELECTION` extends `CREATE` transaction, all the validations for
+        `CREATE` transaction should be inherited
+        """
+        try:
+            validate_transaction_schema(tx)
+        except KeyError:
+            raise SchemaValidationError()
