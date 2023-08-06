bbd8716e55d576ddc51072e99f648eaf6a3ccc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:44 +0000

    Add package st-card-component-elliot/0.0.1/st_card_component_elliot-0.0.1.tar.gz

---
+++ packages/st-card-component-elliot/st_card_component_elliot-0.0.1.tar.gz/st_card_component_elliot-0.0.1/st_card_component_elliot/frontend/build/precache-manifest.b024b67afa5bc32e188488702ca5a717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fdf088f2bbc9ae85bf5f0d7d21f110",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bebf6f8bf814fac7dad",
+    "url": "./static/js/2.9c7d7d97.chunk.js"
+  },
+  {
+    "revision": "465ff09073ee359151abc124f9b2a2ea",
+    "url": "./static/js/2.9c7d7d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86cc94ee2c5b525cc3e",
+    "url": "./static/js/main.6d185def.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
