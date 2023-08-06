d6257725b522cc64ce42c05b6eafd305e405ba2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:38 +0000

    Add package podval/3.0/podval-3.0.tar.gz

---
+++ packages/podval/podval-3.0.tar.gz/podval-3.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='podval',
+      version='3.0',
+      description='Discr',
+      author='VipesPras',
+      packages=['podval','exam'],
+      zip_safe=False)
