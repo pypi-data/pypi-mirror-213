45f383eed2fe3ea7963f1e9f04b12edd059806cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:45 +0000

    Add package gptbase/0.2.3/gptbase-0.2.3-py3-none-any.whl

---
+++ packages/gptbase/gptbase-0.2.3-py3-none-any.whl/gptbase/chat.py
@@ -0,0 +1,23 @@
+import click
+from gptbase.basev2 import ChatAssistant, CompletionParameters
+
+@click.command()
+@click.option('--memory-turns', default=3, help='Number of memory turns.')
+@click.option('--system-prompt', default='', help='System prompt.')
+def chat(memory_turns, system_prompt):
+    cm = CompletionParameters(stream=True)
+    chat = ChatAssistant(memory_turns, system_prompt)
+    while True:
+        try:
+            message = input(">>> ")
+            if message == "exit":
+                break
+            chat.chat(message, cm)
+        except KeyboardInterrupt:
+            break
+        except Exception as err:
+            print(err)
+
+
+if __name__ == "__main__":
+    chat()
