d9be93be258c5837acab83919c8b5cacfa285362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:00 +0000

    Add package agentai/0.0.2/agentai-0.0.2.tar.gz

---
+++ packages/agentai/agentai-0.0.2.tar.gz/agentai-0.0.2/agentai/conversation.py
@@ -0,0 +1,28 @@
+from termcolor import colored
+
+
+class Conversation:
+    def __init__(self):
+        self.conversation_history = []
+
+    def add_message(self, role, content, name=None):
+        if name:
+            message = {"role": role, "name": name, "content": content}
+        else:
+            message = {"role": role, "content": content}
+        self.conversation_history.append(message)
+
+    def display_conversation(self, detailed=False):
+        role_to_color = {
+            "system": "red",
+            "user": "green",
+            "assistant": "blue",
+            "function": "magenta",
+        }
+        for message in self.conversation_history:
+            print(
+                colored(
+                    f"{message['role']}: {message['content']}\n\n",
+                    role_to_color[message["role"]],
+                )
+            )
