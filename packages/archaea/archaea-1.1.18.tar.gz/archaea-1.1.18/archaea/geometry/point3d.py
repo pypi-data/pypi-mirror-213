2b0916229bc664a6437dfcaa136cfffb3162f507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package archaea/1.1.18/archaea-1.1.18.tar.gz

---
+++ packages/archaea/archaea-1.1.18.tar.gz/archaea-1.1.18/archaea/geometry/point3d.py
@@ -0,0 +1,42 @@
+from archaea.geometry.coordinate_array import CoordinateArray
+from archaea.geometry.vector3d import Vector3d
+
+
+# 3 dimensional point.
+class Point3d(CoordinateArray):
+    def __init__(self, *coordinates):
+        filtered_coordinates = [entry for entry in coordinates if isinstance(entry, (int, float))]
+        self.coordinates = filtered_coordinates[:3]
+        super(CoordinateArray, self).__init__(filtered_coordinates)
+
+    @property
+    def x(self):
+        return self.coordinates[0]
+
+    @property
+    def y(self):
+        return self.coordinates[1]
+
+    @property
+    def z(self):
+        return self.coordinates[2]
+
+    def to_a(self):
+        return [self.x, self.y, self.z]
+
+    @property
+    def position_vector(self):
+        return Vector3d(self.x, self.y, self.z)
+
+    def move(self, vector):
+        return Point3d(self.x + vector.x, self.y + vector.y, self.z + vector.z)
+
+    def vector_to(self, other):
+        return Vector3d(other.x - self.x, other.y - self.y, other.z - self.z)
+
+    def distance_to(self, other):
+        return (((self.x - other.x) ** 2) + ((self.y - other.y) ** 2) + ((self.z - other.z) ** 2)) ** 0.5
+
+    @classmethod
+    def origin(cls):
+        return cls(0, 0, 0)
---
+++ packages/archaea/archaea-1.1.18.tar.gz/archaea-1.1.18/archaea/geometry/vector3d.py
@@ -0,0 +1,49 @@
+import math
+
+from archaea.geometry.coordinate_array import CoordinateArray
+from archaea.geometry.vector import Vector
+
+
+# 3 dimensional vector.
+class Vector3d(Vector):
+    def __init__(self, *coordinates):
+        filtered_coordinates = [entry for entry in coordinates if isinstance(entry, (int, float))]
+        self.coordinates = filtered_coordinates[:3]
+        super(CoordinateArray, self).__init__(filtered_coordinates)
+
+    @property
+    def x(self):
+        return self.coordinates[0]
+
+    @property
+    def y(self):
+        return self.coordinates[1]
+
+    @property
+    def z(self):
+        return self.coordinates[2]
+
+    @property
+    def length(self):
+        return ((self.x ** 2) + (self.y ** 2) + (self.z ** 2)) ** 0.5
+
+    def cross_product(self, other):
+        return Vector3d(
+            (self.y * other.z) - (self.z * other.y),
+            (self.z * other.x) - (self.x * other.z),
+            (self.x * other.y) - (self.y * other.x)
+        )
+
+    def azimuth_angle(self):
+        result = 0
+        if self.x > 0:
+            result = (math.pi * 0.5) - math.atan(self.y / self.x)
+        elif self.x < 0:
+            result = (math.pi * 1.5) - math.atan(self.y / self.x)
+        elif self.y > 0:
+            result = 0
+        elif self.y < 0:
+            result = math.pi
+
+        return math.degrees(result)
+
