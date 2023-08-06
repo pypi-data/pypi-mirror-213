059572a41149f3e55c09ad06b9288005f2fbc35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:24 +0000

    Add package pysimconnect/0.2.5/pysimconnect-0.2.5-py3-none-any.whl

---
+++ packages/pysimconnect/pysimconnect-0.2.5-py3-none-any.whl/simconnect/__init__.py
@@ -0,0 +1,5 @@
+from .scdefs import *
+from .sc import SimConnect, RECV_P
+from .receiver import Receiver, ReceiverInstance
+from .datadef import SimData, SimDataHandler , DataDefinition
+from .scvars import SIMVARS, EVENTS, UNITS, DIMENSIONS
