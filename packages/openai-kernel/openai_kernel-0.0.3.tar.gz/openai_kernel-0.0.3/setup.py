51f623c215a4fde811e434a655bfc0aacefb9d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:56 +0000

    Add package openai-kernel/0.0.3/openai_kernel-0.0.3.tar.gz

---
+++ packages/openai-kernel/openai_kernel-0.0.3.tar.gz/openai_kernel-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Grab version from version.py
+__version__ = ""
+exec(open('openai_kernel/version.py').read())
+
+setuptools.setup(
+    name="openai_kernel",
+    version=__version__,
+    author="Oak City Labs",
+    author_email="team@oakcity.io",
+    description="OpenAI jupyter kernel",
+    url="https://github.com/OakCityLabs/openai_kernel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={"openai_kernel": ["*.json"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    install_requires=[
+        "openai>=0.27.4,<1",
+        "metakernel>=0.29.4,<1"
+    ],
+)
