998a52eac89b954fbf3203d00f568dfe54791c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:48 +0000

    Add package pytt-events-api/0.1.3/pytt_events_api-0.1.3.tar.gz

---
+++ packages/pytt-events-api/pytt_events_api-0.1.3.tar.gz/pytt_events_api-0.1.3/pytt_events/properties.py
@@ -0,0 +1,40 @@
+from pydantic import (
+    BaseModel,
+    ValidationError,
+    conlist,
+    constr,
+    PositiveFloat,
+    PositiveInt
+)
+from typing import Optional
+from enum import Enum
+
+class ContentType(str, Enum):
+    """
+    For individual products.
+    """
+    PRODUCT = 'product'
+    """
+    For product groups (Items and variants).
+    """
+    PRODUCT_GROUP = 'product_group'
+
+class Content(BaseModel):
+    content_id: constr(strip_whitespace=True, min_length=1)
+    quantity: PositiveInt
+    price: PositiveFloat
+    content_category: constr(min_length=1)
+    content_name: constr(min_length=1)
+
+class Properties(BaseModel):
+    currency: Optional[constr(strip_whitespace=True, min_length=3, max_length=3)]
+    value: Optional[PositiveFloat]
+    content_type: ContentType
+    description: Optional[constr(min_length=1)]
+    query: Optional[constr(min_length=1)]
+    status: Optional[constr(min_length=1)]
+    contents: Optional[conlist(
+        item_type=Content, unique_items=True, min_items=1
+    )]
+
+
