12ab8edda68f10fc7edaf81a40beeaade86c82b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:41 +0000

    Add package edumud/0.0.8/edumud-0.0.8.tar.gz

---
+++ packages/edumud/edumud-0.0.8.tar.gz/edumud-0.0.8/src/edumud/file_utils.py
@@ -0,0 +1,15 @@
+import yaml
+
+
+def load_config(config_name: str):
+    """load configuration file.
+
+    Args:
+        config_name (str): config (yaml) file path and name
+
+    Returns:
+        (dict): yaml file contents
+    """
+    with open(config_name) as config_file:
+        hp = yaml.safe_load(config_file)  # hp = house_parameters
+        return hp
---
+++ packages/edumud/edumud-0.0.8.tar.gz/edumud-0.0.8/tests/test_module_demo.py
@@ -0,0 +1,5 @@
+
+from edumud.constants import *
+
+print(f"kT: {BOLTZ_T}")
+print(f"electron charge: {E_CHARGE}")
