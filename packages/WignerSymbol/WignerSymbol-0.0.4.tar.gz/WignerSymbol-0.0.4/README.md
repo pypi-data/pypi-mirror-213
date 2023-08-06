0b9d1ffe4a02cc2d36f946e345e726280ac34401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package WignerSymbol/0.0.4/WignerSymbol-0.0.4.tar.gz

---
+++ packages/WignerSymbol/WignerSymbol-0.0.4.tar.gz/WignerSymbol-0.0.4/README.md
@@ -0,0 +1,3 @@
+# WignerSymbol-pybind11
+
+Python bindings for [0382/WignerSymbol](https://github.com/0382/WignerSymbol), a library written in C++. Requires C++17 and the use of pybind11.
---
+++ packages/WignerSymbol/WignerSymbol-0.0.4.tar.gz/WignerSymbol-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+__version__ = "0.0.4"
+
+ext_modules = [
+    Pybind11Extension("WignerSymbol",
+                      ["lib.cpp"],
+                      define_macros=[("VERSION", __version__)]
+                      ),
+]
+
+setup(
+    name='WignerSymbol',
+    version=__version__,
+    author="0382",
+    author_email="18322825326@163.com",
+    url="https://github.com/0382/WignerSymbol-pybind11",
+    description="Python port of 0382/WignerSymbol",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
