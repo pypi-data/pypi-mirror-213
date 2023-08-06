687fc7819799443a437808314b8ef7c2ac48ecc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:52 +0000

    Add package pz-rail-dsps/0.0.1/pz_rail_dsps-0.0.1-py3-none-any.whl

---
+++ packages/pz-rail-dsps/pz_rail_dsps-0.0.1-py3-none-any.whl/rail/dsps/__init__.py
@@ -0,0 +1,5 @@
+from ._version import __version__
+
+from rail.creation.engines.dsps_photometry_creator import *
+from rail.creation.engines.dsps_sed_modeler import *
+from rail.creation.engines.galaxy_population_components import *
