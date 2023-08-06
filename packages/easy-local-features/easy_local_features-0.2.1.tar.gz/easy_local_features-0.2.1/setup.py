133ea9f9a157c027f8f28b123cdf9101e14a9bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package easy-local-features/0.2.1/easy_local_features-0.2.1.tar.gz

---
+++ packages/easy-local-features/easy_local_features-0.2.1.tar.gz/easy_local_features-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='easy_local_features',
+    version='0.2.1',
+    author='eucadar',
+    author_email='python@eucadar.com',
+    packages=find_packages(exclude=('tests', 'docs', 'assets')),
+    include_package_data=True,
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
