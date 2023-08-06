53417d9291a2cbe24744273f098be5e65db06980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:22 +0000

    Add package cg/36.0.2/cg-36.0.2-py3-none-any.whl

---
+++ packages/cg/cg-36.0.2-py3-none-any.whl/cg/apps/demultiplex/sample_sheet/create.py
@@ -0,0 +1,43 @@
+import logging
+from typing import List
+from typing_extensions import Literal
+
+from cg.apps.demultiplex.sample_sheet.novaseq_sample_sheet import SampleSheetCreator
+from cg.apps.demultiplex.sample_sheet.models import FlowCellSample
+from cg.constants.sequencing import Sequencers
+from cg.constants.demultiplexing import BclConverter
+from cg.exc import FlowCellError
+from cg.models.demultiplex.flow_cell import FlowCellDirectoryData
+
+LOG = logging.getLogger(__name__)
+
+
+def create_sample_sheet(
+    bcl_converter: Literal[BclConverter.BCL2FASTQ, BclConverter.DRAGEN],
+    flow_cell: FlowCellDirectoryData,
+    lims_samples: List[FlowCellSample],
+    force: bool = False,
+) -> List[List[str]]:
+    """Create a sample sheet for a flow cell."""
+    if flow_cell.sample_sheet_path.exists():
+        message = f"Sample sheet {flow_cell.sample_sheet_path} already exists!"
+        LOG.warning(message)
+        raise FileExistsError(message)
+
+    flow_cell_sequencer: str = flow_cell.sequencer_type
+
+    if flow_cell_sequencer not in [Sequencers.NOVASEQ, Sequencers.NOVASEQX]:
+        message = f"Only demultiplexing of Novaseq sequence data is currently supported. Found sequencer type: {flow_cell_sequencer}"
+        LOG.warning(message)
+        raise FlowCellError(message=message)
+
+    sample_sheet_creator = SampleSheetCreator(
+        bcl_converter=bcl_converter,
+        flow_cell=flow_cell,
+        lims_samples=lims_samples,
+        force=force,
+    )
+    LOG.info(
+        f"Constructing a {bcl_converter} sample sheet for the {flow_cell_sequencer} flow cell {flow_cell.id}"
+    )
+    return sample_sheet_creator.construct_sample_sheet()
