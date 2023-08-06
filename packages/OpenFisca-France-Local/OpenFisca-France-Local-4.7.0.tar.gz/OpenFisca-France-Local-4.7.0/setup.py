f3078bc353b73c388e1202d9531c6e53878dd3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package OpenFisca-France-Local/4.7.0/OpenFisca-France-Local-4.7.0.tar.gz

---
+++ packages/OpenFisca-France-Local/OpenFisca-France-Local-4.7.0.tar.gz/OpenFisca-France-Local-4.7.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="OpenFisca-France-Local",
+    version="4.7.0",
+    author="OpenFisca Team",
+    author_email="contact@openfisca.fr",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
+    description="Extension OpenFisca pour nos partenariats avec les collectivités territoriales",
+    keywords="benefit france france-local microsimulation social tax",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/openfisca/openfisca-france",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'OpenFisca-Core >= 35.8.0, < 36',
+        'OpenFisca-France >= 139.0.0, < 148',
+        'pandas == 1.0.3'
+        ],
+    extras_require={
+        'test': [
+            'nose',
+            ],
+        'excel-reader': [
+            'xlrd == 1.2.0'
+            ]
+        },
+    scripts=['openfisca_france_local/scripts/openfisca_local_test']
+    )
