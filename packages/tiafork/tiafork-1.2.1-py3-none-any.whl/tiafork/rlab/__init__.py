12d7a71812f4f75fd5ed72d69d241e2a3b9c2eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:35 +0000

    Add package tiafork/1.2.1/tiafork-1.2.1-py3-none-any.whl

---
+++ packages/tiafork/tiafork-1.2.1-py3-none-any.whl/tiafork/rlab/__init__.py
@@ -0,0 +1,8 @@
+# Monkey patch module
+import tiafork.rlab.patch
+
+from tiafork.rlab.components import *
+from tiafork.rlab.table import *
+from tiafork.rlab.builder import *
+from tiafork.rlab.font import *
+
