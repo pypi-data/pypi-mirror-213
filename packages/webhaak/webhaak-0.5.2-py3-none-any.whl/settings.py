9ba2c2a9fb8fffa9b7d8f2be723d5a857035f782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:19 +0000

    Add package webhaak/0.5.2/webhaak-0.5.2-py3-none-any.whl

---
+++ packages/webhaak/webhaak-0.5.2-py3-none-any.whl/settings.py
@@ -0,0 +1,17 @@
+PORT = 8086
+
+MAX_CACHE_AGE = 600  # seconds
+
+DEBUG = True
+
+PROJECTS_FILE = 'projects.yaml'
+# REPOS_CACHE_DIR = '/var/cache/webhaak'
+REPOS_CACHE_DIR = '/tmp/webhaak'
+
+SECRETKEY = 'blah123'
+
+LOG_LOCATION = 'webhaak.log'
+LOG_BACKUP_COUNT = 10
+
+PUSHOVER_USERKEY = 'umuy2covsiv9irfr57on43smideaat'
+PUSHOVER_APPTOKEN = 'ahfjrmhhwwf4p2ct7grn4jqap5j5ho'
