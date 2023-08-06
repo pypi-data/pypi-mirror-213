76c530093e4d8a2db65560378da3c9dc6756dda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package lens-metric/0.1.1/lens_metric-0.1.1.tar.gz

---
+++ packages/lens-metric/lens_metric-0.1.1.tar.gz/lens_metric-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lens_metric',
+    version='0.1.1',
+    description='A new metric for text simplification',
+    author='Mounica Maddela',
+    author_email='mmaddela3@gatech.edu',
+    url='https://github.com/Yao-Dou/LENS',
+    download_url='https://github.com/Yao-Dou/LENS/archive/refs/tags/v0.1.1.tar.gz',
+    license='Apache license',
+    packages=find_packages(),
+    install_requires=[
+        "sentencepiece==0.1.96",
+        "pandas==1.1.5",
+        "transformers>=4.8",
+        "pytorch-lightning==1.6.0",
+        "jsonargparse==3.13.1",
+        "torch >=1.6.0,<2",
+        "numpy >= 1.20.0",
+        "torchmetrics >= 0.7",
+        "sacrebleu >= 2.0.0",
+        "scipy >=1.5.4" ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+    ],
+)
