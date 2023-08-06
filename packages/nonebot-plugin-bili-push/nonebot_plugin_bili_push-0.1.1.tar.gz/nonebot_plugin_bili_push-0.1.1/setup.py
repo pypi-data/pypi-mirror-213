ad7a321874898b18202a89b64fb96ddc22314594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:33 +0000

    Add package nonebot-plugin-bili-push/0.1.1/nonebot_plugin_bili_push-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-bili-push/nonebot_plugin_bili_push-0.1.1.tar.gz/nonebot_plugin_bili_push-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+
+def get_install_requires():
+    reqs = [
+        'pillow>=9.5.0',
+        'requests>=2.30.0',
+        'nonebot2>=2.0.0rc3',
+        'nonebot_plugin_apscheduler>=0.2.0'
+    ]
+    return reqs
+
+
+setup(name='nonebot_plugin_bili_push',
+      version='0.1.1',
+      description='Nonebot2 plugin',
+      author='SuperGuGuGu',
+      author_email='13680478000@163.com',
+      url='https://github.com/SuperGuGuGu/nonebot_plugin_bili_push',
+      packages=find_packages(),
+      python_requires=">=3.8",
+      install_requires=get_install_requires(),
+      package_data={'': ['*.csv', '*.txt', '.toml']},
+      include_package_data=True
+      )
