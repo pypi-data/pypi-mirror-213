267f79ca7eb286ad68b3958a1eb78ea4176792c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:58 +0000

    Add package vpso/1.0.2/vpso-1.0.2-py3-none-any.whl

---
+++ packages/vpso/vpso-1.0.2-py3-none-any.whl/vpso/typing.py
@@ -0,0 +1,9 @@
+from numpy import double, int_
+from numpy.typing import NDArray
+from typing_extensions import TypeAlias
+
+Array1d: TypeAlias = NDArray[double]
+Array2d: TypeAlias = NDArray[double]
+Array3d: TypeAlias = NDArray[double]
+Array1i: TypeAlias = NDArray[int_]
+Array2i: TypeAlias = NDArray[int_]
