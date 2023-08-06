6e095c332a70846fccf852772a06144399b0738c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:58 +0000

    Add package tinynn-py/1.0.1/tinynn-py-1.0.1.tar.gz

---
+++ packages/tinynn-py/tinynn-py-1.0.1.tar.gz/tinynn-py-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tinynn-py',
+    version='1.0.1',
+    packages=find_packages(),
+    author='Praneeth Vasarla',
+    description='A tiny neural network library.',
+    install_requires=[
+        "numpy"
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
