611d455fe8558ee1352aad3a0c616851d4573c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package eodc-faas-sen2like/2023.5.1rc0/eodc_faas_sen2like-2023.5.1rc0-py3-none-any.whl

---
+++ packages/eodc-faas-sen2like/eodc_faas_sen2like-2023.5.1rc0-py3-none-any.whl/sen2like_processor_bindings/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "2023.05.1-rc.0"
+
+from sen2like_processor_bindings.model import Sen2LikeParameters  # noqa: F401
+from sen2like_processor_bindings.workflows import Sen2LikeProcessor  # noqa: F401
