69477192d38f3b9fa5c3f4f1b985139fd46a35b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:21 +0000

    Add package ensta/2.2/ensta-2.2-py3-none-any.whl

---
+++ packages/ensta/ensta-2.2-py3-none-any.whl/ensta/lib/__init__.py
@@ -0,0 +1,16 @@
+from .Commons import (
+    update_session,
+    update_homepage_source,
+    update_app_id,
+    refresh_csrf_token,
+    format_username,
+    format_uid,
+    format_identifier
+)
+
+from .Exceptions import (
+    NetworkError,
+    AuthenticationError,
+    IdentifierError,
+    CodeError
+)
