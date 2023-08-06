4d7db112643359de7807781192a39c9b647b4240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package stale-maccarone/0.1.0/stale_maccarone-0.1.0-py3-none-any.whl

---
+++ packages/stale-maccarone/stale_maccarone-0.1.0-py3-none-any.whl/stale_maccarone/scripts/preprocess.mn.py
@@ -0,0 +1,62 @@
+import os
+import glob
+import logging
+
+from argparse import Namespace
+
+from stale_maccarone.openai import CachedChatAPI
+from stale_maccarone.preprocessor import preprocess_maccarone
+
+logger = logging.getLogger(__name__)
+
+def preprocess(mn_path: str, print_: bool, write: bool, suffix: str) -> None:
+    # produce Python source
+    logger.info("preprocessing %s", mn_path)
+
+    cache_path = mn_path.replace(suffix, ".mn.json")
+    chat_api = CachedChatAPI(cache_path)
+
+    #<<mn_source = read mn_path>>
+
+    py_source = preprocess_maccarone(mn_source, chat_api)
+
+    if write:
+        #<<py_path = regex replace mn_path: f"{suffix}$" -> ".py">>
+
+        logger.info("writing %s", py_path)
+
+        if py_path == mn_path:
+            raise ValueError("won't overwrite input file", mn_path)
+
+        #<<write py_source to py_path>>
+
+    if print_:
+        print(py_source)
+
+def main(path: str, print_: bool, write: bool, suffix: str) -> None:
+    """Preprocess files with Maccarone snippets."""
+
+    if os.path.isdir(path):
+        mn_files = glob.glob(
+            os.path.join(path, f"**/*{suffix}"),
+            recursive=True,
+        )
+    else:
+        mn_files = [path]
+
+    #<<preprocess mn_files>>
+
+def parse_args() -> Namespace:
+    #<<
+    # get args for main() and return; use argparse
+    # default suffix: ".mn.py"
+    #>>
+
+def script_main():
+    logging.basicConfig(level=logging.INFO)
+
+    return main(**vars(parse_args()))
+
+if __name__ == "__main__":
+    script_main()
+
---
+++ packages/stale-maccarone/stale_maccarone-0.1.0-py3-none-any.whl/stale_maccarone/scripts/preprocess.py
@@ -0,0 +1,75 @@
+import os
+import glob
+import logging
+
+from argparse import Namespace
+
+from stale_maccarone.openai import CachedChatAPI
+from stale_maccarone.preprocessor import preprocess_maccarone
+
+logger = logging.getLogger(__name__)
+
+def preprocess(mn_path: str, print_: bool, write: bool, suffix: str) -> None:
+    # produce Python source
+    logger.info("preprocessing %s", mn_path)
+
+    cache_path = mn_path.replace(suffix, ".mn.json")
+    chat_api = CachedChatAPI(cache_path)
+
+    with open(mn_path, "r") as f:
+        mn_source = f.read()
+
+
+    py_source = preprocess_maccarone(mn_source, chat_api)
+
+    if write:
+        import re
+        py_path = re.sub(f"{suffix}$", ".py", mn_path)
+
+
+        logger.info("writing %s", py_path)
+
+        if py_path == mn_path:
+            raise ValueError("won't overwrite input file", mn_path)
+
+        with open(py_path, "w") as f:
+            f.write(py_source)
+
+
+    if print_:
+        print(py_source)
+
+def main(path: str, print_: bool, write: bool, suffix: str) -> None:
+    """Preprocess files with Maccarone snippets."""
+
+    if os.path.isdir(path):
+        mn_files = glob.glob(
+            os.path.join(path, f"**/*{suffix}"),
+            recursive=True,
+        )
+    else:
+        mn_files = [path]
+
+    for mn_file in mn_files:
+        preprocess(mn_file, print_, write, suffix)
+
+
+def parse_args() -> Namespace:
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="Path to the file or directory containing Maccarone snippets")
+    parser.add_argument("--print", dest="print_", action="store_true", help="Print the preprocessed Python source")
+    parser.add_argument("--write", action="store_true", help="Write the preprocessed Python source to a file")
+    parser.add_argument("--suffix", default=".mn.py", help="Suffix for files containing Maccarone snippets (default: .mn.py)")
+    args = parser.parse_args()
+    return args
+
+
+def script_main():
+    logging.basicConfig(level=logging.INFO)
+
+    return main(**vars(parse_args()))
+
+if __name__ == "__main__":
+    script_main()
+
