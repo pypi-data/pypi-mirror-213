b13c48a93b5c596673d29ffff6c832be3cf8c6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:02 +0000

    Add package jjuke/0.0.1.0/jjuke-0.0.1.0-py3-none-any.whl

---
+++ packages/jjuke/jjuke-0.0.1.0-py3-none-any.whl/jjuke/__init__.py
@@ -0,0 +1,5 @@
+from jjuke import logger, metrics, module, options, sched, trainer, utils
+
+# from jjuke import *
+
+__version__ = '0.0.1.0'
---
+++ packages/jjuke/jjuke-0.0.1.0-py3-none-any.whl/jjuke/module/loss/focal.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+from jjuke.module.loss.utils import _reduce
+
+__all__ = ["focal_loss", "FocalLoss"]
+
+
+def focal_loss(gamma: float, logits: Tensor, targets: Tensor, reduction="mean") -> Tensor:
+    """
+    ### input:
+    - logits: b c ...
+    - targets: b ... (long)
+
+    CE(p) = -\log(p)
+    FL(p) = -(1 - p)^\gamma \log(p)
+    """
+    ce = F.cross_entropy(logits, targets, reduction="none")  # b ...
+    scale = torch.pow(1 - torch.exp(-ce), gamma)
+    loss = scale * ce
+    return _reduce(loss, reduction)
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma: float) -> None:
+        super().__init__()
+        self.gamma = gamma
+
+    def forward(self, logits: Tensor, targets: Tensor, reduction="mean") -> Tensor:
+        return focal_loss(self.gamma, logits, targets, reduction=reduction)
