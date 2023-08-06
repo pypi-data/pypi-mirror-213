93a43a353e1baef1dc237136db812ec17d38b6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:52 +0000

    Add package mybar/0.7.2/mybar-0.7.2.tar.gz

---
+++ packages/mybar/mybar-0.7.2.tar.gz/mybar-0.7.2/README.md
@@ -0,0 +1,3 @@
+# mybar
+  A lightweight, highly customizable asynchronous status bar API for terminal and GUI environments. 
+  I use it in tmux and for my i3bar on Linux.
---
+++ packages/mybar/mybar-0.7.2.tar.gz/mybar-0.7.2/mybar/constants.py
@@ -0,0 +1,42 @@
+__all__ = (
+    'CONFIG_FILE',
+    'DEBUG',
+    'CSI',
+    'CLEAR_LINE',
+    'HIDE_CURSOR',
+    'UNHIDE_CURSOR',
+    'FONTAWESOME_ICONS',
+    'USING_FONTAWESOME'
+)
+
+
+from ._types import ConsoleControlCode
+import os.path
+
+
+CONFIG_FILE: str = os.path.abspath(os.path.expanduser(
+    '~/.config/mybar/conf.json'
+))
+'''The default bar config file path.'''
+
+DEBUG: bool = False
+'''The default debug state.'''
+
+
+# Used by Bar:
+CSI: ConsoleControlCode = '\033['
+'''Unix terminal escape code (control sequence introducer).'''
+
+CLEAR_LINE: ConsoleControlCode = '\x1b[2K'
+'''VT100 escape code to clear the line.'''
+
+HIDE_CURSOR: ConsoleControlCode = '?25l'
+'''VT100 escape code to hide the cursor.'''
+
+UNHIDE_CURSOR: ConsoleControlCode = '?25h'
+'''VT100 escape code to unhide the cursor.'''
+
+
+FONTAWESOME_ICONS = tuple('')
+USING_FONTAWESOME = False
+
