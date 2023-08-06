7e9766a34b6f8874d09c5c70ba383570d91182a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:17 +0000

    Add package petab-select/0.1.9/petab_select-0.1.9-py3-none-any.whl

---
+++ packages/petab-select/petab_select-0.1.9-py3-none-any.whl/petab_select/__init__.py
@@ -0,0 +1,11 @@
+"""Model selection extension for PEtab."""
+
+from .candidate_space import *
+from .constants import *
+from .criteria import *
+from .misc import *
+from .model import *
+from .model_space import *
+from .model_subspace import *
+from .problem import *
+from .ui import *
