fb806629ef5d5669c2601f25f3b451bd538651f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:48 +0000

    Add package pyhatching/0.2.0/pyhatching-0.2.0-py3-none-any.whl

---
+++ packages/pyhatching/pyhatching-0.2.0-py3-none-any.whl/pyhatching/__main__.py
@@ -0,0 +1,40 @@
+"""CLI"""
+
+import asyncio
+import os
+
+from . import _cmds, PyHatchingClient
+from ._args import MAIN_PARSER
+from .errors import PyHatchingError
+
+
+async def main():
+    """Main function for the CLI."""
+
+    args = MAIN_PARSER.parse_args()
+
+    if args.token is None:
+        if (token := os.environ.get("HATCHING_TOKEN")):
+            args.token = token
+        else:
+            print("No token in $HATCHING_TOKEN or passed with --token!")
+            return
+
+    if args.command is None:
+        print("Must specify a command!")
+
+    try:
+        cmd = getattr(_cmds, f"do_{args.command}")
+    except AttributeError as err:
+        print(f"Unable to find command func for {args.command}: {err}")
+        return
+
+    async with PyHatchingClient(api_key=args.token) as client:
+        try:
+            await cmd(client, args)
+        except PyHatchingError as err:
+            print(f"{err.__class__.__name__} while executing {args.command}: {err}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
