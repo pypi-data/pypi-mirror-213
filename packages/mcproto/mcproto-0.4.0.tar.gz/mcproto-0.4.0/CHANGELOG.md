8bb9418627dfa150e5163a67768dd9bb3f752518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:28 +0000

    Add package mcproto/0.4.0/mcproto-0.4.0.tar.gz

---
+++ packages/mcproto/mcproto-0.4.0.tar.gz/mcproto-0.4.0/CHANGELOG.md
@@ -0,0 +1,104 @@
+## Version 0.4.0 (2023-06-11)
+
+### Breaking Changes
+
+- [#41](https://github.com/py-mine/mcproto/issues/41): Rename `mcproto.packets.abc` to `mcproto.packets.packet`
+- [#116](https://github.com/py-mine/mcproto/issues/116): Restructure the project, moving to a single protocol version model
+  - This change does NOT have a deprecation period, and will very likely break most existing code-bases. However this change is necessary, as multi-version support was unsustainable (see issue #45 for more details)
+  - Any packets and types will no longer be present in versioned folders (mcproto.packets.v757.xxx), but rather be directly in the parent directory (mcproto.packets.xxx).
+  - This change doesn't affect manual communication with the server, connection, and basic IO writers/readers remain the same.
+
+---
+
+
+## Version 0.3.0 (2023-06-08)
+
+### Features
+
+- [#54](https://github.com/py-mine/mcproto/issues/54): Add support for LOGIN state packets
+  - `LoginStart`
+  - `LoginEncryptionRequest`
+  - `LoginEncryptionResponse`
+  - `LoginSuccess`
+  - `LoginDisconnect`
+  - `LoginPluginRequest`
+  - `LoginPluginResponse`
+  - `LoginSetCompression`
+
+### Bugfixes
+
+- [#75](https://github.com/py-mine/mcproto/issues/75): Increase the stack level of warnings shown on protocol version fallbacks
+- [#113](https://github.com/py-mine/mcproto/issues/113): TCP connections now properly shut down the connection gracefully (TCP FIN)
+
+### Documentation Improvements
+
+- [#2](https://github.com/py-mine/mcproto/issues/2): Add Sphinx and basic docs layout
+- [#18](https://github.com/py-mine/mcproto/issues/18): Rewrite all docstrings into proper Sphinx format, instead of using markdown.
+- [#27](https://github.com/py-mine/mcproto/issues/27): Add changelog page to docs, linking `CHANGELOG.md`, including unreleased changes from fragments.
+- [#28](https://github.com/py-mine/mcproto/issues/28): Use furo theme for the documentation
+- [#34](https://github.com/py-mine/mcproto/issues/34): Add version guarantees page
+- [#40](https://github.com/py-mine/mcproto/issues/40): Move code of conduct to the docs.
+- Improve readability of the changelog readme (changes/README.md)
+   - Mention taskipy `changelog-preview` shorthand command
+   - Add category headers splitting things up, for better readability
+   - Explain how to express multiple changes related to a single goal in a changelog fragment.
+- Include `CHANGELOG.md` file in project's distribution files.
+
+### Internal Changes
+
+- [#12](https://github.com/py-mine/mcproto/issues/12): Replace HassanAbouelela setup-python action with ItsDrike/setup-python in CI workflows
+- [#17](https://github.com/py-mine/mcproto/issues/17): Start using codeclimate to monitor code coverage and it's changes
+- [#35](https://github.com/py-mine/mcproto/issues/35): Add more tests
+- [#38](https://github.com/py-mine/mcproto/issues/38): Replace our implementation of `SemanticVersion` with a community-maintained `semantic-version` package.
+- [#53](https://github.com/py-mine/mcproto/issues/53): Mark all packet classes as `typing.final`, making the type-checker enforce existence of concrete implementations for all abstract methods.
+- [#112](https://github.com/py-mine/mcproto/issues/112): Removed `codespell` linter. This proved too annoying, especially when we already have a lot of linters here. Spelling mistakes can simply be caught in the review process.
+- [#114](https://github.com/py-mine/mcproto/issues/114): Use latest poetry version in CI workflows (remove version lock - at 1.3.1)
+- The `documentation` category of changelog was renamed to shorter `docs`
+
+---
+
+
+## Version 0.2.0 (2022-12-30)
+
+### Features
+
+- [#14](https://github.com/py-mine/mcproto/issues/14): Add `__slots__` to most classes in the project
+  - All connection classes are now slotted
+  - Classes in `mcproto.utils.abc` are now slotted
+- Separate packet interaction functions into `mcproto.packets.interactions`, (though they're reexported in
+  `mcproto.packets`, so no breaking changes)
+
+### Bugfixes
+
+- [#14](https://github.com/py-mine/mcproto/issues/14): Add missing `__slots__` to `ServerBoundPacket` and `ClientBoundPacket` subclasses, which inherited from slotted
+  `Packet`, but didn't themselves define `__slots__`, causing `__dict__` to be needlessly created.
+- The error message produced by `RequiredParamsABCMixin` class when a required no MRO class variable isn't present now
+  includes a previously missing space, making it more readable.
+
+### Documentation Improvements
+
+- [#7](https://github.com/py-mine/mcproto/issues/7): Add and start keeping a changelog, managed by towncrier.
+- [#13](https://github.com/py-mine/mcproto/issues/13): Add a security policy.
+
+### Internal Changes
+
+- [#6](https://github.com/py-mine/mcproto/issues/6): Rework deprecation system
+  - Drop support for date-based deprecations, versions work better
+  - Provide `deprecation_warn` function, which emits warnings directly, no need for a decorator
+  - Add a `SemanticVersion` class, supporting version comparisons
+  - If the project's version is already higher than the specified deprecation removal version, raise a DeprecationWarning
+    as a full exception (rather than just a warning).
+- [#7](https://github.com/py-mine/mcproto/issues/7): Add towncrier for managing changelog
+- [#14](https://github.com/py-mine/mcproto/issues/14): Add slotscheck, ensuring `__slots__` are defined properly everywhere.
+- [#14](https://github.com/py-mine/mcproto/issues/14): Make `typing-extensions` a runtime dependency and use it directly, don't rely on `if typing.TYPE_CHECKING` blocks.
+- [#15](https://github.com/py-mine/mcproto/issues/15): Add codespell tool, to automatically find spelling mistakes.
+- Add README file into the `tests/` folder, explaining how we use unit-tests and some basics of testing.
+- Add `CustomMockMixin` internal class, inheriting from `UnpropagatingMockMixin`, but also allowing to use `spec_set` as
+  class variable, as it will automatically pass it into `__init__` of the mock class.
+- Add several new flake8 extensions, and rework flake8 config file
+- Add support for specifying what child mock type to propagate in `UnpropagatingMockMixin` class (for unit-tests).
+
+---
+
+*The changelog was added during development of 0.2.0, so nothing prior is documented here. Try checking the GitHub
+releases, or git commit history directly.*
---
+++ packages/mcproto/mcproto-0.4.0.tar.gz/mcproto-0.4.0/mcproto/connection.py
@@ -0,0 +1,335 @@
+from __future__ import annotations
+
+import asyncio
+import errno
+import socket
+from abc import ABC, abstractmethod
+from typing import Generic, Optional, TypeVar
+
+import asyncio_dgram
+from typing_extensions import ParamSpec, Self
+
+from mcproto.protocol.base_io import BaseAsyncReader, BaseAsyncWriter, BaseSyncReader, BaseSyncWriter
+
+__all__ = [
+    "AsyncConnection",
+    "SyncConnection",
+    "TCPAsyncConnection",
+    "TCPSyncConnection",
+    "UDPAsyncConnection",
+    "UDPSyncConnection",
+]
+
+P = ParamSpec("P")
+R = TypeVar("R")
+T_SOCK = TypeVar("T_SOCK", bound=socket.socket)
+T_STREAMREADER = TypeVar("T_STREAMREADER", bound=asyncio.StreamReader)
+T_STREAMWRITER = TypeVar("T_STREAMWRITER", bound=asyncio.StreamWriter)
+T_DATAGRAM_CLIENT = TypeVar("T_DATAGRAM_CLIENT", bound=asyncio_dgram.aio.DatagramClient)
+
+
+class SyncConnection(BaseSyncReader, BaseSyncWriter, ABC):
+    """Base class for all classes handling synchronous connections."""
+
+    __slots__ = ("closed",)
+
+    def __init__(self):
+        self.closed = False
+
+    @classmethod
+    @abstractmethod
+    def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _close(self) -> None:
+        """Close the underlying connection."""
+        raise NotImplementedError
+
+    def close(self) -> None:
+        """Close the connection (it cannot be used after this)."""
+        self._close()
+        self.closed = True
+
+    def __enter__(self) -> Self:
+        if self.closed:
+            raise IOError("Connection already closed.")
+        return self
+
+    def __exit__(self, *a, **kw) -> None:
+        self.close()
+
+
+class AsyncConnection(BaseAsyncReader, BaseAsyncWriter, ABC):
+    """Base class for all classes handling asynchronous connections."""
+
+    __slots__ = ("closed",)
+
+    def __init__(self):
+        self.closed = False
+
+    @classmethod
+    @abstractmethod
+    async def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def _close(self) -> None:
+        """Close the underlying connection."""
+        raise NotImplementedError
+
+    async def close(self) -> None:
+        """Close the connection (it cannot be used after this)."""
+        await self._close()
+        self.closed = True
+
+    async def __aenter__(self) -> Self:
+        if self.closed:
+            raise IOError("Connection already closed.")
+        return self
+
+    async def __aexit__(self, *a, **kw) -> None:
+        await self.close()
+
+
+class TCPSyncConnection(SyncConnection, Generic[T_SOCK]):
+    """Synchronous connection using a TCP :class:`~socket.socket`."""
+
+    __slots__ = ("socket",)
+
+    def __init__(self, socket: T_SOCK):
+        super().__init__()
+        self.socket = socket
+
+    @classmethod
+    def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        sock = socket.create_connection(address, timeout=timeout)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        return cls(sock)
+
+    def read(self, length: int) -> bytearray:
+        """Receive data sent through the connection.
+
+        :param length:
+            Amount of bytes to be received. If the requested amount can't be received
+            (server didn't send that much data/server didn't send any data), an :exc:`IOError`
+            will be raised.
+        """
+        result = bytearray()
+        while len(result) < length:
+            new = self.socket.recv(length - len(result))
+            if len(new) == 0:
+                # No information at all
+                if len(result) == 0:
+                    raise IOError("Server did not respond with any information.")
+                # Only sent a few bytes, but we requested more
+                raise IOError(
+                    f"Server stopped responding (got {len(result)} bytes, but expected {length} bytes)."
+                    f" Partial obtained data: {result!r}"
+                )
+            result.extend(new)
+
+        return result
+
+    def write(self, data: bytes) -> None:
+        """Send given ``data`` over the connection."""
+        self.socket.send(data)
+
+    def _close(self) -> None:
+        """Close the underlying connection."""
+        # Gracefully end the connection first (shutdown), informing the other side
+        # we're disconnecting, and waiting for them to disconnect cleanly (TCP FIN)
+        try:
+            self.socket.shutdown(socket.SHUT_RDWR)
+        except OSError as exc:
+            if exc.errno != errno.ENOTCONN:
+                raise
+
+        self.socket.close()
+
+
+class TCPAsyncConnection(AsyncConnection, Generic[T_STREAMREADER, T_STREAMWRITER]):
+    """Asynchronous TCP connection using :class:`~asyncio.StreamWriter` and :class:`~asyncio.StreamReader`."""
+
+    __slots__ = ("reader", "writer", "timeout")
+
+    def __init__(self, reader: T_STREAMREADER, writer: T_STREAMWRITER, timeout: float):
+        super().__init__()
+        self.reader = reader
+        self.writer = writer
+        self.timeout = timeout
+
+    @classmethod
+    async def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        conn = asyncio.open_connection(address[0], address[1])
+        reader, writer = await asyncio.wait_for(conn, timeout=timeout)
+        return cls(reader, writer, timeout)
+
+    async def read(self, length: int) -> bytearray:
+        """Receive data sent through the connection.
+
+        :param length:
+            Amount of bytes to be received. If the requested amount can't be received
+            (server didn't send that much data/server didn't send any data), an :exc:`IOError`
+            will be raised.
+        """
+        result = bytearray()
+        while len(result) < length:
+            new = await asyncio.wait_for(self.reader.read(length - len(result)), timeout=self.timeout)
+            if len(new) == 0:
+                # No information at all
+                if len(result) == 0:
+                    raise IOError("Server did not respond with any information.")
+                # Only sent a few bytes, but we requested more
+                raise IOError(
+                    f"Server stopped responding (got {len(result)} bytes, but expected {length} bytes)."
+                    f" Partial obtained data: {result!r}"
+                )
+            result.extend(new)
+
+        return result
+
+    async def write(self, data: bytes) -> None:
+        """Send given ``data`` over the connection."""
+        self.writer.write(data)
+
+    async def _close(self) -> None:
+        """Close the underlying connection."""
+        # Close automatically performs a graceful TCP connection shutdown too
+        self.writer.close()
+
+    @property
+    def socket(self) -> socket.socket:
+        """Obtain the underlying socket behind the :class:`~asyncio.Transport`."""
+        return self.writer.transport._sock  # type: ignore
+
+
+class UDPSyncConnection(SyncConnection, Generic[T_SOCK]):
+    """Synchronous connection using a UDP :class:`~socket.socket`."""
+
+    __slots__ = ("socket", "address")
+
+    BUFFER_SIZE = 65535
+
+    def __init__(self, socket: T_SOCK, address: tuple[str, int]):
+        super().__init__()
+        self.socket = socket
+        self.address = address
+
+    @classmethod
+    def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(timeout)
+        return cls(sock, address)
+
+    def read(self, length: Optional[int] = None) -> bytearray:
+        """Receive data sent through the connection.
+
+        :param length:
+            For UDP connections, ``length`` parameter is ignored and not required.
+            Instead, UDP connections always read exactly :attr:`.BUFFER_SIZE` bytes.
+
+            If the requested amount can't be received (server didn't send that much
+            data/server didn't send any data), an :exc:`IOError` will be raised.
+        """
+        result = bytearray()
+        while len(result) == 0:
+            received_data, server_addr = self.socket.recvfrom(self.BUFFER_SIZE)
+            result.extend(received_data)
+        return result
+
+    def write(self, data: bytes) -> None:
+        """Send given ``data`` over the connection."""
+        self.socket.sendto(data, self.address)
+
+    def _close(self) -> None:
+        """Close the underlying connection."""
+        self.socket.close()
+
+
+class UDPAsyncConnection(AsyncConnection, Generic[T_DATAGRAM_CLIENT]):
+    """Asynchronous UDP connection using :class:`~asyncio_dgram.DatagramClient`."""
+
+    __slots__ = ("stream", "timeout")
+
+    def __init__(self, stream: T_DATAGRAM_CLIENT, timeout: float):
+        super().__init__()
+        self.stream = stream
+        self.timeout = timeout
+
+    @classmethod
+    async def make_client(cls, address: tuple[str, int], timeout: float) -> Self:
+        """Construct a client connection (Client -> Server) to given server ``address``.
+
+        :param address: Address of the server to connection to.
+        :param timeout:
+            Amount of seconds to wait for the connection to be established.
+            If connection can't be established within this time, :exc:`TimeoutError` will be raised.
+            This timeout is then also used for any further data receiving.
+        """
+        conn = asyncio_dgram.connect(address)
+        stream = await asyncio.wait_for(conn, timeout=timeout)
+        return cls(stream, timeout)
+
+    async def read(self, length: Optional[int] = None) -> bytearray:
+        """Receive data sent through the connection.
+
+        :param length:
+            For UDP connections, ``length`` parameter is ignored and not required.
+
+            If the requested amount can't be received (server didn't send that much
+            data/server didn't send any data), an :exc:`IOError` will be raised.
+        """
+        result = bytearray()
+        while len(result) == 0:
+            received_data, server_addr = await asyncio.wait_for(self.stream.recv(), timeout=self.timeout)
+            result.extend(received_data)
+        return result
+
+    async def write(self, data: bytes) -> None:
+        """Send given ``data`` over the connection."""
+        await self.stream.send(data)
+
+    async def _close(self) -> None:
+        """Close the underlying connection."""
+        self.stream.close()
