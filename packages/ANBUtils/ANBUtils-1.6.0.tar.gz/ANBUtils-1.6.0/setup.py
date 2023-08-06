3973d104479de09eb12b9d2c44417100182d40be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:09 +0000

    Add package ANBUtils/1.6.0/ANBUtils-1.6.0.tar.gz

---
+++ packages/ANBUtils/ANBUtils-1.6.0.tar.gz/ANBUtils-1.6.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('version', 'r', encoding='utf-8') as f:
+    version = f.read()
+
+
+
+setup(
+    name="ANBUtils",
+    version=version,
+    packages=find_packages(),
+    author="redbson",
+    author_email="redbson@gmail.com",
+    url="https://github.com/redbson/ANBUtils",
+    description = "ANBUilts is a versatile Python package that offers a comprehensive set of utility functions and tools for data analysis, database operations, and messaging integration.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8',
+    install_requires=[
+        "matplotlib>=3.0.0",
+        "numpy>=1.0.0",
+        "pandas>=1.0.0",
+        "pymongo>=4.0.2",
+        "requests>=2.0.0",
+        "matplotlib>=3.0.0",
+    ]
+)
