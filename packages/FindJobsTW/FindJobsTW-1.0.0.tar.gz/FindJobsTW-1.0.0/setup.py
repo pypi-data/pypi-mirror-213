d196e162051d63c107baef3c6f7213494de26019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:00 +0000

    Add package FindJobsTW/1.0.0/FindJobsTW-1.0.0.tar.gz

---
+++ packages/FindJobsTW/FindJobsTW-1.0.0.tar.gz/FindJobsTW-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
+setup(
+    name='FindJobsTW',    
+    version='1.0.0',      
+    packages=find_packages(),    
+    install_requires=requirements,
+    author="Danny Fin",
+    author_email="dannyfinselect@outlook.com",
+    description="A Python package to find jobs on 104.com.tw based on specific keywords.",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),# 若Discription.md中有中文 須加上 encoding="utf-8"
+    long_description_content_type="text/markdown",
+    url="https://github.com/DannyFin/FindJobsTW",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
