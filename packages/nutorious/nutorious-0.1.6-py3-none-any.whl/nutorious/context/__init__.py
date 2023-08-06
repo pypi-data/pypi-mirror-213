2335f51f06cf61ff59fe94d13d3230ae0c38d91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:15 +0000

    Add package nutorious/0.1.6/nutorious-0.1.6-py3-none-any.whl

---
+++ packages/nutorious/nutorious-0.1.6-py3-none-any.whl/nutorious/context/__init__.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+from attrs import define
+from nutorious.config import Config
+from nutorious.model import Journal
+
+
+@define
+class Context:
+    config: Config
+    journal: Journal
+    options: Any
