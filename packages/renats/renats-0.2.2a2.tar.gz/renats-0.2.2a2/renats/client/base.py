06f1230b48ecba28d6dee17b34de1fce607a9f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:23 +0000

    Add package renats/0.2.2a2/renats-0.2.2a2.tar.gz

---
+++ packages/renats/renats-0.2.2a2.tar.gz/renats-0.2.2a2/renats/client/base.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Callable, Any
+
+from typing_extensions import Self
+
+from .message import Message
+
+HeadersType = dict[str, str]
+SubscriptionCallbackType = Callable[[Message], Any]
+SubscriptionType = TypeVar("SubscriptionType", bound="Subscription")
+
+
+class NATS(ABC):
+    @abstractmethod
+    async def connect(self, servers: tuple[tuple[str, int]]) -> Self:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def publish(self, subject: str, payload: bytes = b"", reply_subject: str = None, headers: HeadersType = None):
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def subscribe(self, subject: str, callback: SubscriptionCallbackType) -> SubscriptionType:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def unsubscribe(self, subscription_id: str, messages_left: int = 0):
+        raise NotImplementedError()
