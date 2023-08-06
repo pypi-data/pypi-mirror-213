e56a59664833bb40413719b755261d5d5a9ab90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:58 +0000

    Add package kgclitest/0.3.0/kgclitest-0.3.0.tar.gz

---
+++ packages/kgclitest/kgclitest-0.3.0.tar.gz/kgclitest-0.3.0/src/kgclitest/greetings.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: Optional[str] = None):
+    if name:
+        typer.echo(f"Hello {name}")
+    else:
+        typer.echo("Hello World!")
+
+
+@app.command()
+def bye(name: Optional[str] = None):
+    if name:
+        typer.echo(f"Bye {name}")
+    else:
+        typer.echo("Goodbye!")
+
+
