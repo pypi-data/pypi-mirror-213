851aec12fe742c6121b1b041ef4665e6792ad7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package lithic/0.15.1/lithic-0.15.1-py3-none-any.whl

---
+++ packages/lithic/lithic-0.15.1-py3-none-any.whl/lithic/types/auth_rule_update_params.py
@@ -0,0 +1,43 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["AuthRuleUpdateParams"]
+
+
+class AuthRuleUpdateParams(TypedDict, total=False):
+    allowed_countries: List[str]
+    """
+    Array of country codes for which the Auth Rule will permit transactions. Note
+    that only this field or `blocked_countries` can be used for a given Auth Rule.
+    """
+
+    allowed_mcc: List[str]
+    """
+    Array of merchant category codes for which the Auth Rule will permit
+    transactions. Note that only this field or `blocked_mcc` can be used for a given
+    Auth Rule.
+    """
+
+    avs_type: Literal["ZIP_ONLY"]
+    """
+    Address verification to confirm that postal code entered at point of transaction
+    (if applicable) matches the postal code on file for a given card.
+    """
+
+    blocked_countries: List[str]
+    """
+    Array of country codes for which the Auth Rule will automatically decline
+    transactions. Note that only this field or `allowed_countries` can be used for a
+    given Auth Rule.
+    """
+
+    blocked_mcc: List[str]
+    """
+    Array of merchant category codes for which the Auth Rule will automatically
+    decline transactions. Note that only this field or `allowed_mcc` can be used for
+    a given Auth Rule.
+    """
