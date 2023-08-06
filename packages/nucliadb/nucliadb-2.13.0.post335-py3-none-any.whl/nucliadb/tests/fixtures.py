ae0d8882d4723c2d66e8969b83b9d0f100d8e762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:41 +0000

    Add package nucliadb/2.13.0.post335/nucliadb-2.13.0.post335-py3-none-any.whl

---
+++ packages/nucliadb/nucliadb-2.13.0.post335-py3-none-any.whl/nucliadb/tests/fixtures.py
@@ -0,0 +1,358 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import os
+import tempfile
+from unittest.mock import Mock
+
+import pytest
+from grpc import aio
+from httpx import AsyncClient
+from nucliadb_protos.train_pb2_grpc import TrainStub
+from nucliadb_protos.utils_pb2 import Relation, RelationNode
+from nucliadb_protos.writer_pb2 import BrokerMessage
+from nucliadb_protos.writer_pb2_grpc import WriterStub
+
+from nucliadb.standalone.config import config_nucliadb
+from nucliadb.standalone.run import run_async_nucliadb
+from nucliadb.standalone.settings import Settings
+from nucliadb.tests.utils import inject_message
+from nucliadb.writer import API_PREFIX
+from nucliadb_utils.tests import free_port
+from nucliadb_utils.utilities import (
+    Utility,
+    clean_utility,
+    clear_global_cache,
+    get_utility,
+    set_utility,
+)
+
+
+@pytest.fixture(scope="function")
+async def dummy_processing():
+    from nucliadb_utils.settings import nuclia_settings
+
+    nuclia_settings.dummy_processing = True
+
+
+@pytest.fixture(scope="function")
+def telemetry_disabled():
+    os.environ["NUCLIADB_DISABLE_TELEMETRY"] = "True"
+    yield
+    os.environ.pop("NUCLIADB_DISABLE_TELEMETRY")
+
+
+def reset_config():
+    from nucliadb.ingest import settings as ingest_settings
+    from nucliadb.ingest.orm import NODE_CLUSTER
+    from nucliadb.train import settings as train_settings
+    from nucliadb.writer import settings as writer_settings
+    from nucliadb_utils import settings as utils_settings
+    from nucliadb_utils.cache import settings as cache_settings
+
+    ingest_settings.settings.parse_obj(ingest_settings.Settings())
+    train_settings.settings.parse_obj(train_settings.Settings())
+    writer_settings.settings.parse_obj(writer_settings.Settings())
+    cache_settings.settings.parse_obj(cache_settings.Settings())
+
+    utils_settings.audit_settings.parse_obj(utils_settings.AuditSettings())
+    utils_settings.indexing_settings.parse_obj(utils_settings.IndexingSettings())
+    utils_settings.transaction_settings.parse_obj(utils_settings.TransactionSettings())
+    utils_settings.nucliadb_settings.parse_obj(utils_settings.NucliaDBSettings())
+    utils_settings.nuclia_settings.parse_obj(utils_settings.NucliaSettings())
+    utils_settings.storage_settings.parse_obj(utils_settings.StorageSettings())
+
+    NODE_CLUSTER.local_node = None
+
+
+@pytest.fixture(scope="function")
+async def nucliadb(dummy_processing, telemetry_disabled):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        settings = Settings(
+            driver="local",
+            file_backend="local",
+            local_files=f"{tmpdir}/blob",
+            driver_local_url=f"{tmpdir}/main",
+            data_path=f"{tmpdir}/node",
+            http_port=free_port(),
+            ingest_grpc_port=free_port(),
+            train_grpc_port=free_port(),
+        )
+        config_nucliadb(settings)
+        server = await run_async_nucliadb(settings)
+
+        yield settings
+
+        reset_config()
+        clear_global_cache()
+        await server.shutdown()
+
+
+@pytest.fixture(scope="function")
+async def nucliadb_reader(nucliadb: Settings):
+    async with AsyncClient(
+        headers={"X-NUCLIADB-ROLES": "READER"},
+        base_url=f"http://localhost:{nucliadb.http_port}/{API_PREFIX}/v1",
+    ) as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+async def nucliadb_writer(nucliadb: Settings):
+    async with AsyncClient(
+        headers={"X-NUCLIADB-ROLES": "WRITER"},
+        base_url=f"http://localhost:{nucliadb.http_port}/{API_PREFIX}/v1",
+    ) as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+async def nucliadb_manager(nucliadb: Settings):
+    async with AsyncClient(
+        headers={"X-NUCLIADB-ROLES": "MANAGER"},
+        base_url=f"http://localhost:{nucliadb.http_port}/{API_PREFIX}/v1",
+    ) as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+async def knowledgebox(nucliadb_manager: AsyncClient):
+    resp = await nucliadb_manager.post("/kbs", json={"slug": "knowledgebox"})
+    assert resp.status_code == 201
+    uuid = resp.json().get("uuid")
+    yield uuid
+    resp = await nucliadb_manager.delete(f"/kb/{uuid}")
+    assert resp.status_code == 200
+
+
+@pytest.fixture(scope="function")
+async def nucliadb_grpc(nucliadb: Settings):
+    stub = WriterStub(aio.insecure_channel(f"localhost:{nucliadb.ingest_grpc_port}"))
+    return stub
+
+
+@pytest.fixture(scope="function")
+async def nucliadb_train(nucliadb: Settings):
+    stub = TrainStub(aio.insecure_channel(f"localhost:{nucliadb.train_grpc_port}"))
+    return stub
+
+
+@pytest.fixture(scope="function")
+async def knowledge_graph(
+    nucliadb_writer: AsyncClient, nucliadb_grpc: WriterStub, knowledgebox
+):
+    resp = await nucliadb_writer.post(
+        f"/kb/{knowledgebox}/resources",
+        headers={"X-SYNCHRONOUS": "True"},
+        json={
+            "title": "Knowledge graph",
+            "slug": "knowledgegraph",
+            "summary": "Test knowledge graph",
+        },
+    )
+    assert resp.status_code == 201
+    rid = resp.json()["uuid"]
+
+    nodes = {
+        "Animal": RelationNode(
+            value="Animal", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Batman": RelationNode(
+            value="Batman", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Becquer": RelationNode(
+            value="Becquer", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Cat": RelationNode(
+            value="Cat", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Catwoman": RelationNode(
+            value="Catwoman", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Eric": RelationNode(
+            value="Eric", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Fly": RelationNode(
+            value="Fly", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Gravity": RelationNode(
+            value="Gravity", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Joan Antoni": RelationNode(
+            value="Joan Antoni", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Joker": RelationNode(
+            value="Joker", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Newton": RelationNode(
+            value="Newton", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Physics": RelationNode(
+            value="Physics", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Poetry": RelationNode(
+            value="Poetry", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+        "Swallow": RelationNode(
+            value="Swallow", ntype=RelationNode.NodeType.ENTITY, subtype=""
+        ),
+    }
+
+    edges = [
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Batman"],
+            to=nodes["Catwoman"],
+            relation_label="love",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Batman"],
+            to=nodes["Joker"],
+            relation_label="fight",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Joker"],
+            to=nodes["Physics"],
+            relation_label="enjoy",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Catwoman"],
+            to=nodes["Cat"],
+            relation_label="imitate",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Cat"],
+            to=nodes["Animal"],
+            relation_label="species",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Newton"],
+            to=nodes["Physics"],
+            relation_label="study",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Newton"],
+            to=nodes["Gravity"],
+            relation_label="formulate",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Eric"],
+            to=nodes["Cat"],
+            relation_label="like",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Eric"],
+            to=nodes["Joan Antoni"],
+            relation_label="collaborate",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Joan Antoni"],
+            to=nodes["Eric"],
+            relation_label="collaborate",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Joan Antoni"],
+            to=nodes["Becquer"],
+            relation_label="read",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Becquer"],
+            to=nodes["Poetry"],
+            relation_label="write",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Becquer"],
+            to=nodes["Poetry"],
+            relation_label="like",
+        ),
+        Relation(
+            relation=Relation.RelationType.ABOUT,
+            source=nodes["Poetry"],
+            to=nodes["Swallow"],
+            relation_label="about",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Swallow"],
+            to=nodes["Animal"],
+            relation_label="species",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Swallow"],
+            to=nodes["Fly"],
+            relation_label="can",
+        ),
+        Relation(
+            relation=Relation.RelationType.ENTITY,
+            source=nodes["Fly"],
+            to=nodes["Gravity"],
+            relation_label="defy",
+        ),
+    ]
+
+    bm = BrokerMessage()
+    bm.uuid = rid
+    bm.kbid = knowledgebox
+    bm.relations.extend(edges)
+    await inject_message(nucliadb_grpc, bm)
+
+    return (nodes, edges)
+
+
+@pytest.fixture(scope="function")
+async def stream_audit(natsd: str):
+    from nucliadb_utils.audit.stream import StreamAuditStorage
+    from nucliadb_utils.settings import audit_settings
+
+    audit = StreamAuditStorage(
+        [natsd],
+        audit_settings.audit_jetstream_target,  # type: ignore
+        audit_settings.audit_partitions,
+        audit_settings.audit_hash_seed,
+    )
+    await audit.initialize()
+    yield audit
+    await audit.finalize()
+
+
+@pytest.fixture(scope="function")
+def predict_mock() -> Mock:  # type: ignore
+    predict = get_utility(Utility.PREDICT)
+    mock = Mock()
+    set_utility(Utility.PREDICT, mock)
+
+    yield mock
+
+    if predict is None:
+        clean_utility(Utility.PREDICT)
+    else:
+        set_utility(Utility.PREDICT, predict)
