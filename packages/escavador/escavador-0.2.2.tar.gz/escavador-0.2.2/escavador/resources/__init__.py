14c22509d1196de22fa5f6caf63ab45f67666347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package escavador/0.2.2/escavador-0.2.2.tar.gz

---
+++ packages/escavador/escavador-0.2.2.tar.gz/escavador-0.2.2/escavador/resources/__init__.py
@@ -0,0 +1,9 @@
+from .helpers.enums_v1 import (TiposMonitoramentosTribunal,
+                               TiposMonitoramentosDiario,
+                               TiposBusca,
+                               TiposTermo,
+                               FrequenciaMonitoramentoTribunal,
+                               StatusCallback)
+from .helpers.enums_v2 import (SiglaTribunal,
+                               CriterioOrdenacao,
+                               Ordem)
