1b763a58cfc953289c7d8098f36a83b113d51322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:44 +0000

    Add package archbuilder/0.1.1/archbuilder-0.1.1-py3-none-any.whl

---
+++ packages/archbuilder/archbuilder-0.1.1-py3-none-any.whl/builder/builder.py
@@ -0,0 +1,29 @@
+"""
+"""
+from pathlib import Path
+
+
+class Builder:
+    """"""
+
+    def __init__(self, project, template, output="build", root=Path.cwd()):
+        """"""
+        self.project = project
+        self.template = template
+        self.output = output
+        self.root = root
+
+    def build(self):
+        """"""
+        for path in self.paths():
+            if not path.exists():
+                path.parent.mkdir(parents=True, exist_ok=True)
+                path.touch(exist_ok=True)
+
+    def paths(self):
+        """"""
+        paths = list()
+        for tail in self.template.tails():
+            path = Path(self.root / self.output / self.project / tail)
+            paths.append(path)
+        return paths
