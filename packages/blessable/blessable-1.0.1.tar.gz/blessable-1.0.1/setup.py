af4d2079cb41f24c8aa520054e42ad0a57d4034a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:18 +0000

    Add package blessable/1.0.1/blessable-1.0.1.tar.gz

---
+++ packages/blessable/blessable-1.0.1.tar.gz/blessable-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='blessable',
+    version='1.0.1',
+    author='www.mrfake.name',
+    description='"Blessable" - a simple markup language for Blessings.',
+    homepage='https://github.com/fakerybakery/blessable',
+    packages=['blessable'],
+    install_requires=['blessed'],
+)
