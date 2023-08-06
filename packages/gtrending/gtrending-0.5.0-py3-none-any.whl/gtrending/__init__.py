44f74cacecdb02a67df2fe4eb5ac2d46fcc8c1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:50 +0000

    Add package gtrending/0.5.0/gtrending-0.5.0-py3-none-any.whl

---
+++ packages/gtrending/gtrending-0.5.0-py3-none-any.whl/gtrending/__init__.py
@@ -0,0 +1,27 @@
+from .fetch import (
+    fetch_repos,
+    fetch_developers,
+)
+
+from .paramutils import (
+    languages_list,
+    languages_params,
+    languages_dict,
+    languages_names,
+    spoken_languages_list,
+    spoken_languages_codes,
+    spoken_languages_dict,
+    spoken_languages_names,
+    check_language,
+    check_spoken_language_name,
+    check_spoken_language_code,
+    check_spoken_language,
+    check_since,
+    convert_spoken_language_name_to_code,
+    convert_language_name_to_param,
+)
+
+__version__ = "0.5.0"
+__url__ = "https://github.com/hedyhli/gtrending"
+__author__ = "hedyhli"
+__author_email__ = "hedy@tilde.cafe"
