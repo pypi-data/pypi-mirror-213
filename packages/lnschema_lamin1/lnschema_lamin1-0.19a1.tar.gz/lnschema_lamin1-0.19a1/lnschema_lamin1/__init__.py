b19f9657759f32a6325452776555328bfe6595ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:40 +0000

    Add package lnschema_lamin1/0.19a1/lnschema_lamin1-0.19a1.tar.gz

---
+++ packages/lnschema_lamin1/lnschema_lamin1-0.19a1.tar.gz/lnschema_lamin1-0.19a1/lnschema_lamin1/__init__.py
@@ -0,0 +1,35 @@
+"""Lamin's `lamin1` exemplary lab schema.
+
+Import the package::
+
+   import lnschema_lamin1
+
+ORMs:
+
+.. autosummary::
+   :toctree: .
+
+   Experiment
+   ExperimentType
+   Biosample
+   Techsample
+   Treatment
+   Well
+
+"""
+
+_schema_id = "tvhn"
+_name = "lamin1"
+__version__ = "0.19a1"
+
+from lamindb_setup import _check_instance_setup
+
+if _check_instance_setup():
+    from .models import (  # noqa
+        Biosample,
+        Experiment,
+        ExperimentType,
+        Techsample,
+        Treatment,
+        Well,
+    )
