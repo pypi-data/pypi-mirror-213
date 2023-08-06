b38b7d53314d7eea9154cd50923dafa517e96659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:32 +0000

    Add package dbis-relational-model/0.0.10/dbis-relational-model-0.0.10.tar.gz

---
+++ packages/dbis-relational-model/dbis-relational-model-0.0.10.tar.gz/dbis-relational-model-0.0.10/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+class Version(object):
+    name="relational_model"
+    description="RWTH Aachen Computer Science i5/dbis assets for Lecture Datenbanken und Informationssysteme"
+    version='0.0.10'
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent.resolve()
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="dbis-relational-model",
+    version=Version.version,
+    description=Version.description,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rwth-acis/dbis-relational-model.git",
+    author="Philipp Hochmann",
+    author_email="hochmann@dbis.rwth-aachen.de",
+    license="Apache",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    packages=["relational_model"],
+    include_package_data=True,
+    install_requires=['levenshtein']
+)
---
+++ packages/dbis-relational-model/dbis-relational-model-0.0.10.tar.gz/dbis-relational-model-0.0.10/tests/test_Task.py
@@ -0,0 +1,12 @@
+import json
+from locale import ERA
+from tests.basetest import Basetest
+
+class TestRelational(Basetest):
+    def setUp(self, debug=False, profile=True):
+        return super().setUp(debug, profile)
+
+    def testExc(self):
+        '''
+            Todo
+        '''
