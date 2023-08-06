7eac19b4796500e62255e38f6d71f4959a493bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:18 +0000

    Add package mpd-queue/1.0.3/mpd-queue-1.0.3.tar.gz

---
+++ packages/mpd-queue/mpd-queue-1.0.3.tar.gz/mpd-queue-1.0.3/README.md
@@ -0,0 +1,31 @@
+# mpd-queue
+
+A small [Beets](https://github.com/beetbox/beets) plugin to add newly imported tracks to the queue of your [MPD](https://github.com/MusicPlayerDaemon/MPD) server.
+
+## Installation
+
+To install the plugin, simply run the following:
+```python
+pip install mpd-queue
+```
+and then add `mpd-queue` to the list of enabled plugins in your Beets config.
+
+## Configuration
+
+Optionally, to specify a non-default location for the MPD server, add a configuration block like this:
+```yaml
+mpd-queue:
+  host: example.com
+  port: 6601
+```
+
+### Options
+
+| Key  | Description                 | Default value |
+|------|-----------------------------|---------------|
+| host | Hostname of the MPD server  | localhost     |
+| port | Port to use when connecting | 6600          |
+
+## Usage
+
+Simply run `beet import` as you normally would. Any imported files should (quietly) be added to the MPD queue.
---
+++ packages/mpd-queue/mpd-queue-1.0.3.tar.gz/mpd-queue-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mpd-queue',
+    version='1.0.3',
+    description='Beets plugin to add all imported tracks to an MPD server\'s queue',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ui-1/mpd-queue',
+    author='ui-1',
+    author_email='ui-1@posteo.org',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ],
+    packages=['beetsplug'],
+    include_package_data=True,
+    install_requires=['beets', 'python-mpd2']
+)
