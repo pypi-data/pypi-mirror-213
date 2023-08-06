47c27ba7886bf309feb04db89933182e444e6f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package blessable/1.1.1/blessable-1.1.1-py3-none-any.whl

---
+++ packages/blessable/blessable-1.1.1-py3-none-any.whl/blessable/__init__.py
@@ -0,0 +1,3 @@
+# Blessable by @fakerybakery: https://github.com/fakerybakery/blessable
+# Copyright (c) 2023. All rights reserved.
+from .blessable import Blessable
---
+++ packages/blessable/blessable-1.1.1-py3-none-any.whl/blessable/blessable.py
@@ -0,0 +1,18 @@
+# Blessable by @fakerybakery: https://github.com/fakerybakery/blessable
+# Copyright (c) 2023. All rights reserved.
+from .colormap import color_map
+class Blessable:
+    def bless(self, markup):
+        parts = markup.split('<')
+        converted_markup = ''
+        for part in parts:
+            if '>' in part:
+                color_tag, text = part.split('>', 1)
+                color_tag = color_tag.strip()
+                if color_tag in color_map:
+                    converted_markup += color_map[color_tag](text)
+                else:
+                    converted_markup += text
+            else:
+                converted_markup += part
+        return converted_markup
