5ec0e08b14d1d93179033d1720b5d1246e44e946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:13 +0000

    Add package beanie_batteries_queue/0.2.0/beanie_batteries_queue-0.2.0.tar.gz

---
+++ packages/beanie_batteries_queue/beanie_batteries_queue-0.2.0.tar.gz/beanie_batteries_queue-0.2.0/beanie_batteries_queue/__init__.py
@@ -0,0 +1,10 @@
+from beanie_batteries_queue.queue import Task, State, Priority, DependencyType
+
+
+__all__ = [
+    "Task",
+    "State",
+    "Priority",
+    "DependencyType",
+]
+__version__ = "0.2.0"
