fada04f9a47be10ef15e348038114e0340355fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:29 +0000

    Add package PySocketLib/0.2.2/PySocketLib-0.2.2.tar.gz

---
+++ packages/PySocketLib/PySocketLib-0.2.2.tar.gz/PySocketLib-0.2.2/README.md
@@ -0,0 +1,17 @@
+# PySocketLib
+
+PySocketLib is a Python library to simplify working with sockets.
+
+## Features
+
+- TCP servers
+- TCP clients
+- IPv4 and IPv6 support
+
+## Ideas and Issues
+
+Please, write your feedback, ideas or issues [there](https://github.com/Minuta18/PySocketLib/issues). Or, go to the "Issues" tab in github.
+
+## Documentation
+
+Documentation is available at "Wiki" tab in github. Or go [there](https://github.com/Minuta18/PySocketLib/wiki)
---
+++ packages/PySocketLib/PySocketLib-0.2.2.tar.gz/PySocketLib-0.2.2/src/PySocketLib/Client/Client.py
@@ -0,0 +1,34 @@
+import socket
+
+class Client:
+    '''Simple socket client'''
+    def __init__(self, 
+        addr: tuple,
+    ):
+        self.__socket = None
+        if len(addr) == 2:
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif len(addr) == 4:
+            self.__socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        else:
+            raise ValueError(f"Invalid address: {addr}")
+        self.__socket.connect(addr)
+
+    def __del__(self):
+        self.__socket.close()
+
+    def __service_connection(self):
+        recv_data = self.__socket.recv(1024)
+        self.on_receive(recv_data)
+
+    def send(self, data: bytes):
+        '''Actions to do when sending data'''
+        self.__socket.sendall(bytes(data, encoding='utf-8'))
+        return data
+
+    def on_receive(self, data: bytes):
+        '''Actions to do when receiving data'''
+        return data
+    
+    def proceed(self):
+        self.__service_connection()
