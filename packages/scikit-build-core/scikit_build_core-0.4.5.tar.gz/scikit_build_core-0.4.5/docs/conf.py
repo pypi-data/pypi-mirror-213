a47b3fc9a7ad7c81068ebb954774b2d2f8d08428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package scikit-build-core/0.4.5/scikit_build_core-0.4.5.tar.gz

---
+++ packages/scikit-build-core/scikit_build_core-0.4.5.tar.gz/scikit_build_core-0.4.5/docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from __future__ import annotations
+
+# Warning: do not change the path here. To use autodoc, you need to install the
+# package first.
+
+# -- Project information -----------------------------------------------------
+
+project = "scikit-build-core"
+copyright = "2022, The Scikit-Build admins"
+author = "Henry Schreiner"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx_inline_tabs",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = []
+
+source_suffix = [".rst", ".md"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+
+# -- Extension configuration -------------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
