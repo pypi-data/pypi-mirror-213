8c81926d6f82d047a946345c6369c9662f968596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:41 +0000

    Add package dbt-meshify/0.1.0/dbt_meshify-0.1.0.tar.gz

---
+++ packages/dbt-meshify/dbt_meshify-0.1.0.tar.gz/dbt_meshify-0.1.0/dbt_meshify/dbt_projects.py
@@ -0,0 +1,305 @@
+import copy
+import hashlib
+import json
+import logging
+import os
+from pathlib import Path
+from typing import Any, Dict, MutableMapping, Optional, Set, Union
+
+import yaml
+from dbt.contracts.graph.manifest import Manifest
+from dbt.contracts.graph.nodes import ManifestNode, ModelNode, SourceDefinition
+from dbt.contracts.project import Project
+from dbt.contracts.results import CatalogArtifact, CatalogTable
+from dbt.graph import Graph
+
+from dbt_meshify.dbt import Dbt
+
+logger = logging.getLogger()
+
+
+class BaseDbtProject:
+    """A base-level representation of a dbt project."""
+
+    def __init__(
+        self,
+        manifest: Manifest,
+        project: Project,
+        catalog: CatalogArtifact,
+        name: Optional[str] = None,
+    ) -> None:
+        self.manifest = manifest
+        self.project = project
+        self.catalog = catalog
+        self.name = name if name else project.name
+        self.relationships: Dict[str, Set[str]] = {}
+        self._models: Optional[Dict[str, ModelNode]] = None
+
+        self.model_relation_names: Dict[str, str] = {
+            model.relation_name: unique_id
+            for unique_id, model in self.models.items()
+            if model.relation_name is not None
+        }
+        self.source_relation_names: Dict[str, str] = {
+            source.relation_name: unique_id
+            for unique_id, source in self.sources().items()
+            if source.relation_name is not None
+        }
+
+        self._graph = None
+
+        self._changes: Dict[str, str] = {}
+
+    @staticmethod
+    def _load_graph(manifest: Manifest) -> Graph:
+        """Generate a dbt Graph using a project manifest and the internal dbt Compiler and Linker."""
+
+        from dbt.compilation import Compiler, Linker
+
+        compiler = Compiler(config={})
+        linker = Linker()
+        compiler.link_graph(linker=linker, manifest=manifest)
+        return Graph(linker.graph)
+
+    @property
+    def graph(self):
+        """Get the dbt-core Graph for a given project Manifest"""
+        if self._graph:
+            return self._graph
+
+        self._graph = self._load_graph(self.manifest)
+        return self._graph
+
+    def register_relationship(self, project: str, resources: Set[str]) -> None:
+        """Register the relationship between two projects"""
+        logger.debug(f"Registering the relationship between {project} and its resources")
+        entry = self.relationships.get(project, set())
+        self.relationships[project] = entry.union(resources)
+
+    def sources(self) -> MutableMapping[str, SourceDefinition]:
+        return self.manifest.sources
+
+    @property
+    def models(self) -> Dict[str, ModelNode]:
+        if self._models:
+            return self._models
+
+        self._models = {
+            node_name: node
+            for node_name, node in self.manifest.nodes.items()
+            if node.resource_type == "model" and isinstance(node, ModelNode)
+        }
+
+        return self._models
+
+    def installed_packages(self) -> Set[str]:
+        project_packages = []
+        for key in ["nodes", "sources", "exposures", "metrics", "sources", "macros"]:
+            items = getattr(self.manifest, key)
+            for key, item in items.items():
+                if item.package_name:
+                    _hash = hashlib.md5()
+                    _hash.update(item.package_name.encode("utf-8"))
+                    project_packages.append(_hash.hexdigest())
+        return set(project_packages)
+
+    @property
+    def project_id(self) -> Optional[str]:
+        return self.manifest.metadata.project_id
+
+    def installs(self, other) -> bool:
+        """
+        Returns true if this project installs the other project as a package
+        """
+        return self.project_id in other.installed_packages()
+
+    def get_model_by_relation_name(self, relation_name: str) -> Optional[ModelNode]:
+        model_id = self.model_relation_names.get(relation_name)
+        if not model_id:
+            return None
+
+        return self.manifest.nodes.get(model_id)  # type: ignore
+
+    def shares_source_metadata(self, other) -> bool:
+        """
+        Returns true if there is any shared metadata between this project's sources and the models in the other project
+        """
+        my_sources = {k: v.relation_name for k, v in self.sources().items()}
+        their_models = {k: v.relation_name for k, v in other.models().items()}
+        return any(item in set(their_models.values()) for item in my_sources.values())
+
+    def overlapping_sources(self, other) -> Dict[str, Dict[str, Union[SourceDefinition, Any]]]:
+        """
+        Returns any shared metadata between this sources in this project and the models in the other  project
+        """
+        shared_sources = {}
+        for source_id, source in self.sources().items():
+            relation_name = source.relation_name
+            upstream_model = other.get_model_by_relation_name(relation_name)
+            shared_sources[source_id] = {"source": source, "upstream_model": upstream_model}
+
+        return shared_sources
+
+    def depends_on(self, other) -> bool:
+        """
+        Returns true if this project depends on the other project as a package or via shared metadata
+        """
+        return self.installs(other) or self.shares_source_metadata(other)
+
+    def get_catalog_entry(self, unique_id: str) -> Optional[CatalogTable]:
+        """Returns the catalog entry for a model in the dbt project's catalog"""
+        return self.catalog.nodes.get(unique_id)
+
+    def get_manifest_node(self, unique_id: str) -> Optional[ManifestNode]:
+        """Returns the catalog entry for a model in the dbt project's catalog"""
+        return self.manifest.nodes.get(unique_id)
+
+
+class DbtProject(BaseDbtProject):
+    @staticmethod
+    def _load_project(path) -> Project:
+        """Load a dbt Project configuration"""
+        project_dict = yaml.load(open(os.path.join(path, "dbt_project.yml")), Loader=yaml.Loader)
+        return Project.from_dict(project_dict)
+
+    @classmethod
+    def from_directory(cls, directory: os.PathLike) -> "DbtProject":
+        """Create a new DbtProject using a dbt project directory"""
+
+        dbt = Dbt()
+
+        return DbtProject(
+            manifest=dbt.parse(directory),
+            project=cls._load_project(directory),
+            catalog=dbt.docs_generate(directory),
+            dbt=dbt,
+            path=Path(directory),
+        )
+
+    def __init__(
+        self,
+        manifest: Manifest,
+        project: Project,
+        catalog: CatalogArtifact,
+        dbt: Dbt,
+        path: Path = Path(os.getcwd()),
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(manifest, project, catalog, name)
+        self.path = path
+        self.dbt = dbt
+
+    def select_resources(
+        self,
+        select: str,
+        exclude: Optional[str] = None,
+        selector: Optional[str] = None,
+        output_key: Optional[str] = None,
+    ) -> Set[str]:
+        """Select dbt resources using NodeSelection syntax"""
+        args = []
+        if select:
+            args = ["--select", select]
+        if exclude:
+            args.extend(["--exclude", exclude])
+        if selector:
+            args.extend(["--selector", selector])
+
+        results = self.dbt.ls(self.path, args, output_key=output_key)
+        if output_key:
+            results = [json.loads(resource).get(output_key) for resource in results]
+
+        return set(results)
+
+    def split(
+        self,
+        project_name: str,
+        select: str,
+        exclude: Optional[str] = None,
+    ) -> "DbtSubProject":
+        """Create a new DbtSubProject using NodeSelection syntax."""
+
+        subproject_resources = self.select_resources(select, exclude)
+
+        # Construct a new project and inject the new manifest
+        subproject = DbtSubProject(
+            name=project_name, parent_project=copy.deepcopy(self), resources=subproject_resources
+        )
+
+        # Record the subproject to create a cross-project dependency edge list
+        self.register_relationship(project_name, subproject_resources)
+
+        return subproject
+
+
+class DbtSubProject(BaseDbtProject):
+    """
+    DbtSubProjects are a filtered representation of a dbt project that leverage
+    a parent DbtProject for their manifest and project definitions until a real DbtProject
+    is created on disk.
+    """
+
+    def __init__(self, name: str, parent_project: DbtProject, resources: Set[str]):
+        self.name = name
+        self.resources = resources
+        self.parent = parent_project
+
+        self.manifest = parent_project.manifest.deepcopy()
+        self.project = copy.deepcopy(parent_project.project)
+        self.catalog = parent_project.catalog
+
+        super().__init__(self.manifest, self.project, self.catalog)
+
+    def select_resources(self, select: str, exclude: Optional[str] = None) -> Set[str]:
+        """
+        Select resources using the parent DbtProject and filtering down to only include resources in this
+        subproject.
+        """
+        args = ["--select", select]
+        if exclude:
+            args.extend(["--exclude", exclude])
+
+        results = self.parent.dbt.ls(self.parent.path, args)
+
+        return set(results) - self.resources
+
+    def split(
+        self,
+        project_name: str,
+        select: str,
+        exclude: Optional[str] = None,
+    ) -> "DbtSubProject":
+        """Create a new DbtSubProject using NodeSelection syntax."""
+
+        subproject_resources = self.select_resources(select, exclude)
+
+        # Construct a new project and inject the new manifest
+        subproject = DbtSubProject(
+            name=project_name,
+            parent_project=copy.deepcopy(self.parent),
+            resources=subproject_resources,
+        )
+
+        # Record the subproject to create a cross-project dependency edge list
+        self.register_relationship(project_name, subproject_resources)
+
+        return subproject
+
+    def initialize(self, target_directory: os.PathLike):
+        """Initialize this subproject as a full dbt project at the provided `target_directory`."""
+
+        # TODO: Implement project initialization
+
+        raise NotImplementedError
+
+
+class DbtProjectHolder:
+    def __init__(self) -> None:
+        self.projects: Dict[str, BaseDbtProject] = {}
+
+    def project_map(self) -> dict:
+        return self.projects
+
+    def register_project(self, project: BaseDbtProject) -> None:
+        if project.project_id:
+            self.projects[project.project_id] = project
---
+++ packages/dbt-meshify/dbt_meshify-0.1.0.tar.gz/dbt_meshify-0.1.0/dbt_meshify/main.py
@@ -0,0 +1,256 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
+import click
+from dbt.contracts.graph.unparsed import Owner
+
+from .dbt_projects import DbtProject, DbtProjectHolder, DbtSubProject
+from .storage.yaml_editors import DbtMeshModelConstructor
+
+# define common parameters
+project_path = click.option(
+    "--project-path",
+    type=click.Path(exists=True),
+    default=".",
+    help="The path to the dbt project to operate on. Defaults to the current directory.",
+)
+
+exclude = click.option(
+    "--exclude",
+    "-e",
+    default=None,
+    help="The dbt selection syntax specifying the resources to exclude in the operation",
+)
+
+group_yml_path = click.option(
+    "--group-yml-path",
+    type=click.Path(exists=False),
+    help="An optional path to store the new group YAML definition.",
+)
+
+select = click.option(
+    "--select",
+    "-s",
+    default=None,
+    help="The dbt selection syntax specifying the resources to include in the operation",
+)
+
+owner = click.option(
+    "--owner",
+    nargs=2,
+    multiple=True,
+    type=click.Tuple([str, str]),
+    help="A tuple of Owner information for the group. For example " "`--owner name example`",
+)
+
+selector = click.option(
+    "--selector",
+    default=None,
+    help="The name of the YML selector specifying the resources to include in the operation",
+)
+
+
+# define cli group
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def operation():
+    """
+    Set of subcommands for performing mesh operations on dbt projects
+    """
+    pass
+
+
+@cli.command(name="connect")
+@click.argument("projects-dir", type=click.Path(exists=True), default=".")
+def connect(projects_dir):
+    """
+    !!! info
+        This command is not yet implemented
+
+    Connects multiple dbt projects together by adding all necessary dbt Mesh constructs
+    """
+    holder = DbtProjectHolder()
+
+    while True:
+        path_string = input("Enter the relative path to a dbt project (enter 'done' to finish): ")
+        if path_string == "done":
+            break
+
+        path = Path(path_string).expanduser().resolve()
+        project = DbtProject.from_directory(path)
+        holder.register_project(project)
+
+    print(holder.project_map())
+
+
+@cli.command(name="split")
+@exclude
+@project_path
+@select
+@selector
+def split():
+    """
+    !!! info
+        This command is not yet implemented
+
+    Splits dbt projects apart by adding all necessary dbt Mesh constructs based on the selection syntax.
+
+    """
+    path_string = input("Enter the relative path to a dbt project you'd like to split: ")
+
+    holder = DbtProjectHolder()
+
+    path = Path(path_string).expanduser().resolve()
+    project = DbtProject.from_directory(path)
+    holder.register_project(project)
+
+    while True:
+        subproject_name = input("Enter the name for your subproject ('done' to finish): ")
+        if subproject_name == "done":
+            break
+        subproject_selector = input(
+            f"Enter the selector that represents the subproject {subproject_name}: "
+        )
+
+        subproject: DbtSubProject = project.split(
+            project_name=subproject_name, select=subproject_selector
+        )
+        holder.register_project(subproject)
+
+    print(holder.project_map())
+
+
+@operation.command(name="add-contract")
+@exclude
+@project_path
+@select
+@selector
+def add_contract(select, exclude, project_path, selector, public_only=False):
+    """
+    Adds a contract to all selected models.
+    """
+    path = Path(project_path).expanduser().resolve()
+    project = DbtProject.from_directory(path)
+    resources = list(
+        project.select_resources(
+            select=select, exclude=exclude, selector=selector, output_key="unique_id"
+        )
+    )
+    models = filter(lambda x: x.startswith("model"), resources)
+    if public_only:
+        models = filter(lambda x: project.get_manifest_node(x).access == "public", models)
+    for model_unique_id in models:
+        model_node = project.get_manifest_node(model_unique_id)
+        model_catalog = project.get_catalog_entry(model_unique_id)
+        meshify_constructor = DbtMeshModelConstructor(
+            project_path=project_path, model_node=model_node, model_catalog=model_catalog
+        )
+        meshify_constructor.add_model_contract()
+
+
+@operation.command(name="add-version")
+@exclude
+@project_path
+@select
+@selector
+@click.option("--prerelease", "--pre", default=False, is_flag=True)
+@click.option("--defined-in", default=None)
+def add_version(select, exclude, project_path, selector, prerelease, defined_in):
+    """
+    Adds/increments model versions for all selected models.
+    """
+    path = Path(project_path).expanduser().resolve()
+    project = DbtProject.from_directory(path)
+    resources = list(
+        project.select_resources(
+            select=select, exclude=exclude, selector=selector, output_key="unique_id"
+        )
+    )
+    models = filter(lambda x: x.startswith("model"), resources)
+    for model_unique_id in models:
+        model_node = project.get_manifest_node(model_unique_id)
+        if model_node.version == model_node.latest_version:
+            meshify_constructor = DbtMeshModelConstructor(
+                project_path=project_path, model_node=model_node
+            )
+            meshify_constructor.add_model_version(prerelease=prerelease, defined_in=defined_in)
+
+
+@operation.command(name="create-group")
+@exclude
+@project_path
+@select
+@selector
+@click.argument("name")
+@owner
+@group_yml_path
+def create_group(
+    name,
+    project_path: os.PathLike,
+    owner: List[Tuple[str, str]],
+    group_yml_path: os.PathLike,
+    select: str,
+    exclude: Optional[str] = None,
+    selector: Optional[str] = None,
+):
+    """
+    Create a group and add selected resources to the group.
+    """
+    from dbt_meshify.utilities.grouper import ResourceGrouper
+
+    path = Path(project_path).expanduser().resolve()
+    project = DbtProject.from_directory(path)
+
+    if group_yml_path is None:
+        group_yml_path = (path / Path("models/_groups.yml")).resolve()
+    else:
+        group_yml_path = Path(group_yml_path).resolve()
+
+    if not str(os.path.commonpath([group_yml_path, path])) == str(path):
+        raise Exception(
+            "The provided group-yml-path is not contained within the provided dbt project."
+        )
+
+    owner: Owner = Owner(**{key: value for key, value in owner})
+
+    grouper = ResourceGrouper(project)
+    grouper.add_group(
+        name=name,
+        owner=owner,
+        select=select,
+        exclude=exclude,
+        selector=selector,
+        path=group_yml_path,
+    )
+
+
+@cli.command(name="group")
+@exclude
+@project_path
+@select
+@selector
+@click.argument("name")
+@owner
+@group_yml_path
+@click.pass_context
+def group(
+    ctx,
+    name,
+    project_path: os.PathLike,
+    owner: List[Tuple[str, str]],
+    group_yml_path: os.PathLike,
+    select: str,
+    exclude: Optional[str] = None,
+    selector: Optional[str] = None,
+):
+    """
+    Creates a new dbt group based on the selection syntax
+    Detects the edges of the group, makes their access public, and adds contracts to them
+    """
+    ctx.forward(create_group)
+    ctx.invoke(add_contract, select=f'group:{name}', project_path=project_path, public_only=True)
