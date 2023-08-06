03ccd62549f4e5ce64df8673371d6d1b8a8291c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:03 +0000

    Add package brainfk/0.0.2/brainfk-0.0.2.tar.gz

---
+++ packages/brainfk/brainfk-0.0.2.tar.gz/brainfk-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+VERSION = "0.0.2"
+DESCRIPTION = "A Brainfuck interpreter package"
+with open("README.txt", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="brainfk",
+    version=VERSION,
+    author="Estifanose Sahilu",
+    packages=["brainfk"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/plain",
+    install_requires=["argparse"],
+    author_email="<estifanoswork@gmail.com>",
+    entry_points={"console_scripts": ["brainfk = brainfk.interpreter:main"]},
+    url="https://github.com/estif0/brainfk",
+    keywords=["python", "package", "cli", "interpreter", "brainfuck"],
+)
