9ee1c420c357a6d52a87f4e0be6fe40e3b71860b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:56 +0000

    Add package django-linear-migrations/2.9.0/django_linear_migrations-2.9.0.tar.gz

---
+++ packages/django-linear-migrations/django_linear_migrations-2.9.0.tar.gz/django_linear_migrations-2.9.0/src/django_linear_migrations/management/commands/makemigrations.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from django.core.management.commands.makemigrations import Command as BaseCommand
+from django.db.migrations import Migration
+
+from django_linear_migrations.apps import first_party_app_configs
+from django_linear_migrations.apps import MigrationDetails
+
+
+class Command(BaseCommand):
+    def write_migration_files(self, changes: dict[str, list[Migration]]) -> None:
+        super().write_migration_files(changes)
+        if self.dry_run:
+            return
+
+        first_party_app_labels = {
+            app_config.label for app_config in first_party_app_configs()
+        }
+
+        for app_label, app_migrations in changes.items():
+            if app_label not in first_party_app_labels:
+                continue
+
+            # Reload required as we've generated changes
+            migration_details = MigrationDetails(app_label, do_reload=True)
+            max_migration_name = app_migrations[-1].name
+            max_migration_txt = migration_details.dir / "max_migration.txt"
+            max_migration_txt.write_text(max_migration_name + "\n")
