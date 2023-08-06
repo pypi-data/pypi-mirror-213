496d8925d27faac1e134b30509ff9614996a4ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:24 +0000

    Add package HDGTest/0.0.0.3/HDGTest-0.0.0.3-py3-none-any.whl

---
+++ packages/HDGTest/HDGTest-0.0.0.3-py3-none-any.whl/HDGTest/test1.py
@@ -0,0 +1,5 @@
+def print_test1():
+
+    print("Hi, This is test1.py page!")
+    print("\n 버전 업글 연습입니다.")
+    print("\n BMI 를 만들어 보았습니다.")
---
+++ packages/HDGTest/HDGTest-0.0.0.3-py3-none-any.whl/HDGTest/test2.py
@@ -0,0 +1,5 @@
+def print_test2():
+
+    print("안녕하세요, 이것은 test2.py 페이지입니다.")
+    print("\n 잘됬으면 좋겠습니다.")
+    print("\n 오류만 안뜨면 좋겠습니다.")
