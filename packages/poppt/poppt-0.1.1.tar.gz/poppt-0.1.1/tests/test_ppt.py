0dfcc6de3921258e056d3c1e910e4fac26dfc116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:35 +0000

    Add package poppt/0.1.1/poppt-0.1.1.tar.gz

---
+++ packages/poppt/poppt-0.1.1.tar.gz/poppt-0.1.1/tests/test_ppt.py
@@ -0,0 +1,17 @@
+import unittest
+
+import office
+
+from poppt.api.ppt import *
+
+
+class TestPPT(unittest.TestCase):
+    def test_ppt2pdf(self):
+        ppt2pdf(path=r'./ppt/test_ppt_img', output_path=r'./ppt/test_ppt_img/imgs')
+
+    # def test_ppt2img(self):
+    #     ppt2img(intput_path=r'D:\test\py310\ppt_test')
+    def test_single_ppt2imgg(self):
+        office.ppt.ppt2img(input_path=r'C:\Users\Lenovo\Desktop\temp\test\test\ppt\a',
+                           output_path=r'./ppt/test_pdf2/imgs23',
+                           img_type='jpg')
