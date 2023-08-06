71452faeb8a890f66765d009cd7ea9888a727432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package ape-trezor/0.6.2/ape-trezor-0.6.2.tar.gz

---
+++ packages/ape-trezor/ape-trezor-0.6.2.tar.gz/ape-trezor-0.6.2/setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+extras_require = {
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "pytest-mock",  # For creating mocks
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
+        "eip712",  # Used for cleaner test cases
+    ],
+    "lint": [
+        "black>=23.3.0,<24",  # auto-formatter and linter
+        "mypy>=0.991,<1",  # Static type analyzer
+        "types-setuptools",  # Needed for mypy typeshed
+        "types-PyYAML",  # Needed for mypy typeshed
+        "flake8>=6.0.0,<7",  # Style linter
+        "isort>=5.10.1,<6",  # Import sorting linter
+        "mdformat>=0.7.16",  # Auto-formatter for markdown
+        "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
+        "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
+    ],
+    "doc": [
+        "Sphinx>=3.4.3,<4",  # Documentation generator
+        "sphinx_rtd_theme>=0.1.9,<1",  # Readthedocs.org theme
+        "towncrier>=19.2.0, <20",  # Generate release notes
+    ],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "setuptools-scm",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
+    ],
+    "dev": [
+        "commitizen",  # Manage commits and publishing releases
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
+    ],
+}
+
+# NOTE: `pip install -e .[dev]` to install package
+extras_require["dev"] = (
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+    + extras_require["release"]
+    + extras_require["dev"]
+)
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="ape-trezor",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="""ape-trezor: Plugin for Trezor Hardware Wallets""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ApeWorX Ltd.",
+    author_email="admin@apeworx.io",
+    url="https://github.com/ApeWorX/ape-trezor",
+    include_package_data=True,
+    install_requires=[
+        "eth-ape>=0.6.9,<0.7",
+        "eth-account",  # Use same version as eth-ape
+        "eth-typing>=3.1",  # Influenced by eth-ape so no upper pin
+        "click",  # Use same version as eth-ape
+        "trezor[ethereum]>=0.13.6,<0.14",
+    ],
+    entry_points={
+        "ape_cli_subcommands": [
+            "ape_trezor=ape_trezor._cli:cli",
+        ],
+    },
+    python_requires=">=3.8,<4",
+    extras_require=extras_require,
+    py_modules=["ape_trezor"],
+    license="Apache-2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"ape_trezor": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
