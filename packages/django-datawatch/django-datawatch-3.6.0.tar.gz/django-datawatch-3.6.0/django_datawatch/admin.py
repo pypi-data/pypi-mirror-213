4af803e1580bb5f86bb678d94a24464bfdd21213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:48 +0000

    Add package django-datawatch/3.6.0/django-datawatch-3.6.0.tar.gz

---
+++ packages/django-datawatch/django-datawatch-3.6.0.tar.gz/django-datawatch-3.6.0/django_datawatch/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from django_datawatch.models import Result, CheckExecution, ResultStatusHistory
+
+
+@admin.register(Result)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'identifier', 'status')
+    readonly_fields = ('created', 'modified')
+    search_fields = ('slug', 'identifier', 'payload_description')
+    list_filter = ('status', 'slug', 'assigned_to_group')
+
+
+@admin.register(CheckExecution)
+class CheckExecutionAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'last_run')
+    search_fields = ('slug',)
+
+
+@admin.register(ResultStatusHistory)
+class ResultStatusHistory(admin.ModelAdmin):
+    list_display = (
+        'result',
+        'from_status',
+        'to_status',
+        'created',
+    )
+    autocomplete_fields = ('result',)
+    ordering = ('result', 'created')
+    list_select_related = ['result']
