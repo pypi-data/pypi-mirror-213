99821e1d3ea4163fc2043f927af257825ba92f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:40 +0000

    Add package summa-embed/0.17.0/summa_embed-0.17.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/summa-embed/summa_embed-0.17.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/summa_embed/__init__.py
@@ -0,0 +1,35 @@
+from typing import Dict, Optional
+
+from izihawa_utils.pb_to_json import ParseDict
+
+from .proto import index_service_pb2, query_pb2, search_service_pb2
+from .summa_embed_bin import IndexRegistry as IndexRegistryBin
+
+
+class IndexRegistry:
+    def __init__(self):
+        self.index_registry = IndexRegistryBin()
+
+    async def add(self, index_config, index_name: str) -> index_service_pb2.IndexAttributes:
+        parsed_index_config = index_service_pb2.IndexEngineConfig()
+        ParseDict(index_config, parsed_index_config)
+        index_attributes_bytes = await self.index_registry.add(
+            parsed_index_config.SerializeToString(),
+            index_name=index_name,
+        )
+        index_attributes = index_service_pb2.IndexAttributes()
+        index_attributes.ParseFromString(index_attributes_bytes)
+        return index_attributes
+
+    async def search(self, index_queries) -> query_pb2.SearchResponse:
+        search_request = search_service_pb2.SearchRequest()
+        for index_query in index_queries:
+            if isinstance(index_query, Dict):
+                dict_index_query = index_query
+                index_query = search_service_pb2.IndexQuery()
+                ParseDict(dict_index_query, index_query)
+            search_request.index_queries.append(index_query)
+        search_response_bytes = await self.index_registry.search(search_request.SerializeToString())
+        search_response = query_pb2.SearchResponse()
+        search_response.ParseFromString(search_response_bytes)
+        return search_response
