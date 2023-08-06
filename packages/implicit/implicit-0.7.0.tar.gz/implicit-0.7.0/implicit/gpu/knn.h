e8d5ef5ff490a8a9a9922c987753bf641c8fc35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:58 +0000

    Add package implicit/0.7.0/implicit-0.7.0.tar.gz

---
+++ packages/implicit/implicit-0.7.0.tar.gz/implicit-0.7.0/implicit/gpu/knn.h
@@ -0,0 +1,38 @@
+#ifndef IMPLICIT_GPU_KNN_H_
+#define IMPLICIT_GPU_KNN_H_
+#include <memory>
+
+#include <raft/core/resources.hpp>
+#include <rmm/mr/device/device_memory_resource.hpp>
+
+#include "implicit/gpu/matrix.h"
+
+namespace implicit {
+namespace gpu {
+class KnnQuery {
+public:
+  KnnQuery(size_t temp_memory = 0);
+  ~KnnQuery();
+
+  void topk(const Matrix &items, const Matrix &query, int k, int *indices,
+            float *distances, Matrix *item_norms = NULL,
+            const COOMatrix *query_filter = NULL,
+            Vector<int> *item_filter = NULL);
+
+  template <typename T>
+  void topk_impl(const Matrix &items, const Matrix &query, int k, int *indices,
+                 float *distances, Matrix *item_norms = NULL,
+                 const COOMatrix *query_filter = NULL,
+                 Vector<int> *item_filter = NULL);
+
+  void argsort(const int *input_indices, const float *input_distances, int rows,
+               int cols, int *indices, float *distances);
+
+protected:
+  std::unique_ptr<rmm::mr::device_memory_resource> mr;
+  raft::resources handle;
+  size_t max_temp_memory;
+};
+} // namespace gpu
+} // namespace implicit
+#endif // IMPLICIT_GPU_KNN_H_
