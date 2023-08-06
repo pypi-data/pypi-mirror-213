0bec23e323b507a397428e692acf96223d96f617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:34 +0000

    Add package pypi-python-translator/2.2/pypi_python_translator-2.2.tar.gz

---
+++ packages/pypi-python-translator/pypi_python_translator-2.2.tar.gz/pypi_python_translator-2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypi_python_translator",
+    version="2.2",
+    author="SForces",
+    author_email="osmntn08@gmail.com",
+    description="A Python package for translation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SForces/pypi_python_translator",
+    packages=["translator"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
