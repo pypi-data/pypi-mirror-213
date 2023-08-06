3b16d5dcad054e2389dbd2206a913fd66d4ae0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:34 +0000

    Add package pdm/2.7.4/pdm-2.7.4.tar.gz

---
+++ packages/pdm/pdm-2.7.4.tar.gz/pdm-2.7.4/src/pdm/cli/commands/config.py
@@ -0,0 +1,135 @@
+import argparse
+from typing import Any, Mapping
+
+from pdm import termui
+from pdm._types import RepositoryConfig
+from pdm.cli.commands.base import BaseCommand
+from pdm.project import Project
+from pdm.project.config import DEFAULT_REPOSITORIES, REPOSITORY, SOURCE, Config
+
+
+class Command(BaseCommand):
+    """Display the current configuration"""
+
+    ui: termui.UI
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "-l",
+            "--local",
+            action="store_true",
+            help="Set config in the project's local configuration file",
+        )
+        parser.add_argument("-d", "--delete", action="store_true", help="Unset a configuration key")
+        parser.add_argument("key", help="Config key", nargs="?")
+        parser.add_argument("value", help="Config value", nargs="?")
+
+    def handle(self, project: Project, options: argparse.Namespace) -> None:
+        if options.delete:
+            self._delete_config(project, options)
+        elif options.value:
+            self._set_config(project, options)
+        elif options.key:
+            self._get_config(project, options)
+        else:
+            self._list_config(project, options)
+
+    def _get_config(self, project: Project, options: argparse.Namespace) -> None:
+        if options.key in project.project_config.deprecated:  # pragma: no cover
+            project.core.ui.echo(
+                f"DEPRECATED: the config has been renamed to {project.project_config.deprecated[options.key]}",
+                style="warning",
+                err=True,
+            )
+            options.key = project.project_config.deprecated[options.key]
+        try:
+            value = project.project_config[options.key]
+        except KeyError:
+            value = project.global_config[options.key]
+        if options.key.endswith(".password"):
+            value = "[i]<hidden>[/i]"
+        project.core.ui.echo(value)
+
+    def _set_config(self, project: Project, options: argparse.Namespace) -> None:
+        config = project.project_config if options.local else project.global_config
+        if options.key in config.deprecated:  # pragma: no cover
+            project.core.ui.echo(
+                f"DEPRECATED: the config has been renamed to {config.deprecated[options.key]}",
+                style="warning",
+                err=True,
+            )
+        config[options.key] = options.value
+
+    def _show_config(self, config: Mapping[str, Any], supersedes: Mapping[str, Any]) -> None:
+        assert Config.site is not None
+        for key in sorted(config):
+            deprecated = ""
+            canonical_key = key
+            superseded = key in supersedes
+            if key in Config.site.deprecated:  # pragma: no cover
+                canonical_key = Config.site.deprecated[key]
+                if canonical_key in supersedes:
+                    superseded = True
+                deprecated = f"[error](deprecating: {key})[/]"
+            elif key not in Config._config_map and not (key.startswith("pypi.") or key.startswith(REPOSITORY)):
+                continue
+            extra_style = "dim" if superseded else None
+            if canonical_key not in Config._config_map:
+                prefix, name = key.split(".", 1)
+                if prefix in (SOURCE, REPOSITORY):
+                    title = "non-default PyPI index" if prefix == SOURCE else "custom repository"
+                    self.ui.echo(
+                        f"[warning]# Configuration of {title} `{name}`",
+                        style=extra_style,
+                        verbosity=termui.Verbosity.DETAIL,
+                    )
+                    repository = RepositoryConfig(**config[key], config_prefix=prefix, name=name)
+                    if not repository.url and name in DEFAULT_REPOSITORIES:
+                        repository.url = DEFAULT_REPOSITORIES[name]
+                    self.ui.echo(repository)
+                continue
+            config_item = Config._config_map[canonical_key]
+            self.ui.echo(
+                f"[warning]# {config_item.description}",
+                style=extra_style,
+                verbosity=termui.Verbosity.DETAIL,
+            )
+            value = "[i]<hidden>[/]" if key.endswith("password") else config[key]
+            self.ui.echo(
+                f"[primary]{canonical_key}[/]{deprecated} = {value}",
+                style=extra_style,
+            )
+
+    def _list_config(self, project: Project, options: argparse.Namespace) -> None:
+        self.ui = project.core.ui
+        assert Config.site is not None
+        self.ui.echo(
+            f"Site/default configuration ([success]{Config.site.config_file}[/]):",
+            style="bold",
+        )
+        self._show_config(
+            Config.get_defaults(),
+            {**project.global_config.self_data, **project.project_config.self_data},
+        )
+
+        self.ui.echo(
+            f"\nHome configuration ([success]{project.global_config.config_file}[/]):",
+            style="bold",
+        )
+        self._show_config(project.global_config.self_data, project.project_config.self_data)
+
+        self.ui.echo(
+            f"\nProject configuration ([success]{project.project_config.config_file}[/]):",
+            style="bold",
+        )
+        self._show_config(project.project_config.self_data, {})
+
+    def _delete_config(self, project: Project, options: argparse.Namespace) -> None:
+        config = project.project_config if options.local else project.global_config
+        if options.key in config.deprecated:  # pragma: no cover
+            project.core.ui.echo(
+                f"DEPRECATED: the config has been renamed to {config.deprecated[options.key]}",
+                style="warning",
+                err=True,
+            )
+        del config[options.key]
---
+++ packages/pdm/pdm-2.7.4.tar.gz/pdm-2.7.4/tests/cli/test_init.py
@@ -0,0 +1,128 @@
+import sys
+from unittest.mock import ANY
+
+import pytest
+
+from pdm.cli.commands.init import Command
+from pdm.models.backends import get_backend
+from pdm.models.python import PythonInfo
+
+PYTHON_VERSION = f"{sys.version_info[0]}.{sys.version_info[1]}"
+
+
+def test_init_validate_python_requires(project_no_init):
+    with pytest.raises(ValueError):
+        Command.do_init(project_no_init, python_requires="3.7")
+
+
+def test_init_command(project_no_init, pdm, mocker):
+    mocker.patch(
+        "pdm.cli.commands.init.get_user_email_from_git",
+        return_value=("Testing", "me@example.org"),
+    )
+    do_init = mocker.patch.object(Command, "do_init")
+    pdm(["init"], input="\n\n\n\n\n\n", strict=True, obj=project_no_init)
+    python_version = f"{project_no_init.python.major}.{project_no_init.python.minor}"
+    do_init.assert_called_with(
+        project_no_init,
+        name="",
+        version="",
+        description="",
+        license="MIT",
+        author="Testing",
+        email="me@example.org",
+        python_requires=f">={python_version}",
+        build_backend=None,
+        hooks=ANY,
+    )
+
+
+def test_init_command_library(project_no_init, pdm, mocker):
+    mocker.patch(
+        "pdm.cli.commands.init.get_user_email_from_git",
+        return_value=("Testing", "me@example.org"),
+    )
+    do_init = mocker.patch.object(Command, "do_init")
+    result = pdm(
+        ["init"],
+        input="\ny\ntest-project\n\nTest Project\n1\n\n\n\n\n",
+        obj=project_no_init,
+    )
+    assert result.exit_code == 0
+    python_version = f"{project_no_init.python.major}.{project_no_init.python.minor}"
+    do_init.assert_called_with(
+        project_no_init,
+        name="test-project",
+        version="0.1.0",
+        description="Test Project",
+        license="MIT",
+        author="Testing",
+        email="me@example.org",
+        python_requires=f">={python_version}",
+        build_backend=get_backend("setuptools"),
+        hooks=ANY,
+    )
+
+
+def test_init_non_interactive(project_no_init, pdm, mocker):
+    mocker.patch(
+        "pdm.cli.commands.init.get_user_email_from_git",
+        return_value=("Testing", "me@example.org"),
+    )
+    do_init = mocker.patch.object(Command, "do_init")
+    do_use = mocker.patch("pdm.cli.commands.use.Command.do_use", return_value=PythonInfo.from_path(sys.executable))
+    result = pdm(["init", "-n"], obj=project_no_init)
+    assert result.exit_code == 0
+    python_version = f"{project_no_init.python.major}.{project_no_init.python.minor}"
+    do_use.assert_called_once_with(
+        project_no_init,
+        ANY,
+        first=True,
+        ignore_remembered=True,
+        ignore_requires_python=True,
+        save=False,
+        hooks=ANY,
+    )
+    do_init.assert_called_with(
+        project_no_init,
+        name="",
+        version="",
+        description="",
+        license="MIT",
+        author="Testing",
+        email="me@example.org",
+        python_requires=f">={python_version}",
+        build_backend=None,
+        hooks=ANY,
+    )
+
+
+def test_init_auto_create_venv(project_no_init, pdm, mocker):
+    mocker.patch("pdm.models.python.PythonInfo.get_venv", return_value=None)
+    project_no_init.project_config["python.use_venv"] = True
+    result = pdm(["init"], input="\n\n\n\n\n\n\n", obj=project_no_init)
+    assert result.exit_code == 0
+    assert project_no_init.python.executable.parent.parent == project_no_init.root / ".venv"
+    assert ".pdm-python" in (project_no_init.root / ".gitignore").read_text()
+
+
+def test_init_auto_create_venv_specify_python(project_no_init, pdm, mocker):
+    mocker.patch("pdm.models.python.PythonInfo.get_venv", return_value=None)
+    project_no_init.project_config["python.use_venv"] = True
+    result = pdm(
+        ["init", f"--python={PYTHON_VERSION}"],
+        input="\n\n\n\n\n\n",
+        obj=project_no_init,
+    )
+    assert result.exit_code == 0
+    assert project_no_init.python.executable.parent.parent == project_no_init.root / ".venv"
+
+
+def test_init_auto_create_venv_answer_no(project_no_init, pdm, mocker):
+    mocker.patch("pdm.models.python.PythonInfo.get_venv", return_value=None)
+    creator = mocker.patch("pdm.cli.commands.venv.backends.Backend.create")
+    project_no_init.project_config["python.use_venv"] = True
+    result = pdm(["init"], input="\nn\n\n\n\n\n\n\n", obj=project_no_init)
+    assert result.exit_code == 0
+    creator.assert_not_called()
+    assert project_no_init.python.executable.parent.parent != project_no_init.root / ".venv"
