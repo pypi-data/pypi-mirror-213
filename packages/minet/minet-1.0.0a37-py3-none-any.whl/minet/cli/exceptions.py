3e3496194436b07688549e9fdb946fbf226b9d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:08 +0000

    Add package minet/1.0.0a37/minet-1.0.0a37-py3-none-any.whl

---
+++ packages/minet/minet-1.0.0a37-py3-none-any.whl/minet/cli/exceptions.py
@@ -0,0 +1,29 @@
+# =============================================================================
+# Minet Custom CLI Exceptions
+# =============================================================================
+#
+# Collection of handy custom exceptions.
+#
+from minet.exceptions import MinetError
+
+
+class MinetCLIError(MinetError):
+    pass
+
+
+class InvalidArgumentsError(MinetCLIError):
+    pass
+
+
+class MissingColumnError(MinetCLIError):
+    def __init__(self, message, column):
+        super().__init__(message)
+        self.column = column
+
+
+class NotResumableError(MinetCLIError):
+    pass
+
+
+class FatalError(MinetCLIError):
+    pass
