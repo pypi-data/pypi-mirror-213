829fe82d5d604d486a239ad0cdc06545e8be12c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:41 +0000

    Add package vietanhdev/0.0.1/vietanhdev-0.0.1.tar.gz

---
+++ packages/vietanhdev/vietanhdev-0.0.1.tar.gz/vietanhdev-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Hi! My name is Viet Anh. I'm a software developer from Vietnam.
+
+This is my personal utility library for software development. It contains
+a lot of useful stuff that I use in my projects. It is not intended to be
+a general purpose library, but it may be useful for you too.
---
+++ packages/vietanhdev/vietanhdev-0.0.1.tar.gz/vietanhdev-0.0.1/setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+setup(
+    name="vietanhdev",
+    version="0.0.1",
+    url="http://github.com/vietanhdev/vietanhdev-python",
+    license='MIT',
+    author="Viet Anh Nguyen",
+    author_email="vietanh.dev@gmail.com",
+    description="Personal Utilities for Software Development - vietanhdev",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
