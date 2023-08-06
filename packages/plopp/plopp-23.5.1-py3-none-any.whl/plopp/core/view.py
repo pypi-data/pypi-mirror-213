407a89b8a8658918dd32c9855191bd58918af1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:58 +0000

    Add package plopp/23.5.1/plopp-23.5.1-py3-none-any.whl

---
+++ packages/plopp/plopp-23.5.1-py3-none-any.whl/plopp/core/view.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+import uuid
+from abc import abstractmethod
+
+
+class View:
+    """
+    A (typically graphical) representation of the data.
+    A view must be attached to one or more :class:`Node`.
+    Upon receiving a notification from a parent node, it usually requests data from that
+    node in order to display it visually.
+
+    Parameters
+    ----------
+    *nodes:
+        The nodes that are attached to the view.
+    """
+
+    def __init__(self, *nodes):
+        self._id = uuid.uuid4().hex
+        self.graph_nodes = {}
+        for node in nodes:
+            node.add_view(self)
+
+    @abstractmethod
+    def notify_view(self, _):
+        """
+        The function that will be called when a parent node is told to notify its
+        children and its views.
+        This has to be overridden by any child class.
+        """
+        return
+
+    @property
+    def id(self):
+        """
+        The unique id of the view.
+        """
+        return self._id
---
+++ packages/plopp/plopp-23.5.1-py3-none-any.whl/plopp/graphics/basefig.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+from abc import abstractmethod
+from typing import Any, Dict
+
+import scipp as sc
+
+from ..core import View
+
+
+class BaseFig(View):
+    """
+    A :class:`View` for figures.
+
+    Parameters
+    ----------
+    *nodes:
+        The nodes that are attached to the view.
+    """
+
+    def __init__(self, *nodes):
+        super().__init__(*nodes)
+        self.artists = {}
+
+    def notify_view(self, message: Dict[str, Any]):
+        """
+        When a notification is received, request data from the corresponding parent node
+        and update the relevant artist.
+
+        Parameters
+        ----------
+        *message:
+            The notification message containing the node id it originated from.
+        """
+        node_id = message["node_id"]
+        new_values = self.graph_nodes[node_id].request_data()
+        self.update(new_values=new_values, key=node_id)
+
+    @abstractmethod
+    def update(self, new_values: sc.DataArray, key: str, draw: bool):
+        """
+        Update function which is called when a notification is received.
+        This has to be overridden by any child class.
+        """
+        return
+
+    def render(self):
+        """
+        At the end of figure creation, this function is called to request data from
+        all parent nodes and draw the figure.
+        """
+        for node in self.graph_nodes.values():
+            new_values = node.request_data()
+            self.update(new_values=new_values, key=node.id)
