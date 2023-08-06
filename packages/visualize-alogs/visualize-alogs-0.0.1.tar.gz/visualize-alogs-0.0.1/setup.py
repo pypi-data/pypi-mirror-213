a8e25d697bc73273dee9d1a40ac13a3fd0d12e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:13 +0000

    Add package visualize-alogs/0.0.1/visualize-alogs-0.0.1.tar.gz

---
+++ packages/visualize-alogs/visualize-alogs-0.0.1.tar.gz/visualize-alogs-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="visualize-alogs",
+    version="0.0.1",
+    author="Dhvanik Viradiya",
+    author_email="viradiyadhvanik@gmail.com",
+    description="Visualize path finding algorithms simultaneously for the same condition in UI.",
+    long_description="There are only limited algorithms implemented. In follow up versions more algorithms will be added and will be used to visualize the algorithms easily.",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
