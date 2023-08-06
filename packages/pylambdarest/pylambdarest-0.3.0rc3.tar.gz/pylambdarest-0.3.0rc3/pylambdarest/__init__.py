6ddb04b7e50c154d583bb3e78406c3c343884c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:49 +0000

    Add package pylambdarest/0.3.0rc3/pylambdarest-0.3.0rc3.tar.gz

---
+++ packages/pylambdarest/pylambdarest-0.3.0rc3.tar.gz/pylambdarest-0.3.0rc3/pylambdarest/__init__.py
@@ -0,0 +1,8 @@
+"""
+pylambdarest - RESTful API with AWS Lambda and API Gateway.
+"""
+
+from pylambdarest.applications import App
+from pylambdarest.request import Request
+
+__all__ = ["App", "Request"]
