613e5dba83f2d96a98d7db5a7d83fff14681823a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:25 +0000

    Add package pikajson/0.1.7/pikajson-0.1.7.tar.gz

---
+++ packages/pikajson/pikajson-0.1.7.tar.gz/pikajson-0.1.7/pikajson/helpers/helpers.py
@@ -0,0 +1,16 @@
+
+import pika
+
+def get_rmq_count(user, password, host, port, queue):
+    credentials = pika.PlainCredentials(user, password)
+    parameters = pika.ConnectionParameters(host=host, port=port, credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    res = channel.queue_declare(
+        queue=queue,
+        durable=True,
+        exclusive=False,
+        auto_delete=False,
+        passive=True
+    )
+    return res.method.message_count
