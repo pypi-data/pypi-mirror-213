b96ff111f45ccf01a30df88b45b3576774cd0364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:44 +0000

    Add package ma-notification-center/2.1.1/ma_notification_center-2.1.1.tar.gz

---
+++ packages/ma-notification-center/ma_notification_center-2.1.1.tar.gz/ma_notification_center-2.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ma_notification_center",
+    version="2.1.1",
+    author="Fatima Medlij",
+    author_email="medlijfatima@gmail.com",
+    description="MobileArts Notification Center Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires  = ['boto3', 'email_validator'], 
+    license = 'MIT'
+)
