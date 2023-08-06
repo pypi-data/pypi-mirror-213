e5a32de143f0ff76fbb1a3dc83113b9f5f45d0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:23 +0000

    Add package seolTest/0.0.0.2/seolTest-0.0.0.2-py3-none-any.whl

---
+++ packages/seolTest/seolTest-0.0.0.2-py3-none-any.whl/seolTest/test1.py
@@ -0,0 +1,4 @@
+def print_test1():
+    '''테스트 문장을 출력한다1'''
+    print('안녕 이건 test1.py 페이지야')
+    print('new version 이랍니다.')
---
+++ packages/seolTest/seolTest-0.0.0.2-py3-none-any.whl/seolTest/test2.py
@@ -0,0 +1,4 @@
+def print_test2():
+    '''테스트 문장을 출력한다2'''
+    print('안녕 이건 test2.py 페이지야')
+    print("새로운 버전이얌.")
