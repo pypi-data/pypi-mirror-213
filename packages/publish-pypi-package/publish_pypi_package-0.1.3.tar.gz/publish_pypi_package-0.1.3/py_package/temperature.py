fc6140278a743bb081baac1fdb181ba2c74c4c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:09 +0000

    Add package publish-pypi-package/0.1.3/publish_pypi_package-0.1.3.tar.gz

---
+++ packages/publish-pypi-package/publish_pypi_package-0.1.3.tar.gz/publish_pypi_package-0.1.3/py_package/temperature.py
@@ -0,0 +1,11 @@
+def hello() -> None:
+    print('Hello, welcome to py_package.')
+
+def new_release() -> None:
+    print('This is the latest code.')
+
+def c2f(celcius):
+    return (float(celcius) * 9/5) + 32
+
+def f2c(fahrenheit):
+    return (float(fahrenheit) - 32) * 5/9
