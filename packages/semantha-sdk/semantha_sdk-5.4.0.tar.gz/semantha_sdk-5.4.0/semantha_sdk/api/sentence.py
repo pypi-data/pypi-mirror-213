a91dfff7c04d41ede7e2b913ccfccdd1e5da5da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:35 +0000

    Add package semantha-sdk/5.4.0/semantha_sdk-5.4.0.tar.gz

---
+++ packages/semantha-sdk/semantha_sdk-5.4.0.tar.gz/semantha_sdk-5.4.0/semantha_sdk/api/sentence.py
@@ -0,0 +1,27 @@
+from semantha_sdk.api.semantha_endpoint import SemanthaAPIEndpoint
+from semantha_sdk.model.sentence import Sentence, SentenceSchema
+from semantha_sdk.rest.rest_client import RestClient
+
+
+class SentenceEndpoint(SemanthaAPIEndpoint):
+    """ author semantha, this is a generated class do not change manually! """
+
+    @property
+    def _endpoint(self) -> str:
+        return self._parent_endpoint + f"/{self._id}"
+
+    def get(self
+        ) -> Sentence:
+        """
+        Get sentence by ID 
+        Args:
+            
+        """
+        q_params = {}
+        return self._session.get(self._endpoint, q_params=q_params).execute().to(SentenceSchema)
+
+
+    def __init__(self, session: RestClient, parent_endpoint: str, id: str) -> None:
+        super().__init__(session, parent_endpoint)
+        self._id=id
+
---
+++ packages/semantha-sdk/semantha_sdk-5.4.0.tar.gz/semantha_sdk-5.4.0/semantha_sdk/model/statistic.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from marshmallow_dataclass import class_schema
+
+from semantha_sdk.model.semantha_entity import SemanthaModelEntity, SemanthaSchema
+
+from semantha_sdk.model.tag_docs import TagDocs
+from typing import List, Optional
+
+
+@dataclass(frozen=True)
+class Statistic(SemanthaModelEntity):
+	""" author semantha, this is a generated class do not change manually! """
+	library_size: Optional[int]
+	size: Optional[int]
+	number_of_sentences: Optional[int]
+	docs_per_tag: Optional[List[TagDocs]]
+	
+	
+StatisticSchema = class_schema(Statistic, base_schema=SemanthaSchema)
