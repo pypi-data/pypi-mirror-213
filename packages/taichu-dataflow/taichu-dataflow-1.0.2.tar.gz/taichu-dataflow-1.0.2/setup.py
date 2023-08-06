ee08c3eae05615615f7c2239b1702d73906245f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:39 +0000

    Add package taichu-dataflow/1.0.2/taichu-dataflow-1.0.2.tar.gz

---
+++ packages/taichu-dataflow/taichu-dataflow-1.0.2.tar.gz/taichu-dataflow-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    name = 'taichu-dataflow'
+
+    requirements = ['boto==2.49.0']
+
+    long_description = ''
+    # with open('README.md', 'r') as f:
+    #     long_description = f.read()
+
+    setup(
+        name=name,
+        version='1.0.2',
+        description='taichu serve is a tool for serving dataflow',
+        long_description=long_description,
+        author='taichu platform team',
+        python_requires=">=3.6.0",
+        url='',
+        keywords='taichu',
+        packages=find_packages(),
+        install_requires=requirements,
+        include_package_data=True,
+    )
