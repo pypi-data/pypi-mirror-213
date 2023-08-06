ef2ea9cfbf39c646c5a7005e59b733ef0c3db2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:19 +0000

    Add package mct-nightly/1.8.0.20230612.post436/mct-nightly-1.8.0.20230612.post436.tar.gz

---
+++ packages/mct-nightly/mct-nightly-1.8.0.20230612.post436.tar.gz/mct-nightly-1.8.0.20230612.post436/model_compression_toolkit/exporter/model_exporter/pytorch/base_pytorch_exporter.py
@@ -0,0 +1,43 @@
+# Copyright 2022 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Callable
+
+import torch.nn
+
+from model_compression_toolkit.exporter.model_exporter.fw_agonstic.exporter import Exporter
+
+
+class BasePyTorchExporter(Exporter):
+    """
+    Base PyTorch exporter class.
+    """
+
+    def __init__(self,
+                 model: torch.nn.Module,
+                 is_layer_exportable_fn: Callable,
+                 save_model_path: str,
+                 repr_dataset: Callable):
+        """
+        Args:
+            model: Model to export.
+            is_layer_exportable_fn: Callable to check whether a layer can be exported or not.
+            save_model_path: Path to save the exported model.
+            repr_dataset: Representative dataset (needed for creating torch script).
+
+        """
+        super().__init__(model,
+                         is_layer_exportable_fn,
+                         save_model_path)
+        self.repr_dataset = repr_dataset
