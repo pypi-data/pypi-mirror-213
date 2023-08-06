9a354fab14e8e2787c2bccac295ce462b0dd7c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:54 +0000

    Add package irispy-lmsal/0.2.0rc4/irispy_lmsal-0.2.0rc4-py3-none-any.whl

---
+++ packages/irispy-lmsal/irispy_lmsal-0.2.0rc4-py3-none-any.whl/irispy/__init__.py
@@ -0,0 +1,9 @@
+"""
+============
+irispy-lmsal
+============
+"""
+
+from .sji import *  # NOQA: F403
+from .spectrograph import *  # NOQA: F403
+from .version import version as __version__  # NOQA: F401
