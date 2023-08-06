9e37c964101e4b50a83cc24331413725fb1ba289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:44 +0000

    Add package solarwinds-apm/0.12.1/solarwinds_apm-0.12.1.tar.gz

---
+++ packages/solarwinds-apm/solarwinds_apm-0.12.1.tar.gz/solarwinds_apm-0.12.1/solarwinds_apm/distro.py
@@ -0,0 +1,40 @@
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+"""Module to configure OpenTelemetry to work with SolarWinds backend"""
+
+from os import environ
+
+from opentelemetry.environment_variables import (
+    OTEL_PROPAGATORS,
+    OTEL_TRACES_EXPORTER,
+)
+from opentelemetry.instrumentation.distro import BaseDistro
+from opentelemetry.instrumentation.logging.environment_variables import (
+    OTEL_PYTHON_LOG_FORMAT,
+)
+
+from solarwinds_apm.apm_constants import (
+    INTL_SWO_DEFAULT_PROPAGATORS,
+    INTL_SWO_DEFAULT_TRACES_EXPORTER,
+)
+
+
+class SolarWindsDistro(BaseDistro):
+    """OpenTelemetry Distro for SolarWinds reporting environment"""
+
+    def _configure(self, **kwargs):
+        """Configure default OTel exporter and propagators"""
+        environ.setdefault(
+            OTEL_TRACES_EXPORTER, INTL_SWO_DEFAULT_TRACES_EXPORTER
+        )
+        environ.setdefault(
+            OTEL_PROPAGATORS, ",".join(INTL_SWO_DEFAULT_PROPAGATORS)
+        )
+        environ.setdefault(
+            OTEL_PYTHON_LOG_FORMAT,
+            "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s trace_flags=%(otelTraceSampled)02d resource.service.name=%(otelServiceName)s] - %(message)s",
+        )
