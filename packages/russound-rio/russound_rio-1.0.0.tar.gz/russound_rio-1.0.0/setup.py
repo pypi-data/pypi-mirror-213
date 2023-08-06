6c6f09adceba653a59af567f8cf75eee435ac7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package russound-rio/1.0.0/russound_rio-1.0.0.tar.gz

---
+++ packages/russound-rio/russound_rio-1.0.0.tar.gz/russound_rio-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='russound_rio',
+        version='1.0.0',
+        packages=['russound_rio'],
+        license='MIT',
+        author='Chris Phillips',
+        url='https://github.com/chphilli/russound_rio',
+        description='Asyncio client for Russound RIO devices',
+        long_description=open('README.rst').read(),
+)
